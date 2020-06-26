79f6da05562279be934bd14f7ec81b721d24c384
unconst <jake@for.ai>
2020-06-26 13:06:39 -0400

    adding client tests; adding rpc request for Fwd and Bwd

--- tests/test_server.py
+++ tests/test_server.py
@@ -9,13 +9,41 @@ class Opentensor(proto_grpc.OpentensorServicer):
     def __init__(self):
         pass
 
-def test_create():
+    def Fwd(self, context, request):
+        response = proto_pb2.FwdResponse()
+        return response
+  
+
+    def Bwd(self, contect, request):
+        response = proto_pb2.BwdResponse()
+        return response
+ 
+def create_server():
     address = "[::]:8888"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     opentensor = Opentensor()
     proto_grpc.add_OpentensorServicer_to_server(opentensor, server)
     server.add_insecure_port(address)
-
+    return server
+    
+def test_create():
+    server = create_server()
     server.start()
+    server.stop(0)
+
+def test_client():
+
+    server = create_server()
+    server.start()
+
+    address = "localhost:8888"
+    channel = grpc.insecure_channel(address)
+    stub = proto_grpc.OpentensorStub(channel)
+
+    request = proto_pb2.FwdRequest(version=1.0)
+    response = stub.Fwd(request)
+
+    request = proto_pb2.BwdRequest(version=1.0)
+    response = stub.Bwd(request)
     server.stop(0)
 
