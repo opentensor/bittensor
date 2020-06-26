489a180e18112fa4188164b038538e2aa52fea26
unconst <jake@for.ai>
2020-06-26 10:44:43 -0400

    add google-api-python-client as requirements

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(
         exclude=['data', 'contract', 'assets', 'scripts', 'docs']),
     python_requires='>=3.5',
     install_requires=[
-        'grpcio'
+        'grpcio', 'google-api-python-client'
     ],  # Optional
 )
