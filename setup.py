06e301dfb0f0cfcaed4fa7a409434932ebc2d8fc
unconst <semita.recta.est@gmail.com>
2020-06-29 13:58:36 -0400

    updates and extensions to the Fwd and Bwd request protos

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='opentensor-proto',
-    version='0.0.0',
+    version='0.0.2',
     description='Opentensor protocol buffers',
     long_description=long_description,
     long_description_content_type='text/markdown',
