e4570ca924bb04dea0b358cd49050dc9df3bbdc3
unconst <semita.recta.est@gmail.com>
2020-07-03 15:52:53 -0400

    updates to fit reho

--- opentensor_proto/__init__.py
+++ opentensor_proto/__init__.py
@@ -1,8 +1,43 @@
-import numpy 
+from opentensor_proto import opentensor_pb2 as proto_pb2
+
 import pickle
+from io import BytesIO
 
-def serialize(array):
-    return pickle.dumps(array, protocol=0)
+import joblib
+import torch
+import umsgpack
 
-def deserialize(bytes_content):
-    return pickle.loads(bytes_content)
+import torch
+
+class SerializerBase:
+    @staticmethod
+    def dumps(obj: object) -> bytes:
+        raise NotImplementedError()
+
+    @staticmethod
+    def loads(buf: bytes) -> object:
+        raise NotImplementedError()
+
+class PytorchSerializer(SerializerBase):
+
+    @staticmethod
+    def serialize(obj: torch.Tensor) -> proto_pb2.Tensor:
+        data_buffer = PytorchSerializer.dumps(obj)
+        tensor = proto_pb2.Tensor(buffer=data_buffer)
+        return tensor
+    
+    @staticmethod
+    def deserialize(proto: proto_pb2.Tensor) -> torch.Tensor:    
+        torch_tensor = PytorchSerializer.loads(proto.buffer) 
+        return torch_tensor
+
+    @staticmethod
+    def dumps(obj: object) -> bytes:
+        s = BytesIO()
+        torch.save(obj, s, pickle_protocol=pickle.HIGHEST_PROTOCOL)
+        return s.getvalue()
+
+    @staticmethod
+    def loads(buf: bytes) -> object:
+        return torch.load(BytesIO(buf))
+
