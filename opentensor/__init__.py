f75ecab8c49d7cfbdfbf9f1cebd16600beb1b483
Ala Shaabana <shaabana@gmail.com>
2020-07-05 22:53:23 -0400

    Cleaning up pipeline

--- opentensor/__init__.py
+++ opentensor/__init__.py
@@ -7,7 +7,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from opentensor.node import Node
 from opentensor.keys import Keys
 from opentensor.identity import Identity
-from opentensor.synapse import Synapse
 from opentensor.dendrite import Dendrite
 from opentensor.metagraph import Metagraph
 from opentensor.gate import Gate
