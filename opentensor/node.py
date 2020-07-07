6fa3bd5bc0ec888fbf0cd9fc49dc150903541632
unconst <semita.recta.est@gmail.com>
2020-07-07 09:13:21 -0400

    plint minor fixes

--- opentensor/node.py
+++ opentensor/node.py
@@ -1,22 +1,21 @@
-from opentensor import opentensor_pb2
 import torch
+
 
 class Node():
     def __init__(self):
         pass
-    
-    def definition (self) -> str:
+
+    def definition(self) -> str:
         raise NotImplementedError
 
-    def indef (self) -> torch.Tensor:
+    def indef(self) -> torch.Tensor:
         raise NotImplementedError
 
-    def outdef (self) -> torch.Tensor:
+    def outdef(self) -> torch.Tensor:
         raise NotImplementedError
 
-    def fwd (self, key, tensor) -> torch.Tensor:
+    def fwd(self, key, tensor) -> torch.Tensor:
         raise NotImplementedError
 
-    def bwd (self, key, tensor) -> torch.Tensor:
+    def bwd(self, key, tensor) -> torch.Tensor:
         raise NotImplementedError
- 
