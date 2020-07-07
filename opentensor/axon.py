2ccbccaac84ba0bd3e44c916b2dd8200d26693a0
unconst <semita.recta.est@gmail.com>
2020-07-06 21:34:14 -0400

    adding service definition to the nodes

--- opentensor/axon.py
+++ opentensor/axon.py
@@ -17,6 +17,7 @@ class Axon(opentensor_grpc.OpentensorServicer):
         tensor = opentensor.Serializer.deserialize(tensor)
         tensor = self._metagraph.Fwd(source_id, target_id, tensor)  
         tensor = opentensor.Serializer.serialize(tensor)
+        
         response = opentensor_pb2.TensorMessage(
             version = version,
             public_key = self._metagraph.identity.public_key(),
