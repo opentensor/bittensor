c0868d68b03a173d7b91f540ce5d4da0846efc03
unconst <semita.recta.est@gmail.com>
2020-07-07 09:46:07 -0400

    lint changes

--- opentensor/axon.py
+++ opentensor/axon.py
@@ -2,10 +2,11 @@ from opentensor import opentensor_pb2_grpc as opentensor_grpc
 from opentensor import opentensor_pb2
 import opentensor
 
+
 class Axon(opentensor_grpc.OpentensorServicer):
     def __init__(self, metagraph):
         self._metagraph = metagraph
-    
+
     def Fwd(self, request, context):
         version = request.version
         public_key = request.public_key
@@ -15,19 +16,16 @@ class Axon(opentensor_grpc.OpentensorServicer):
         tensor = request.tensors[0]
 
         tensor = opentensor.Serializer.deserialize(tensor)
-        tensor = self._metagraph.Fwd(source_id, target_id, tensor)  
+        tensor = self._metagraph.Fwd(source_id, target_id, tensor)
         tensor = opentensor.Serializer.serialize(tensor)
-        
+
         response = opentensor_pb2.TensorMessage(
-            version = version,
-            public_key = self._metagraph.identity.public_key(),
-            source_id = target_id,
-            target_id = source_id,
-            tensors = [tensor]
-        )
-        return response 
-    
+            version=version,
+            public_key=self._metagraph.identity.public_key(),
+            source_id=target_id,
+            target_id=source_id,
+            tensors=[tensor])
+        return response
+
     def Bwd(self, request, context):
         self._metagraph.Bwd(request, context)
-
-
--- opentensor/keys.py
+++ opentensor/keys.py
@@ -5,21 +5,26 @@ import opentensor
 from typing import List
 import torch
 
+
 def torch_to_bytes(key):
     key = key.cpu().detach().numpy()
     key = key.tobytes()
     return key
 
+
 def bytes_to_torch(key):
     torchkey = torch.Tensor(np.frombuffer(key, dtype=np.float32))
     return torchkey
 
+
 def new_node_id():
     return os.urandom(12)
+
 
 def new_key(dim):
     new_key = torch.rand(dim, dtype=torch.float32, requires_grad=False)
     return new_key
+
 
 class Keys():
     def __init__(self, key_dim):
@@ -31,7 +36,7 @@ class Keys():
         key = new_key(self._key_dim)
         self._key_for_node[node.identity] = key
         self._node_for_key[torch_to_bytes(key)] = node
-        
+
     def toKeys(self, nodes: List[opentensor_pb2.Node]):
         torch_keys = []
         for node in nodes:
@@ -44,6 +49,6 @@ class Keys():
         nodes = []
         for k in keys:
             kb = torch_to_bytes(k)
-            assert(kb in self._node_for_key)
+            assert (kb in self._node_for_key)
             nodes.append(self._node_for_key[kb])
         return nodes
