63f8ae2dc701aaeb143c48eb66c5cca5bce237b9
unconst <semita.recta.est@gmail.com>
2020-07-13 15:59:51 -0400

    dendrite zero shape

--- opentensor/serializer.py
+++ opentensor/serializer.py
@@ -27,18 +27,22 @@ class SerializerBase:
 
 class Serializer(SerializerBase):
     @staticmethod
-    def zeros_for_def(tensor_def: proto_pb2.TensorDef) -> torch.Tensor:
+    def zeros_for_def(in_tensor: torch.Tensor,
+                      tensor_def: proto_pb2.TensorDef) -> torch.Tensor:
+        batch_dim = in_tensor.shape[0]
+        out_shape = tensor_def.shape[1:]
+        shape = tuple([batch_dim] + out_shape)
         if tensor_def.dtype == proto_pb2.DataType.DT_FLOAT32:
-            return torch.Tensor(tensor_def.shape, torch.float32)
+            return torch.zeros(shape, dtype=torch.float32)
 
         elif tensor_def.dtype == proto_pb2.DataType.DT_FLOAT64:
-            return torch.Tensor(tensor_def.shape, torch.float64)
+            return torch.zeros(shape, dtype=torch.float64)
 
         elif tensor_def.dtype == proto_pb2.DataType.DT_INT32:
-            return torch.Tensor(tensor_def.shape, torch.int32)
+            return torch.zeros(shape, dtype=torch.int32)
 
         elif tensor_def.dtype == proto_pb2.DataType.DT_INT64:
-            return torch.Tensor(tensor_def.shape, torch.int64)
+            return torch.zeros(shape, dtype=torch.int64)
         else:
             raise ValueError
 
