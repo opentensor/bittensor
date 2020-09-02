ebb6a8d8070c81912bb395eb8ec84dcf4fe4f9bc
Ala Shaabana <shaabana@gmail.com>
2020-09-02 16:08:08 -0400

    Some small fixes

--- examples/rehoboam/main.py
+++ examples/rehoboam/main.py
@@ -13,6 +13,7 @@ import torch.nn.functional as F
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from transformer import TransformerModel
 from dataset import Dataset
+from loguru import logger
 
 
 class TransformerSynapse(bittensor.Synapse):
@@ -25,8 +26,8 @@ class TransformerSynapse(bittensor.Synapse):
     def indef(self):
         x_def = bittensor_pb2.TensorDef(
                     version = bittensor.__version__,
-                    shape = [-1, 784],
-                    dtype = bittensor_pb2.FLOAT32,
+                    shape = [-1, 700],
+                    dtype = bittensor_pb2.INT64,
                     requires_grad = True,
                 )
         return [x_def]
@@ -35,14 +36,14 @@ class TransformerSynapse(bittensor.Synapse):
         y_def = bittensor_pb2.TensorDef(
                     version = bittensor.__version__,
                     shape = [-1, 10],
-                    dtype = bittensor_pb2.FLOAT32,
+                    dtype = bittensor_pb2.INT64,
                     requires_grad = True,
                 )
         return [y_def]
     
     def forward(self, x):
-        x = x.view(-1, 1, 28, 28)
-        x = transformer.encode(x)
+        x = x.view(-1, 1, 35, 20)
+        x = self.transformer.encode(x)
         return x
 
 def main(hparams):
@@ -59,7 +60,7 @@ def main(hparams):
 
     # Transformer model architecture
     ntokens = len(dataset.TEXT.vocab.stoi)  # the size of vocabulary
-    emsize = 200  # embedding dimension
+    emsize = 20  # embedding dimension
     nhid = 200  # the dimension of the feedforward network model in nn.TransformerEncoder
     nlayers = 2  # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
     nhead = 2  # the number of heads in the multiheadattention models
@@ -74,7 +75,7 @@ def main(hparams):
     neuron = bittensor.Neuron(config)
     
     # Init a trainable request router.
-    router = bittensor.Router(x_dim = 784, key_dim = 100, topk = 10)
+    router = bittensor.Router(x_dim = dataset.bptt * emsize, key_dim = 100, topk = 10)
     
     # Build local network.
     net = TransformerSynapse(transformer)
@@ -82,14 +83,15 @@ def main(hparams):
     # Subscribe the local network to the network
     neuron.subscribe(net)
     
+    # Start the neuron backend.
+    neuron.start()
+    
     # Optimizer.
     criterion = nn.CrossEntropyLoss()  # loss function
     lr = 5.0  # learning rate
     optimizer = torch.optim.SGD(net.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
     
-    # Start the neuron backend.
-    neuron.start()
     
     def train(dataset, transformer):
         transformer.train()  # Turn on the train mode
@@ -103,87 +105,95 @@ def main(hparams):
             optimizer.zero_grad()
             
             # data 
-            #print (data.shape)
             
             # Flatten encoder inputs inputs
             inputs = data.view(-1, bptt, emsize)
             inputs = torch.flatten(inputs, start_dim=1)
-            
+
             # Query the remote network.
             synapses = neuron.synapses() # Returns a list of synapses on the network.
-            requests, scores = router.route(inputs, synapses) # routes inputs to network.
+            requests, scores = router.route(inputs.float(), synapses) # routes inputs to network.
+            
+            # Convert request indices back to type long()
+            request_list = [*requests]
+            request_list[0] = requests[0].type(torch.LongTensor)
+            requests = *request_list,
+
             responses = neuron(requests, synapses) # Makes network calls.
             remote = router.join(responses) # Joins responses based on scores.
 
             # Encode sequence inputs.
-            encodings = transformer.encode(data)  # (seq_len, batch_size, embedding_size)
-
+            #encodings = net.encode(data)  # (seq_len, batch_size, embedding_size)
+            #print(encodings)
             # Get nodes from metagraph.
             # and map nodes to torch keys.
-            axons = neuron.axons()  # List[bittensor_pb2.Node]))
-            keys = keymap.toKeys(axons)  # (-1, key_dim)
+           # axons = neuron.axons()  # List[bittensor_pb2.Node]))
+            #keys = keymap.toKeys(axons)  # (-1, key_dim)
 
             # Learning a map from the gate_inputs to keys
             # gates[i, j] = score for the jth key for input i
-            gate_inputs = encodings.view(
-                batch_size, x_dim)  # (batch_size, seq_len * embedding_size)
-            gates = gate(gate_inputs, keys, topk=min(len(keys), topk))
+            #gate_inputs = encodings.view(
+            #    batch_size, x_dim)  # (batch_size, seq_len * embedding_size)
+            #gates = gate(gate_inputs, keys, topk=min(len(keys), topk))
 
             # Dispatch data to inputs for each key.
             # when gates[i, j] == 0, the key j does not recieve input i
-            dispatch_inputs = data.view(batch_size,
-                                        -1)  # (batch_size, sequence_length)
-            dispatch = dispatcher.dispatch(dispatch_inputs,
-                                           gates)  # List[(-1, seq_len)]
+            #dispatch_inputs = data.view(batch_size,
+            #                            -1)  # (batch_size, sequence_length)
+            #dispatch = dispatcher.dispatch(dispatch_inputs,
+            #                               gates)  # List[(-1, seq_len)]
 
             # Query the network by mapping from keys to node endpoints.
             # results = list[torch.Tensor], len(results) = len(keys)
-            axons = keymap.toAxons(keys)  # List[bittensor_pb2.Node]
-            query = neuron(dispatch, axons)  # List[(-1, embedding_size)]
+            #axons = keymap.toAxons(keys)  # List[bittensor_pb2.Node]
+            #query = neuron(dispatch, axons)  # List[(-1, embedding_size)]
 
             # Join results using gates to combine inputs.
-            results = dispatcher.combine(
-                query, gates)  # (batch_size, seq_len * embedding_size)
+            #results = dispatcher.combine(
+            #    query, gates)  # (batch_size, seq_len * embedding_size)
 
             # Decode responses.
-            results = results.view(
-                -1, batch_size,
-                emsize)  # (seq_len, batch_size, embedding_size)
-            to_decode = results + encodings
-            output = transformer.decode(
-                to_decode)  # (target_len, batch_size, embedding_size)
+            #results = results.view(
+            #    -1, batch_size,
+            #    emsize)  # (seq_len, batch_size, embedding_size)
+            #to_decode = results + encodings
+            #output = transformer.decode(
+            #    to_decode)  # (target_len, batch_size, embedding_size)
 
             # Loss and optimizer step
-            loss = criterion(output.view(-1, ntokens), targets)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(transformer.parameters(), 0.5)
-            optimizer.step()
+            #loss = criterion(output.view(-1, ntokens), targets)
+            #loss.backward()
+            #torch.nn.utils.clip_grad_norm_(transformer.parameters(), 0.5)
+            #optimizer.step()
 
             # Update bittensor weights
-            weights = neuron.getweights(axons)
-            weights = (0.95) * weights + (0.05) * torch.mean(gates, dim=0)
-            neuron.setweights(axons, weights)
+            #weights = neuron.getweights(axons)
+            #weights = (0.95) * weights + (0.05) * torch.mean(gates, dim=0)
+            #neuron.setweights(axons, weights)
 
-            total_loss += loss.item()
-            log_interval = 1
-            if batch % log_interval == 0 and batch > 0:
-                cur_loss = total_loss / log_interval
-                elapsed = time.time() - start_time
-                print('| epoch {:3d} | {:5d}/{:5d} batches | '
-                      'lr {:02.2f} | ms/batch {:5.2f} | '
-                      'loss {:5.2f} | ppl {:8.2f}'.format(
-                          epoch, batch,
-                          len(train_data) // dataset.bptt,
-                          scheduler.get_lr()[0], elapsed * 1000 / log_interval,
-                          cur_loss, math.exp(cur_loss)))
-                total_loss = 0
-                start_time = time.time()
+            #total_loss += loss.item()
+            #log_interval = 1
+            #if batch % log_interval == 0 and batch > 0:
+            #    cur_loss = total_loss / log_interval
+            #    elapsed = time.time() - start_time
+            #    print('| epoch {:3d} | {:5d}/{:5d} batches | '
+            #          'lr {:02.2f} | ms/batch {:5.2f} | '
+            #          'loss {:5.2f} | ppl {:8.2f}'.format(
+            #              epoch, batch,
+            #              len(train_data) // dataset.bptt,
+            #              scheduler.get_lr()[0], elapsed * 1000 / log_interval,
+            #              cur_loss, math.exp(cur_loss)))
+            #    total_loss = 0
+            #    start_time = time.time()
 
-    epochs = 0
+    epochs = 10
     global_step = 0
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
-        train(dataset, transformer)
+        train(dataset, net)
+        epoch_end_time = time.time() - epoch_start_time
+        logger.info('Train Epoch: {} finished in: {} seconds'.format(
+                    epoch, epoch_end_time))
         scheduler.step()
 
 
