1c082afe8867a3a1a9a60951bb33b97ea21c8d4e
shibshib <shaabana@gmail.com>
2020-09-23 11:03:54 -0400

    Updated CIFAR with a DPN model

--- examples/cifar/main.py
+++ examples/cifar/main.py
@@ -3,6 +3,7 @@ from loguru import logger
 import argparse
 import torch
 import torchvision
+import copy
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,45 +15,82 @@ import bittensor
 from bittensor import bittensor_pb2
 
 class CIFAR(bittensor.Synapse):
-        
     def __init__(self, config: bittensor.Config):
         super(CIFAR, self).__init__(config)
+        model_config = self.DPN26()
+        in_planes, out_planes = model_config['in_planes'], model_config['out_planes']
+        num_blocks, dense_depth = model_config['num_blocks'], model_config['dense_depth']
         
         # Main Network
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-        
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.last_planes = 64
+        self.layer1 = self._make_layer(in_planes[0], out_planes[0], num_blocks[0], dense_depth[0], stride=1)
+        self.layer2 = self._make_layer(in_planes[1], out_planes[1], num_blocks[1], dense_depth[1], stride=2)
+        self.layer3 = self._make_layer(in_planes[2], out_planes[2], num_blocks[2], dense_depth[2], stride=1)
+        self.layer4 = self._make_layer(in_planes[3], out_planes[3], num_blocks[3], dense_depth[3], stride=2)
+        self.linear = nn.Linear((out_planes[3] * 4)+(((num_blocks[3]+1) * 4)*dense_depth[3]), 10)
+
         # Distill Network
-        self.dist_conv1 = nn.Conv2d(3, 6, 5)
-        self.dist_pool = nn.MaxPool2d(2, 2)
-        self.dist_conv2 = nn.Conv2d(6, 16, 5)
-        self.dist_fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.dist_fc2 = nn.Linear(120, 84)
-        self.dist_fc3 = nn.Linear(84, 10)
+        self.dist_conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.dist_bn1 = nn.BatchNorm2d(64)
+        self.dist_layer1 = copy.deepcopy(self.layer1) 
+        self.dist_layer2 = copy.deepcopy(self.layer2) 
+        self.dist_layer3 = copy.deepcopy(self.layer3)
+        self.dist_layer4 = copy.deepcopy(self.layer4)
+
+    def _make_layer(self, in_planes, out_planes, num_blocks, dense_depth, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for i,stride in enumerate(strides):
+            layers.append(self.Bottleneck(self.last_planes, in_planes, out_planes, dense_depth, stride, i==0))
+            self.last_planes = out_planes + (i+2) * dense_depth
+        return nn.Sequential(*layers)
+
+    def DPN92(self):
+        cfg = {
+            'in_planes': (96,192,384,768),
+            'out_planes': (256,512,1024,2048),
+            'num_blocks': (3,4,20,3),
+            'dense_depth': (16,32,24,128)
+        }
+        return cfg
+    
+    def DPN26(self):
+        cfg = {
+            'in_planes': (96,192,384,768),
+            'out_planes': (256,512,1024,2048),
+            'num_blocks': (2,2,2,2),
+            'dense_depth': (16,32,24,128)
+        }
+        return cfg
 
     def distill(self, x):   
+        x = x.to(self.device)
         x = x.view(-1, 3, 32, 32)
-        x = self.pool(F.relu(self.dist_conv1(x)))
-        x = self.pool(F.relu(self.dist_conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.dist_fc1(x))
-        x = F.relu(self.dist_fc2(x))
-        x = self.dist_fc3(x)
+        x = F.relu(self.dist_bn1(self.dist_conv1(x)))
+        x = self.dist_layer1(x)
+        x = self.dist_layer2(x)
+        x = self.dist_layer3(x)
+        x = self.dist_layer4(x)
+        x = F.avg_pool2d(x, 4)
+        x = torch.flatten(x, start_dim=1)        
+        x = self.linear(x)
         return x
     
     def forward(self, x, y = None):
-        y = self.distill(x) if y == None else y   
+        y = self.distill(x) if y == None else y  
+        x = x.to(self.device)
+        y = y.to(self.device)
         x = x.view(-1, 3, 32, 32)
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = F.avg_pool2d(x, 4)
+        x = torch.flatten(x, start_dim=1)
+        x = self.linear(x)
         x = F.log_softmax(x)
         return x
     
@@ -63,11 +101,42 @@ class CIFAR(bittensor.Synapse):
     @property
     def output_shape(self):
         return [-1, 10]
+    
+    class Bottleneck(nn.Module):
+        def __init__(self, last_planes, in_planes, out_planes, dense_depth, stride, first_layer):
+            super(CIFAR.Bottleneck, self).__init__()
+            self.out_planes = out_planes
+            self.dense_depth = dense_depth
+
+            self.conv1 = nn.Conv2d(last_planes, in_planes, kernel_size=1, bias=False)
+            self.bn1 = nn.BatchNorm2d(in_planes)
+            self.conv2 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=32, bias=False)
+            self.bn2 = nn.BatchNorm2d(in_planes)
+            self.conv3 = nn.Conv2d(in_planes, out_planes+dense_depth, kernel_size=1, bias=False)
+            self.bn3 = nn.BatchNorm2d(out_planes + dense_depth)
+
+            self.shortcut = nn.Sequential()
+            if first_layer:
+                self.shortcut = nn.Sequential(
+                    nn.Conv2d(last_planes, out_planes + dense_depth, kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(out_planes + dense_depth)
+                )
+        
+        def forward(self, x):
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = F.relu(self.bn2(self.conv2(out)))
+            out = self.bn3(self.conv3(out))
+            x = self.shortcut(x)
+            d = self.out_planes
+            out = torch.cat([x[:,:d,:,:]+out[:,:d,:,:], x[:,d:,:,:], out[:,d:,:,:]], 1)
+            out = F.relu(out)
+            return out
+                
         
 def main(hparams):
 
-    batch_size_train = 50
-    batch_size_test = 50
+    batch_size_train = 32
+    batch_size_test = 16
     input_dimension = 3072
     output_dimension = 10
     learning_rate = 0.01
@@ -78,17 +147,25 @@ def main(hparams):
     config = bittensor.Config(hparams)
 
     # Load CIFAR
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
     trainset = torchvision.datasets.CIFAR10(root="~/tmp/bittensor/data/", train=True,
-                                        download=True, transform=transform)
+                                        download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size_train,
                                           shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root="~/tmp/bittensor/data/", train=False,
-                                       download=True, transform=transform)
+                                       download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size = batch_size_test,
                                          shuffle=False, num_workers=2)
 
@@ -98,7 +175,10 @@ def main(hparams):
     # Build local synapse to serve on the network.
     model = CIFAR(config) # Synapses take a config object.
     model.to( device ) # Set model to device.
-    
+
+    if device == 'cuda':
+        model = torch.nn.DataParallel(model)
+
     # Build and start the metagraph background object.
     # The metagraph is responsible for connecting to the blockchain
     # and finding the other neurons on the network.
@@ -122,26 +202,26 @@ def main(hparams):
     # Build the optimizer.
     criterion = nn.CrossEntropyLoss()
     params = list(router.parameters()) + list(model.parameters())
-    optimizer = optim.SGD(params, lr=learning_rate, momentum=momentum)
+    optimizer = optim.SGD(params, lr=learning_rate, momentum=momentum, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10.0, gamma=0.1)
 
     def train(model, epoch, global_step):
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        for i, (inputs, targets) in enumerate(trainloader, 0):
 
             # zero the parameter gradients
             optimizer.zero_grad()
             
             # get the inputs; data is a list of [inputs, labels]
-            inputs, targets = data
-            inputs.to(device)
-            targets.to(device)
+            inputs, targets = inputs.to(device), targets.to(device)
             
             # Flatten cifar inputs to [batch_size, input_dimension]
-            inputs_flatten = torch.flatten(inputs, start_dim=1)
+            inputs_flatten = torch.flatten(inputs, start_dim=1).to(device)
             
             # Query the remote network.
             synapses = metagraph.get_synapses( 1000 ) # Returns a list of synapses on the network. [...]
             requests, scores = router.route( synapses, inputs_flatten ) # routes inputs to network.
+            
             responses = dendrite ( synapses, requests ) # Makes network calls.
             network_outputs = router.join(responses) # Joins responses based on scores.
             
@@ -152,7 +232,8 @@ def main(hparams):
             # Query the local network.
             local_output = model.forward(inputs, network_outputs)
             target_loss = criterion(local_output, targets)
-            
+            torch.nn.utils.clip_grad_norm_(router.parameters(), 0.5)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
             loss = (target_loss + dist_loss)
             loss.backward()
             optimizer.step()
@@ -175,15 +256,17 @@ def main(hparams):
                 n = len(trainloader.dataset)
                 logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tDistill Loss: {:.6f}'.format(
                     epoch, (i * batch_size_train), n, (100. * i * batch_size_train)/n, loss.item(), dist_loss.item()))
+            
+            # Empty device cache
+            if device == 'cuda':
+                torch.cuda.empty_cache()
 
     def test(model):
         
         # Turns off Dropoutlayers, BatchNorm etc.
         model.eval()
-        
         # Turns off gradient computation for inference speed up.
         with torch.no_grad():
-            
             loss = 0.0
             correct = 0.0
             for i, data in enumerate(testloader, 0):
