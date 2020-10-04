2bdd5d85fc34b1e0ea89fb583346f09ed6d9d67c
unconst <jake@bittensor.com>
2020-10-04 10:36:56 -0500

    bert nsp batchify

--- examples/bert/main.py
+++ examples/bert/main.py
@@ -1,46 +1,34 @@
 from bittensor import bittensor_pb2
 import bittensor
 
-import os, sys
 import argparse
+from datasets import load_dataset, list_metrics, load_metric
+from loguru import logger
+import os, sys
 import math
+import random
 import time
-
 import torch
 from torch import nn
 import torch.nn.functional as F
-from typing import List, Tuple, Dict, Optional
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from loguru import logger
-
 import transformers
-from datasets import load_dataset, list_metrics, load_metric
 from transformers import DataCollatorForNextSentencePrediction
 from transformers import BertTokenizer
+from typing import List, Tuple, Dict, Optional
 
 class BertNSPSynapse(bittensor.Synapse):
     """ An bittensor endpoint trained on wiki corpus.
     """
-    def __init__(self):
+    def __init__(self, config: transformers.modeling_bert.BertConfig):
         super(BertNSPSynapse, self).__init__()                
-        self.config = transformers.modeling_bert.BertConfig(hidden_size=256, num_hidden_layers=2, num_attention_heads=2, intermediate_size=512, is_decoder=False)
-        
+        self.config = config
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
         self.embeddings = transformers.modeling_bert.BertEmbeddings(self.config)
-
         self.encoder = transformers.modeling_bert.BertEncoder(self.config)
-
         self.pooler = transformers.modeling_bert.BertPooler(self.config)
-
         self.student_encoder = transformers.modeling_bert.BertEncoder(self.config)
-
         self.student_pooler = transformers.modeling_bert.BertPooler(self.config)
-
         self.nsp = transformers.modeling_bert.BertOnlyNSPHead(self.config) 
-
         self.nsp_loss_fct = torch.nn.CrossEntropyLoss()
 
     def forward_text(self, inputs: List[str]):
@@ -127,17 +115,43 @@ class BertNSPSynapse(bittensor.Synapse):
             'student_target_loss': local_target_loss,
             'distillation_loss': distillation_loss
         }
+
+def nsp_batchify(data, batch_size):
+    batch_inputs = []
+    batch_next = []
+    batch_labels = []
+    for _ in range(batch_size):
+        if random.random() > 0.5:
+            pos = random.randint(0, len(data))
+            batch_inputs.append(data[pos]['text'])
+            batch_next.append(data[pos + 1]['text'])
+            batch_labels.append(0)
+        else:
+            while True:
+                pos_1 = random.randint(0, len(data))
+                pos_2 = random.randint(0, len(data))
+                batch_inputs.append(data[pos_1]['text'])
+                batch_next.append(data[pos_2]['text'])
+                batch_labels.append(1)
+                if (pos_1 != pos_2) and (pos_1 != pos_2 - 1):
+                    break
+    yield batch_inputs, batch_next, torch.tensor(batch_labels, dtype=torch.long)
             
 def main(hparams):
     # Args
     config = bittensor.Config( hparams )
+    batch_size = 10
+    log_interval = 100
+    epoch_size = 1000
+    hidden_size = 256
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Dataset
     dataset = load_dataset('bookcorpus')
 
     # Build Synapse
-    model = BertNSPSynapse()
+    model_config = transformers.modeling_bert.BertConfig(hidden_size=hidden_size, num_hidden_layers=2, num_attention_heads=2, intermediate_size=512, is_decoder=False)
+    model = BertNSPSynapse(model_config)
     model.to(device)
 
     # Build and start the metagraph background object.
@@ -158,45 +172,52 @@ def main(hparams):
     # The dendrite is a torch object which makes calls to synapses across the network
     # The router is responsible for learning which synapses to call.
     dendrite = bittensor.Dendrite( config )
-    router = bittensor.Router(x_dim = 256, key_dim = 100, topk = 10)
+    router = bittensor.Router(x_dim = hidden_size, key_dim = 100, topk = 10)
     
     # Optimizer.
     lr = 3.0 # learning rate
     params = list(router.parameters()) + list(model.parameters())
     optimizer = torch.optim.SGD(params, lr=lr)
     
-    def train(dataset, transformer, epoch):
+    def train(dataset, model, epoch):
         model.train()  # Turn on the train mode.
         optimizer.zero_grad() # Zero out lingering gradients.
 
-        batch_size = 10
-        inputs = dataset['train'][0: batch_size]['text']
-        next_inputs = dataset['train'][1: batch_size+1]['text']
-        labels = torch.ones(batch_size, dtype=torch.long)
-        
-        # Get routing context
-        context = model.forward_text( inputs )
-        
-        # Query the remote network.
-        # Flatten mnist inputs for routing.
-        synapses = metagraph.get_synapses( 1000 ) # Returns a list of synapses on the network (max 1000).
-        requests, scores = router.route( synapses, context, inputs ) # routes inputs to network.
-        responses = dendrite.forward_text( synapses, requests ) # Makes network calls.
-        network = router.join( responses ) # Joins responses based on scores..
-        
-        # Compute full pass and get loss.
-        output = model.forward(inputs, next_inputs, labels, network)
-        
-        loss = output['loss']
-        loss.backward()
-        optimizer.step()
+        step = 0
+        dataiterator = nsp_batchify(dataset['train'], batch_size)
+        while step < epoch_size:
+            # Next batch.
+            inputs, next_inputs, labels = next(dataiterator)
 
-        logger.info("loss {}", loss.item())
-          
-        # Set network weights.
-        weights = metagraph.getweights(synapses).to(model.device)
-        weights = (0.99) * weights + 0.01 * torch.mean(scores, dim=0)
-        metagraph.setweights(synapses, weights)
+            # Get routing context
+            context = model.forward_text( inputs )
+            
+            # Query the remote network.
+            # Flatten mnist inputs for routing.
+            synapses = metagraph.get_synapses( 1000 ) # Returns a list of synapses on the network (max 1000).
+            requests, scores = router.route( synapses, context, inputs ) # routes inputs to network.
+            responses = dendrite.forward_text( synapses, requests ) # Makes network calls.
+            network = router.join( responses ) # Joins responses based on scores..
+            
+            # Compute full pass and get loss.
+            output = model.forward(inputs, next_inputs, labels, network)
+            
+            loss = output['loss']
+            loss.backward()
+            optimizer.step()
+
+            logger.info("loss {}", loss.item())
+            
+            # Set network weights.
+            weights = metagraph.getweights(synapses).to(model.device)
+            weights = (0.99) * weights + 0.01 * torch.mean(scores, dim=0)
+            metagraph.setweights(synapses, weights)
+
+            # Logs:
+            step += 1
+            if step % log_interval == 0:
+                logger.info('Train Step: {} [{}/{} ({:.0f}%)]\t Network Loss: {:.6f}\ Local Loss: {:.6f}\Distilation Loss: {:.6f}\tnP|nS: {}|{}'.format(
+                    epoch, step, epoch_size, output['network_target_loss'].item(), output['local_target_loss'].item(), output['distillation_loss'].item()))
       
     epoch = 0
     try:
