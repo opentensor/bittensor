ae8c2f8d16d3f52246b07b1dfb227ab6ce4022d7
Ala Shaabana <shaabana@gmail.com>
2020-10-19 09:56:27 -0400

    Added DPN for CIFAR-10 dataset

--- examples/cifar/main.py
+++ examples/cifar/main.py
@@ -1,13 +1,14 @@
-"""Training a MNIST Neuron.
+"""Training a CIFAR Neuron.
 
-This file demonstrates a training pipeline for an MNIST Neuron.
+This file demonstrates a training pipeline for an CIFAR Neuron.
 
 Example:
-        $ python examples/mnist/main.py
+        $ python examples/cifar/main.py
 """
 
 import bittensor
-from bittensor.synapses.mnist.model import MnistSynapse
+from bittensor.synapses.dpn.model import DPNSynapse
+from bittensor.synapses.dpn.dpn_configuration import DPNConfig
 from bittensor.utils.model_utils import ModelToolbox
 
 import argparse
@@ -22,9 +23,9 @@ import traceback
 def main(hparams):
      
     # Additional training params.
-    batch_size_train = 64
-    batch_size_test = 64
-    learning_rate = 0.05
+    batch_size_train = 32
+    batch_size_test = 16
+    learning_rate = 0.001
     momentum = 0.9
     log_interval = 10
     epoch = 0
@@ -42,23 +43,30 @@ def main(hparams):
     bittensor.start()
 
     # Setup model toolbox
-    model_toolbox = ModelToolbox(config.datapath, "mnist")
+    model_toolbox = ModelToolbox(config.datapath, "cifar")
     model_toolbox.setup_model_directory()
     
     # Build local synapse to serve on the network.
-    model = MnistSynapse()
+    model_config = DPNConfig()
+    model = DPNSynapse( model_config )
     model.to( device ) # Set model to device.
-    bittensor.serve( model.deepcopy() )
+    bittensor.serve( model )
 
     # Build the optimizer.
     optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10.0, gamma=0.1)
 
     # Load (Train, Test) datasets into memory.
-    train_data = torchvision.datasets.MNIST(root = config.datapath + "datasets/", train=True, download=True, transform=transforms.ToTensor())
+    train_data = torchvision.datasets.CIFAR10(
+        root=config.datapath, train=True, download=True, transform=transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ]))
     trainloader = torch.utils.data.DataLoader(train_data, batch_size = batch_size_train, shuffle=True, num_workers=2)
-    test_data = torchvision.datasets.MNIST(root = config.datapath + "datasets/", train=False, download=True, transform=transforms.ToTensor())
+    test_data = torchvision.datasets.CIFAR10(root=config.datapath, train=False, download=True, transform=transforms.ToTensor())
     testloader = torch.utils.data.DataLoader(test_data, batch_size = batch_size_test, shuffle=False, num_workers=2)
+    model_toolbox.setup_model_directory()
 
     # Train loop: Single threaded training of MNIST.
     def train(model, epoch, global_step):
@@ -67,10 +75,11 @@ def main(hparams):
         for batch_idx, (images, targets) in enumerate(trainloader):
             # Clear gradients.
             optimizer.zero_grad()
+
             # Forward pass.
             images = images.to(device)
             targets = torch.LongTensor(targets).to(device)
-            output = model(images, targets, query = True)
+            output = model(images, targets, remote = True)
 
             # Backprop.
             loss = output['loss']
@@ -81,9 +90,9 @@ def main(hparams):
             # Logs:
             if batch_idx % log_interval == 0:            
                 n = len(train_data)
-                max_logit = output['network_target'].data.max(1, keepdim=True)[1]
+                max_logit = output['remote_target'].data.max(1, keepdim=True)[1]
                 correct = max_logit.eq( targets.data.view_as(max_logit) ).sum()
-                loss_item  = output['network_target_loss'].item()
+                loss_item  = output['remote_target_loss'].item()
                 processed = ((batch_idx + 1) * batch_size_train)
                 
                 progress = (100. * processed) / n
@@ -105,18 +114,19 @@ def main(hparams):
             loss = 0.0
             correct = 0.0
             for _, (images, labels) in enumerate(testloader):                
-                
+               
                 images = images.to(device)
+
                 # Labels to Tensor
                 labels = torch.LongTensor(labels).to(device)
-
+                
                 # Compute full pass and get loss.
-                outputs = model.forward(images, labels, query=False)
+                outputs = model (images, labels, remote = False)
                 loss = loss + outputs['loss']
                 
                 # Count accurate predictions.
-                max_logit = outputs['student_target'].data.max(1, keepdim=True)[1]
-                correct = correct + max_logit.eq( labels.data.view_as(max_logit) ).sum()
+                max_logit = outputs['local_target'].data.max(1, keepdim=True)[1]
+                correct += max_logit.eq( labels.data.view_as(max_logit) ).sum()
         
         # # Log results.
         n = len(test_data)
@@ -124,6 +134,7 @@ def main(hparams):
         accuracy = (100. * correct) / n
         logger.info('Test set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(loss, correct, n, accuracy))        
         return loss, accuracy
+    
     
     while True:
         try:
@@ -138,12 +149,9 @@ def main(hparams):
                 # Update best loss.
                 best_test_loss = test_loss
                 
-                # Save and serve the new best local model.
-                logger.info('Saving/Serving model: epoch: {}, loss: {}, path: {}', epoch, test_loss, config.datapath + model_toolbox.trial_id + '/model.torch')
+                # Save the best local model.
+                logger.info('Serving / Saving model: epoch: {}, loss: {}, path: {}', epoch, test_loss, config.datapath + model_toolbox.trial_id + '/model.torch')
                 model_toolbox.save_model(model, epoch, optimizer, test_loss)
-                #torch.save({ 'epoch': epoch, 'model': model.state_dict(), 'test_loss': test_loss}, config.datapath + trial_uid + '/model.torch')
-                bittensor.serve( model.deepcopy()  )
-
             epoch += 1
 
         except Exception as e:
