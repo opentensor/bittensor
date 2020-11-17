d09bc365fda868831fad4bd915a3a1cae19ab0d0
unconst <jake@bittensor.com>
2020-11-17 18:27:35 -0500

    fix dendrite tests

--- tests/unit_tests/bittensor_tests/test_dendrite.py
+++ tests/unit_tests/bittensor_tests/test_dendrite.py
@@ -1,5 +1,4 @@
 
-from bittensor.serializer import PyTorchSerializer
 import grpc
 import torchvision.transforms as transforms
 import torch
@@ -9,56 +8,69 @@ import random
 import pytest
 import torchvision
 
-from bittensor.dendrite import RemoteSynapse, _RemoteModuleCall
+from bittensor.config import Config
+from substrateinterface import Keypair
+from bittensor.dendrite import RemoteNeuron, _RemoteModuleCall
 from bittensor import bittensor_pb2_grpc as bittensor_grpc
 from bittensor import bittensor_pb2
 from unittest.mock import MagicMock
+from bittensor.serializer import PyTorchSerializer
 
-def mock_synapse():
-    private_key = bittensor.crypto.Crypto.generate_private_ed25519()
-    public_key = bittensor.crypto.Crypto.public_key_from_private(private_key)
-    synapse = bittensor_pb2.Synapse(
-        version = bittensor.__version__,
-        neuron_key = bittensor.crypto.Crypto.public_key_to_string(public_key),
-        synapse_key = bittensor.crypto.Crypto.public_key_to_string(public_key),
-        address = '0.0.0.0',
-        port = 12345,
-        block_hash = None
-    )
-    return synapse
+
+defualt_config = """
+    session_settings:
+        axon_port: 8081
+        chain_endpoint: http://127.0.0.1:9933
+        logdir: /tmp/
+        remote_ip: 127.0.0.1
+    neuron:
+        neuron_path: /bittensor/neurons/mnist
+        datapath: /tmp/
+    training:
+        batch_size: 10
+"""
 
 class TestDendrite(unittest.TestCase):
 
+    # def __init__(self):
+    #     self.config = Config.load(from_yaml = defualt_config)
+    #     mnemonic = Keypair.generate_mnemonic()
+    #     self.keypair = Keypair.create_from_mnemonic(mnemonic)
+    #     self.session = bittensor.init(self.config, self.keypair)
+    #     self.neuron = bittensor_pb2.Neuron(
+    #         version = bittensor.__version__,
+    #         public_key = self.keypair.public_key,
+    #         address = '0.0.0.0',
+    #         port = 12345,
+    #     )
+
     def setUp(self):
-        axon_port = random.randint(8000, 9000)
-        metagraph_port = random.randint(8000, 9000)
-        bp_host = 'localhost'
-        bp_port = metagraph_port
-        self._config = bittensor.Config(     axon_port = axon_port,
-                                        metagraph_port = metagraph_port,
-                                        bp_host = bp_host,
-                                        bp_port = bp_port
-                                    )
-        self._config.log()
-        self.dendrite = bittensor.Dendrite(self._config)
-        self.mocked_synapse = mock_synapse()
+        self.config = Config.load(from_yaml = defualt_config)
+        mnemonic = Keypair.generate_mnemonic()
+        self.keypair = Keypair.create_from_mnemonic(mnemonic)
+        self.session = bittensor.init(self.config, self.keypair)
+        self.neuron = bittensor_pb2.Neuron(
+            version = bittensor.__version__,
+            public_key = self.keypair.public_key,
+            address = '0.0.0.0',
+            port = 12345,
+        )
 
     def test_dendrite_forward_tensor(self):
         x = torch.rand(3, 3, 3)
-        output = self.dendrite.forward_tensor([self.mocked_synapse], [x])
+        output = self.session.dendrite.forward_tensor([self.neuron], [x])
         assert len(output) == 1
         assert output[0].shape == torch.Size([3, 3, bittensor.__network_dim__])
 
         # Let's try and break the forward_tensor call
         x = torch.rand(3)
         with pytest.raises(ValueError):
-            self.dendrite.forward_tensor([self.mocked_synapse], [x])
+            self.session.dendrite.forward_tensor([self.neuron], [x])
 
     def test_dendrite_forward_image(self):
         # Let's grab some image data
-        batch_size = 64
-        data = torchvision.datasets.MNIST(root = self._config.datapath + "datasets/", train=True, download=True, transform=transforms.ToTensor())
-        trainloader = torch.utils.data.DataLoader(data, batch_size = batch_size, shuffle=True, num_workers=2)
+        data = torchvision.datasets.MNIST(root = self.config.neuron.datapath + "datasets/", train=True, download=True, transform=transforms.ToTensor())
+        trainloader = torch.utils.data.DataLoader(data, batch_size = self.config.training.batch_size, shuffle=True, num_workers=2)
         # Let's grab a random image, and give it a crazy type to break the system
 
         image = None
@@ -68,13 +80,13 @@ class TestDendrite(unittest.TestCase):
         
         # Add sequence dimension to the image.
         sequenced_image = image.unsqueeze(1)
-        output = self.dendrite.forward_image([self.mocked_synapse], [sequenced_image])
+        output = self.session.dendrite.forward_image([self.neuron], [sequenced_image])
         assert len(output) == 1
-        assert output[0].shape == torch.Size([batch_size, 1, bittensor.__network_dim__])
+        assert output[0].shape == torch.Size([self.config.training.batch_size, 1, bittensor.__network_dim__])
 
         # Let's try and break the forward_image call
         with pytest.raises(ValueError):
-           self.dendrite.forward_image([self.mocked_synapse], [image])
+            self.session.dendrite.forward_image([self.neuron], [image])
     
     def test_dendrite_forward_text(self):
         words = ["This", "is", "a", "word", "list"]
@@ -88,7 +100,7 @@ class TestDendrite(unittest.TestCase):
         for i, ts in enumerate(ts_list):
             word_tensor[i, 0:ts.size()[0]] = ts
 
-        output = self.dendrite.forward_text([self.mocked_synapse], [word_tensor])
+        output = self.session.dendrite.forward_text([self.neuron], [word_tensor])
         assert len(output) == 1
         word_tensor_size = list(word_tensor.shape)
         assert output[0].shape == torch.Size([word_tensor_size[0], word_tensor_size[1], bittensor.__network_dim__])
@@ -96,35 +108,31 @@ class TestDendrite(unittest.TestCase):
         # Let's try and break the forward_text call
         word_tensor = word_tensor.unsqueeze(1)
         with pytest.raises(ValueError):
-            self.dendrite.forward_text([self.mocked_synapse], [word_tensor])
+            self.session.dendrite.forward_text([self.neuron], [word_tensor])
 
 class TestRemoteModuleCall(unittest.TestCase):
 
     def setUp(self):
-        axon_port = random.randint(8000, 9000)
-        metagraph_port = random.randint(8000, 9000)
-        bp_host = 'localhost'
-        bp_port = metagraph_port
-        self._config = bittensor.Config(     axon_port = axon_port,
-                                        metagraph_port = metagraph_port,
-                                        bp_host = bp_host,
-                                        bp_port = bp_port
-                                    )
-        self._config.log()
-        self.dendrite = bittensor.Dendrite(self._config)
-        self.synapse = mock_synapse()
-        self.remote_synapse = RemoteSynapse(self.synapse, self._config)
-        self.dummy = torch.empty(0, requires_grad=True)
-        self.local_neuron_key = self.synapse.neuron_key
+        self.config = Config.load(from_yaml = defualt_config)
+        mnemonic = Keypair.generate_mnemonic()
+        self.keypair = Keypair.create_from_mnemonic(mnemonic)
+        self.session = bittensor.init(self.config, self.keypair)
+        self.neuron = bittensor_pb2.Neuron(
+            version = bittensor.__version__,
+            public_key = self.keypair.public_key,
+            address = '0.0.0.0',
+            port = 12345,
+        )
         self.nounce = None
         self.signature = None
+        self.remote_neuron = RemoteNeuron(self.neuron, self.config, self.keypair)
         self.channel = grpc.insecure_channel(
             'localhost',
             options=[('grpc.max_send_message_length', -1),
-                     ('grpc.max_receive_message_length', -1)])
+                     ('grpc.max_receive_message_length', -1)])          
         self.stub = bittensor_grpc.BittensorStub(self.channel)
-        self.remote_synapse = RemoteSynapse(self.synapse, self._config)        
-
+        self.dummy = torch.empty(0, requires_grad=True)
+        
     def test_remote_module_forward(self):
         # Let's create some tensor
         x = torch.rand(3, 3, bittensor.__network_dim__)
@@ -132,8 +140,7 @@ class TestRemoteModuleCall(unittest.TestCase):
         # Mock remote module call
         fwd_return_value = bittensor_pb2.TensorMessage(
             version=bittensor.__version__,
-            neuron_key=self._config.neuron_key,
-            synapse_key=self._config.synapse_key,
+            public_key=self.keypair.public_key,
             tensors=[PyTorchSerializer.serialize_tensor(x)])
 
         # Some fake modality that doesn't exist
@@ -150,3 +157,8 @@ class TestRemoteModuleCall(unittest.TestCase):
         output = _RemoteModuleCall.apply(self, self.dummy, x, modality)
         assert len(output) == x.size(0)
         assert torch.all(output == x)
+
+
+if __name__ == "__main__":
+    td = TestDendrite()
+    td.test_dendrite_forward_tensor()
