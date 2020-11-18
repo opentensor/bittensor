af79605dd7b3965771543bab46037ea05c66a761
unconst <jake@bittensor.com>
2020-11-18 09:07:22 -0500

    mnist node test

--- tests/integration_tests/cpu_tests/bittensor/test_mnist_node.py
+++ tests/integration_tests/cpu_tests/bittensor/test_mnist_node.py
@@ -7,7 +7,10 @@ Example:
 """
 
 import bittensor
+from bittensor.synapse import Synapse
+from bittensor.config import Config
 from bittensor.synapses.ffnn import FFNNSynapse, FFNNConfig
+from substrateinterface import Keypair
 
 import random
 from loguru import logger
@@ -19,74 +22,77 @@ import torchvision.transforms as transforms
 import traceback
 import unittest
 
+defualt_config = """
+    session_settings:
+        axon_port: 8081
+        chain_endpoint: http://127.0.0.1:9933
+        logdir: /tmp/
+
+    training:
+        datapath: /tmp/
+        batch_size: 10
+        log_interval: 10
+        total_epochs: 1
+        batch_size_train: 64
+        batch_size_test: 64
+        learning_rate: 0.05
+        momentum: 0.9
+"""
+
 class MnistNode(unittest.TestCase):
-    log_interval = 10
-    epoch = 0
-    total_epochs = 1
-    global_step = 0
-    best_test_loss = math.inf
-    best_accuracy = 0
-    global_step = 0
-    scheduler = None
-    trainloader = None
-    train_data = None
-    testloader = None
-    test_data = None
-    optimizer = None
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = None
-    # Additional training params.
-    batch_size_train = 64
-    batch_size_test = 64
-    learning_rate = 0.05
-    momentum = 0.9
-    meta = None
-    # Evaluation parameters
-    accuracy = 0
-    loss = 0
 
     def setUp(self):
-        # Setup Bittensor.
-        # Create background objects.
-        # Connect the metagraph.
-        # Start the axon server.
-        
-        bootstrap = '99.238.136.56:8092'
-        metagraph_port = random.randint(8000, 9000)
-        axon_port = 8091
-
-        config = bittensor.Config(  axon_port = axon_port,
-                                    metagraph_port = metagraph_port,
-                                    bootstrap = bootstrap)
-
-        logger.info(config)
-
-        self.meta = bittensor.Metagraph( config )
-        axon = bittensor.Axon( config )
-        dendrite = bittensor.Dendrite( config )
-        
-        bittensor.dendrite = dendrite
-        bittensor.metagraph = self.meta
-        bittensor.axon = axon
-
-        self.meta.start()
-        axon.start()
-        # Build local synapse to serve on the network.
+        # Load config, keys, and build session.
+        self.config = Config.load_from_yaml_string(yaml_str = defualt_config)
+        self.config = Config.obtain_ip_address(self.config)
+        mnemonic = Keypair.generate_mnemonic()
+        self.keypair = Keypair.create_from_mnemonic(mnemonic)
+        self.session = bittensor.init(self.config, self.keypair)
+    
+        # Build and server the synapse.
         model_config = FFNNConfig()
-        self.model = FFNNSynapse(model_config)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = FFNNSynapse(model_config, self.session)
         self.model.to( self.device ) # Set model to device.
-        bittensor.serve( self.model.deepcopy() )
+        self.session.serve( self.model.deepcopy() )
 
         # Build the optimizer.
-        self.optimizer = optim.SGD(self.model.parameters(), lr=self.learning_rate, momentum=self.momentum)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=self.config.training.learning_rate, momentum=self.config.training.momentum)
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=10.0, gamma=0.1)
 
         # Load (Train, Test) datasets into memory.
-        self.train_data = torchvision.datasets.MNIST(root = config.datapath + "datasets/", train=True, download=True, transform=transforms.ToTensor())
-        self.trainloader = torch.utils.data.DataLoader(self.train_data, batch_size = self.batch_size_train, shuffle=True, num_workers=2)
-        self.test_data = torchvision.datasets.MNIST(root = config.datapath + "datasets/", train=False, download=True, transform=transforms.ToTensor())
-        self.testloader = torch.utils.data.DataLoader(self.test_data, batch_size = self.batch_size_test, shuffle=False, num_workers=2)
+        self.train_data = torchvision.datasets.MNIST(root = self.config.training.datapath + "datasets/", train=True, download=True, transform=transforms.ToTensor())
+        self.trainloader = torch.utils.data.DataLoader(self.train_data, batch_size = self.config.training.batch_size_train, shuffle=True, num_workers=2)
+        self.test_data = torchvision.datasets.MNIST(root = self.config.training.datapath + "datasets/", train=False, download=True, transform=transforms.ToTensor())
+        self.testloader = torch.utils.data.DataLoader(self.test_data, batch_size = self.config.training.batch_size_test, shuffle=False, num_workers=2)
     
+    # Test training.
+    def test(self):
+        with self.session:
+            best_loss = math.inf
+            best_accuracy = 0
+            epoch = 0
+            total_epochs = 1
+            while epoch < total_epochs:
+
+                self.train(self.model, epoch)
+                test_loss, accuracy = self.test(self.model)
+                self.scheduler.step()
+                epoch += 1
+
+                best_accuracy = accuracy if accuracy >= best_accuracy else best_accuracy
+                if test_loss < best_loss:
+                    best_loss = test_loss
+                    logger.info("Best test loss: {}".format(best_loss))
+                    self.session.serve( self.model.deepcopy() )
+
+            assert best_test_loss <= 0.1
+            assert best_accuracy >= 75 
+            # Neurons should be 3, but we are setting to >= 2 due to the
+            # unpredictability of circleci booting up neurons, some neurons might
+            # start and finish before others even boot up.
+            assert len(self.session.metagraph.neurons()) >= 2
+
     # Train loop: Single threaded training of MNIST.
     def train(self, model, epoch):
         # Turn on Dropoutlayers BatchNorm etc.
@@ -94,6 +100,7 @@ class MnistNode(unittest.TestCase):
         for batch_idx, (images, targets) in enumerate(self.trainloader):
             # Clear gradients.
             self.optimizer.zero_grad()
+
             # Forward pass.
             images = images.to(self.device)
             targets = torch.LongTensor(targets).to(self.device)
@@ -103,25 +110,24 @@ class MnistNode(unittest.TestCase):
             loss = output.remote_target_loss + output.distillation_loss
             loss.backward()
             self.optimizer.step()
-            self.global_step += 1
 
             # Logs:
-            if (batch_idx + 1) % self.log_interval == 0: 
+            if (batch_idx + 1) % self.config.training.log_interval == 0: 
                 n = len(self.train_data)
                 max_logit = output.remote_target.data.max(1, keepdim=True)[1]
                 correct = max_logit.eq( targets.data.view_as(max_logit) ).sum()
                 loss_item  = output.remote_target_loss.item()
-                processed = ((batch_idx + 1) * self.batch_size_train)
+                processed = ((batch_idx + 1) * self.config.training.batch_size_train)
                 
                 progress = (100. * processed) / n
-                accuracy = (100.0 * correct) / self.batch_size_train
+                accuracy = (100.0 * correct) / self.config.training.batch_size_train
                 logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLocal Loss: {:.6f}\t Accuracy: {:.6f}\t nP: {}', 
-                    epoch, processed, n, progress, loss_item, accuracy, len(bittensor.metagraph.peers()))
+                    epoch, processed, n, progress, loss_item, accuracy, len(self.session.metagraph.neurons()))
 
     # Test loop.
     # Evaluates the local model on the hold-out set.
     # Returns the test_accuracy and test_loss.
-    def validate(self, model: bittensor.Synapse):
+    def validate(self, model: Synapse):
         
         # Turns off Dropoutlayers, BatchNorm etc.
         model.eval()
@@ -151,43 +157,14 @@ class MnistNode(unittest.TestCase):
         accuracy = (100. * correct) / n
         logger.info('Test set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(loss, correct, n, accuracy))  
         return loss, accuracy
-    
-    def testModelTraining(self):
-        while self.epoch < self.total_epochs:
-            try:
-                # Train model
-                logger.info("Training epoch {}...".format(self.epoch))
-                self.train( self.model, self.epoch )
-                self.scheduler.step()
-                # Test model.
-                test_loss, accuracy = self.validate( self.model )
-            
-                if accuracy > self.best_accuracy:
-                    self.best_accuracy = accuracy
-                # Save best model. 
-                if test_loss < self.best_test_loss:
-                    # Update best loss.
-                    self.best_test_loss = test_loss
-                    logger.info("Best test loss: {}".format(self.best_test_loss))
-                    # Save and serve the new best local model.
-                    #logger.info( 'Saving/Serving model: epoch: {}, loss: {}, path: {}', epoch, test_loss, config.logdir + '/model.torch' )
-                    #torch.save( {'epoch': self.epoch, 'model': model.state_dict(), 'test_loss': test_loss}, config.logdir + '/model.torch' )
-                    bittensor.serve( self.model.deepcopy() )
-                 
-                self.epoch += 1
-                
-            except Exception as e:
-                traceback.print_exc()
-                logger.error(e)
-                
-                bittensor.stop()
-                break
-        
-        assert self.best_test_loss <= 0.1
-        assert self.best_accuracy >= 75 
 
-        # Peers should be 3, but we are setting to >= 2 due to the
-        # unpredictability of circleci booting up peers, some peers might
-        # start and finish before others even boot up.
-        assert len(self.meta.peers()) >= 2
-        
+
+
+
+    
+if __name__ == "__main__":
+    node = MnistNode()
+    node.setUp()
+    node.test()
+    
+
