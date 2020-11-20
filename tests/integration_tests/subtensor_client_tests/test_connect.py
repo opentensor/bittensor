fd1c82e8be7f13648cea11bf88b671f41dcaabfd
parall4x <parallax@bittensor.com>
2020-11-20 13:38:41 -0500

    [#175820195] - Added neuron to play with in ./scratchpad - Removed http:// in default config to be able to play well with the ws client - Disabled old interface - Added Client() to session init

--- tests/integration_tests/subtensor_client_tests/test_connect.py
+++ tests/integration_tests/subtensor_client_tests/test_connect.py
@@ -7,13 +7,24 @@ import unittest
 logger.remove() # Shut up loguru
 
 
-socket = "localhost:9944"
-keypair = Keypair.create_from_uri('//Alice')
-client = WSClient(socket, keypair)
 
 
 @pytest.mark.asyncio
-async def test_connect():
+async def test_connect_success():
+    socket = "localhost:9944"
+    keypair = Keypair.create_from_uri('//Alice')
+    client = WSClient(socket, keypair)
+
     client.connect()
     result = await client.is_connected()
     assert result == True
+
+@pytest.mark.asyncio
+async def test_connect_failed():
+    socket = 'localhost:9999'
+    keypair = Keypair.create_from_uri('//Alice')
+    client = WSClient(socket, keypair)
+
+    client.connect()
+    result = await client.is_connected()
+    assert result == False
