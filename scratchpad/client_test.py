189309d2a387cf9fae2099f348adc8a1c1e9012a
parall4x <parallax@bittensor.com>
2020-11-20 12:16:14 -0500

    [#175820195] - Created rudimentary test for client

--- scratchpad/client_test.py
+++ scratchpad/client_test.py
@@ -1,3 +1,22 @@
 from bittensor.subtensor import SubstrateWSInterface
 from bittensor.subtensor import Keypair
+from bittensor.subtensor import WSClient
+import asyncio
+
+
+kp = Keypair.create_from_uri('//Alice')
+client = WSClient("localhost:9944", kp)
+
+
+async def test():
+    client.connect()
+
+    await client.is_connected()
+
+    print("DONE")
+
+
+loop = asyncio.get_event_loop()
+# loop.create_task(test())
+loop.run_until_complete(test())
 
