0fb90256765b00b08f30459e2a2eab90da92cc2c
Ala Shaabana <shaabana@gmail.com>
2020-11-23 14:42:50 -0500

    Fixed numpy() on str

--- bittensor/utils/keys.py
+++ bittensor/utils/keys.py
@@ -8,7 +8,7 @@ from bittensor import bittensor_pb2
 
 
 def torch_to_bytes(key):
-    key = key.cpu().detach().numpy()
+    #key = key.cpu().detach().numpy()
     key = key.tobytes()
     return key
 
