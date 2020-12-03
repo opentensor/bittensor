46abcacc0bef49631f6caf89f1944a91f95e9117
unconst <jake@bittensor.com>
2020-12-03 15:21:49 -0500

    reverse order sorting and chain weights

--- bittensor/utils/logging.py
+++ bittensor/utils/logging.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 import pandas as pd
 from loguru import logger
@@ -20,7 +21,9 @@ def log_training_output_history(session, epoch, batch_idx, batch_size, total_exa
     processed_str = colored('{}'.format(processed), 'green')
     progress_str = colored('{:.2f}%'.format(progress), 'green')
     logger.info('Epoch: {} [{}/{} ({})]', epoch, processed_str, total_str, progress_str)
+    print('\n')
 
+    print ('Outputs: \n ')
     rows = []
     cols = ['Batch Size', 'Total Loss', 'Local Loss', 'Remote Loss', 'Distillation Loss'] + list(history[0].metadata.keys())
     for output in history:
@@ -36,32 +39,39 @@ def log_training_output_history(session, epoch, batch_idx, batch_size, total_exa
     pd.set_option('display.float_format', lambda x: '%.3f' % x)
     df = pd.DataFrame(rows, columns=cols)
     print (df)
+    print('\n')
 
     # Log chain weights
+    print ('Chain weights: \n ')
     uids = session.metagraph.state.uids.tolist()
-    weights = session.metagraph.state.weights.tolist()
-    weights, uids  = zip(*sorted(zip(weights, uids)))
-    df = pd.DataFrame(weights, columns=uids)
+    weights = session.metagraph.chain_weights().tolist()
+    weights, uids  = zip(*sorted(zip(weights, uids), reverse=True))
+    df = pd.DataFrame([weights], columns=uids)
     df.rename_axis('[batch]').rename_axis("[uid]", axis=1)
     print (df)
+    print('\n')
 
     # Log batch weights
+    print ('Weights: \n ')
     batch_weights_list = []
     for output in history:
-        batch_weights = output.weights
-        batch_weights = torch.mean(batch_weights, axis=0)
-        _, batch_weights  = zip(*sorted(zip(weights, batch_weights)))
+        batch_weights = torch.mean(output.weights, axis=0).tolist()
+        _, batch_weights  = zip(*sorted( zip(weights, batch_weights), reverse=True))
         batch_weights_list.append(batch_weights)
     df = pd.DataFrame(batch_weights_list, columns=uids)
     df.rename_axis('[batch]').rename_axis("[uid]", axis=1)
     print (df)
-
+    print('\n')
+    
     # Log return ops.
+    print('Requests: \n ')
     retops = []
     for output in history:
-        rtops = output.retops
-        _, rtops  = zip(*sorted(zip(weights, rtops)))
+        rtops = output.retops.tolist()
+        _, rtops  = zip(*sorted(zip(weights, rtops), reverse=True))
         retops.append(rtops)
     pd.set_option('display.float_format', lambda x: '%.1f' % x)
-    df = pd.DataFrame(retops_sorted, columns=uids, name = 'batch return ops.')
+    df = pd.DataFrame(retops, columns=uids)
     df.rename_axis('[batch]').rename_axis("[uid]", axis=1)
+    print (df)
+    print('\n')
