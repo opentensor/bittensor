eb81ddff65316d2b3eaa95fdc467de33a63c9de3
Parallax <parall4x@users.noreply.github.com>
2020-12-07 11:56:43 -0500

    [#176034966]
    - Implemented rollbar integration
    - Refactored bittensor.py
    
    There are two command line arguments added:
    -- rollbar-token | The rollbar token
    -- env | The enviroment in which the instance of bittensor runs
    
    When the token is set, it starts logging exceptions in rollbar
    The default environment is production

--- bittensor.py
+++ bittensor.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 import argparse
 import bittensor
 import os
@@ -7,42 +8,31 @@ from importlib.machinery import SourceFileLoader
 from bittensor.subtensor import Keypair
 from bittensor.config import Config
 import asyncio
+import rollbar
+from argparse import ArgumentParser
+
 
 def main():
-
-    # 1. Load passed --neuron_path for custom config.
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--neuron_path', default='/bittensor/neurons/mnist/', type=str, help='directory path to a neuron.py class.')
-    parser.add_argument('--neuron_name', type=str, help='Name of the neuron to be run, or parent directory of the neuron.py class')
-    params = parser.parse_known_args()[0]
+    # 1. Load customr configuration arguments
+    params = load_custom_config_args()
 
     # 2. Load Config.
-    logger.info('Load Config ...')
-    config = Config.load(params.neuron_path)
+    config = load_config(params)
 
     # If neuron name not set, force set it in the config.
-    neuron_name = params.neuron_name
-    if not neuron_name:
-        neuron_name = params.neuron_path.rsplit("/",1)[1]
-        neuron_name_dict = {"neuron_name": neuron_name}
-        config.neuron.update(neuron_name_dict)
+    force_neuron_name_into_config(config, params)
 
+    # Display current configuration
     logger.info(Config.toString(config))
 
     # 3. Load Keypair.
-    logger.info('Load Keyfile ...')
-    mnemonic = Keypair.generate_mnemonic()
-    keypair = Keypair.create_from_mnemonic(mnemonic)
-   
+    keypair = load_keypair()
+
     # 4. Load Neuron.
-    logger.info('Load Neuron ... ')
-    neuron_module = SourceFileLoader("Neuron", os.getcwd() + '/' + params.neuron_path + '/neuron.py').load_module()
-    neuron = neuron_module.Neuron( config )
-    
+    neuron = load_neuron(config, params)
 
     # 5. Load Session.
-    logger.info('Build Session ... ')
-    session = bittensor.init(config, keypair)
+    session = load_session(config, keypair)
 
     # 6. Start Neuron.
     logger.info('Start ... ')
@@ -50,5 +40,63 @@ def main():
     with session:
         neuron.start(session)
 
+
+def load_session(config, keypair):
+    logger.info('Build Session ... ')
+    session = bittensor.init(config, keypair)
+    return session
+
+
+def load_neuron(config, params):
+    logger.info('Load Neuron ... ')
+    neuron_module = SourceFileLoader("Neuron", os.getcwd() + '/' + params.neuron_path + '/neuron.py').load_module()
+    neuron = neuron_module.Neuron(config)
+    return neuron
+
+
+def load_keypair():
+    logger.info('Load Keyfile ...')
+    mnemonic = Keypair.generate_mnemonic()
+    keypair = Keypair.create_from_mnemonic(mnemonic)
+    return keypair
+
+
+def force_neuron_name_into_config(config, params):
+    neuron_name = params.neuron_name
+    if not neuron_name:
+        neuron_name = params.neuron_path.rsplit("/", 1)[1]
+        neuron_name_dict = {"neuron_name": neuron_name}
+        config.neuron.update(neuron_name_dict)
+
+
+def load_custom_config_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--neuron_path', default='/bittensor/neurons/mnist/', type=str,
+                        help='directory path to a neuron.py class.')
+    parser.add_argument('--neuron_name', type=str,
+                        help='Name of the neuron to be run, or parent directory of the neuron.py class')
+    params = parser.parse_known_args()[0]
+    return params
+
+
+def load_config(params):
+    logger.info('Load Config ...')
+    config = Config.load(params.neuron_path)
+    return config
+
+
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser.add_argument("--rollbar-token", required=False, dest='rb_token', default=None)
+    parser.add_argument("--env", required=False, default='production')
+    args = parser.parse_args()
+
+    if not args.rb_token:
+        main()
+    else:
+        rollbar.init(args.rb_token, args.env)
+
+        try:
+            main()
+        except:
+            rollbar.report_exc_info()
