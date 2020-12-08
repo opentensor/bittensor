094d4ee86488b4dbc834f470e84765213434b21f
Parallax <parall4x@users.noreply.github.com>
2020-12-08 15:19:04 -0500

    A global catcher should derive from BaseException

--- bittensor/exceptions/handlers/rollbar/__init__.py
+++ bittensor/exceptions/handlers/rollbar/__init__.py
@@ -22,7 +22,7 @@ def is_enabled():
 def run(func):
     try:
         func
-    except Exception as e:
+    except BaseException as e:
         logger.debug("Sending exception to rollbar")
         rollbar.report_exc_info()
         raise e
