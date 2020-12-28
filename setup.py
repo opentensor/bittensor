05957dc94880416d8edae6faa6a48195716b8a05
unconst <jake@bittensor.com>
2020-12-28 17:10:02 -0500

    add arg parse sub command

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     author_email='',
     license='MIT',
     install_requires=install_requires,
+    scripts=['bin/bittensor'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
