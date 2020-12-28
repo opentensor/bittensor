6954a4d44b3bb94080edff6916473de2e8c49a51
unconst <jake@bittensor.com>
2020-12-28 17:10:02 -0500

    add arg parse sub command

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -2,20 +2,13 @@ from loguru import logger
 import os
 import sys
 from bittensor.subtensor.interface import Keypair
-from transformers import GPT2Tokenizer
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from bittensor.config import Config
 from bittensor.session import Session
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 __version__ = '0.0.0'
 __blocktime__ = 6 # seconds
 __network_dim__ = 512
-__tokenizer__ = GPT2Tokenizer.from_pretrained("gpt2", local_files_only=False)
-__tokenizer__.pad_token = '[PAD]'
-__tokenizer__.mask_token = -100
-__vocab_size__ = 204483
 
 # Default logger
 logger_config = {
@@ -32,8 +25,20 @@ logger.configure(**logger_config)
 session = None
 def init(config: Config):
     global session
-
     session = Session(config)
     return session
+
+# Tokenizer
+__vocab_size__ = 204483
+def __tokenizer__():
+    from transformers import GPT2Tokenizer
+    if __version__ == "0.0.0":
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2", local_files_only=False)
+        tokenizer.pad_token = '[PAD]'
+        tokenizer.mask_token = -100
+        return tokenizer
+    else:
+        raise ValueError
+
 
 
