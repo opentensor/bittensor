944de5df1a8bfbf5ad73dbeed8b104adf2e15906
unconst <jake@bittensor.com>
2020-12-29 16:24:52 -0500

    intermediate

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -9,15 +9,14 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from bittensor.config import Config
 from bittensor.session import Session
 
-__version__ = 0
-__compatability__ = [ 0 ] # Version compatability
+__version__ = '0.0.0'
+__compatability__ = [ '0.0.0' ] # Version compatability
 __blocktime__ = 6 # seconds
 __network_dim__ = 512 # Tensor.shape = [batch_size, sequence_lenght, 512]
 __tokenizer__ = GPT2Tokenizer.from_pretrained("gpt2", local_files_only=False)
 __tokenizer__.pad_token = '[PAD]'
 __tokenizer__.mask_token = -100
 __vocab_size__ = 204483
-
 
 # Default logger
 logger_config = {
