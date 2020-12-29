a03a893a1f20f28cb5baa6699f6da0548fcdf6e9
unconst <jake@bittensor.com>
2020-12-29 16:49:56 -0500

    compatability

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -9,12 +9,22 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from bittensor.config import Config
 from bittensor.session import Session
 
-# Bittensor code verion.
+# Bittensor code and protocol version.
 __version__ = '0.0.0'
+# Version compatiability, users running these code versions should be able to speak with each other
+__compatability__ = ['0.0.0']
 
-# Bittensor proto version.
-__proto_version__ = 0
-__proto_compatability__ = [ 0 ] # Version compatability
+# Tokenizer
+__vocab_size__ = 204483
+def __tokenizer__():
+    from transformers import GPT2Tokenizer
+    if __version__ == "0.0.0":
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2", local_files_only=False)
+        tokenizer.pad_token = '[PAD]'
+        tokenizer.mask_token = -100
+        return tokenizer
+    else:
+        raise ValueError
 
 __blocktime__ = 6 # seconds
 __network_dim__ = 512 # Tensor.shape = [batch_size, sequence_lenght, 512]
@@ -37,5 +47,3 @@ def init(config: Config):
 
     session = Session(config)
     return session
-
-
