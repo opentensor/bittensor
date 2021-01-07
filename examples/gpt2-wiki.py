7f93617b07cdb07f4336bc46068a7f672219d2d9
unconst <jake@bittensor.com>
2021-01-07 14:31:32 -0500

    cli now has wallets

--- examples/gpt2-wiki.py
+++ examples/gpt2-wiki.py
@@ -63,7 +63,7 @@ class Session():
         parser.add_argument('--session.log_interval', default=10, type=int, help='Batches before we log session info.')
         parser.add_argument('--session.accumulation_interval', default=1, type=int, help='Batches before we apply acummulated gradients.')
         parser.add_argument('--session.apply_remote_gradients', default=False, type=bool, help='If true, neuron applies gradients which accumulate from remotes calls.')
-        parser.add_argument('--session.root_dir', default='data/', type=str,  help='Root path to load and save data associated with each session')
+        parser.add_argument('--session.root_dir', default='~/.bittensor/sessions/', type=str,  help='Root path to load and save data associated with each session')
         parser.add_argument('--session.name', default='gpt-wiki', type=str, help='Trials for this session go in session.root / session.name')
         parser.add_argument('--session.trial_uid', default=str(time.time()).split('.')[0], type=str, help='Saved models go in session.root_dir / session.name / session.uid')
         parser.add_argument('--session.record_log', default=True, help='Record all logs when running this session')
