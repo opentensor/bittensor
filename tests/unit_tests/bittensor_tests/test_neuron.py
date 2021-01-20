59bd09a85f611388b4c6d0727206a7e33fa919b3
unconst <jake@bittensor.com>
2021-01-20 17:05:18 -0500

    remove mainnet tests

--- tests/unit_tests/bittensor_tests/test_neuron.py
+++ tests/unit_tests/bittensor_tests/test_neuron.py
@@ -8,24 +8,5 @@ from bittensor.subtensor.interface import Keypair
 def test_create_neuron():
     neuron = bittensor.neuron.Neuron()
 
-def test_neuron_start():
-    config = bittensor.neuron.Neuron.build_config()
-    config.metagraph.chain_endpoint = "feynman.kusanagi.bittensor.com:9944"
-    neuron = bittensor.neuron.Neuron( config )
-    neuron.start()
-
-def test_neuron_with():
-    config = bittensor.neuron.Neuron.build_config()
-    config.metagraph.chain_endpoint = "feynman.kusanagi.bittensor.com:9944"
-    neuron = bittensor.neuron.Neuron( config )
-    with neuron:
-        pass
-
-def test_neuron_serve():
-    config = bittensor.neuron.Neuron.build_config()
-    config.metagraph.chain_endpoint = "feynman.kusanagi.bittensor.com:9944"
-    neuron = bittensor.neuron.Neuron( config )
-    neuron.serve( bittensor.synapse.Synapse() )
-
 if __name__ == "__main__":
     test_create_neuron()
