86201aace752fbc29d27a8a73600efde3712dab1
Ala Shaabana <shaabana@gmail.com>
2021-02-10 16:17:58 -0500

    request sizes needs to also go to GPU

--- bittensor/routers/pkm.py
+++ bittensor/routers/pkm.py
@@ -243,6 +243,7 @@ class PKMRouter():
         # filled_sizes: (torch.LongTensor): number of examples queried to each uid.
         # filled_sizes.shape = [metagraph.n]
         filled_request_sizes = torch.zeros(neuron.metagraph.n, dtype=torch.long).to(self.device)
+        request_sizes = torch.tensor(request_sizes)
         filled_request_sizes.scatter_(0, indices, torch.tensor(request_sizes).to(self.device))
 
         # Return.
