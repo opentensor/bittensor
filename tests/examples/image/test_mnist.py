71a97efe269f72c294b0ad010496bfec2f1b79f3
Ala Shaabana <shaabana@gmail.com>
2021-02-12 15:36:36 -0500

    Fixed mnist test

--- tests/examples/image/test_mnist.py
+++ tests/examples/image/test_mnist.py
@@ -1,38 +1,39 @@
 import os, sys, time
 from unittest.mock import MagicMock
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append("examples/IMAGE/")
+sys.path.append("miners/IMAGE/")
 import bittensor
 import torch
 import numpy
-from mnist import Session
+from mnist import Miner
+
 def test_run_mnist():
-    mnist_session_config = Session.build_config()
-    mnist_session_config.metagraph.chain_endpoint = 'feynman.akira.bittensor.com:9944'
-    mnist_session_config.session.n_epochs = 1
-    mnist_session_config.session.epoch_length = 1
-    mnist_session = Session(mnist_session_config)
-    mnist_session.neuron.metagraph.connect = MagicMock(return_value = (bittensor.metagraph.Metagraph.ConnectSuccess, ""))    
-    mnist_session.neuron.metagraph.subscribe = MagicMock(return_value = (bittensor.metagraph.Metagraph.SubscribeSuccess, ""))   
-    mnist_session.neuron.metagraph.set_weights = MagicMock()   
-    mnist_session.neuron.metagraph.sync = MagicMock()  
+    mnist_miner_config = Miner.build_config()
+    mnist_miner_config.metagraph.chain_endpoint = 'feynman.akira.bittensor.com:9944'
+    mnist_miner_config.miner.n_epochs = 1
+    mnist_miner_config.miner.epoch_length = 1
+    mnist_miner = Miner(mnist_miner_config)
+    mnist_miner.neuron.metagraph.connect = MagicMock(return_value = (bittensor.metagraph.Metagraph.ConnectSuccess, ""))    
+    mnist_miner.neuron.metagraph.subscribe = MagicMock(return_value = (bittensor.metagraph.Metagraph.SubscribeSuccess, ""))   
+    mnist_miner.neuron.metagraph.set_weights = MagicMock()   
+    mnist_miner.neuron.metagraph.sync = MagicMock()  
     neuron = bittensor.proto.Neuron(
         version = bittensor.__version__,
-        public_key = mnist_session.neuron.wallet.keypair.public_key,
-        address = mnist_session_config.axon.external_ip,
-        port = mnist_session_config.axon.external_port,
+        public_key = mnist_miner.neuron.wallet.keypair.public_key,
+        address = mnist_miner_config.axon.external_ip,
+        port = mnist_miner_config.axon.external_port,
         uid = 0,
     )
-    mnist_session.neuron.metagraph.uid = 0
-    mnist_session.neuron.metagraph.state.n = 1
-    mnist_session.neuron.metagraph.state.tau = torch.tensor([0.5], dtype = torch.float32)
-    mnist_session.neuron.metagraph.state.neurons = [neuron]
-    mnist_session.neuron.metagraph.state.indices = torch.tensor([0], dtype=torch.int64)
-    mnist_session.neuron.metagraph.state.uids = torch.tensor([0], dtype=torch.int64)
-    mnist_session.neuron.metagraph.state.lastemit = torch.tensor([0], dtype=torch.int64)
-    mnist_session.neuron.metagraph.state.stake = torch.tensor([0], dtype=torch.float32)
-    mnist_session.neuron.metagraph.state.uid_for_pubkey[mnist_session.neuron.wallet.keypair.public_key] = 0
-    mnist_session.neuron.metagraph.state.index_for_uid[0] = 0
-    mnist_session.neuron.metagraph.state.W = torch.tensor( numpy.ones( (1, 1) ), dtype=torch.float32)
-    mnist_session.run()
+    mnist_miner.neuron.metagraph.uid = 0
+    mnist_miner.neuron.metagraph.state.n = 1
+    mnist_miner.neuron.metagraph.state.tau = torch.tensor([0.5], dtype = torch.float32)
+    mnist_miner.neuron.metagraph.state.neurons = [neuron]
+    mnist_miner.neuron.metagraph.state.indices = torch.tensor([0], dtype=torch.int64)
+    mnist_miner.neuron.metagraph.state.uids = torch.tensor([0], dtype=torch.int64)
+    mnist_miner.neuron.metagraph.state.lastemit = torch.tensor([0], dtype=torch.int64)
+    mnist_miner.neuron.metagraph.state.stake = torch.tensor([0], dtype=torch.float32)
+    mnist_miner.neuron.metagraph.state.uid_for_pubkey[mnist_miner.neuron.wallet.keypair.public_key] = 0
+    mnist_miner.neuron.metagraph.state.index_for_uid[0] = 0
+    mnist_miner.neuron.metagraph.state.W = torch.tensor( numpy.ones( (1, 1) ), dtype=torch.float32)
+    mnist_miner.run()
 test_run_mnist()
