33eca451fa5acca04c63c65d1e09a86e7ba3345c
Ala Shaabana <shaabana@gmail.com>
2021-02-17 14:03:54 -0500

    Updated xlm tokenizer and mining config

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -66,6 +66,48 @@ def __tokenizer__( version = __version__ ):
 
     return tokenizer
 
+# XLM Tokenizer
+# NOTE (const): tokenizers are guaranteed to improve and expand as time progresses. We version the tokenizer here.
+# neurons must be aware that versions will increase and be ready to convert between tokenizers.
+# TODO (const): Add functionality to allow tokenizer conversion. i.e. for input token conversion.
+__vocab_size__ = (50278 + 100)  # Must match the __tokenizer__() vocab size.
+def __xlm_tokenizer__( version = __version__ ):
+    from transformers import XLMTokenizer
+    
+    tokenizer = XLMTokenizer.from_pretrained("xlm-mlm-en-2048")
+    tokenizer.padding_side = "left"
+    tokenizer.add_prefix_space = False
+    tokenizer.add_special_tokens({'bos_token': "[BOS]"}) # A special token representing the beginning of a sentence.
+    tokenizer.add_special_tokens({'eos_token': "[EOS]"}) # A special token representing the end of a sentence.
+    tokenizer.add_special_tokens({'unk_token': "[UNK]"}) # A special token representing an out-of-vocabulary token.
+    tokenizer.add_special_tokens({'sep_token': "[SEP]"}) # A special token separating two different sentences in the same input (used by BERT for instance)
+    tokenizer.add_special_tokens({'pad_token': "[PAD]"}) # A special token used to make arrays of tokens the same size for batching purpose. Will then be ignored by attention mechanisms or loss computation.
+    tokenizer.add_special_tokens({'cls_token': "[CLS]"}) # A special token representing the class of the input (used by BERT for instance).
+    tokenizer.add_special_tokens({'mask_token': "[MASK]"}) # A special token representing a masked token (used by masked-language modeling pretraining objectives, like BERT).
+    additional_special_tokens = [
+        "<s>NOTUSED",  # Used by BARThez
+        "</s>NOTUSED", # Used by BARThez
+        "<eop>", # Used by MarianMT
+        "<eod>", # Used by MarianMT
+        "<formula>", # Used by Transformer XL
+        "<mask_1>" # Used by Pegasus
+        "<special0>", # Used by XLM
+        "<special1>", # Used by XLM
+        "<special2>", # Used by XLM
+        "<special3>", # Used by XLM
+        "<special4>", # Used by XLM
+        "<special5>", # Used by XLM
+        "<special6>", # Used by XLM
+        "<special7>", # Used by XLM
+        "<special8>", # Used by XLM
+        "<special9>" # Used by XLM
+    ]
+    tokenizer.additional_special_tokens = additional_special_tokens
+    global __vocab_size__
+    __vocab_size__ = len(tokenizer) + len(additional_special_tokens) + 100 # Plus 100 for eventual toke size increase.
+
+    return tokenizer
+
 # Hardcoded entry point nodes. 
 # TODO(const): make these curl based.
 __akira_entrypoints__ = [
