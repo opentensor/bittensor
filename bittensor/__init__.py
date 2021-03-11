fix forward gradients

bittensor/__init__.py
@@ -249,6 +249,19 @@ def init( config: Munch = None,  wallet: 'bittensor.wallet.Wallet' = None, **kwa
 # dummy tensor that triggers autograd in a RemoteExpert
 DUMMY = torch.empty(0, requires_grad=True)
 
+def nested_flatten(t):
+    """
+    Turn nested list/tuple/dict into a flat iterator.
+    """
+    if isinstance(t, (list, tuple)):
+        for x in t:
+            yield from nested_flatten(x)
+    elif isinstance(t, dict):
+        for k, v in sorted(t.items()):
+            yield from nested_flatten(v)
+    else:
+        yield t
+
 class _ForwardCall(torch.autograd.Function):
     @staticmethod
     def forward(
@@ -292,13 +305,14 @@ class _ForwardCall(torch.autograd.Function):
             mode = mode
         )
         ctx.forward_codes = forward_codes
-        return outputs, torch.tensor(forward_codes, dtype=torch.int64)
+        return (torch.tensor(forward_codes, dtype=torch.int64), *outputs)
 
     @staticmethod
     @once_differentiable
     def backward( 
             ctx, 
-            *grads: torch.FloatTensor
+            code_grads: torch.FloatTensor,
+            *output_grads: torch.FloatTensor
         ) -> Tuple[ Optional[torch.Tensor], ... ]:
         """ Internal autograd-friendly Backward RPC call to a remote neurons.
 
@@ -315,15 +329,16 @@ class _ForwardCall(torch.autograd.Function):
                     Gradient results for each input.
 
         """
-        grads_cpu = [tensor.cpu() for tensor in grads]
-        outputs, _ = neuron.dendrite.backward (
+        print ('output grads', output_grads)
+        grads_cpu = [tensor.cpu() for tensor in output_grads]
+        input_grads, _ = neuron.dendrite.backward (
             neurons = ctx.neurons, 
             inputs = ctx.inputs, 
             grads = grads_cpu, 
             codes = ctx.forward_codes, 
             mode = ctx.mode
         )
-        return (DUMMY, None, None, *outputs)
+        return (None, None, None, *input_grads)
 
 def _internal_forward(
             neurons: List[bittensor.proto.Neuron],
@@ -350,12 +365,15 @@ def _internal_forward(
                 return_codes (:obj:`List[torch.LongTensor]` of shape :obj:`[num_neurons]`, `required`):
                     dendrite call return ops.
         """
-        return _ForwardCall.forward(
-                dummy = DUMMY, 
-                neurons = neurons, 
-                mode = mode, 
+        forward_response = _ForwardCall.apply(
+                DUMMY, 
+                neurons, 
+                mode,
                 *inputs
             )
+        codes = forward_response[0]
+        tensors = forward_response[1:]
+        return codes, tensors
 
 def forward_text(
             neurons: List[bittensor.proto.Neuron],
@@ -390,8 +408,8 @@ def forward_text(
 
         return _internal_forward (
             neurons = neurons, 
-            inputs = inputs, 
-            mode = bittensor.proto.Modality.TEXT
+            mode = bittensor.proto.Modality.TEXT,
+            inputs = inputs
         )
 
 def forward_image(
@@ -428,8 +446,8 @@ def forward_image(
 
     return _internal_forward (
             neurons = neurons, 
-            inputs = inputs, 
-            mode = bittensor.proto.Modality.TEXT
+            mode = bittensor.proto.Modality.TEXT,
+            inputs = inputs
     )
 
 def forward_tensor(
@@ -468,6 +486,6 @@ def forward_tensor(
 
     return _internal_forward (
             neurons = neurons, 
-            inputs = inputs, 
-            mode = bittensor.proto.Modality.TEXT
+            mode = bittensor.proto.Modality.TEXT,
+            inputs = inputs
     )

tests/unit_tests/bittensor_tests/test_forward.py
@@ -2,6 +2,7 @@ import bittensor
 import torch
 import pytest
 from unittest.mock import MagicMock
+from torch.autograd import Variable
 
 bittensor.init()
 bittensor.neuron.dendrite.forward = MagicMock(return_value = [torch.tensor([]), [0]]) 
@@ -32,20 +33,105 @@ def test_dendrite_forward_text_shape_error():
 def test_dendrite_forward_text():
     x = torch.tensor([[1,2,3,4],[5,6,7,8]], dtype=torch.long)
     bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [torch.zeros([2, 4, bittensor.__network_dim__])] , [0]]) 
-    out, ops = bittensor.forward_text( neurons=[neuron_pb2], inputs=[x])
-    assert ops[0].item() == bittensor.proto.ReturnCode.Success
-    assert list(out[0].shape) == [2, 4, bittensor.__network_dim__]
+    codes, tensors  = bittensor.forward_text( neurons=[neuron_pb2], inputs=[x])
+    assert codes[0].item() == bittensor.proto.ReturnCode.Success
+    assert list(tensors[0].shape) == [2, 4, bittensor.__network_dim__]
 
 def test_dendrite_forward_image():
     x = torch.tensor([ [ [ [ [ 1 ] ] ] ] ])
     bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [torch.zeros([1, 1, bittensor.__network_dim__])] , [0]]) 
-    out, ops = bittensor.forward_image( neurons=[neuron_pb2], inputs=[x])
-    assert ops[0].item() == bittensor.proto.ReturnCode.Success
-    assert list(out[0].shape) == [1, 1, bittensor.__network_dim__]
+    codes, tensors  = bittensor.forward_image( neurons=[neuron_pb2], inputs=[x])
+    assert codes[0].item() == bittensor.proto.ReturnCode.Success
+    assert list(tensors[0].shape) == [1, 1, bittensor.__network_dim__]
 
 def test_dendrite_forward_tensor():
     x = torch.rand(3, 3, bittensor.__network_dim__)
     bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [torch.zeros([3, 3, bittensor.__network_dim__])], [0]]) 
-    out, ops = bittensor.forward_tensor( neurons=[neuron_pb2], inputs=[x])
-    assert ops[0].item() == bittensor.proto.ReturnCode.Success
-    assert list(out[0].shape) == [3, 3, bittensor.__network_dim__]
+    codes, tensors = bittensor.forward_tensor( neurons=[neuron_pb2], inputs=[x])
+    assert codes[0].item() == bittensor.proto.ReturnCode.Success
+    assert list(tensors[0].shape) == [3, 3, bittensor.__network_dim__]
+
+def test_dendrite_forward_tensor_pass_through_text():
+    x = torch.rand(3, 3)
+    y = torch.zeros([3, 3, bittensor.__network_dim__])
+    bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [y, y, y] , [0, 0, 0]]) 
+    codes, tensors = bittensor.forward_text( neurons=[neuron_pb2, neuron_pb2, neuron_pb2], inputs=[x, x, x])
+    assert codes[0].item() == bittensor.proto.ReturnCode.Success
+    assert codes[1].item() == bittensor.proto.ReturnCode.Success
+    assert codes[2].item() == bittensor.proto.ReturnCode.Success
+    assert tensors[0].shape == y.shape
+    assert tensors[1].shape == y.shape
+    assert tensors[2].shape == y.shape
+
+def test_dendrite_forward_tensor_pass_through_image():
+    x = torch.rand(3, 3, 3, 3, 3)
+    y = torch.zeros([3, 3, bittensor.__network_dim__])
+    bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [y, y, y] , [0, 0, 0]]) 
+    codes, tensors = bittensor.forward_image( neurons=[neuron_pb2, neuron_pb2, neuron_pb2], inputs=[x, x, x])
+    assert codes[0].item() == bittensor.proto.ReturnCode.Success
+    assert codes[1].item() == bittensor.proto.ReturnCode.Success
+    assert codes[2].item() == bittensor.proto.ReturnCode.Success
+    assert tensors[0].shape == y.shape
+    assert tensors[1].shape == y.shape
+    assert tensors[2].shape == y.shape
+
+def test_dendrite_forward_tensor_pass_through_tensor():
+    x = torch.rand(3, 3, bittensor.__network_dim__)
+    y = torch.zeros([3, 3, bittensor.__network_dim__])
+    bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [y, y, y] , [0, 0, 0]]) 
+    codes, tensors = bittensor.forward_tensor( neurons=[neuron_pb2, neuron_pb2, neuron_pb2], inputs=[x, x, x])
+    assert codes[0].item() == bittensor.proto.ReturnCode.Success
+    assert codes[1].item() == bittensor.proto.ReturnCode.Success
+    assert codes[2].item() == bittensor.proto.ReturnCode.Success
+    assert tensors[0].shape == y.shape
+    assert tensors[1].shape == y.shape
+    assert tensors[2].shape == y.shape
+
+def test_dendrite_backward():
+    x = Variable(torch.ones(1, 1), requires_grad=True)
+    y = torch.ones(1, 1)
+    bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [y], [0]]) 
+    bittensor.neuron.dendrite.backward = MagicMock(return_value = [ [y], [0]]) 
+    codes, tensors = bittensor.forward_text( neurons=[ neuron_pb2 ], inputs=[ x ])
+    tensors[0].backward()
+    assert x.grad.item() == 1
+
+def test_dendrite_backward_large():
+    x = Variable(torch.ones(1, 2), requires_grad=True)
+    y = torch.ones(1, 2)
+    bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [y], [0]]) 
+    bittensor.neuron.dendrite.backward = MagicMock(return_value = [ [y], [0]]) 
+    codes, tensors = bittensor.forward_text( neurons=[ neuron_pb2 ], inputs=[ x ])
+    tensors[0].sum().backward()
+    assert list(x.grad.shape) == [1, 2]
+    assert x.grad.tolist() == y.tolist()
+
+def test_dendrite_backward_multiple():
+    x1 = Variable(torch.rand(1, 1), requires_grad=True)
+    x2 = Variable(torch.rand(1, 1), requires_grad=True)
+    x3 = Variable(torch.rand(1, 1), requires_grad=True)
+    y1 = torch.ones(1, 1)
+    y2 = torch.ones(1, 1)
+    y3 = torch.ones(1, 1)
+
+    bittensor.neuron.dendrite.forward = MagicMock(return_value = [ [y1, y2, y3], [0,0,0]]) 
+    bittensor.neuron.dendrite.backward = MagicMock(return_value = [ [y1, y2, y3], [0,0,0]]) 
+    codes, tensors = bittensor.forward_text( neurons=[ neuron_pb2, neuron_pb2, neuron_pb2 ], inputs=[ x1, x2, x3 ])
+    tensors[0].backward()
+    assert list(x1.grad.shape) == [1, 1]
+    assert x1.grad.tolist() == y1.tolist()
+    x2.grad.data.zero_()
+    assert x2.grad.tolist() == [[0]] 
+    tensors[1].backward()
+    assert list(x2.grad.shape) == [1, 1]
+    assert x2.grad.tolist() == y2.tolist()
+    x3.grad.data.zero_()
+    assert x3.grad.tolist() == [[0]] 
+    tensors[2].backward()
+    assert list(x3.grad.shape) == [1, 1]
+    assert x3.grad.tolist() == y3.tolist()
+
+
+if __name__  == "__main__":
+    test_dendrite_backward_multiple()
+