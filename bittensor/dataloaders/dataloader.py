988c4c3632e731aa205e127a54a5721fa61b5bea
Ala Shaabana <shaabana@gmail.com>
2021-04-27 12:07:06 -0400

    Reduced max file size

--- bittensor/dataloaders/dataloader.py
+++ bittensor/dataloaders/dataloader.py
@@ -35,7 +35,7 @@ class BittensorDataLoader():
     def add_args(parser: argparse.ArgumentParser):
         """ Add model params
         """
-        parser.add_argument('--dataloader.max_file_size', default=5e+6, type=int, 
+        parser.add_argument('--dataloader.max_file_size', default=1e+6, type=int, 
                                 help='Maximum text file size (in bytes) to load into memory.')
         parser.add_argument('--dataloader.num_workers', default=1, type=int, 
                                 help='Number of workers for data loader.')
