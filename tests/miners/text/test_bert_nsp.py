9132ebe646e2addcab9553251067bffaf3b47504
Ala Shaabana <shaabana@gmail.com>
2021-04-28 10:34:43 -0400

    Hotfix miner tests (#289)

--- tests/miners/text/test_bert_nsp.py
+++ tests/miners/text/test_bert_nsp.py
@@ -1,10 +1,10 @@
-import os, sys, time
+import os, sys
 from unittest.mock import MagicMock
-import bittensor
-import torch
-import numpy
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append("miners/TEXT/")
 
-from miners.text.bert_nsp.bert_nsp import Miner
+from bert_nsp.bert_nsp import Miner
+
 
 def test_run_bert_nsp():
     miner = Miner(
