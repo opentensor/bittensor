34ab44e35fbf927b6288b9cfd7f400d8254ae02e
unconst <jake@bittensor.com>
2021-05-11 09:19:19 -0500

    add os to neuron

--- bittensor/neuron.py
+++ bittensor/neuron.py
@@ -15,6 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
 
+import os
 import bittensor
 import argparse
 import bittensor.utils.networking as net
