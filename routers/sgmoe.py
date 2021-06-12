fix the rotuer softmax dim

routers/sgmoe.py
@@ -190,7 +190,7 @@ class SGMOERouter( router.Router ):
         weighted_responses = torch.zeros( ( batch_size, inputs.shape[1], bittensor.__network_dim__ )).to(self.device)
         indices = torch.where(retops != 0)[0].to(self.device)
         if torch.numel(indices) > 0:
-            soft_topk_weights = F.softmax(topk_weights[indices]).to(self.device)
+            soft_topk_weights = F.softmax(topk_weights[indices], dim=0).to(self.device)
             if torch.numel(indices[0]) != 0:
                 for soft_topk_weight, index in list(zip(soft_topk_weights, indices)): 
                     weighted_responses += responses[index].to(self.device) * soft_topk_weight