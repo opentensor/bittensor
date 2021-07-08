4169519f47540ad6eb5e11dd68387b6597acccda
Eugene-hu <85906264+Eugene-hu@users.noreply.github.com>
2021-07-08 14:18:38 -0700

    Loading Config file

--- miners/text/gpt2_exodus.py
+++ miners/text/gpt2_exodus.py
@@ -108,8 +108,8 @@ class neuron:
         r""" Fills a config namespace object with defaults or information from the command line.
         """
         parser = argparse.ArgumentParser()
-        parser.add_argument('--master.config', type=str, help='If set, arguments are overridden by passed file.')
-        config_file_path = vars(parser.parse_known_args()[0])['master.config']
+        parser.add_argument('--neuron.config', type=str, help='If set, arguments are overridden by passed file.')
+        config_file_path = vars(parser.parse_known_args()[0])['neuron.config']
         
         parser.add_argument('--neuron.modality', type=int, help='''Miner network modality. TEXT=0, IMAGE=1. Currently only allowed TEXT''', default=0)
         parser.add_argument('--neuron.use_upnpc', action='store_true', help='''Turns on port forwarding on your router using upnpc.''', default=False)
