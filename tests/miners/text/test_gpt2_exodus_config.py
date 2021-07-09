84cbad9be67515b11c4e147287ceb07bd0c88d19
Eugene-hu <85906264+Eugene-hu@users.noreply.github.com>
2021-07-09 12:06:42 -0700

    Added Assertions

--- tests/miners/text/test_gpt2_exodus_config.py
+++ tests/miners/text/test_gpt2_exodus_config.py
@@ -11,6 +11,8 @@ def test_run_gpt2_config():
     PATH = str(pathlib.Path(__file__).parent.resolve()) + '/' + 'test_config.yml'
     sys.argv = [sys.argv[0], '--neuron.config',PATH]
     config = neuron.config()
+    assert config['neuron']['n_epochs'] == 1
+    assert config['neuron']['epoch_length'] == 2
     gpt2_exodus_miner = neuron( config = config )
     gpt2_exodus_miner.subtensor.connect = MagicMock(return_value = True)  
     gpt2_exodus_miner.subtensor.is_connected = MagicMock(return_value = True)      
