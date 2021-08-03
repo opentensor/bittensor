7ec5bfdd22421373aa9738a84e514a1b42e1f857
athrun <etesting007@gmail.com>
2021-08-03 13:33:41 -0700

    Tests Update

--- miners/text/template_miner.py
+++ miners/text/template_miner.py
@@ -241,6 +241,14 @@ class Miner:
             weight_decay = self.config.miner.weight_decay,
         )
 
+        #bittensor backend
+        self.neuron = bittensor.init (  
+                config = self.config,
+                root_dir = self.config.miner.full_path,
+                axon_forward_callback = self.forward,
+                axon_backward_callback = self.backward,
+            ) 
+
     @staticmethod
     def config() -> 'bittensor.Config':
         r""" Fills a config namespace object with defaults or information from the command line.
@@ -294,12 +302,7 @@ class Miner:
         r""" Miner main loop.
         """
         # ---- Build Bittensor neuron ----
-        with bittensor.init (  
-                config = self.config,
-                root_dir = self.config.miner.full_path,
-                axon_forward_callback = self.forward,
-                axon_backward_callback = self.backward,
-            ):
+        with self.neuron:
             if self.config.neuron.use_wandb:
                 bittensor.neuron.wandb.watch([self.nucleus.local_hidden, self.nucleus.local_encoder, self.nucleus.remote_hidden], self.nucleus.loss_fct, log ='all', log_freq=10 )
             
