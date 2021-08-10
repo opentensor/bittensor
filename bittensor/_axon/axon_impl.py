ea06afd823be2c43dc6065a877293b48eb4e374b
athrun <etesting007@gmail.com>
2021-08-10 14:37:45 -0700

    axon_imple update

--- bittensor/_axon/axon_impl.py
+++ bittensor/_axon/axon_impl.py
@@ -165,7 +165,7 @@ class Axon( bittensor.grpc.BittensorServicer ):
         
         # Make forward call.
         try:
-            future = self.thread_pool.submit(self.forward_callback(),pubkey = public_key, inputs_x= inputs_x, modality=modality )
+            future = self.thread_pool.submit(self.forward_callback,pubkey = public_key, inputs_x= inputs_x, modality= modality )
             response_tensor = future.result()
             message = "Success"
             code = bittensor.proto.ReturnCode.Success
