30665ca73c303a248b30d522eb4a5d7f25bc773c
Unconst <32490803+unconst@users.noreply.github.com>
2021-08-23 18:36:22 -0500

    Wallet functions (#457)
    * initial commit
    
    * add client server examples
    
    * update version
    
    * fix path
    
    * add colab for client
    
    * fix subscribe
    
    * fix version
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * initial commit
    
    * metagraph tensor endpoints
    
    * fix api for dendrite
    
    * fix unbind logic
    
    * initial commit
    
    * pull master
    
    * remove ipynb
    
    * readme
    
    * metagraph block
    
    * remove ipynb
    
    * fix subscribe function;
    
    * fix Axon string
    
    * fix python3 version for pip install
    
    * fix tests
    
    * fix subtensor functions;
    
    * rm ipynb
    
    * add testts
    
    * fix the uid bug
    
    Co-authored-by: unconst <jake@bittensor.com>
    Co-authored-by: Ala Shaabana <shaabana@gmail.com>

--- tests/unit_tests/bittensor_tests/test_wallet.py
+++ tests/unit_tests/bittensor_tests/test_wallet.py
@@ -1,11 +1,12 @@
 import bittensor
+from unittest.mock import MagicMock
 import os
 
-the_wallet = bittensor.wallet(
+the_wallet = bittensor.wallet (
     path = '/tmp/pytest',
     name = 'pytest',
     hotkey = 'pytest',
-) 
+)
 
 def test_create_wallet():
     the_wallet.create_new_coldkey( use_password=False, overwrite = True )
@@ -18,5 +19,38 @@ def test_wallet_keypair():
     the_wallet.hotkey
     the_wallet.coldkeypub
 
+def test_wallet_uid():
+    uid = the_wallet.get_uid()
+    assert uid == 0 
+
+    s = bittensor.subtensor()
+    s.get_uid_for_pubkey = MagicMock( return_value = 10 )
+    
+    uid = the_wallet.get_uid( subtensor = s)
+    assert uid == 10 
+
+def test_wallet_stake():
+    stake = the_wallet.get_stake( )
+    assert stake.rao == 0  # the stake balance is zero, it is not subscribed
+
+    s = bittensor.subtensor()
+    s.get_stake_for_uid = MagicMock( return_value = bittensor.Balance(10) )
+    stake = the_wallet.get_stake( subtensor = s )
+    assert stake.rao == 10  # the stake balance is zero, it is not subscribed
+
+def test_wallet_balance():
+    balance = the_wallet.get_balance()
+    assert balance.rao == 0
+
+    s = bittensor.subtensor()
+    s.get_balance = MagicMock( return_value = bittensor.Balance(10) )
+    balance = the_wallet.get_balance( subtensor = s )
+    assert balance.rao == 10
+
 test_create_wallet()
 test_wallet_keypair()
+test_wallet_balance()
+test_wallet_stake()
+test_wallet_uid()
+test_wallet_keypair()
+test_create_wallet()
