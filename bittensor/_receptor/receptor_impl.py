06f160f3d72c0e5bda0a4c292e5b73b44d0e8b41
athrun <etesting007@gmail.com>
2021-08-23 11:42:23 -0700

    bug fix

--- bittensor/_receptor/receptor_impl.py
+++ bittensor/_receptor/receptor_impl.py
@@ -255,7 +255,7 @@ class Receptor(nn.Module):
                                              metadata = (
                                                         ('rpc-auth-header','Bittensor'),
                                                         ('bittensor-signature',self.sign()),
-                                                        ('bittensor-version',bittensor.__version_as_int__),
+                                                        ('bittensor-version',str(bittensor.__version_as_int__)),
                                                         ))
                 self.stats.forward_bytes_in.update(sys.getsizeof(response))
                 self.stats.forward_elapsed_time.update((time.time() - start_time))
@@ -433,7 +433,7 @@ class Receptor(nn.Module):
                                           metadata = (
                                                     ('rpc-auth-header','Bittensor'),
                                                     ('bittensor-signature',self.sign()),
-                                                    ('bittensor-version',bittensor.__version_as_int__),
+                                                    ('bittensor-version',str(bittensor.__version_as_int__)),
                                                     ))
 
             # Get message
