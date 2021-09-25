f3202574b4684bb77952a2975206901fe2622dbe
unconst <jake@bittensor.com>
2021-09-25 11:11:16 -0500

    add new functions and comments to wallet files

--- tests/unit_tests/bittensor_tests/test_wallet.py
+++ tests/unit_tests/bittensor_tests/test_wallet.py
@@ -11,6 +11,29 @@ the_wallet = bittensor.wallet (
 def test_create_wallet():
     the_wallet.create_new_coldkey( use_password=False, overwrite = True )
     the_wallet.create_new_hotkey( use_password=False, overwrite = True )
+    the_wallet.new_coldkey( use_password=False, overwrite = True )
+    the_wallet.new_hotkey( use_password=False, overwrite = True )
+    assert os.path.isfile(the_wallet.coldkeyfile)
+    assert os.path.isfile(the_wallet.hotkeyfile)
+    assert os.path.isfile(the_wallet.coldkeypubfile)
+
+def test_wallet_uri():
+    the_wallet.create_coldkey_from_uri( uri = "/Alice", use_password=False, overwrite = True )
+    the_wallet.create_hotkey_from_uri( uri = "/Alice", use_password=False, overwrite = True )
+    assert os.path.isfile(the_wallet.coldkeyfile)
+    assert os.path.isfile(the_wallet.hotkeyfile)
+    assert os.path.isfile(the_wallet.coldkeypubfile)
+
+def test_wallet_mnemonic_create():
+    the_wallet.regenerate_coldkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse",  use_password=False, overwrite = True )
+    the_wallet.regenerate_coldkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse".split(),  use_password=False, overwrite = True )
+    the_wallet.regenerate_hotkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse", use_password=False, overwrite = True )
+    the_wallet.regenerate_hotkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse".split(),  use_password=False, overwrite = True )
+
+    the_wallet.regen_coldkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse",  use_password=False, overwrite = True )
+    the_wallet.regen_coldkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse".split(),  use_password=False, overwrite = True )
+    the_wallet.regen_hotkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse", use_password=False, overwrite = True )
+    the_wallet.regen_hotkey( mnemonic = "solve arrive guilt syrup dust sea used phone flock vital narrow endorse".split(),  use_password=False, overwrite = True )
     assert os.path.isfile(the_wallet.coldkeyfile)
     assert os.path.isfile(the_wallet.hotkeyfile)
     assert os.path.isfile(the_wallet.coldkeypubfile)
