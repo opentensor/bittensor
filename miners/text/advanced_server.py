889bdc0cc9d7c086d6fb80b6e66a4aeb75b915b6
athrun <etesting007@gmail.com>
2021-09-28 13:07:56 -0700

    pbd

--- miners/text/advanced_server.py
+++ miners/text/advanced_server.py
@@ -189,8 +189,8 @@ def main( config ):
                     try:
                         logger.info('Backpropagation Started: Locking all threads')
                         mutex.acquire()
+                        import pdb;pdb.set_trace()
                         losses.backward()
-                        print(gp_server.outputs_cache.size(), gp_server.gradients_cache.size())
                         if gp_server.outputs_cache != None:
                             torch.autograd.backward (
                                 tensors = [ gp_server.outputs_cache ],
