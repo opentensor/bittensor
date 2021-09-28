29036e5b07b7c1df3d240671fb369f445a018788
isabella618033@gmail.com <isabella618033@gmail.com>
2021-09-27 23:58:11 -0400

    .

--- miners/text/template_validator.py
+++ miners/text/template_validator.py
@@ -270,7 +270,7 @@ def main( config ):
             }
             
             for weight, uid_j in list(zip(final_weights.tolist(), topk_uids.tolist())):
-                color = 'green' if (validator.chain_weights.grad and validator.chain_weights.grad[ uid_j ] < 0) else 'red'
+                color = 'green' if (validator.chain_weights.grad != None and validator.chain_weights.grad[ uid_j ] < 0) else 'red'
                 if weight > 0.001: info[ str(uid_j) ] = colored('{:.4f}'.format( weight ), color)
             
             progress.set_infos( info )
