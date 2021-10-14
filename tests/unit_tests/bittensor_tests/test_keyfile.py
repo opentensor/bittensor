78057dd71bd25e27e9d4f5b9655c053dcad555a9
unconst <jake@bittensor.com>
2021-10-14 15:14:23 -0500

    add fix for coldkey format destruction

--- tests/unit_tests/bittensor_tests/test_keyfile.py
+++ tests/unit_tests/bittensor_tests/test_keyfile.py
@@ -32,3 +32,19 @@ def test_create():
     keyfile.get_keypair( password = 'thisisafakepassword' ).seed_hex == bob.seed_hex
     keyfile.get_keypair( password = 'thisisafakepassword' ).private_key == bob.private_key
     keyfile.get_keypair( password = 'thisisafakepassword' ).public_key == bob.public_key
+
+def test_legacy_coldkey():
+    keyfile = bittensor.keyfile (path = '/tmp/pytest/coldlegacy_keyfile' )
+    keyfile.make_dirs()
+    keyfile_data = b'0x32939b6abc4d81f02dff04d2b8d1d01cc8e71c5e4c7492e4fa6a238cdca3512f'
+    with open('/tmp/pytest/coldlegacy_keyfile', "wb") as keyfile_obj:
+        keyfile_obj.write( keyfile_data )
+    assert keyfile.keyfile_data == keyfile_data
+    keyfile.encrypt( password = 'this is the fake password' )
+    keyfile.decrypt( password = 'this is the fake password' )
+    keypair_bytes = b'{"accountId": "0x32939b6abc4d81f02dff04d2b8d1d01cc8e71c5e4c7492e4fa6a238cdca3512f", "publicKey": "0x32939b6abc4d81f02dff04d2b8d1d01cc8e71c5e4c7492e4fa6a238cdca3512f", "secretPhrase": null, "secretSeed": null, "ss58Address": "5DD26kC2kxajmwfbbZmVmxhrY9VeeyR1Gpzy9i8wxLUg6zxm"}'
+    assert keyfile.keyfile_data == keypair_bytes
+    assert keyfile.get_keypair().ss58_address == "5DD26kC2kxajmwfbbZmVmxhrY9VeeyR1Gpzy9i8wxLUg6zxm"
+    assert keyfile.get_keypair().public_key == "0x32939b6abc4d81f02dff04d2b8d1d01cc8e71c5e4c7492e4fa6a238cdca3512f"
+
+test_legacy_coldkey()
