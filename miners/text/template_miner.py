1532977691a31d3ffdcd9ec2ce4b332b4ca4ce07
isabella618033 <49876827+isabella618033@users.noreply.github.com>
2021-10-18 13:00:51 -0400

    Bit 120 fisher validator #restart miner flag (#543)
    * wallet utils
    
    * initial
    
    * update version number
    
    * update version number
    
    * added signal.timer to sync every 15 block time
    
    * wandb log not-normalized chain weight, and respond rate
    
    * check to sync inside train()
    
    * removed logging for sync()
    
    * added sync debug logging
    
    * check to sync after training each batch
    
    * .
    
    * .
    
    * added logging for axon
    
    * fix axon log
    
    * fix
    
    * axon log fix
    
    * axon fix
    
    * .
    
    * fix wandb log time
    
    * .
    
    * fix wandb logging period
    
    * increased timeout and less punishment
    
    * added logging for epoch loss and data size
    
    * logging at the end of epoch
    
    * fix for wandb log when number of peers increase
    
    * .
    
    * validator moves on a block step
    
    * remove ipynb
    
    * added logs for axon and respond rate
    
    * fix
    
    * added epoch loss
    
    * logs fix
    
    * .
    
    * .
    
    * .
    
    * chain weight init from zeros
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * separated learning rate for chain_weight
    
    * wandb to project and group
    
    * .
    
    * fixed double normed chain_weight
    
    * .
    
    * added epoch loss
    
    * added fisher info
    
    * initial commit
    
    * add fishers scoring
    
    * add ema scoring method
    
    * peer scores
    
    * changed optimizaer also for template miner
    
    * .
    
    * .
    
    * small fix
    
    * .
    
    * template miner config file code missing
    
    * chain_weights -> peer_weights fix
    
    * aline ema_scores with chain_growth
    
    * respond rate bug fix
    
    * to uid 4,5,6,9,32,39,20
    
    * small fix
    
    * small fix
    
    * wandb loss fix, cleaner code
    
    * added scheduler for validator
    
    * create hotkey_use_password = False
    
    * create_if_non_existent hotkey_use_password = False
    
    * active peer bug fix
    
    * wallet and active peer fix
    
    * active peer fix
    
    * template miner logging info to self.logs NameSpace
    
    * added fisher infor to template miner, respond time log,
    
    * small change
    
    * .
    
    * removed config reading
    
    * reverted config file reading changes for validator
    
    * 0.0000001 to constant noise_offset
    
    * updated for proto return code
    
    * set punishment = 0 for validator
    
    * added restart option
    
    * comment
    
    * restart default to false
    
    * axon wandb logs
    
    * miner test fix
    
    * moved the wandb logging to axon and dend  as to_wandb
    
    * added comments, removed weird line
    
    * validator logging through axon, dend and cleaner simplenamespace stat
    
    * axon logging with pubkey and dend logging with stats_utils
    
    * miner fix
    
    * .
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * reverted fisher for miner
    
    * revert fisher for validator
    
    * miner revert peer_weights to chain_weights
    
    * validator revert peer_weight to chain_weight
    
    * miner revert sync by block time
    
    * dend fix
    
    * miner cleaner code, revert changes for optimizer
    
    * template validator revert optmizer
    
    * clean
    
    Co-authored-by: unconst <jake@bittensor.com>
    Co-authored-by: Unconst <32490803+unconst@users.noreply.github.com>

--- miners/text/template_miner.py
+++ miners/text/template_miner.py
@@ -336,7 +336,8 @@ class Miner:
         parser.add_argument('--miner.device', type=str, help='miner default training device cpu/cuda', default=("cuda" if torch.cuda.is_available() else "cpu"))
         parser.add_argument('--miner.timeout', type=int, help='Number of seconds to wait for axon request', default=10)
         parser.add_argument('--miner.blacklist', type=float, help='Amount of stake (tao) in order not to get blacklisted', default=0)
-
+        parser.add_argument('--miner.restart', type=bool, help='If True, train the miner from the beginning', default=False)
+        
         bittensor.add_args( parser )
         Nucleus.add_args( parser ) 
         bittensor.prioritythreadpool.add_args( parser )
@@ -371,7 +372,10 @@ class Miner:
             # ---- Init run state ----
             self.epoch = 0            
 
-            # ---- reloads previous run ----
+            # ---- reloads previous run if not restart ----
+            if self.config.miner.restart:
+                self.save()
+
             try:
                 self.reload()
                 self.neuron.axon.check()
