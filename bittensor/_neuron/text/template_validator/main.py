3f64413427a02f65aa62b3969f17712504c8de2d
isabella618033@gmail.com <isabella618033@gmail.com>
2021-11-01 16:47:18 -0400

    reverse change

--- bittensor/_neuron/text/template_validator/main.py
+++ bittensor/_neuron/text/template_validator/main.py
@@ -1,6 +1,4 @@
 import bittensor
 if __name__ == "__main__":
 
-    neuron = bittensor.neurons.template_miner.neuron()
-    with neuron:
-        neuron.run()
+    template = bittensor.neurons.template_miner.neuron().run()
