398bb390d797a8bdd61c26ad3e0afb11f1319daf
unconst <jake@bittensor.com>
2021-11-02 15:34:04 -0500

    receptor

--- bittensor/_axon/__init__.py
+++ bittensor/_axon/__init__.py
@@ -303,23 +303,26 @@ class AuthInterceptor(grpc.ServerInterceptor):
         nounce = variable_length_messages[0]
         pubkey = variable_length_messages[1]
         message = variable_length_messages[2]
+        receptor_uid = variable_length_messages[3]
         data_time = datetime.strptime(nounce,'%m%d%Y%H%M%S%f')
         _keypair = Keypair(ss58_address=pubkey)
 
+        # Unique key that specifies the endpoint.
+        endpoint_key = str(pubkey) + str(receptor_uid)
         
         #checking the time of creation, compared to previous messages
-        if pubkey in self.nounce_dic.keys():
-            prev_data_time = self.nounce_dic[pubkey]
+        if endpoint_key in self.nounce_dic.keys():
+            prev_data_time = self.nounce_dic[ endpoint_key ]
             if data_time - prev_data_time >= timedelta(milliseconds=1):
-                self.nounce_dic[pubkey] = data_time
+                self.nounce_dic[ endpoint_key ] = data_time
 
                 #decrypting the message and verify that message is correct
-                verification = _keypair.verify(nounce+pubkey, message)
+                verification = _keypair.verify( nounce + pubkey + receptor_uid, message)
             else:
                 verification = False
         else:
-            self.nounce_dic[pubkey] = data_time
-            verification = _keypair.verify(nounce+pubkey,message)
+            self.nounce_dic[ endpoint_key ] = data_time
+            verification = _keypair.verify( nounce + pubkey + receptor_uid, message)
 
         return verification
 
