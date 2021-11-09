5d0ba153d7319b9d88f0e12db4b93648b4a91d87
athrun <etesting007@gmail.com>
2021-11-09 10:50:45 -0800

    max 100

--- tests/unit_tests/bittensor_tests/test_forward_backward.py
+++ tests/unit_tests/bittensor_tests/test_forward_backward.py
@@ -160,7 +160,7 @@ def test_axon_receptor_forward_works():
         port = 8080,
         ip = '0.0.0.0',
         wallet = wallet,
-        maximum_concurrent_rpcs = 500
+        maximum_concurrent_rpcs = 100
     )
     axon.attach_forward_callback( forward,  modality = bittensor.proto.Modality.TENSOR )
     axon.start()
