e9e12ac7db81a531d684682cd0da811d05c04486
unconst <jake@bittensor.com>
2021-11-23 09:06:45 -0500

    add metagraph wandb to template miner

--- bittensor/_neuron/text/template_miner/neuron_impl.py
+++ bittensor/_neuron/text/template_miner/neuron_impl.py
@@ -496,9 +496,10 @@ class Neuron:
 
             wandb_info_axon = self.axon.to_wandb()
             wandb_info_dend = self.dendrite.to_wandb()
+            wandb_info_metagraph = self.metagraph.to_wandb()   
                 
             try:
-                wandb.log({**wandb_info, **wandb_info_axon, **wandb_info_dend})
+                wandb.log({**wandb_info, **wandb_info_axon, **wandb_info_dend, **wandb_info_metagraph})
             except Exception as e:
                 logger.warning('Failed to update weights and biases with error:{}', e)
 
