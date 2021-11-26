85597120b885f8d07a43a94dab720d4732b41685
Eugene <etesting007@gmail.com>
2021-11-25 18:07:12 -0800

    dont sync

--- bittensor/_neuron/text/sgmoe_validator/__init__.py
+++ bittensor/_neuron/text/sgmoe_validator/__init__.py
@@ -53,7 +53,7 @@ class neuron:
         self.subtensor = bittensor.subtensor ( config = config )
     
         # Load/Sync/Save our metagraph.
-        self.metagraph = bittensor.metagraph ( subtensor = self.subtensor ).load().sync().save()
+        self.metagraph = bittensor.metagraph ( subtensor = self.subtensor ).load()
         
         self.uid = self.metagraph.hotkeys.index ( self.wallet.hotkey.ss58_address )
 
