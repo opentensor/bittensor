020f5e7b61c3d9a0ce18932dd944689cf9e2af34
isabella618033 <isabella618033@gmail.com>
2022-01-25 11:21:45 -0500

    init

--- bittensor/_neuron/__init__.py
+++ bittensor/_neuron/__init__.py
@@ -20,15 +20,17 @@ __version__ = '0.0.0'
 version_split = __version__.split(".")
 __version_as_int__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
-from .text import template_miner,template_server,advanced_server,template_validator,sgmoe_validator
+from .text import template_miner,template_server,advanced_server,template_validator,sgmoe_validator, ddp_server
 
 __all_neurons__ =  { 'text_template_miner': template_miner.neuron, 
                      'text_template_validator': template_validator.neuron,
                      'text_template_server':template_server.neuron,
                      'text_advanced_server':advanced_server.neuron,
-                     'sgmoe_validator':sgmoe_validator.neuron}
+                     'sgmoe_validator':sgmoe_validator.neuron,
+                     'ddp_server': ddp_server}
 __text_neurons__ =  { 'template_miner': template_miner.neuron, 
                      'template_validator': template_validator.neuron,
                      'template_server':template_server.neuron,
                      'advanced_server':advanced_server.neuron,
-                     'sgmoe_validator':sgmoe_validator.neuron}
+                     'sgmoe_validator':sgmoe_validator.neuron,
+                     'ddp_server': ddp_server}
