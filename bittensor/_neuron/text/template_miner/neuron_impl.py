002dc2b293473d8390a9a6182e0f52155240bef6
Eugene <etesting007@gmail.com>
2022-02-01 09:47:54 -0800

    loss backwards

--- bittensor/_neuron/text/template_miner/neuron_impl.py
+++ bittensor/_neuron/text/template_miner/neuron_impl.py
@@ -172,11 +172,14 @@ class Neuron:
                             
                             # ---- Backward pass ----
                             output.loss = output.local_target_loss + output.distillation_loss + output.remote_target_loss
-                            scores = torch.nn.functional.normalize ( torch.relu( self.nucleus.compute_scores(output.remote_target_loss) ), p=1, dim = 0 )
-                            scores[output.query_uids] += 1e-6
 
                             output.loss.backward() # Accumulates gradients on the nucleus.
                             clip_grad_norm_(self.nucleus.parameters(), self.config.neuron.clip_gradients)
+
+                            scores = torch.nn.functional.normalize ( torch.relu( self.nucleus.compute_scores(output.remote_target_loss) ), p=1, dim = 0 )
+                            scores[output.query_uids] += 1e-6
+
+
                             
                             # ---- Apply and zero accumulated gradients.
                             self.optimizer.step() 
