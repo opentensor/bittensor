8d54059c56d42b3d46a64459c1d1deab81fd70aa
isabella <isabella618033@gmail.com>
2022-02-03 20:11:04 +0000

    cuda progression

--- bittensor/_neuron/text/multitron_server/ddp_run.py
+++ bittensor/_neuron/text/multitron_server/ddp_run.py
@@ -102,6 +102,7 @@ class DDPPipe():
             self.device = torch.device( device = self.config.neuron.device )
         
         self.gp_server.device = self.device
+        self.gp_server.pre_model = self.gp_server.pre_model.to(self.device)
         self.subtensor = bittensor.subtensor ( config = self.config )
         self.metagraph = bittensor.metagraph ( config = self.config, subtensor = self.subtensor )
         self.metagraph.sync()
@@ -130,12 +131,17 @@ class DDPPipe():
         )
 
     def run(self, rank = 0, world_size = 0):
+        # torch.multiprocessing.set_start_method('spawn')
         self.init_bit(rank)
-        self.init_process(rank)
+        # self.init_process(rank)
         if self.config.neuron.no_restart != True:
             self.gp_server.load(self.config.neuron.full_path)
 
-        self.gp_server = DDP(self.gp_server) #, device_ids = [rank]) 
+        print('danggg', rank, self.device)
+        self.gp_server = self.gp_server.to(self.device) 
+        # self.gp_server = DDP(self.gp_server, device_ids = [rank]) 
+        print('danggg', rank)
+        # self.gp_server = seld.gp_server.to(self.device)
 
         if rank == 0 and self.config.wandb.api_key != 'default':
             # --- Init Wandb.
@@ -160,9 +166,13 @@ class DDPPipe():
                 try:
                     request_id, inputs_x = self.forward_q.get()
                     if inputs_x != None:
-                        bittensor.logging.success('got inputs', sufix = f'{inputs_x.shape}')
+                        inputs_x = inputs_x.to(self.device)
+                        bittensor.logging.success('got input', sufix = f'{rank, self.device}')
+                        bittensor.logging.success('got input', sufix = f'{rank, next(self.gp_server.pre_model.parameters()).device}')
+                        bittensor.logging.success('got input', sufix = f'{rank, next(self.gp_server.parameters()).device}')
                         output = self.gp_server.forward(inputs_x)
-                        self.outputs[request_id] = output.detach()
+                        output_clone = output.detach().clone().to(device = 'cpu')
+                        self.outputs[request_id] = output_clone
                         self.events[request_id].set()
                 except Exception as e:
                     bittensor.logging.success('got exception', sufix = f'rank: {rank}, {e}')
@@ -246,7 +256,6 @@ class Server:
     def __init__( self, config: 'bittensor.config', gp_server):
         r""" Initializes the neuron with the passed config.
         """
-        
         self.config = config
         self.wallet = bittensor.wallet( config = config ).create().register()
         self.subtensor = bittensor.subtensor ( config = self.config )
--- bittensor/_neuron/text/multitron_server/nucleus_impl.py
+++ bittensor/_neuron/text/multitron_server/nucleus_impl.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 from transformers import AutoModel,AutoTokenizer,AutoConfig
 from torch.nn.utils.rnn import pad_sequence
-from loguru import logger; logger = logger.opt(colors=True)
+# from loguru import logger; logger = logger.opt(colors=True)
 
 class server(torch.nn.Module):
     def __init__(self, 
@@ -130,6 +130,9 @@ class server(torch.nn.Module):
         inputs = self.token_remap(inputs,tokenizer).to(self.device)
         pre_hidden = self.pre_model(inputs).last_hidden_state
 
+        print('in forward', inputs.device)
+
+        print('in forward', self.device , f'{ next(self.pre_model.parameters()).device}')
         if self.interpolate:
             down= F.interpolate(pre_hidden.unsqueeze(1),size=[sen_len[1],pre_hidden.size()[2]],mode=self.inter_degree).squeeze(1)
         elif self.mapping_function:
@@ -143,7 +146,7 @@ class server(torch.nn.Module):
             encoded_hidden = F.pad(down, (padding_l, padding_r),  "constant", 0)
         else:
             encoded_hidden = self.mapping(down)
-            bittensor.logging.success('nucleus forward return')
+            # bittensor.logging.success('nucleus forward return')
         return encoded_hidden
 
     def remapping_token(self,input, old_tokenizer=None):
@@ -181,9 +184,10 @@ class server(torch.nn.Module):
             if self.padding == False:
                 state_dict['mapping'] = self.mapping.state_dict()
             torch.save( state_dict, "{}/model.torch".format( path) )
-            bittensor.logging.success(prefix='Saved model', sufix='<blue>{}/model.torch</blue>'.format( path ) )
+            # bittensor.logging.success(prefix='Saved model', sufix='<blue>{}/model.torch</blue>'.format( path ) )
+            print(f'Saved model: {path}/model.torch' )
         except Exception as e:
-            logger.exception('Failed to save model with error:{}', e)
+            print('Failed to save model with error:{}', e)
 
     def load(self, path):
         try:
@@ -194,11 +198,12 @@ class server(torch.nn.Module):
                 if self.padding == False:
                     self.mapping.load_state_dict(state_dict['mapping'])
 
-                bittensor.logging.success( prefix = 'Reloaded model', sufix = '<blue>{}/model.torch</blue>'.format( path ))
+                # bittensor.logging.success( prefix = 'Reloaded model', sufix = '<blue>{}/model.torch</blue>'.format( path ))
+                print( f'Reloaded model {path}/model.torch')
 
 
         except Exception as e:
-            logger.warning('No saved model found with error: {}', e)
+            print('No saved model found with error: {}', e)
 
     @staticmethod
     def config ():
