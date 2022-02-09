1d7429261aeff22b7e3522af49504c0ca35657b5
Eugene <etesting007@gmail.com>
2022-02-09 14:34:59 -0800

    grad and softmax

--- bittensor/_neuron/text/template_miner/nucleus_impl.py
+++ bittensor/_neuron/text/template_miner/nucleus_impl.py
@@ -100,9 +100,12 @@ class Nucleus(nn.Module):
                 print(uid,loss, partial_remote_target_loss)
                 validator_scores[uid] =  (partial_remote_target_loss - loss.item())/loss.item()
                 
+        peer_weights_d1 = jacobian(loss, self.peer_weights, create_graph=True)
+        first_order = (peer_weights_d1.detach()* -self.peer_weights.detach())
+        validator_scores= F.softmax(validator_scores) + F.softmax(first_order)
         print(validator_scores)
         return validator_scores
-
+        
     def local_forward(self, inputs: torch.LongTensor, training: bool = True) -> SimpleNamespace:
         """ Forward pass through local transformer model of nucleus.
             Args:
