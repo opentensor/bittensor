6f62566b03990b930574b571e606055a93261f90
isabella618033 <isabella618033@gmail.com>
2022-02-10 15:10:13 -0500

    .

--- bittensor/_neuron/text/multitron_server/nucleus_impl.py
+++ bittensor/_neuron/text/multitron_server/nucleus_impl.py
@@ -126,7 +126,14 @@ class server(torch.nn.Module):
                     The nucleus's outputs as a torch tensor of shape [batch_size, sequence_len, __network_dim__]
         """
         sen_len = inputs.size()
-        inputs = self.token_remap(inputs,tokenizer).to(self.device)
+        
+        try:
+            inputs = self.token_remap(inputs,tokenizer).to(self.device)
+        except RuntimeError:
+            if inputs != None:
+                del inputs
+                return torch.tensor([])
+
         pre_hidden = self.pre_model(inputs).last_hidden_state
 
         if self.interpolate:
