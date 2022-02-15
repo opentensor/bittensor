1a7d4e28b2db2974b9dd19394a530ba13e15c18f
Eugene <etesting007@gmail.com>
2022-02-15 13:01:40 -0800

    doc strings and changes

--- bittensor/_neuron/text/neuron_utilities.py
+++ bittensor/_neuron/text/neuron_utilities.py
@@ -4,7 +4,23 @@ import torch.nn.functional as F
 
 
 def joining_context(return_ops, topk_weights, responses):
-    # ---- Join based on weights ----
+    """
+    Joins response embbedings depending on the return codes 
+        Args:
+            return_ops  (:type:`pytorch.LongTensor`, `required`):
+                The return codes of dendrite call return ops.
+            topk_weights  (:type:`pytorch.FloatTensor`, `required`):
+                The topk weights selected for joining
+            responses  (:type:`pytorch.FloatTensor`, `required`):
+                The embeddings that sent by the peers
+
+        Returns:
+            output (:type:`pytorch.FloatTensor``, `required):
+                The joinned output embedding using the weights
+            joining_uids  (:type:`pytorch.LongTensor`, `required`):
+                The uids used to create output
+    
+    """
     joining_uids= torch.where( return_ops == bittensor.proto.ReturnCode.Success )[0]
     joining_weights = F.softmax( topk_weights[(return_ops == bittensor.proto.ReturnCode.Success)], dim = 0 ) 
     output = torch.zeros( (responses[0].shape[0], responses[0].shape[1], bittensor.__network_dim__))
@@ -12,7 +28,26 @@ def joining_context(return_ops, topk_weights, responses):
         output += responses[joining_uids[index]]* joining_weight
     return output, joining_uids
 
-def jacobian(y, x, create_graph=False,hessian =False):                                                               
+def jacobian(y, x, create_graph=False,hessian =False): 
+
+    """
+    Calulates the Jacobian from the inputs; adapted from : https://gist.github.com/apaszke/226abdf867c4e9d6698bd198f3b45fb7
+        Args:
+            y  (:type:`pytorch.FloatTensor`, `required`):
+                The loss function
+            x  (:type:`pytorch.FloatTensor`, `required`):
+                The parameters to differentiate loss by
+            create_graph  (:type:`bool`, `optional`):
+                If we should pass parameter to grad function
+            hessian (:type:`bool`, `optional`):
+                turn on if the calculation is for a hessian instead of jacobian
+
+        Returns:
+            jacobian (:type:`pytorch.FloatTensor``, `required):
+                The jacobian matrix which contains the partial differentials 
+    
+    """
+                                                                  
     jac = []                                                                                          
     flat_y = y.reshape(-1)                                                                            
     grad_y = torch.zeros_like(flat_y)                                                                 
@@ -28,7 +63,22 @@ def jacobian(y, x, create_graph=False,hessian =False):
             grad_y[i] = 0.                                                                                
     return torch.stack(jac).reshape(y.shape + x.shape)     
 
-def fisher_score_approixmation(loss, peer_weights, ):
+def fisher_score_approximation(loss, peer_weights, ):
+    """
+    Uses the jacobian function to approximate the saliency scores, currently not used
+
+        Args:
+            loss  (:type:`pytorch.Loss`, `required`):
+                The remote target loss 
+            peer_weights  (:type:`pytorch.FloatTensor`, `required`):
+                The peer weights which was used to calculate the loss
+
+        Returns:
+            validator_scores (:type:`pytorch.FloatTensor``, `required):
+                A saliency score that approximates the fisher information of each peer
+    
+    
+    """
     peer_weights_d1 = jacobian(loss, peer_weights, create_graph=True)
     if peer_weights_d1 == None: return torch.ones_like( peer_weights ) # None if no grad w.r.t the chain weights.
     peer_weights_d2 = jacobian(peer_weights_d1, peer_weights, hessian=True)
@@ -38,6 +88,22 @@ def fisher_score_approixmation(loss, peer_weights, ):
     return validator_scores
 
 def partial_contexts(return_ops, topk_uids, topk_weights, responses):
+    """
+    Creates the partial contexts which are used to calculate the shapley scores 
+
+        Args:
+            return_ops  (:type:`pytorch.LongTensor`, `required`):
+                The return codes of dendrite call return ops.
+            topk_weights  (:type:`pytorch.FloatTensor`, `required`):
+                The topk weights selected for joining
+            responses  (:type:`pytorch.FloatTensor`, `required`):
+                The embeddings that sent by the peers
+
+        Returns:
+            partial_context (:type:`Dictionary``, `required):
+                A dict containing all of joinned contexts with a single peer masked out 
+    
+    """
     partial_context = {}
     with torch.no_grad():
         for i, uid in enumerate(topk_uids):
