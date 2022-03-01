25396be81f6168a5244e68ba24bfc83fc7fd4500
Eugene <etesting007@gmail.com>
2022-03-01 14:09:02 -0800

    messed up

--- bittensor/_neuron/text/core_validator/validator_impl.py
+++ bittensor/_neuron/text/core_validator/validator_impl.py
@@ -159,6 +159,7 @@ class Neuron:
         # Here we run until blocks_per_epochs have progressed.
         start_block = self.subtensor.block
         current_block = start_block
+        decay = 0.99
         while self.subtensor.block < start_block + self.config.neuron.blocks_per_epoch:
             start_time = time.time()
 
