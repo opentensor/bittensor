52fe31611dc0cc33779df58abff795b515dbecec
Eugene <etesting007@gmail.com>
2022-03-11 10:20:29 -0800

    num batches update

--- bittensor/_dataset/__init__.py
+++ bittensor/_dataset/__init__.py
@@ -129,7 +129,7 @@ class dataset:
             parser.add_argument('--dataset.save_dataset', action='store_true', help='Save the downloaded dataset or not.', default = bittensor.defaults.dataset.save_dataset)
             parser.add_argument('--dataset.max_datasets',  type=int, help='Number of datasets to load', default = bittensor.defaults.dataset.max_datasets)
             parser.add_argument('--dataset.no_tokenizer', action='store_true', help='To return non-tokenized text (EXPERIMENTAL, DO NOT USE)',default=False)
-            parser.add_argument('--dataset.num_batches', type=int, help='The number of data to download each time(measured by the number of batches).', default=1000)
+            parser.add_argument('--dataset.num_batches', type=int, help='The number of data to download each time(measured by the number of batches).', default=150)
             parser.add_argument('--dataset._mock', action='store_true', help='To turn on dataset mocking for testing purposes.', default=False)
 
         except argparse.ArgumentError:
