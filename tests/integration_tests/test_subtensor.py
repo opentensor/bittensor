9c62bc3fe5a8802658cebbff74a154b578549690
Eugene <etesting007@gmail.com>
2022-03-18 13:35:25 -0700

    test fix

--- tests/integration_tests/test_subtensor.py
+++ tests/integration_tests/test_subtensor.py
@@ -17,6 +17,7 @@
 
 from typing import DefaultDict
 from unittest import mock
+from unittest.mock import patch
 import bittensor
 import pytest
 import psutil  
@@ -355,22 +356,33 @@ class TestSubtensor(unittest.TestCase):
         assert register == False
 
     def test_registration_multiprocessed_already_registered( self ):
+        class success():
+            def __init__(self):
+                self.is_success = True
+            def process_events(self):
+                return True
+            
         workblocks_before_is_registered = random.randint(5, 10)
         # return False each work block but return True after a random number of blocks
         is_registered_return_values = [False for _ in range(workblocks_before_is_registered)] + [True] + [True, False]
         # this should pass the initial False check in the subtensor class and then return True because the neuron is already registered
 
-        wallet = MagicMock()
-        wallet.is_registered = MagicMock( side_effect=is_registered_return_values )
-
-        mock_neuron = MagicMock()
+        mock_neuron = MagicMock()           
         mock_neuron.is_null = True
-        self.subtensor.neuron_for_pubkey = MagicMock( return_value=mock_neuron )
 
-        # should return True
-        assert self.subtensor.register(wallet=wallet,)
-        # calls until True and once again before exiting subtensor class
-        assert wallet.is_registered.call_count == workblocks_before_is_registered + 2      
+        with patch('bittensor.Subtensor.difficulty'):
+
+            wallet = MagicMock()
+            wallet.is_registered = MagicMock( side_effect=is_registered_return_values )
+
+            self.subtensor.difficulty=1
+            self.subtensor.neuron_for_pubkey = MagicMock( return_value=mock_neuron )
+            self.subtensor.substrate.submit_extrinsic = MagicMock(return_value = success())
+
+            # should return True
+            assert self.subtensor.register(wallet=wallet,)
+            # calls until True and once again before exiting subtensor class
+            assert wallet.is_registered.call_count == workblocks_before_is_registered + 2      
 
 
 def test_subtensor_mock():
@@ -454,9 +466,7 @@ def test_subtensor_mock_functions():
     sub.validator_batch_size
     sub.difficulty
 
-test_subtensor_mock()
-test_create_mock_process()
-test_mock_from_mock_arg()
-test_mock_from_network_arg()
-test_create_from_config()
-test_two_subtensor_ownership()
+if __name__ == "__main__":
+    sub = TestSubtensor()
+    sub.setUp()
+    sub.test_registration_multiprocessed_already_registered()
