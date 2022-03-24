c0a9b1f58c7fc2e040ce5c805b919e961549c340
isabella <isabella618033@gmail.com>
2022-03-24 20:33:03 +0000

    fix test

--- tests/unit_tests/bittensor_tests/utils/test_utils.py
+++ tests/unit_tests/bittensor_tests/utils/test_utils.py
@@ -168,7 +168,7 @@ def test_solve_for_difficulty_fast_registered_already():
         block_hash = '0xba7ea4eb0b16dee271dbef5911838c3f359fcf598c74da65a54b919b68b67279'
         subtensor = MagicMock()
         subtensor.get_current_block = MagicMock( return_value=1 )
-        subtensor.difficulty = 2560000000 # set high to make solving take a long time
+        subtensor.difficulty = 100000000000000000000000000 # set high to make solving take a long time
         subtensor.substrate = MagicMock()
         subtensor.substrate.get_block_hash = MagicMock( return_value=block_hash )
         wallet = MagicMock()
@@ -183,3 +183,7 @@ def test_solve_for_difficulty_fast_registered_already():
         assert e is None
         # called every time until True
         assert wallet.is_registered.call_count == workblocks_before_is_registered + 1
+
+
+if __name__ == "__main__":
+    test_solve_for_difficulty_fast_registered_already()
