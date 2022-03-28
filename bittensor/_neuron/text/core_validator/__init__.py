b2cd64e934d93230b208bfe40ff8b98478ba62c3
Eugene <etesting007@gmail.com>
2022-03-28 17:00:44 -0400

    50

--- bittensor/_neuron/text/core_validator/__init__.py
+++ bittensor/_neuron/text/core_validator/__init__.py
@@ -291,7 +291,7 @@ class neuron:
             current_block = self.subtensor.block
             step_time = time.time() - start_time
 
-            step_topk_scores, step_topk_uids = bittensor.unbiased_topk( moving_avg_scores, k = 10 )
+            step_topk_scores, step_topk_uids = bittensor.unbiased_topk( moving_avg_scores, k = 50 )
             print(step_topk_scores, step_topk_uids)
             step_topk_normalized = bittensor.utils.weight_utils.normalize_max_multiple( x = step_topk_scores, multiple = max_allowed_ratio )
             for i, w in list(zip(step_topk_uids.tolist(), step_topk_normalized.tolist()) ):
