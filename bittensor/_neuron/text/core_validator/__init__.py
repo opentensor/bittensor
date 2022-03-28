4135c9da652c7993219cd05c38b0af19cb04456c
Eugene <etesting007@gmail.com>
2022-03-28 16:47:56 -0400

    core step

--- bittensor/_neuron/text/core_validator/__init__.py
+++ bittensor/_neuron/text/core_validator/__init__.py
@@ -291,10 +291,11 @@ class neuron:
             current_block = self.subtensor.block
             step_time = time.time() - start_time
 
-            step_topk_scores, step_topk_uids = bittensor.unbiased_topk( moving_avg_scores, k = n_topk_peer_weights )
+            step_topk_scores, step_topk_uids = bittensor.unbiased_topk( moving_avg_scores, k = 10 )
+            print(step_topk_scores, step_topk_uids)
             step_topk_normalized = bittensor.utils.weight_utils.normalize_max_multiple( x = step_topk_scores, multiple = max_allowed_ratio )
             for i, w in list(zip(step_topk_uids.tolist(), step_topk_normalized.tolist()) ):
-                print( 'weights/w_{}'.format( i ),w )
+                print( 'weights/w_{}'.format( i ), w )
 
             # === Logs ===
             print( '\nStep:', '\n\t epoch:', self.epoch, '\n\t epoch_steps:', epoch_steps, '\n\t global_steps:', self.global_step, '\n\t step_time:', step_time, '\n\t loss:', loss.item(),
