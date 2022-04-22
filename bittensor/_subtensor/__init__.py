c25c0120f8c2f6c55565ece58e644288bb1abbc2
Unconst <32490803+unconst@users.noreply.github.com>
2022-04-22 17:20:24 -0500

    Update __init__.py (#742)
    Co-authored-by: Ala Shaabana <shaabana@gmail.com>

--- bittensor/_subtensor/__init__.py
+++ bittensor/_subtensor/__init__.py
@@ -192,7 +192,7 @@ class subtensor:
         """ Adds parser defaults to object from enviroment variables.
         """
         defaults.subtensor = bittensor.Config()
-        defaults.subtensor.network = os.getenv('BT_SUBTENSOR_NETWORK') if os.getenv('BT_SUBTENSOR_NETWORK') != None else 'local'
+        defaults.subtensor.network = os.getenv('BT_SUBTENSOR_NETWORK') if os.getenv('BT_SUBTENSOR_NETWORK') != None else 'nakamoto'
         defaults.subtensor.chain_endpoint = os.getenv('BT_SUBTENSOR_CHAIN_ENDPOINT') if os.getenv('BT_SUBTENSOR_CHAIN_ENDPOINT') != None else None
         defaults.subtensor._mock = os.getenv('BT_SUBTENSOR_MOCK') if os.getenv('BT_SUBTENSOR_MOCK') != None else False
 
