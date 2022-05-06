be473e0970f51893c94ff5889a105bf075311a2e
isabella618033 <isabella618033@gmail.com>
2022-05-06 14:58:22 -0400

    constant.py approach instead

--- tests/integration_tests/test_dataset.py
+++ tests/integration_tests/test_dataset.py
@@ -15,31 +15,25 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
 
-import os
-import yaml
-from munch import Munch
 import bittensor
+import constant
 from unittest.mock import MagicMock
 logging = bittensor.logging()
 
-with open( os.path.expanduser('~/.bittensor/bittensor/tests/integration_tests/config_for_test.yaml'), "r") as config_file:
-    config = Munch().fromDict(yaml.safe_load(config_file))
-    print(config)
-    
 def test_construct_text_corpus():
-    dataset = bittensor.dataset(num_batches = config.dataset.num_batches, save_dataset = True, dataset_name = config.dataset.dataset_name)
+    dataset = bittensor.dataset(num_batches = constant.dataset.num_batches, save_dataset = True, dataset_name = constant.dataset.dataset_name)
     dataset.construct_text_corpus()
     dataset.close()
 
 def test_next():
-    dataset = bittensor.dataset(num_batches = config.dataset.num_batches, dataset_name = config.dataset.dataset_name)
+    dataset = bittensor.dataset(num_batches = constant.dataset.num_batches, dataset_name = constant.dataset.dataset_name)
     next(dataset)
     next(dataset)
     next(dataset)
     dataset.close()
 
 def test_mock():
-    dataset = bittensor.dataset(_mock=True, dataset_name = config.dataset.dataset_name)
+    dataset = bittensor.dataset(_mock=True, dataset_name = constant.dataset.dataset_name)
     next(dataset)
     next(dataset)
     next(dataset)
@@ -53,7 +47,7 @@ def test_mock_function():
     dataset.close()
 
 def test_fail_IPFS_server():
-    dataset = bittensor.dataset(num_batches = config.dataset.num_batches, dataset_name = config.dataset.dataset_name)
+    dataset = bittensor.dataset(num_batches = constant.dataset.num_batches, dataset_name = constant.dataset.dataset_name)
     dataset.requests_retry_session = MagicMock(return_value = None)
     next(dataset)
     next(dataset)
@@ -63,7 +57,7 @@ def test_fail_IPFS_server():
 def test_change_data_size():
     data_sizes = [(10,20), (15.5, 20.5),(30, 40), (25,35)]
     result_data_sizes = [(10,20), (10,20),(30, 40), (25,35)]
-    dataset = bittensor.dataset(num_batches = config.dataset.num_batches, dataset_name = config.dataset.dataset_name)
+    dataset = bittensor.dataset(num_batches = constant.dataset.num_batches, dataset_name = constant.dataset.dataset_name)
     for data_size, result_data_size in zip(data_sizes, result_data_sizes):
         dataset.set_data_size(*data_size)
         assert next(dataset).size() == result_data_size
