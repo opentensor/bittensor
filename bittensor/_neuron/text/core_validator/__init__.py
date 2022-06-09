e9379b299a65e791d2cc9868e1a132d6aafa1888
Eugene <etesting007@gmail.com>
2022-06-09 11:50:39 -0400

    debug commit

--- bittensor/_neuron/text/core_validator/__init__.py
+++ bittensor/_neuron/text/core_validator/__init__.py
@@ -345,6 +345,7 @@ class neuron:
         score_key = 'shapley_values_val'  # server score based on validation Shapley value approximation
         moving_avg_scores = torch.zeros_like(self.metagraph.S)  # allow unevaluated UIDs to be selected to meet minimum topk
         moving_avg_scores[list(self.server_stats.keys())] = torch.tensor([s[score_key].item() for s in self.server_stats.values()])
+        print(moving_avg_scores)
         # Find the n_topk_peer_weights peers to set weights to.
         topk_scores, topk_uids = bittensor.unbiased_topk(moving_avg_scores, k=n_topk_peer_weights)
         topk_scores = bittensor.utils.weight_utils.normalize_max_multiple(x=topk_scores, multiple=max_allowed_ratio)
