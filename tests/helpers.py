af05165003819741f4452164b758a2a160033149
camfairchild <cameron@fairchild.dev>
2022-06-23 11:24:15 -0400

    fix package structure and fix range check for CLOSE_IN_VALUE

--- tests/helpers.py
+++ tests/helpers.py
@@ -11,4 +11,6 @@ class CLOSE_IN_VALUE():
 
     def __eq__(self, __o: Union[float, int, Balance]) -> bool:
         # True if __o \in [value - tolerance, value + tolerance]
-        return (self.value - self.tolerance) <= __o and __o <= (self.value + self.tolerance)
+        # or if value \in [__o - tolerance, __o + tolerance]
+        return ((self.value - self.tolerance) <= __o and __o <= (self.value + self.tolerance)) or \
+                ((__o - self.tolerance) <= self.value and self.value <= (__o + self.tolerance))
