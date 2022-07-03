a8d70a78774cf54940f04beecfa5ddd21559b615
opentaco <opentaco@protonmail.com>
2022-07-03 19:53:15 +0200

    Add forward_casual_lm_next() to server
    To be used for TextCausalLMNext synapse, already compacts/encodes response for transfer to remote dendrite with backward support.
    
    Forward pass through the pretrained model and select topk tokenizer logits and retokenize with std_tokenizer, then compact new token phrases and probabilities into 1-D tensor [ > batch_size * 2 * topk + 1] prob + at least 1 token per phrase + floor_prob. The floor probability is the mean probability of token phrases not captured in topk, required since the server tokenizer vocab_size may not be known to the receiver/validator.

--- bittensor/_neuron/text/core_server/run.py
+++ bittensor/_neuron/text/core_server/run.py
@@ -99,6 +99,14 @@ def serve(
     def forward_casual_lm(inputs_x:torch.FloatTensor, synapse, model_output = None):
         model_output, logits = model.encode_forward_causallm(inputs_x.to(model.device), model_output = model_output)
         return model_output, logits
+
+    def forward_casual_lm_next(inputs_x: torch.FloatTensor, synapse, model_output=None):
+        model_output, topk_token_phrases = model.encode_forward_causallmnext(inputs_x.to(model.device),
+                                                                             topk=synapse.topk,
+                                                                             model_output=model_output)
+        # topk_token_phrases: [sum_b(sum_k(len(phrase_k) + 1)_b)] contains topk token phrases and probabilities
+        #   Compacted 1-D tensor > batch_size * 2 * topk + 1
+        return model_output, topk_token_phrases
     
     def optimizer_step():
         optimizer.step()
