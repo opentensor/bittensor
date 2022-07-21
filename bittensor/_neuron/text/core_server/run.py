d673d217ef59f3198d786365f1d240621c09911d
Eugene <etesting007@gmail.com>
2022-07-21 15:16:55 -0400

    basic bug fixes

--- bittensor/_neuron/text/core_server/run.py
+++ bittensor/_neuron/text/core_server/run.py
@@ -276,8 +276,8 @@ def serve(
             synapse_last_hidden = forward_hidden_state if model.config.neuron.lasthidden else None,
             synapse_causal_lm = forward_casual_lm if model.config.neuron.causallm else None,
             synapse_seq_2_seq = forward_generate if model.config.neuron.seq2seq else None ,
-            blacklist = blacklist,
-            priority = priority,
+            blacklist = blacklist if not model.config.neuron.disable_blacklist else None,
+            priority = priority if not model.config.neuron.disable_priority else None,
         ).start().serve(subtensor=subtensor)
     
     axon.optimizer_step = optimizer_step
