a48bb6f19e66ee3aeed15562736d4ba999aeb7e8
opentaco <opentaco@protonmail.com>
2022-07-31 14:27:54 +0200

    Update nill responses for TextCausalLMNext synapses

--- bittensor/_synapse/text_causallmnext_impl.py
+++ bittensor/_synapse/text_causallmnext_impl.py
@@ -175,13 +175,11 @@ class TextCausalLMNext(Synapse):
         return decoded_gradient  # [batch_size, (topk + 1), max_len]
 
     def nill_forward_response_tensor(self, forward_request_tensor: torch.Tensor) -> torch.Tensor:
-        try:
-            return torch.zeros((forward_request_tensor.shape[0], (self.topk + 1), 1 + 1), dtype=torch.float32)
-        except:
+        if forward_request_tensor.dim() == 0:
             return torch.tensor([])
+        return torch.zeros((forward_request_tensor.shape[0], (self.topk + 1), 1 + 1), dtype=torch.float32)
 
     def nill_backward_response_tensor(self, forward_request_tensor: torch.Tensor) -> torch.Tensor:
-        try:
-            return torch.zeros((forward_request_tensor.shape[0], (self.topk + 1), 1 + 1), dtype=torch.float32)
-        except:
+        if forward_request_tensor.dim() == 0:
             return torch.tensor([])
+        return torch.zeros((forward_request_tensor.shape[0], (self.topk + 1), 1 + 1), dtype=torch.float32)
