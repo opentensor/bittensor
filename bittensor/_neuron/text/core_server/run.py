f0518df3c2f11c1536f6077561bd34c8a034f5b4
Carro <robertmyers@hey.com>
2022-08-10 21:09:54 -0500

    removed whitespace

--- bittensor/_neuron/text/core_server/run.py
+++ bittensor/_neuron/text/core_server/run.py
@@ -134,7 +134,6 @@ def serve(
             message, model_output, topk_token_phrases = model.encode_forward_causallmnext(inputs_x.to(model.device),
                                                                                         topk=synapse.topk,
                                                                                         model_output=model_output)
-
         # topk_token_phrases: [sum_b(sum_k(len(phrase_k) + 1)_b)] contains topk token phrases and probabilities
         #   Compacted 1-D tensor >= batch_size * (2 * topk + 1)
         return message, model_output, topk_token_phrases
