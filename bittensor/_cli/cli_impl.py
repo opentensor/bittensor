7faa071a9252f6e9aba2c1d3be48e820205cd713
Cameron Fairchild <cameron.fairchild@mail.utoronto.ca>
2022-08-12 13:55:19 -0400

    [hotfix] fix flags for multiproc register limit (#876)
    * add dot get
    
    * add to subtensor args and defaults
    
    * remove dot get because in subtensor args
    
    * typo
    
    * fix test

--- bittensor/_cli/cli_impl.py
+++ bittensor/_cli/cli_impl.py
@@ -239,7 +239,7 @@ class CLI:
         """
         wallet = bittensor.wallet( config = self.config )
         subtensor = bittensor.subtensor( config = self.config )
-        subtensor.register( wallet = wallet, prompt = not self.config.no_prompt, num_processes = self.config.num_processes, update_interval = self.config.update_interval )
+        subtensor.register( wallet = wallet, prompt = not self.config.no_prompt, num_processes = self.config.subtensor.register.num_processes, update_interval = self.config.subtensor.register.update_interval )
 
     def transfer( self ):
         r""" Transfer token of amount to destination.
