b8520f7ed00319923e67c16dc7c6e1a80f24c891
Cameron Fairchild <cameron@opentensor.ai>
2022-08-22 13:59:26 -0400

    create new test file add/update copyright notices

--- bittensor/_config/config_impl.py
+++ bittensor/_config/config_impl.py
@@ -3,6 +3,7 @@ Implementation of the config class, which manages the config of different bitten
 """
 # The MIT License (MIT)
 # Copyright © 2021 Yuma Rao
+# Copyright © 2022 Opentensor Foundation
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation 
