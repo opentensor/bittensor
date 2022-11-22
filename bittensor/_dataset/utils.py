00036a0555c6aaea6bec6603d2447d55979a38bb
salvivona <vivonasg@gmail.com>
2022-11-22 19:13:57 +0000

    added asyncio import to utils

--- bittensor/_dataset/utils.py
+++ bittensor/_dataset/utils.py
@@ -18,7 +18,7 @@ Utilities for the DataImplementation
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
 
-
+import asyncio
 
 def sync_wrapper(fn:'asyncio.callable') -> 'callable':
     '''
