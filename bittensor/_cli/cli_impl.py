59660213705307262f4fef814221277d89dce479
Cameron Fairchild <cameron@opentensor.ai>
2022-12-23 12:45:50 -0500

    new overview layout for subnets

--- bittensor/_cli/cli_impl.py
+++ bittensor/_cli/cli_impl.py
@@ -17,7 +17,7 @@
 
 import os
 import sys
-from typing import List, Union, Optional
+from typing import List, Union, Optional, Dict
 
 from cachetools import Cache
 
@@ -28,6 +28,7 @@ from rich import print
 from rich.prompt import Confirm
 from rich.table import Table
 from rich.tree import Tree
+from rich.align import Align
 from tqdm import tqdm
 
 
@@ -695,142 +696,166 @@ class CLI:
         # Pull neuron info for all keys.            
         neurons = []
         block = subtensor.block
+
+        # TODO (Cameron): Get netuids from chain.
+        netuids = [1]
+
         with console.status(":satellite: Syncing with chain: [white]{}[/white] ...".format(self.config.subtensor.get('network', bittensor.defaults.subtensor.network))):
-            try:
-                if self.config.subtensor.get('network', bittensor.defaults.subtensor.network) not in ('local', 'nakamoto'):
-                    # We only cache neurons for local/nakamoto.
-                    raise CacheException("This network is not cached, defaulting to regular overview.")
-            
-                if self.config.get('no_cache'):
-                    raise CacheException("Flag was set to not use cache, defaulting to regular overview.")
-
-                metagraph: bittensor.Metagraph = bittensor.metagraph( subtensor = subtensor )
+            for netuid in netuids:
                 try:
-                    # Grab cached neurons from IPFS
-                    all_neurons = metagraph.retrieve_cached_neurons()
-                except Exception:
-                    raise CacheException("Failed to retrieve cached neurons, defaulting to regular overview.")
-                # Map the hotkeys to uids
-                hotkey_to_neurons = {n.hotkey: n.uid for n in all_neurons}
-                for wallet in tqdm(all_hotkeys):
-                    uid = hotkey_to_neurons.get(wallet.hotkey.ss58_address)
-                    if uid is not None:
-                        nn = all_neurons[uid]
-                        neurons.append( (nn, wallet) )
-            except CacheException:
-                for wallet in tqdm(all_hotkeys):
-                    # Get overview without cache
-                    nn = subtensor.neuron_for_pubkey( wallet.hotkey.ss58_address )
-                    if not nn.is_null:
-                      neurons.append( (nn, wallet) )
-                      
+                    if self.config.subtensor.get('network', bittensor.defaults.subtensor.network) not in ('local', 'nakamoto'):
+                        # We only cache neurons for local/nakamoto.
+                        raise CacheException("This network is not cached, defaulting to regular overview.")
+                
+                    if self.config.get('no_cache'):
+                        raise CacheException("Flag was set to not use cache, defaulting to regular overview.")
 
-        TABLE_DATA = []  
-        total_stake = 0.0
-        total_rank = 0.0
-        total_trust = 0.0
-        total_consensus = 0.0
-        total_incentive = 0.0
-        total_dividends = 0.0
-        total_emission = 0   
+                    metagraph: bittensor.Metagraph = bittensor.metagraph( subtensor = subtensor, netuid = 1 )
+                    try:
+                        # Grab cached neurons from IPFS
+                        all_neurons = metagraph.retrieve_cached_neurons()
+                    except Exception:
+                        raise CacheException("Failed to retrieve cached neurons, defaulting to regular overview.")
+                    # Map the hotkeys to uids
+                    hotkey_to_neurons = {n.hotkey: n.uid for n in all_neurons}
+                    for wallet in tqdm(all_hotkeys):
+                        uid = hotkey_to_neurons.get(wallet.hotkey.ss58_address)
+                        if uid is not None:
+                            nn = all_neurons[uid]
+                            neurons.append( (nn, wallet) )
+                except CacheException:
+                    for wallet in tqdm(all_hotkeys):
+                        # Get overview without cache
+                        nn = subtensor.neuron_for_pubkey( wallet.hotkey.ss58_address, netuid = netuid )
+                        if not nn.is_null:
+                            neurons.append( (nn, wallet) )
+        
+        # Setup outer table.
+        grid = Table.grid(pad_edge=False)
 
-        for nn, hotwallet in tqdm(neurons):
-            uid = nn.uid
-            active = nn.active
-            stake = nn.stake
-            rank = nn.rank
-            trust = nn.trust
-            consensus = nn.consensus
-            incentive = nn.incentive
-            dividends = nn.dividends
-            emission = int(nn.emission * 1000000000)
-            last_update = int(block -  nn.last_update)
-            row = [
-                hotwallet.name,
-                hotwallet.hotkey_str,
-                str(uid), 
-                str(active), 
-                '{:.5f}'.format(stake),
-                '{:.5f}'.format(rank), 
-                '{:.5f}'.format(trust), 
-                '{:.5f}'.format(consensus), 
-                '{:.5f}'.format(incentive),
-                '{:.5f}'.format(dividends),
-                '{}'.format(emission),
-                str(last_update),
-                bittensor.utils.networking.int_to_ip( nn.ip) + ':' + str(nn.port) if nn.port != 0 else '[yellow]none[/yellow]', 
-                nn.hotkey
-            ]
-            total_stake += stake
-            total_rank += rank
-            total_trust += trust
-            total_consensus += consensus
-            total_incentive += incentive
-            total_dividends += dividends
-            total_emission += emission
-            TABLE_DATA.append(row)
-            
-        total_neurons = len(neurons)                
-        table = Table(show_footer=False, width=self.config.get('width', None), pad_edge=False, box=None)
+        title: str = ""
         if not self.config.all:
-            table.title = ( "[white]Wallet - {}:{}".format(self.config.wallet.name, wallet.coldkeypub.ss58_address) )
+            title = ( "[bold white italic]Wallet - {}:{}".format(self.config.wallet.name, wallet.coldkeypub.ss58_address) )
         else:
-            table.title = ( "[white]All Wallets:" )
-        table.add_column("[overline white]COLDKEY",  str(total_neurons), footer_style = "overline white", style='bold white')
-        table.add_column("[overline white]HOTKEY",  str(total_neurons), footer_style = "overline white", style='white')
-        table.add_column("[overline white]UID",  str(total_neurons), footer_style = "overline white", style='yellow')
-        table.add_column("[overline white]ACTIVE", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]STAKE(\u03C4)", '\u03C4{:.5f}'.format(total_stake), footer_style = "overline white", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]RANK", '{:.5f}'.format(total_rank), footer_style = "overline white", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]TRUST", '{:.5f}'.format(total_trust), footer_style = "overline white", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]CONSENSUS", '{:.5f}'.format(total_consensus), footer_style = "overline white", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]INCENTIVE", '{:.5f}'.format(total_incentive), footer_style = "overline white", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]DIVIDENDS", '{:.5f}'.format(total_dividends), footer_style = "overline white", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]EMISSION(\u03C1)", '\u03C1{}'.format(int(total_emission)), footer_style = "overline white", justify='right', style='green', no_wrap=True)
-        table.add_column("[overline white]UPDATED", justify='right', no_wrap=True)
-        table.add_column("[overline white]AXON", justify='left', style='dim blue', no_wrap=True) 
-        table.add_column("[overline white]HOTKEY_SS58", style='dim blue', no_wrap=False)
-        table.show_footer = True
-        table.caption = "[white]Wallet balance: [green]\u03C4" + str(total_balance.tao)
+            title = ( "[bold whit italic]All Wallets:" )
+
+        # Add title
+        grid.add_row(Align(title, vertical="middle", align="center"))
+
+        # Generate rows per netuid
+        for netuid in netuids:
+            TABLE_DATA = []  
+            total_stake = 0.0
+            total_rank = 0.0
+            total_trust = 0.0
+            total_consensus = 0.0
+            total_incentive = 0.0
+            total_dividends = 0.0
+            total_emission = 0   
+
+            for nn, hotwallet in tqdm(neurons):
+                uid = nn.uid
+                active = nn.active
+                stake = nn.stake
+                rank = nn.rank
+                trust = nn.trust
+                consensus = nn.consensus
+                incentive = nn.incentive
+                dividends = nn.dividends
+                emission = int(nn.emission * 1000000000)
+                last_update = int(block -  nn.last_update)
+                row = [
+                    hotwallet.name,
+                    hotwallet.hotkey_str,
+                    str(uid), 
+                    str(active), 
+                    '{:.5f}'.format(stake),
+                    '{:.5f}'.format(rank), 
+                    '{:.5f}'.format(trust), 
+                    '{:.5f}'.format(consensus), 
+                    '{:.5f}'.format(incentive),
+                    '{:.5f}'.format(dividends),
+                    '{}'.format(emission),
+                    str(last_update),
+                    bittensor.utils.networking.int_to_ip( nn.ip) + ':' + str(nn.port) if nn.port != 0 else '[yellow]none[/yellow]', 
+                    nn.hotkey
+                ]
+                total_stake += stake
+                total_rank += rank
+                total_trust += trust
+                total_consensus += consensus
+                total_incentive += incentive
+                total_dividends += dividends
+                total_emission += emission
+                TABLE_DATA.append(row)
+                
+            total_neurons = len(neurons)
+
+            # Add subnet header
+            grid.add_row(f"Subnet: [bold white]{1}[/bold white]")
+
+            table = Table(show_footer=False, width=self.config.get('width', None), pad_edge=False, box=None)
+            table.add_column("[overline white]COLDKEY",  str(total_neurons), footer_style = "overline white", style='bold white')
+            table.add_column("[overline white]HOTKEY",  str(total_neurons), footer_style = "overline white", style='white')
+            table.add_column("[overline white]UID",  str(total_neurons), footer_style = "overline white", style='yellow')
+            table.add_column("[overline white]ACTIVE", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]STAKE(\u03C4)", '\u03C4{:.5f}'.format(total_stake), footer_style = "overline white", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]RANK", '{:.5f}'.format(total_rank), footer_style = "overline white", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]TRUST", '{:.5f}'.format(total_trust), footer_style = "overline white", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]CONSENSUS", '{:.5f}'.format(total_consensus), footer_style = "overline white", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]INCENTIVE", '{:.5f}'.format(total_incentive), footer_style = "overline white", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]DIVIDENDS", '{:.5f}'.format(total_dividends), footer_style = "overline white", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]EMISSION(\u03C1)", '\u03C1{}'.format(int(total_emission)), footer_style = "overline white", justify='right', style='green', no_wrap=True)
+            table.add_column("[overline white]UPDATED", justify='right', no_wrap=True)
+            table.add_column("[overline white]AXON", justify='left', style='dim blue', no_wrap=True) 
+            table.add_column("[overline white]HOTKEY_SS58", style='dim blue', no_wrap=False)
+            table.show_footer = True
+
+            sort_by: Optional[str] = self.config.get('sort_by', None)
+            sort_order: Optional[str] = self.config.get('sort_order', None)
+
+            if sort_by is not None and sort_by != "":
+                column_to_sort_by: int = 0
+                highest_matching_ratio: int = 0
+                sort_descending: bool = False # Default sort_order to ascending
+
+                for index, column in zip(range(len(table.columns)), table.columns):
+                    # Fuzzy match the column name. Default to the first column.
+                    column_name = column.header.lower().replace('[overline white]', '')
+                    match_ratio = fuzz.ratio(sort_by.lower(), column_name)
+                    # Finds the best matching column
+                    if  match_ratio > highest_matching_ratio:
+                        highest_matching_ratio = match_ratio
+                        column_to_sort_by = index
+                
+                if sort_order.lower() in { 'desc', 'descending', 'reverse'}:
+                    # Sort descending if the sort_order matches desc, descending, or reverse
+                    sort_descending = True
+                
+                def overview_sort_function(row):
+                    data = row[column_to_sort_by]
+                    # Try to convert to number if possible
+                    try:
+                        data = float(data)
+                    except ValueError:
+                        pass
+                    return data
+
+                TABLE_DATA.sort(key=overview_sort_function, reverse=sort_descending)
+
+            for row in TABLE_DATA:
+                table.add_row(*row)
+
+            grid.add_row(table)
+
 
         console.clear()
 
-        sort_by: Optional[str] = self.config.get('sort_by', None)
-        sort_order: Optional[str] = self.config.get('sort_order', None)
+        caption = "[italic][dim][white]Wallet balance: [green]\u03C4" + str(total_balance.tao)
+        grid.add_row(Align(caption, vertical="middle", align="center"))
 
-        if sort_by is not None and sort_by != "":
-            column_to_sort_by: int = 0
-            highest_matching_ratio: int = 0
-            sort_descending: bool = False # Default sort_order to ascending
-
-            for index, column in zip(range(len(table.columns)), table.columns):
-                # Fuzzy match the column name. Default to the first column.
-                column_name = column.header.lower().replace('[overline white]', '')
-                match_ratio = fuzz.ratio(sort_by.lower(), column_name)
-                # Finds the best matching column
-                if  match_ratio > highest_matching_ratio:
-                    highest_matching_ratio = match_ratio
-                    column_to_sort_by = index
-            
-            if sort_order.lower() in { 'desc', 'descending', 'reverse'}:
-                # Sort descending if the sort_order matches desc, descending, or reverse
-                sort_descending = True
-            
-            def overview_sort_function(row):
-                data = row[column_to_sort_by]
-                # Try to convert to number if possible
-                try:
-                    data = float(data)
-                except ValueError:
-                    pass
-                return data
-
-            TABLE_DATA.sort(key=overview_sort_function, reverse=sort_descending)
-
-        for row in TABLE_DATA:
-            table.add_row(*row)
-        
-        console.print(table, width=self.config.get('width', None))
+        # Print the entire table/grid
+        console.print(grid, width=self.config.get('width', None))
 
     def full(self):
         r""" Prints an overview for the wallet's colkey.
