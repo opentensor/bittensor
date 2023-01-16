1ffd8291ccb1d341a2a4624cc45ecf9e9e41372e
Cameron Fairchild <cameron@opentensor.ai>
2023-01-16 15:37:26 -0500

    make mock helper

--- tests/helpers.py
+++ tests/helpers.py
@@ -16,7 +16,8 @@
 # DEALINGS IN THE SOFTWARE.
 
 from typing import Union
-from bittensor import Balance
+from bittensor import Balance, NeuronInfo, AxonInfo, PrometheusInfo, Keypair, __ss58_format__
+from scalecodec import ss58_encode
 
 class CLOSE_IN_VALUE():
     value: Union[float, int, Balance]
@@ -31,3 +32,72 @@ class CLOSE_IN_VALUE():
         # or if value \in [__o - tolerance, __o + tolerance]
         return ((self.value - self.tolerance) <= __o and __o <= (self.value + self.tolerance)) or \
                 ((__o - self.tolerance) <= self.value and self.value <= (__o + self.tolerance))
+
+
+def get_mock_keypair( uid: int ) -> Keypair:
+    return Keypair(ss58_encode(int.to_bytes(uid, 32, 'big', signed=False), __ss58_format__))
+
+def get_mock_hotkey( uid: int ) -> str:
+    return get_mock_keypair(uid).ss58_address
+
+def get_mock_coldkey( uid: int ) -> str:
+    return get_mock_keypair(uid).ss58_address
+
+def get_mock_neuron(**kwargs) -> NeuronInfo:
+    """
+    Returns a mock neuron with the given kwargs overriding the default values.
+    """
+
+    mock_neuron_d = dict({
+                "netuid": -1, # mock netuid
+                "axon_info": AxonInfo(
+                    block = 0,
+                    version = 1,
+                    ip = 0,
+                    port = 0,
+                    ip_type = 0,
+                    protocol = 0,
+                    placeholder1 = 0,
+                    placeholder2 = 0
+                ),
+                "prometheus_info": PrometheusInfo(
+                    block = 0,
+                    version = 1,
+                    ip = 0,
+                    port = 0,
+                    ip_type = 0
+                ),
+                "validator_permit": True,
+                "uid":1,
+                "hotkey":'some_hotkey',
+                "coldkey":'some_coldkey',
+                "active":0,
+                "last_update":0,
+                "stake": {
+                    "some_coldkey": 1e12
+                },
+                "total_stake":1e12,
+                "rank":0.0,
+                "trust":0.0,
+                "consensus":0.0,
+                "incentive":0.0,
+                "dividends":0.0,
+                "emission":0.0,
+                "bonds":[],
+                "weights":[],
+                "is_null":False
+            })
+
+    mock_neuron_d.update(kwargs) # update with kwargs
+
+    if kwargs.get('stake') is None and kwargs.get('coldkey') is not None:
+        mock_neuron_d['stake'] = { kwargs.get('coldkey'): 1e12 }
+
+    if kwargs.get('total_stake') is None:
+        mock_neuron_d['total_stake'] = sum(mock_neuron_d['stake'].values())
+
+    mock_neuron = NeuronInfo._neuron_dict_to_namespace(
+        mock_neuron_d
+    )
+
+    return mock_neuron
