161a71471aacc910abec9fcefd31712385e5bfba
Cameron Fairchild <cameron@opentensor.ai>
2023-01-16 20:40:53 -0500

    update copyright notices

--- bittensor/_subtensor/extrinsics/unstaking.py
+++ bittensor/_subtensor/extrinsics/unstaking.py
@@ -1,5 +1,6 @@
 # The MIT License (MIT)
 # Copyright © 2021 Yuma Rao
+# Copyright © 2023 Opentensor Foundation
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation 
@@ -14,6 +15,7 @@
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
+
 import bittensor
 
 from rich.prompt import Confirm
