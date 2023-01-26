b32a7640a4b535f53270ab5baa4c6d8c06beab7a
Cameron Fairchild <cameron@opentensor.ai>
2023-01-26 14:56:48 -0500

    fix get mock neuron

--- tests/helpers.py
+++ tests/helpers.py
@@ -80,6 +80,8 @@ def get_mock_neuron(**kwargs) -> NeuronInfo:
                 "rank":0.0,
                 "trust":0.0,
                 "consensus":0.0,
+                "validator_trust": 0.0,
+                "weight_consensus": 0.0,
                 "incentive":0.0,
                 "dividends":0.0,
                 "emission":0.0,
