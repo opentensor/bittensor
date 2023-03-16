ec674cb9287993d609d9e04874d9d3bb107de024
isabella618033 <isabella618033@gmail.com>
2023-03-16 16:04:28 -0400

    removed neuron init

--- bittensor/_neuron/__init__.py
+++ bittensor/_neuron/__init__.py
@@ -14,19 +14,3 @@
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
-
-# Bittensor neurons
-__version__ = '0.0.0'
-version_split = __version__.split(".")
-__version_as_int__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
-
-from .text import core_validator, core_server
-__all_neurons__ =  {  
-                     'text_core_validator': core_validator.neuron,
-                    #  'core_server': core_server.neuron
-                    }
-
-__text_neurons__ =  { 
-                     'core_validator': core_validator.neuron,
-                    #  'core_server': core_server.neuron
-                     }
