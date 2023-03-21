0e8669a846c900b239cf12c7a0127ba9132fdb2a
Prism <ngoc.them.dev@gmail.com>
2023-03-21 08:15:38 -0400

    changed explorer_url for transaction on finney chain

--- bittensor/_subtensor/naka_subtensor_impl.py
+++ bittensor/_subtensor/naka_subtensor_impl.py
@@ -1157,7 +1157,7 @@ class Subtensor:
                     bittensor.__console__.print(":white_heavy_check_mark: [green]Finalized[/green]")
                     block_hash = response.block_hash
                     bittensor.__console__.print("[green]Block Hash: {}[/green]".format( block_hash ))
-                    explorer_url = "https://explorer.nakamoto.opentensor.ai/#/explorer/query/{block_hash}".format( block_hash = block_hash )
+                    explorer_url = "https://explorer.finney.opentensor.ai/#/explorer/query/{block_hash}".format( block_hash = block_hash )
                     bittensor.__console__.print("[green]Explorer Link: {}[/green]".format( explorer_url ))
                 else:
                     bittensor.__console__.print(":cross_mark: [red]Failed[/red]: error:{}".format(response.error_message))
