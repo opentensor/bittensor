8a685e5c55937ef7fd5cbe36eaac213428745ed3
unconst <jake@bittensor.com>
2023-03-29 16:30:05 -0600

    clean vali

--- bittensor/_neuron/text/prompting/validator/__init__.py
+++ bittensor/_neuron/text/prompting/validator/__init__.py
@@ -14,106 +14,70 @@ from bittensor._neuron.text.prompting.validator.model_impl import PromptingValid
 from .reward_impl import GPTRewardModel
 
 class neuron:
-    def __init__(
-        self, 
-        config: 'bittensor.config' = None,
-        subtensor: 'bittensor.subtensor' = None,
-        wallet: 'bittensor.wallet' = None,
-        axon: 'bittensor.axon' = None,
-        metagraph: 'bittensor.metagraph' = None,
-        model: 'bittensor.neurons.text.core_server.server' = None,
-        netuid: int = None
-    ):
+    @classmethod
+    def check_config( cls, config: 'bittensor.Config' ):
+        r""" Checks/validates the config namespace object.
+        """
+        PromptingValidator.check_config( config )
+        bittensor.logging.check_config( config )
+        bittensor.wallet.check_config( config )
+        bittensor.subtensor.check_config( config )
+        bittensor.metagraph.check_config( config )
+        bittensor.dataset.check_config( config )
+        bittensor.wandb.check_config( config )
+        bittensor.prometheus.check_config( config )
+        full_path = os.path.expanduser('{}/{}/{}/netuid{}/{}'.format( config.logging.logging_dir, config.wallet.name, config.wallet.hotkey, config.netuid, config.neuron.name ))
+        config.neuron.full_path = os.path.expanduser(full_path)
+        config.using_wandb = config.wandb.api_key != 'default'
+        if not os.path.exists(config.neuron.full_path):
+            os.makedirs(config.neuron.full_path)
 
-        if config == None: config = neuron.config()
-        self.config = config
-        subtensor = bittensor.subtensor ( config = config ) if subtensor == None else subtensor
-        if config.subtensor.network != 'nakamoto' and config.netuid == None:
-            config.netuid = subtensor.get_subnets()[0]
+    @classmethod
+    def add_args( cls, parser ):
+        # Netuid Arg
+        parser.add_argument('--netuid', type=int , help='Subnet netuid', default=11)
 
-        # Verify subnet exists
-        if config.subtensor.network != 'nakamoto' and not subtensor.subnet_exists( netuid = config.netuid ):
-            bittensor.__console__.print(f"[red]Subnet {config.netuid} does not exist[/red]")
-            sys.exit(1)
-        
+    @classmethod
+    def config ( cls ):
+        parser = argparse.ArgumentParser()    
+        cls.add_args( parser )
+        PromptingValidator.add_args( parser )    
+        bittensor.wallet.add_args( parser )
+        bittensor.subtensor.add_args( parser )
+        bittensor.metagraph.add_args( parser )
+        bittensor.logging.add_args( parser )
+        bittensor.dataset.add_args( parser )
+        bittensor.wandb.add_args(parser)
+        bittensor.prometheus.add_args( parser )
+        return bittensor.config( parser )
+    
+    def __init__( self ):
+        self.config = neuron.config()
+        self.subtensor = bittensor.subtensor ( config = self.config )
         self.messages = []
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.wallet = bittensor.wallet ( config = self.config ) if wallet == None else wallet
-        self.subtensor = subtensor
+        self.wallet = bittensor.wallet ( config = self.config )
         self.metagraph = self.subtensor.metagraph(11)
-
-
-    def sync_metagraph(self):
-        self.metagraph.sync(netuid=self.config.netuid, subtensor=self.subtensor).save()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        print(exc_type, exc_value, traceback)
-
-    def __enter__(self):
-        config = self.config
-
-        self.wallet.create()
-        self.wallet.reregister( subtensor = self.subtensor, netuid=self.config.netuid )
-        self.uid = self.wallet.get_uid( subtensor = self.subtensor, netuid=self.config.netuid )  
-
+        self.wallet.create_if_non_existent()
+        self.wallet.reregister( subtensor = self.subtensor, netuid = self.config.netuid )
+        self.uid = self.wallet.get_uid( subtensor = self.subtensor, netuid = self.config.netuid )  
         self.model = PromptingValidator(
-            config = config,
-            model_name = config.nucleus.model_name,
-            min_tokens = config.nucleus.min_tokens,
-            max_tokens = config.nucleus.max_tokens,
-            temperature = config.nucleus.temperature,
-            top_p = config.nucleus.top_p,
-            logprobs = config.nucleus.logprobs,
-            repetition_penalty = config.nucleus.repetition_penalty
+            config = self.config,
+            model_name = self.config.nucleus.model_name,
+            min_tokens = self.config.nucleus.min_tokens,
+            max_tokens = self.config.nucleus.max_tokens,
+            temperature = self.config.nucleus.temperature,
+            top_p = self.config.nucleus.top_p,
+            logprobs = self.config.nucleus.logprobs,
+            repetition_penalty = self.config.nucleus.repetition_penalty
         )
-
         self.reward_model = GPTRewardModel('Dahoas/gptj-rm-IHP', 'EleutherAI/gpt-j-6B')
         self.reward_model.to(self.device)
-
-    def reward_fn(self, samples):
-        scores_list = []
-        batch_size = 2
-        for i in range(0, len(samples), batch_size):
-            sub_samples = samples[i : i + batch_size]
-            sub_samples = [
-                "<|startoftext|>" + chosen + "<|endoftext|>" for chosen in sub_samples
-            ]
-            encodings_dict = self.reward_model.tokenizer(
-                sub_samples,
-                truncation=True,
-                max_length=550,
-                padding="max_length",
-                return_tensors="pt",
-            )
-            input_ids = encodings_dict["input_ids"].to(self.device)
-            attn_masks = encodings_dict["attention_mask"].to(self.device)
-            input_ids = input_ids.repeat(2, 1)
-            attn_masks = attn_masks.repeat(2, 1)
-            with torch.no_grad():
-                sub_scores = self.reward_model(input_ids=input_ids, attention_mask=attn_masks)
-            scores_list.append(sub_scores["chosen_end_scores"])
-        scores = torch.cat(scores_list, dim=0)
-        return scores
-
-    def create_user_prompt(self, message):
-        message = {
-            "role": "user",
-            "content": message
-        }
-        return (json.dumps(message))
-
-    # an input that creates a user prompt
-    def debug_input(self):
-        user_message = input("User> ")
-        return user_message
-
-        
 
     def run(self):
         with self:
             while True:
-                user_message = self.debug_input()
-                # message = self.create_user_prompt(user_message)
+                user_message = input("User> ")
                 responses = []
                 values = {}
                 for endpoint in (self.metagraph.endpoint_objs):
@@ -172,45 +136,29 @@ class neuron:
                 print('sorted_values: ', sorted_values)
                 print('responses: ', responses)
 
+    def reward_fn(self, samples):
+        scores_list = []
+        batch_size = 2
+        for i in range(0, len(samples), batch_size):
+            sub_samples = samples[i : i + batch_size]
+            sub_samples = [
+                "<|startoftext|>" + chosen + "<|endoftext|>" for chosen in sub_samples
+            ]
+            encodings_dict = self.reward_model.tokenizer(
+                sub_samples,
+                truncation=True,
+                max_length=550,
+                padding="max_length",
+                return_tensors="pt",
+            )
+            input_ids = encodings_dict["input_ids"].to(self.device)
+            attn_masks = encodings_dict["attention_mask"].to(self.device)
+            input_ids = input_ids.repeat(2, 1)
+            attn_masks = attn_masks.repeat(2, 1)
+            with torch.no_grad():
+                sub_scores = self.reward_model(input_ids=input_ids, attention_mask=attn_masks)
+            scores_list.append(sub_scores["chosen_end_scores"])
+        scores = torch.cat(scores_list, dim=0)
+        return scores
 
-    
 
-
-    @classmethod
-    def check_config( cls, config: 'bittensor.Config' ):
-        r""" Checks/validates the config namespace object.
-        """
-        PromptingValidator.check_config( config )
-        bittensor.logging.check_config( config )
-        bittensor.wallet.check_config( config )
-        bittensor.subtensor.check_config( config )
-        bittensor.metagraph.check_config( config )
-        bittensor.dataset.check_config( config )
-        bittensor.wandb.check_config( config )
-        bittensor.prometheus.check_config( config )
-        full_path = os.path.expanduser('{}/{}/{}/netuid{}/{}'.format( config.logging.logging_dir, config.wallet.name, config.wallet.hotkey, config.netuid, config.neuron.name ))
-        config.neuron.full_path = os.path.expanduser(full_path)
-        config.using_wandb = config.wandb.api_key != 'default'
-        if not os.path.exists(config.neuron.full_path):
-            os.makedirs(config.neuron.full_path)
-
-    @classmethod
-    def add_args( cls, parser ):
-        # Netuid Arg
-        parser.add_argument('--netuid', type=int , help='Subnet netuid', default=11)
-
-    @classmethod
-    def config ( cls ):
-        parser = argparse.ArgumentParser()    
-        cls.add_args( parser )
-        PromptingValidator.add_args( parser )    
-    
-
-        bittensor.wallet.add_args( parser )
-        bittensor.subtensor.add_args( parser )
-        bittensor.metagraph.add_args( parser )
-        bittensor.logging.add_args( parser )
-        bittensor.dataset.add_args( parser )
-        bittensor.wandb.add_args(parser)
-        bittensor.prometheus.add_args( parser )
-        return bittensor.config( parser )
