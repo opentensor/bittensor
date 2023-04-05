3f3335243fda865692fe61057066f7d5484b6430
unconst <ak@bittensor.com>
2023-04-05 15:23:21 +0000

    add changes

--- neurons/text/prompting/validators/core/neuron.py
+++ neurons/text/prompting/validators/core/neuron.py
@@ -16,6 +16,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 import os
+import queue
 import torch
 import argparse
 import bittensor as bt
@@ -59,6 +60,7 @@ class neuron:
         parser.add_argument('--neuron.inference_topk', type = str, help = 'At inference time, how many miners to we query and return the top rewarded.', default = 10 )
         parser.add_argument('--neuron.training_topk', type = str, help = 'During training time, how many miners to we query for each batch based on scores from gating network.', default = 10 )
         parser.add_argument('--neuron.epoch_length', type = str, help = 'During training time, how many miners to we query for each batch based on scores from gating network.', default = 10 )
+        parser.add_argument('--neuron.max_history', type = int, help = 'Maximum number history values to store at any time.', default = 100 )
 
     @classmethod
     def config ( cls ):
@@ -83,7 +85,7 @@ class neuron:
         self.reward_model = RewardModel( self.config.neuron.reward_model_name ).to(self.device)
         self.gating_model = GatingModel( metagraph = self.metagraph, config = self.config ).to(self.device)
         self.dendrite_pool = bt.text_prompting_pool( metagraph = self.metagraph, wallet = self.wallet )
-        self.history = []
+        self.history = queue.Queue( maxsize = self.config.neuron.max_history )
 
     def complute_weights( self ) -> torch.FloatTensor:
         """
@@ -94,23 +96,25 @@ class neuron:
                 weights ( torch.FloatTensor, shape = (n) ): 
                     The weights for each uid.
         """
+        if len(self.history) == 0:
+            
         # Averages the rewards for each uid across non-zero values.
         rewards = []
 
         # Iterate over all events in the `history` list.
         for event in self.history:
             # Normalize the rewards for the current event using L1 normalization.
-            normalized_rewards = torch.nn.functional.normalize( event.rewards, p=1, dim=0 )
+            normalized_rewards = torch.nn.functional.normalize( event.rewards.to( self.device ), p=1, dim=0 )
 
             # Use the `uids` of the current event to scatter the normalized rewards
             # into a zero-initialized tensor with the same shape as `self.metagraph.n`.
-            scattered_rewards = torch.zeros((self.metagraph.n)).scatter(0, event.uids, normalized_rewards )
+            scattered_rewards = torch.zeros((self.metagraph.n)).to( self.device ).scatter(0, event.uids.to( self.device ), normalized_rewards.to( self.device ) )
 
             # Append the scattered rewards to the `rewards` list.
             rewards.append(scattered_rewards)
 
         # Stack the scattered rewards tensors along the second dimension.
-        rewards = torch.stack( rewards, 1) 
+        rewards = torch.stack( rewards, 1 ).to( self.device )
 
         # Calculate the average reward for each uid across non-zero values.
         # Replace any NaN values with 0.
@@ -132,54 +136,59 @@ class neuron:
                 message (str): The message to query the network with.
         """
         print ('forward --------------------' )
+        print ('message', message )
 
         # Set `topk` to the number of items in `self.metagraph.n` if `topk` is not provided or is -1.
         # Find the available `uids` that are currently serving.
         # If `topk` is larger than the number of available `uids`, set `topk` to the number of available `uids`.
-        available_uids = torch.tensor( [ uid for uid, ep in enumerate( self.metagraph.endpoint_objs ) if ep.is_serving ], dtype = torch.int64 )
+        available_uids = torch.tensor( [ uid for uid, ep in enumerate( self.metagraph.endpoint_objs ) if ep.is_serving ], dtype = torch.int64 ).to( self.device )
         if topk is None or topk == -1: topk = self.metagraph.n.item()
         if topk > len(available_uids): topk = len(available_uids)
-        print ('topk', topk)
+        print ('\ntopk', topk)
+        if len( available_uids ) == 0: return print('no available uids'); None
 
         # We run the gating network here to get the best uids
         # Use the gating model to generate scores for each `uid`.
-        scores = self.gating_model( message )
-        print ('scores', scores.size(), scores)
+        scores = self.gating_model( message ).to( self.device )
+        print ('\nscores', scores.size(), scores)
 
         # Select the top `topk` `uids` based on the highest `scores`.
         # Use the selected `uids` to query the dendrite pool.
         # Print the `completions`.
         topk_uids = available_uids[ scores[ available_uids ].sort()[ 1 ][ -topk: ]]
         completions = self.dendrite_pool( prompt = self.config.neuron.base_prompt, message = message, uids = topk_uids )
-        print ('topk_uids',  len(topk_uids), topk_uids)
-        print ('completions', len(completions), completions)
+        print ('\ntopk_uids',  len(topk_uids), topk_uids)
+        print ('\ncompletions', len(completions), completions)
 
         # Filter out any `None` `completions`.
+        successful_uids = torch.tensor( [ uid for uid, completion in list( zip( topk_uids, completions ) ) if completion is not None and not '' ], dtype = torch.int64 ).to( self.device )
+        successful_completions = [ completion for completion in completions if completion is not None and not '' ]
+        print ('\nsuccessful_uids', len(successful_uids), successful_uids)
+        print ('\nsuccessful_completions', len(successful_completions), successful_completions)
+        if len( successful_completions ) == 0: print ('no successful queries'); return None
+
         # Calculate the rewards for the successful `completions` using the reward model.
         # Print the rewards for all `uids`.
-        successful_uids = [ uid for uid, completion in list( zip( topk_uids, completions ) ) if completion is not None ]
-        successful_completions = [ completions[ uid ] for uid in successful_uids ]
-        rewards = torch.zeros( ( self.metagraph.n.item() ) )
-        rewards[ successful_uids ] = self.reward_model.reward( successful_completions )
-        print ('successful_uids', len(successful_uids), successful_uids)
-        print ('rewards', rewards.size(), rewards)
+        rewards = self.reward_model.reward( successful_completions ).to( self.device )
+        print ('\nrewards', rewards.size(), rewards)
 
         # Train the gating model using the scores and rewards of the successful `completions`.
-        self.gating_model.backward( scores = scores[ successful_uids ], rewards = rewards[ successful_uids ] )
+        self.gating_model.backward( scores = scores[ successful_uids ], rewards = rewards )
 
         # Save the query history in a `result` object.
         # Return the `completion` with the highest reward.
         result = SimpleNamespace( 
-            completion = completions[ rewards.argmax( dim = 0 ) ],
+            completion = successful_completions[ rewards.argmax( dim = 0 ) ],
             message = message,  
-            uids = topk_uids,
+            uids = successful_uids,
             rewards = rewards,
-            scores = scores[ topk_uids ],
+            scores = scores,
             all_completions = completions
         )
         self.history.append( result )
 
         # Return the completion with the highest reward.
+        print ('result', result )
         return result
     
     # User queries here.
@@ -201,12 +210,13 @@ class neuron:
             
             # Query the network for a random question.
             question = self.forward( self.config.neuron.question_prompt ).completion
+            if question == None: continue # no responses from network.
             
             # Ask the network to complete the random question, training the gating network.
             self.forward( question, topk = self.config.neuron.training_topk )
             
             # Check if enough epoch blocks have elapsed since the last epoch.
-            if self.subtensor.block - last_epoch_block > self.subtensor.validator_epoch_length( self.config.netuid ) :
+            if self.subtensor.block > last_epoch_block: # run every block. # > self.subtensor.validator_epoch_length( self.config.netuid ) :
                 
                 # Update the last epoch block to the current epoch block.
                 last_epoch_block = self.subtensor.block
@@ -217,8 +227,11 @@ class neuron:
                 
                 # Set the weights on chain via our subtensor connection.
                 self.subtensor.set_weights(
+                    wallet = self.wallet,
+                    netuid = self.config.netuid,
                     uids = self.metagraph.uids,
                     weights = weights,
+                    wait_for_finalization = True,
                 )
             
 if __name__ == '__main__':
