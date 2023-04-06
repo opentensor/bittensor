fix tests and remove duplicate sign

bittensor/_receptor/receptor_impl.py
@@ -123,7 +123,7 @@ class Receptor(nn.Module):
     def __exit__ ( self ):
         self.__del__()
 
-    def sign_v2(self):
+    def sign(self):
         nonce = f"{self.nonce()}"
         sender_hotkey = self.wallet.hotkey.ss58_address
         receiver_hotkey = self.endpoint.hotkey
@@ -131,8 +131,6 @@ class Receptor(nn.Module):
         signature = f"0x{self.wallet.hotkey.sign(message).hex()}"
         return ".".join([nonce, sender_hotkey, signature, self.receptor_uid])
 
-    def sign(self):
-        return self.sign_v2()
 
     def nonce ( self ):
         r"""creates a string representation of the time

tests/unit_tests/bittensor_tests/test_neuron.py
@@ -341,7 +341,7 @@ class TestBlacklist(unittest.TestCase):
                 mock_hotkey,
                 mock_hotkey_1,
             ],
-            total_stake=[
+            S=[
                 torch.tensor(100), # stake for mock_hotkey, uid 0
                 torch.tensor(1001), # stake for mock_hotkey_1, uid 1
             ]
@@ -379,12 +379,13 @@ class TestBlacklist(unittest.TestCase):
             # args, kwargs
             _, kwargs = mock_new_axon.call_args
             blacklist = kwargs['blacklist']
-
             # Check that the blacklist rejects below min stake
-            assert blacklist(mock_hotkey, bittensor.proto.RequestType.FORWARD) == True
+            check, error = blacklist(mock_hotkey, bittensor.proto.RequestType.FORWARD)
+            assert check == True
 
             # Check that the blacklist accepts above min stake
-            assert blacklist(mock_hotkey_1, bittensor.proto.RequestType.FORWARD) == False
+            check, error = blacklist(mock_hotkey_1, bittensor.proto.RequestType.FORWARD) 
+            assert check == False
 
 
 if __name__ == '__main__':