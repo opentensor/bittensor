3f4c5cb7a93a03a0af02672b8727a77e06a76de6
unconst <jake@bittensor.com>
2023-04-10 10:47:55 +0100

    abstractify the cohere miner

--- neurons/text/prompting/miners/cohere/neuron.py
+++ neurons/text/prompting/miners/cohere/neuron.py
@@ -16,191 +16,62 @@
 # DEALINGS IN THE SOFTWARE.
 
 # General.
-import os
 import json
-import time
-import torch
 import argparse
 import bittensor
-
 from typing import List, Dict
-from rich import print
-from datetime import datetime
-
-# Torch tooling.
-from torch.nn.utils.rnn import pad_sequence
 from langchain.llms import Cohere
 
-# Check run config.
-def check_config(config: 'bittensor.Config'):
-    bittensor.logging.check_config(config)
-    bittensor.wallet.check_config(config)
-    bittensor.subtensor.check_config(config)
-    bittensor.metagraph.check_config(config)
-    bittensor.axon.check_config(config)
-    full_path = os.path.expanduser(
-        '{}/{}/{}/{}'.format(config.logging.logging_dir, config.wallet.get('name', bittensor.defaults.wallet.name),
-                             config.wallet.get('hotkey', bittensor.defaults.wallet.hotkey), config.neuron.name))
-    config.neuron.full_path = os.path.expanduser(full_path)
-    if not os.path.exists(config.neuron.full_path):
-        os.makedirs(config.neuron.full_path)
+class CohereMiner( bittensor.BasePromptingMiner ):
 
+    @classmethod
+    def check_config( cls, config: 'bittensor.Config' ):
+        assert config.cohere.api_key != None, 'the miner requires passing --cohere.api_key as an argument of the config.'
 
-# Create run config.
-def get_config():
-    parser = argparse.ArgumentParser()
+    @classmethod
+    def add_args( cls, parser: argparse.ArgumentParser ):
+        parser.add_argument('--cohere.model_name', type=str, help='Name of the model.', default='command-xlarge-nightly')
+        parser.add_argument('--cohere.max_tokens', type=int, help='Number of tokens to generate.', default=256)
+        parser.add_argument('--cohere.temperature', type=float, help='Temperature of generation.', default=0.75)
+        parser.add_argument('--cohere.k', type=int, help='Number of most likely tokens to consider at each step.', default=0)
+        parser.add_argument('--cohere.p', type=int, help='Total probability mass of tokens to consider at each step.', default=1)
+        parser.add_argument('--cohere.frequency_penalty', type=float, help='Penalizes repeated tokens according to frequency.', default=0.0)
+        parser.add_argument('--cohere.presence_penalty', type=float, help='Penalizes repeated tokens.', default=0.0)
+        parser.add_argument('--cohere.truncate', type=str, help='Specify how the client handles inputs longer than the maximum token length: Truncate from START, END or NONE', default=None)
+        parser.add_argument('--cohere.stop', type=str, help='List of tokens to stop generation on.', default=None)
+        parser.add_argument('--cohere.api_key', type=str, help='API key for Cohere.', default=None)
 
-    # Choene
-    parser.add_argument('--netuid', type=int, help='Subnet netuid', default=21)
-    parser.add_argument('--config', type=str, help='If set, defaults are overridden by passed file.')
-    parser.add_argument('--neuron.model_name', type=str, help='Name of the model.', default='command-xlarge-nightly')
-    parser.add_argument('--neuron.max_tokens', type=int, help='Number of tokens to generate.', default=256)
-    parser.add_argument('--neuron.temperature', type=float, help='Temperature of generation.', default=0.75)
-    parser.add_argument('--neuron.k', type=int, help='Number of most likely tokens to consider at each step.', default=0)
-    parser.add_argument('--neuron.p', type=int, help='Total probability mass of tokens to consider at each step.',
-                        default=1)
-    parser.add_argument('--neuron.frequency_penalty', type=float, help='Penalizes repeated tokens according to frequency.', default=0.0)
-    parser.add_argument('--neuron.presence_penalty', type=float, help='Penalizes repeated tokens.', default=0.0)
-    parser.add_argument('--neuron.truncate', type=str, help='Specify how the client handles inputs longer than the maximum token length: Truncate from START, END or NONE', default=None)
-    parser.add_argument('--neuron.stop', type=str, help='List of tokens to stop generation on.', default=None)
-    parser.add_argument('--neuron.api_key', type=str, help='API key for Cohere.', default=None)
+    def __init__( self ):
+        super( CohereMiner, self ).__init__()
+        print ( self.config )
+        # TODO(jason): add cohere specific args to config.
+        self.model = Cohere(
+            model = self.config.cohere.model_name, 
+            cohere_api_key = self.config.cohere.api_key
+        )
 
-    # Miner arguements
-    parser.add_argument('--neuron.name', type=str,
-                        help='Trials for this miner go in miner.root / (wallet_cold - wallet_hot) / miner.name ',
-                        default='cohere_prompting_miner')
-    parser.add_argument('--neuron.blocks_per_epoch', type=str, help='Blocks until the miner sets weights on chain',
-                        default=100)
-    parser.add_argument('--neuron.no_set_weights', action='store_true', help='If True, the model does not set weights.',
-                        default=False)
-    parser.add_argument('--neuron.max_batch_size', type=int, help='The maximum batch size for forward requests.',
-                        default=-1)
-    parser.add_argument('--neuron.max_sequence_len', type=int, help='The maximum sequence length for forward requests.',
-                        default=-1)
-    parser.add_argument('--neuron.blacklist.hotkeys', type=str, required=False, nargs='*', action='store',
-                        help='To blacklist certain hotkeys', default=[])
+    def priority( self, forward_call: "bittensor.TextPromptingForwardCall" ) -> float:
+        return 0.0
 
-    bittensor.wallet.add_args(parser)
-    bittensor.axon.add_args(parser)
-    bittensor.subtensor.add_args(parser)
-    bittensor.logging.add_args(parser)
-    bittensor.metagraph.add_args(parser)
-    # bittensor.TextPromptingSynapse.add_args(parser)
-    return bittensor.config(parser)
-
-
-# Main entry point for model serving.
-def main():
-    # --- Build, Check, Set and Print the run config.
-    config = get_config()
-    config.to_defaults()
-    check_config(config)
-    print(config)
-
-    # --- Turn on logging.
-    bittensor.logging(config=config, logging_dir=config.neuron.full_path)
-    
-    # --- Create our chain connection.
-    subtensor = bittensor.subtensor(config)
-    
-    # --- Create our wallet and register it to the subnetwork.
-    wallet = bittensor.wallet(config)
-    wallet.register(netuid=config.netuid, subtensor=subtensor)
-    
-    # --- Create our network state cache
-    metagraph = bittensor.metagraph(config=config, netuid=config.netuid, )
-    metagraph.sync(netuid=config.netuid, subtensor=subtensor).save()
-    uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
-    
-    # --- Build /Load our model and set the device.
-    with bittensor.__console__.status(f"Loading {config.neuron.model_name} ..."):
-        bittensor.logging.info('Loading', config.neuron.model_name )
-        model = Cohere(model=config.neuron.model_name, cohere_api_key=config.neuron.api_key)
-    
-    # --- Build axon server and start it.tensor.loggi
-    axon = bittensor.axon(
-        wallet=wallet,
-        metagraph=metagraph,
-        config=config,
-    )
-
-    def _process_history(history: List[str]) -> str:
+    def blacklist( self, forward_call: "bittensor.TextPromptingForwardCall" ) -> bool:
+        return False
+        
+    @staticmethod
+    def _process_history( history: List[Dict[str, str]] ) -> str:
         processed_history = ''
         for message in history:
             if message['role'] == 'system':
                 processed_history += 'system: ' + message['content'] + '\n'
-
             if message['role'] == 'assistant':
                 processed_history += 'assistant: ' + message['content'] + '\n'
-            
             if message['role'] == 'user':
                 processed_history += 'user: ' + message['content'] + '\n'
         return processed_history
 
-    class Synapse(bittensor.TextPromptingSynapse):
-        def _priority(self, forward_call: "bittensor.TextPromptingForwardCall") -> float:
-            return 0.0
-
-        def _blacklist(self, forward_call: "bittensor.TextPromptingForwardCall") -> bool:
-            return False
-        
-        def backward( self, messages: List[Dict[str, str]], response: str, rewards: torch.FloatTensor ) -> str:
-            pass
-
-        def forward(self, messages: List[str]) -> str:
-            history = _process_history(messages)
-            resp = model(history)
-            return resp
-
-    syn = Synapse()
-    axon.attach(syn)
-    axon.start()
-    axon.netuid = config.netuid
-    axon.protocol = 4
-    subtensor.serve_axon( axon )  
-    print (axon)
-
-    # --- Run Forever.
-    last_update = subtensor.get_current_block()
-    while True:
-
-        # --- Wait until next epoch.
-        current_block = subtensor.get_current_block()
-        while (current_block - last_update) < config.neuron.blocks_per_epoch:
-            time.sleep(bittensor.__blocktime__)
-            current_block = subtensor.get_current_block()
-        last_update = subtensor.get_current_block()
-
-        # --- Update the metagraph with the latest network state.
-        metagraph.sync(netuid=config.netuid, subtensor=subtensor)
-        uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
-
-        # --- Log performance.
-        print(
-            f"[white not bold]{datetime.now():%Y-%m-%d %H:%M:%S}[/white not bold]{' ' * 4} | "
-            f"{f'UID [bright_cyan]{uid}[/bright_cyan]'.center(16 + len('[bright_cyan][/bright_cyan]'))} | "
-            f'[dim white not bold] [green]{str(metagraph.S[uid].item()):.4}[/green] Stake [/dim white not bold]'
-            f'[dim white not bold]| [yellow]{str(metagraph.trust[uid].item()) :.3}[/yellow] Trust [/dim white not bold]'
-            f'[dim white not bold]| [green]{str(metagraph.incentive[uid].item()):.3}[/green] Incentive [/dim white not bold]')
-
-        # --- Set weights.
-        if not config.neuron.no_set_weights:
-            try:
-                # --- query the chain for the most current number of peers on the network
-                chain_weights = torch.zeros(subtensor.subnetwork_n(netuid=config.netuid))
-                chain_weights[uid] = 1
-                did_set = subtensor.set_weights(
-                    uids=torch.arange(0, len(chain_weights)),
-                    netuid=config.netuid,
-                    weights=chain_weights,
-                    wait_for_inclusion=False,
-                    wallet=wallet,
-                    version_key=1
-                )
-            except:
-                pass
-
+    def forward( self, messages: List[Dict[str, str]]  ) -> str:
+        history = self._process_history( messages )
+        return self.model( history )
 
 if __name__ == "__main__":
     bittensor.utils.version_checking()
-    main()
+    CohereMiner().run()
