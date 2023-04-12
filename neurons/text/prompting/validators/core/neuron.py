2c108873e580b985b70c572c5a1d351d8c6854c1
Carro <robertmyers@hey.com>
2023-04-11 22:51:35 -0500

    changing to gpt-j reward model for now

--- neurons/text/prompting/validators/core/neuron.py
+++ neurons/text/prompting/validators/core/neuron.py
@@ -113,7 +113,7 @@ class neuron:
                 checkpoint = os.path.join( self.config.neuron.reward_path, fpath )
                 break
         ckpt_state = torch.load(checkpoint)
-        ckpt_state = {k:v for k, v in ckpt_state.items() if not k.startswith('model.')}
+        # ckpt_state = {k:v for k, v in ckpt_state.items() if not k.startswith('model.')}
         self.reward_model.load_state_dict(ckpt_state)
         self.reward_model.eval()
         self.reward_model.requires_grad_(False)
