cdc1173e21c6c31d783c37b0c1e0cccf1c7b69c6
ifrit98 <ifrit98@gmail.com>
2023-05-29 16:20:38 +0000

    update protos for speech-to-text, update STT synapse

--- bittensor/_synapse/speech_to_text/synapse.py
+++ bittensor/_synapse/speech_to_text/synapse.py
@@ -23,6 +23,7 @@ import bittensor
 from fastapi import FastAPI, APIRouter
 from typing import List, Dict, Union, Callable
 from abc import ABC, abstractmethod
+from pydantic import BaseModel
 
 class SpeechToTextForward( bittensor.SynapseCall ):
     name: str = "speech_to_text_forward"
@@ -56,25 +57,30 @@ class SpeechToTextForward( bittensor.SynapseCall ):
         bittensor.logging.trace( "SpeechToTextForward.get_outputs_shape()" )
         return torch.Size( [ len(self.text) ]  ) if self.text != None else None
 
+
+class SpeechToText( BaseModel ):
+    speech: str
+    timeout: int = 12
+
 class SpeechToTextSynapse( bittensor.Synapse, bittensor.grpc.TextToImageServicer ):
     name: str = "speech_to_text"
 
     def attach( self, axon: 'bittensor.axon.Axon' ):
         bittensor.grpc.add_SpeechToTextServicer_to_server( self, self.axon.server )
         self.router = APIRouter()
-        self.router.add_api_route("/SpeechToText/Forward/", self.fast_api_forward_speech_to_text, methods=["GET"])
+        self.router.add_api_route("/SpeechToText/Forward/", self.fast_api_forward_speech_to_text, methods=["GET", "POST"])
         self.axon.fastapi_app.include_router( self.router )
         
     @abstractmethod
     def forward( self, speech: bytes ) -> str: 
         ...
 
-    def fast_api_forward_speech_to_text( self, hotkey: str, timeout: int, speech: bytes ) -> str:
+    def fast_api_forward_speech_to_text( self, hotkey: str, item: SpeechToText ) -> str:
         request_proto = bittensor.proto.ForwardSpeechToTextRequest( 
             hotkey = hotkey, 
             version = bittensor.__version_as_int__,
-            timeout = timeout, 
-            speech = speech
+            timeout = item.timeout, 
+            speech = item.speech
         )
         call = SpeechToTextForward( self, request_proto, self.forward )
         bittensor.logging.trace( 'FastSpeechToText: {} '.format( call ) )
