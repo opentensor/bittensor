92a83da12a51c221c66d12a3f1d7338d09cb7f69
ifrit98 <ifrit98@gmail.com>
2023-05-30 12:23:19 +0000

    run release versioning and changelog scripts

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -27,7 +27,7 @@ import nest_asyncio
 nest_asyncio.apply()
 
 # Bittensor code and protocol version.
-__version__ = '5.0.0'
+__version__ = '5.1.0'
 version_split = __version__.split(".")
 __version_as_int__ = (100 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 __new_signature_version__ = 360
