20c1718ce627098191c1f9abd000e4e905c402e5
Ala Shaabana <shaabana@gmail.com>
2023-05-31 14:10:31 -0400

    Fix meta sync block display (#1368)
    * Fixes metagraph block number when syncing historically
    
    * Better code
    
    ---------
    
    Co-authored-by: Ala Shaabana <ala@bittensor.com>

--- bittensor/_metagraph/__init__.py
+++ bittensor/_metagraph/__init__.py
@@ -118,7 +118,7 @@ class metagraph( torch.nn.Module ):
         self.lite = lite
         self.n = torch.nn.Parameter( torch.tensor( len(self.neurons), dtype=torch.int64 ), requires_grad=False )
         self.version = torch.nn.Parameter( torch.tensor( [bittensor.__version_as_int__], dtype=torch.int64 ), requires_grad=False )
-        self.block = torch.nn.Parameter( torch.tensor( subtensor.block, dtype=torch.int64 ), requires_grad=False )
+        self.block = torch.nn.Parameter( torch.tensor( block if block else subtensor.block, dtype=torch.int64 ), requires_grad=False )
         self.uids = torch.nn.Parameter( torch.tensor( [ neuron.uid for neuron in self.neurons ], dtype=torch.int64 ), requires_grad=False )
         self.trust = torch.nn.Parameter( torch.tensor( [ neuron.trust for neuron in self.neurons ], dtype=torch.float32 ), requires_grad=False )
         self.consensus = torch.nn.Parameter( torch.tensor( [ neuron.consensus for neuron in self.neurons ], dtype=torch.float32 ), requires_grad=False )
