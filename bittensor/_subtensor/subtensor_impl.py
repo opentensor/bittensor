343fab0bc3d011a953c599e9395a97f75cfc8c50
Cameron Fairchild <cameron@opentensor.ai>
2023-06-12 16:04:50 -0400

    allow block for is delegate

--- bittensor/_subtensor/subtensor_impl.py
+++ bittensor/_subtensor/subtensor_impl.py
@@ -934,8 +934,8 @@ class Subtensor:
     ####################
     #### Nomination ####
     ####################
-    def is_hotkey_delegate( self, hotkey_ss58: str ) -> bool:
-        return hotkey_ss58 in [ info.hotkey_ss58 for info in self.get_delegates() ]
+    def is_hotkey_delegate( self, hotkey_ss58: str, block: Optional[int] = None ) -> bool:
+        return hotkey_ss58 in [ info.hotkey_ss58 for info in self.get_delegates( block = block ) ]
 
     def get_delegate_take( self, hotkey_ss58: str, block: Optional[int] = None ) -> Optional[float]:
         return U16_NORMALIZED_FLOAT( self.query_subtensor( 'Delegates', block, [ hotkey_ss58 ] ).value )
