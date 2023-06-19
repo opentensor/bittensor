14ed226226f23ce2210220253590caa44630136d
Cameron Fairchild <cameron@opentensor.ai>
2023-06-19 19:16:50 -0400

    fix prometheus test

--- tests/integration_tests/test_prometheus.py
+++ tests/integration_tests/test_prometheus.py
@@ -22,29 +22,16 @@ def setUpModule():
 class TestPrometheus(unittest.TestCase):
 
     def setUp(self):
-        class success():
-            def __init__(self):
-                self.is_success = True
-            def process_events(self):
-                return True
-        class fail():
-            def __init__(self):
-                self.is_success = False
-                self.error_message = 'Mock failure'
-            def process_events(self):
-                return True
         self.subtensor = bittensor.subtensor(network = 'mock')
         self.wallet = bittensor.wallet.mock()
-        self.success = success()
-        self.fail = fail()
 
     def test_init_prometheus_success(self):
-        with patch.object(self.subtensor.substrate, 'submit_extrinsic', return_value = self.success):
+        with patch.object(self.subtensor, 'do_serve_prometheus', return_value = (True, None)):
             with patch("prometheus_client.start_http_server"): 
                 self.assertTrue( bittensor.prometheus(wallet = self.wallet, subtensor = self.subtensor, netuid=3) )
 
     def test_init_prometheus_failed(self):
-        with patch.object(self.subtensor.substrate, 'submit_extrinsic', return_value = self.fail):
+        with patch.object(self.subtensor, 'do_serve_prometheus', return_value = (False, 'Mock failure')):
             with patch("prometheus_client.start_http_server"): 
                 with pytest.raises(Exception):
                     bittensor.prometheus(wallet = self.wallet, subtensor = self.subtensor, netuid=3)
