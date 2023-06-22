c4daa9b309eb0687f23f87b01bafaa42976a4731
Cameron Fairchild <cameron@opentensor.ai>
2023-06-22 01:52:59 -0400

    add mocks

--- tests/mocks/wallet_mock.py
+++ tests/mocks/wallet_mock.py
@@ -1,3 +1,22 @@
+# The MIT License (MIT)
+
+# Copyright © 2021 Yuma Rao
+# Copyright © 2022 Opentensor Foundation
+# Copyright © 2023 Opentensor Technologies
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 
 import os
 import bittensor
@@ -26,7 +45,7 @@ class Wallet_mock(openwallet.Wallet):
         print("---- MOCKED WALLET INITIALIZED- ---")
 
     @property
-    def hotkey_file(self) -> 'bittensor.Keyfile':
+    def hotkey_file(self) -> 'openwallet.Keyfile':
         if self._is_mock:
             if self._mocked_hotkey_keyfile == None:
                 self._mocked_hotkey_keyfile = bittensor.keyfile(path='MockedHotkey', _mock = True)
@@ -37,7 +56,7 @@ class Wallet_mock(openwallet.Wallet):
             return bittensor.keyfile( path = hotkey_path )
 
     @property
-    def coldkey_file(self) -> 'bittensor.Keyfile':
+    def coldkey_file(self) -> 'openwallet.Keyfile':
         if self._is_mock:
             if self._mocked_coldkey_keyfile == None:
                 self._mocked_coldkey_keyfile = bittensor.keyfile(path='MockedColdkey', _mock = True)
@@ -48,7 +67,7 @@ class Wallet_mock(openwallet.Wallet):
             return bittensor.keyfile( path = coldkey_path )
 
     @property
-    def coldkeypub_file(self) -> 'bittensor.Keyfile':
+    def coldkeypub_file(self) -> 'openwallet.Keyfile':
         if self._is_mock:
             if self._mocked_coldkey_keyfile == None:
                 self._mocked_coldkey_keyfile = bittensor.keyfile(path='MockedColdkeyPub', _mock = True)
@@ -56,4 +75,4 @@ class Wallet_mock(openwallet.Wallet):
         else:
             wallet_path = os.path.expanduser(os.path.join(self.path, self.name))
             coldkeypub_path = os.path.join(wallet_path, "coldkeypub.txt")
-            return bittensor.Keyfile( path = coldkeypub_path )
+            return openwallet.Keyfile( path = coldkeypub_path )
