4e40f899cbac5ec8478d819b7356df69ca53a160
philanthrope <stgeorgejas@gmail.com>
2023-06-29 16:37:55 -0400

    fix error calling metagraph from subtensor without passing (#1426)
    * fix error calling metagraph from subtensor without passing
    
    * Update bittensor/_subtensor/subtensor_impl.py
    
    Co-authored-by: Cameron Fairchild <cameron@opentensor.dev>
    
    ---------
    
    Co-authored-by: Cameron Fairchild <cameron@opentensor.dev>

--- bittensor/_subtensor/subtensor_impl.py
+++ bittensor/_subtensor/subtensor_impl.py
@@ -1399,7 +1399,7 @@ class Subtensor:
 
         return NeuronInfoLite.list_from_vec_u8( result )
 
-    def metagraph( self, netuid: int, lite: bool = True, block: Optional[int] = None ) -> 'bittensor.Metagraph':
+    def metagraph( self, netuid: int, lite: bool = True, block: Optional[int] = None, sync: bool = False ) -> 'bittensor.Metagraph':
         r""" Returns the metagraph for the subnet.
         Args:
             netuid ( int ):
@@ -1413,7 +1413,8 @@ class Subtensor:
                 The metagraph for the subnet at the block.
         """        
         metagraph_ = bittensor.metagraph( network = self.network, netuid = netuid, lite = lite, sync = False )
-        metagraph_.sync( block = block, lite = lite, subtensor = self)
+        if sync:
+                metagraph_.sync( block = block, lite = lite, subtensor = self )
 
         return metagraph_
     
