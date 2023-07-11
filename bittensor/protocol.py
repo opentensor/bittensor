f94a24631c0fe9a0aa658ada058e50d614335cd7
unconst <jake@bittensor.com>
2023-07-11 11:35:22 -0500

    initial

--- bittensor/protocol.py
+++ bittensor/protocol.py
@@ -205,3 +205,28 @@ class Synapse( pydantic.BaseModel ):
         return synapse
 
 
+
+class Tensor( pydantic.BaseModel ):
+
+    class Config:
+        validate_assignment = True
+
+    # Defines the http route name which is set on axon.attach( callable( request: RequestName ))
+    data: Optional[ bytes ] = pydantic.Field(
+        title = 'data',
+        description = 'Tensor bytes data',
+        examples = 'Tensor',
+        allow_mutation = True,
+        default = None,
+        repr = True
+    )
+
+    # Defines the http route name which is set on axon.attach( callable( request: RequestName ))
+    serializer: Optional[ bytes ] = pydantic.Field(
+        title = 'data',
+        description = 'Tensor bytes data',
+        examples = 'Tensor',
+        allow_mutation = True,
+        default = None,
+        repr = True
+    )
