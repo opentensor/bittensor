d83a451d71fc7a4a12db279af53c2bea1b0f03b4
Cameron Fairchild <cameron@opentensor.dev>
2023-07-13 18:10:55 -0400

    rearrange import

--- bittensor/commands/__init__.py
+++ bittensor/commands/__init__.py
@@ -1,15 +1,3 @@
-from .stake import StakeCommand
-from .unstake import UnStakeCommand
-from .overview import OverviewCommand
-from .register import RegisterCommand, RecycleRegisterCommand
-from .delegates import NominateCommand, ListDelegatesCommand, DelegateStakeCommand, DelegateUnstakeCommand, MyDelegatesCommand
-from .wallets import NewColdkeyCommand, NewHotkeyCommand, RegenColdkeyCommand, RegenColdkeypubCommand, RegenHotkeyCommand
-from .transfer import TransferCommand
-from .inspect import InspectCommand
-from .metagraph import MetagraphCommand
-from .list import ListCommand
-from .misc import UpdateCommand, ListSubnetsCommand
-from .senate import SenateCommand, ProposalsCommand, ShowVotesCommand, SenateRegisterCommand, SenateLeaveCommand, VoteCommand
 
 from bittensor_config import Config
 
@@ -62,3 +50,16 @@ defaults: Config = Config.fromDict(
         },
     }
 )
+
+from .stake import StakeCommand
+from .unstake import UnStakeCommand
+from .overview import OverviewCommand
+from .register import RegisterCommand, RecycleRegisterCommand
+from .delegates import NominateCommand, ListDelegatesCommand, DelegateStakeCommand, DelegateUnstakeCommand, MyDelegatesCommand
+from .wallets import NewColdkeyCommand, NewHotkeyCommand, RegenColdkeyCommand, RegenColdkeypubCommand, RegenHotkeyCommand
+from .transfer import TransferCommand
+from .inspect import InspectCommand
+from .metagraph import MetagraphCommand
+from .list import ListCommand
+from .misc import UpdateCommand, ListSubnetsCommand
+from .senate import SenateCommand, ProposalsCommand, ShowVotesCommand, SenateRegisterCommand, SenateLeaveCommand, VoteCommand
