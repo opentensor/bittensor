fc94c067c82166deb84d928bc6798cafcc0a23be
Cameron Fairchild <cameron@opentensor.dev>
2023-07-13 16:34:30 -0400

    fix license header

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 # The MIT License (MIT)
 # Copyright © 2022 Yuma Rao
 # Copyright © 2022-2023 Opentensor Foundation
-# Copyright © 2023 Opentensor Technologies
+# Copyright © 2023 Opentensor Technologies Inc
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
