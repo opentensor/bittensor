8e171d0aec8802acec74fd8456a1ee1f4adeca20
Cameron Fairchild <cameron@opentensor.dev>
2023-07-13 16:14:20 -0400

    use generate wallet from btwallet

--- bittensor/utils/__init__.py
+++ bittensor/utils/__init__.py
@@ -210,36 +210,12 @@ def select_port():
     port = random.randrange(1000, 65536, 5)
     return port
 
-def generate_wallet(coldkey : 'Keypair' = None, hotkey: 'Keypair' = None):
-    wallet = bittensor.wallet(_mock=True)
-
-    if not coldkey:
-        coldkey = Keypair.create_from_mnemonic(Keypair.generate_mnemonic())
-    if not hotkey:
-        hotkey = Keypair.create_from_mnemonic(Keypair.generate_mnemonic())
-
-    wallet.set_coldkey(coldkey, encrypt=False, overwrite=True)
-    wallet.set_coldkeypub(coldkey, encrypt=False, overwrite=True)
-    wallet.set_hotkey(hotkey, encrypt=False, overwrite=True)
-
-    return wallet
-
 def setup_subtensor( port:int ):
     chain_endpoint = "localhost:{}".format(port)
     subtensor = bittensor.subtensor(
         chain_endpoint = chain_endpoint,
     )
     return subtensor, port
-
-def construct_config():
-    defaults = bittensor.Config()
-    bittensor.subtensor.add_defaults( defaults )
-    bittensor.dendrite.add_defaults( defaults )
-    bittensor.axon.add_defaults( defaults )
-    bittensor.wallet.add_defaults( defaults )
-    bittensor.dataset.add_defaults( defaults )
-
-    return defaults
 
 def test_unbiased_topk():
     input_tensor = torch.FloatTensor([1., 2., 3., 4., 5., 6., 7., 8., 9., 10.])
