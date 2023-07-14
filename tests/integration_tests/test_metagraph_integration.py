5a4d5f48903646ef9251a828d8c223205726ca5d
Cameron Fairchild <cameron@opentensor.dev>
2023-07-13 21:12:50 -0400

    fix metagraph test, pass sub

--- tests/integration_tests/test_metagraph_integration.py
+++ tests/integration_tests/test_metagraph_integration.py
@@ -40,22 +40,22 @@ def setUpModule():
 class TestMetagraph:
     def setup_method(self):
         self.sub = MockSubtensor( )
-        self.metagraph = bittensor.metagraph(netuid=3, network="mock")
+        self.metagraph = bittensor.metagraph(netuid=3, network="mock", sync=False)
 
     def test_print_empty(self):
         print(self.metagraph)
 
     def test_lite_sync(self):
-        self.metagraph.sync(lite=True)
+        self.metagraph.sync(lite=True, subtensor=self.sub)
 
     def test_full_sync(self):
-        self.metagraph.sync(lite=False)
+        self.metagraph.sync(lite=False, subtensor=self.sub)
 
     def test_sync_block_0(self):
-        self.metagraph.sync(lite=True, block=0)
+        self.metagraph.sync(lite=True, block=0, subtensor=self.sub)
 
     def test_load_sync_save(self):
-        self.metagraph.sync(lite=True)
+        self.metagraph.sync(lite=True, subtensor=self.sub)
         self.metagraph.save()
         self.metagraph.load()
         self.metagraph.save()
