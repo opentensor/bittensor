761d7ac8d08796ff93655366f1fd29b2b8fcfac9
Cameron Fairchild <cameron@opentensor.dev>
2023-07-13 20:35:30 -0400

    export commands

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -106,7 +106,7 @@ from .utils.balance import Balance as Balance
 from .chain_data import *
 from .errors import *
 from .subtensor import subtensor as subtensor
-from .cli import cli as cli
+from .cli import cli as cli, COMMANDS as ALL_COMMANDS
 from .logging import logging as logging
 from .metagraph import metagraph as metagraph
 from .threadpool import PriorityThreadPoolExecutor
