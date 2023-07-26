78d3bf19b44562b6f3ea6acd12e51a6f9a27e12c
Inquinim <114695354+Inquinim@users.noreply.github.com>
2023-07-26 11:01:58 +1000

    Fix blacklist vpermit_required (#1452)
    * - Adjusted blacklist argument default to False
    - Tweaked description
    
    * - Fix blacklist vpermit rejecting validators instead of non-validators
    
    * - Blacklist vpermit return true without permit, return false with permit
    
    * - Added text response to the return value for vpermit

--- bittensor/_blacklist/__init__.py
+++ bittensor/_blacklist/__init__.py
@@ -106,9 +106,12 @@ class blacklist:
                 return True, 'pubkey stake below min_allowed_stake'
 
         # Check for vpermit.
-        if metagraph is not None and self.config.blacklist.vpermit_required and is_registered:
+        if is_registered and self.config.blacklist.vpermit_required:
             uid = metagraph.hotkeys.index(src_hotkey)
-            return metagraph.neurons[uid].validator_permit
+            # Return False (pass) if there is a permit, and True (fail) if there isn't.
+            if metagraph.neurons[uid].validator_permit:
+                return False, "has vpermit"
+            return True, "no vpermit"
 
         # All checks passed.
         return False, 'passed blacklist'
