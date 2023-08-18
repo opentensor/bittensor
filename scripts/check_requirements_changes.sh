68d0a09d2f98152ac1bdce03875c8d2d8cd8bcc2
ifrit98 <ifrit98@gmail.com>
2023-08-18 15:09:16 +0000

    fix check_reqs ci

--- scripts/check_requirements_changes.sh
+++ scripts/check_requirements_changes.sh
@@ -3,8 +3,8 @@
 # Check if requirements files have changed in the last commit
 if git diff --name-only HEAD~1 | grep -E 'requirements/prod.txt|requirements/dev.txt'; then
     echo "Requirements files have changed. Running compatibility checks..."
-    exit 0
+    echo 'export REQUIREMENTS_CHANGED="true"' >> $BASH_ENV
 else
     echo "Requirements files have not changed. Skipping compatibility checks..."
-    exit 1
+    echo 'export REQUIREMENTS_CHANGED="false"' >> $BASH_ENV
 fi
