b03bb23fbcb9c57a3add036a406136d8a326e70d
ifrit98 <ifrit98@gmail.com>
2023-10-26 21:08:31 +0000

    rm debug prints

--- bittensor/commands/overview.py
+++ bittensor/commands/overview.py
@@ -119,14 +119,8 @@ class OverviewCommand:
             hotkey_coldkey_to_hotkey_wallet[hotkey_wallet.hotkey.ss58_address][
                 hotkey_wallet.coldkeypub.ss58_address
             ] = hotkey_wallet
-        from pprint import pformat
 
-        print("Hotkey wallets:", pformat(hotkey_coldkey_to_hotkey_wallet))
-        import pdb
-
-        pdb.set_trace()
         all_hotkey_addresses = list(hotkey_coldkey_to_hotkey_wallet.keys())
-
         with console.status(
             ":satellite: Syncing with chain: [white]{}[/white] ...".format(
                 cli.config.subtensor.get(
