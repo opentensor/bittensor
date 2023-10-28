3b297ae8c7f10e45777652523495db6659cd4c87
ifrit98 <ifrit98@gmail.com>
2023-10-28 04:33:49 +0000

    finish balances command

--- bittensor/commands/wallets.py
+++ bittensor/commands/wallets.py
@@ -20,6 +20,7 @@ import bittensor
 import os
 import sys
 from rich.prompt import Prompt, Confirm
+from rich.table import Table
 from typing import Optional, List
 from . import defaults
 
@@ -535,43 +536,91 @@ def _get_coldkey_ss58_addresses_for_path(path: str) -> List[str]:
     def list_coldkeypub_files(dir_path):
         abspath = os.path.abspath(os.path.expanduser(dir_path))
         return [
-        os.path.join(abspath, file, "coldkeypub.txt") \
-        for file in os.listdir(abspath)
-    ]
+            os.path.join(abspath, file, "coldkeypub.txt")
+            for file in os.listdir(abspath)
+        ]
 
     return [
         bittensor.keyfile(file).keypair.ss58_address
         for file in list_coldkeypub_files(path)
     ]
 
+
 class WalletBalanceCommand:
     @staticmethod
     def run(cli):
         """Check the balance of the wallet."""
+        wallet_names = os.listdir(os.path.expanduser(cli.config.wallet.path))
         coldkeys = _get_coldkey_ss58_addresses_for_path(cli.config.wallet.path)
         subtensor = bittensor.subtensor(config=cli.config)
+
         free_balances = [
             subtensor.get_balance(coldkeys[i]) for i in range(len(coldkeys))
         ]
         staked_balances = [
-            subtensor.get_total_stake_for_coldkey(coldkeys[i]) for i in range(len(coldkeys))
+            subtensor.get_total_stake_for_coldkey(coldkeys[i])
+            for i in range(len(coldkeys))
         ]
         total_free_balance = sum(free_balances)
         total_staked_balance = sum(staked_balances)
-        print("\n===== ", cli.config.wallet.path, " =====")
-        print("Total free balance: ", total_free_balance)
-        print("Total staked balance: ", total_staked_balance)
-        print("Total balance: ", total_free_balance + total_staked_balance)
-        print("Coldkeys: ", coldkeys)
 
         balances = {
-            coldkey: (
-                subtensor.get_balance(coldkey),
-                subtensor.get_total_stake_for_coldkey(coldkey)
+            name: (coldkey, free, staked)
+            for name, coldkey, free, staked in sorted(
+                zip(wallet_names, coldkeys, free_balances, staked_balances)
             )
-            for coldkey in coldkeys
         }
-        print(balances)
+
+        table = Table(show_footer=False)
+        table.title = "[white]Wallet Coldkey Balances"
+        table.add_column(
+            "[white]Wallet Name",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        table.add_column(
+            "[white]Coldkey Address",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        for typestr in ["Free", "Staked", "Total"]:
+            table.add_column(
+                f"[white]{typestr} Balance",
+                header_style="overline white",
+                footer_style="overline white",
+                justify="right",
+                style="green",
+                no_wrap=True,
+            )
+
+        for name, (coldkey, free, staked) in balances.items():
+            table.add_row(
+                name,
+                coldkey,
+                str(free),
+                str(staked),
+                str(free + staked),
+            )
+        table.add_row()
+        table.add_row(
+            "Total Balance Across All Coldkeys",
+            "",
+            str(total_free_balance),
+            str(total_staked_balance),
+            str(total_free_balance + total_staked_balance),
+        )
+        table.show_footer = True
+
+        table.box = None
+        table.pad_edge = False
+        table.width = None
+        bittensor.__console__.print(table)
 
     @staticmethod
     def add_args(parser: argparse.ArgumentParser):
@@ -589,10 +638,12 @@ class WalletBalanceCommand:
 
         if not config.is_set("subtensor.network") and not config.no_prompt:
             network = Prompt.ask(
-                "Enter network", default=defaults.subtensor.network,
-                choices=bittensor.__networks__
+                "Enter network",
+                default=defaults.subtensor.network,
+                choices=bittensor.__networks__,
             )
             config.subtensor.network = str(network)
-            _, config.subtensor.chain_endpoint = bittensor.subtensor.determine_chain_endpoint_and_network(
-                str(network)
-            )
+            (
+                _,
+                config.subtensor.chain_endpoint,
+            ) = bittensor.subtensor.determine_chain_endpoint_and_network(str(network))
