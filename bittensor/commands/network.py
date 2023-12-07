bf8447c313ce6e6d745b684e1e21cb8550fe2fc8
Eugene-hu <85906264+Eugene-hu@users.noreply.github.com>
2023-12-07 15:57:32 -0500

    Sudo/hyperparams pallet update (#1612)
    * Update hyperparams set extrinsic to use AdminUtils pallet
    
    * update metadata call
    
    ---------
    
    Co-authored-by: Ayden Brewer <dalegribble@riseup.net>
    Co-authored-by: philanthrope <ifrit98@gmail.com>

--- bittensor/commands/network.py
+++ bittensor/commands/network.py
@@ -249,7 +249,6 @@ HYPERPARAMS = {
     "immunity_period": "sudo_set_immunity_period",
     "min_allowed_weights": "sudo_set_min_allowed_weights",
     "activity_cutoff": "sudo_set_activity_cutoff",
-    "max_validators": "sudo_set_max_allowed_validators",
 }
 
 
