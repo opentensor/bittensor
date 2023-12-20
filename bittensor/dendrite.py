a8ff7b4d705c52e44ddfd1cc39fe2f2b70c7e6bb
ifrit98 <ifrit98@gmail.com>
2023-12-20 20:53:47 +0000

    convert .dict() -> json.loads(synapse.json()) for more serialization types

--- bittensor/dendrite.py
+++ bittensor/dendrite.py
@@ -524,7 +524,7 @@ class dendrite(torch.nn.Module):
             async with (await self.session).post(
                 url,
                 headers=synapse.to_headers(),
-                json=synapse.dict(),
+                json=json.loads(synapse.json()),
                 timeout=timeout,
             ) as response:
                 # Extract the JSON response from the server
@@ -606,7 +606,7 @@ class dendrite(torch.nn.Module):
             async with (await self.session).post(
                 url,
                 headers=synapse.to_headers(),
-                json=synapse.dict(),
+                json=json.loads(synapse.json()),
                 timeout=timeout,
             ) as response:
                 # Use synapse subclass' process_streaming_response method to yield the response chunks
