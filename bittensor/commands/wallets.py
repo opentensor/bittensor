b5efae5f5fd9b2f27f84ac1bf768dddfaa1c0047
Saqib Nawaz <nawaz.saqib.msu@gmail.com>
2024-01-02 09:39:14 -0500

    feat(wallets.py): add wallet history command

--- bittensor/commands/wallets.py
+++ bittensor/commands/wallets.py
@@ -23,7 +23,7 @@ from rich.prompt import Prompt, Confirm
 from rich.table import Table
 from typing import Optional, List
 from . import defaults
-
+import requests
 
 class RegenColdkeyCommand:
     """
@@ -827,3 +827,144 @@ class WalletBalanceCommand:
                 _,
                 config.subtensor.chain_endpoint,
             ) = bittensor.subtensor.determine_chain_endpoint_and_network(str(network))
+
+class GetWalletHistoryCommand:
+    """
+    Executes the 'history' command to fetch the latest transfers of the provided wallet on the Bittensor network.
+   
+    This command provides a detailed view of the transfers carried out on the wallet.
+
+    Usage:
+    The command lists the latest transfers of the provided wallet, showing the From, To, Amount, Extrinsic Id and Block Number.
+
+    Optional arguments:
+    None. The command uses the wallet and subtensor configurations to fetch latest transfer data associated with a wallet.
+
+    Example usage:
+    >>> btcli wallet history
+
+    Note:
+    This command is essential for users to monitor their financial status on the Bittensor network.
+    It helps in fetching info on all the transfers so that user can easily tally and cross check the transactions.
+    """
+
+    @staticmethod
+    def run(cli):
+        r"""Check the transfer history of the provided wallet."""
+        wallet = bittensor.wallet(config=cli.config)
+        wallet_address = wallet.coldkey.ss58_address
+
+        #Fetch all transfers
+        transfers = get_wallet_transfers(wallet_address)
+
+        # Create output table
+        table = Table(show_footer=False)
+        table.title = "[white]Wallet Transfers"
+        table.add_column(
+            "[white]Id",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        table.add_column(
+            "[white]From",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        table.add_column(
+            "[white]To",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        table.add_column(
+            "[white]Amount",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        table.add_column(
+            "[white]Extrinsic Id",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        table.add_column(
+            "[white]Block Number",
+            header_style="overline white",
+            footer_style="overline white",
+            style="rgb(50,163,219)",
+            no_wrap=True,
+        )
+
+        for item in transfers:
+            table.add_row(
+                item['id'],
+                item['from'],
+                item['to'],
+                item['amount'],
+                str(item['extrinsicId']),
+                item['blockNumber'],
+            )
+        table.add_row()
+        table.show_footer = True
+        table.box = None
+        table.pad_edge = False
+        table.width = None
+        bittensor.__console__.print(table)
+            
+    @staticmethod
+    def add_args(parser: argparse.ArgumentParser):
+        history_parser = parser.add_parser(
+            "history", help="""Fetch transfer history associated with the provided wallet"""
+        )
+        bittensor.wallet.add_args(history_parser)
+        bittensor.subtensor.add_args(history_parser)
+
+    @staticmethod
+    def check_config(config: "bittensor.config"):
+        if not config.is_set("wallet.name") and not config.no_prompt:
+            wallet_name = Prompt.ask("Enter wallet name", default=defaults.wallet.name)
+            config.wallet.name = str(wallet_name)
+
+def get_wallet_transfers(wallet_address) -> List[dict]:
+    """Get all transfers associated with the provided wallet address."""
+    
+    url = "https://api.subquery.network/sq/TaoStats/bittensor-indexer"
+    query = "query ($first: Int!, $after: Cursor, $filter: TransferFilter, $order: [TransfersOrderBy!]!) {\n\t\t\ttransfers(first: $first, after: $after, filter: $filter, orderBy: $order) {\n\t\t\t\tnodes {\n\t\t\t\t\tid\n\t\t\t\t\tfrom\n\t\t\t\t\tto\n\t\t\t\t\tamount\n\t\t\t\t\textrinsicId\n\t\t\t\t\tblockNumber\n\t\t\t\t}\n\t\t\t\tpageInfo {\n\t\t\t\t\tendCursor\n\t\t\t\t\thasNextPage\n\t\t\t\t\thasPreviousPage\n\t\t\t\t}\n\t\t\t\ttotalCount\n\t\t\t}\n\t\t}"
+    variables = {"first":10,"filter":{"or":[{"from":{"equalTo": wallet_address}},{"to":{"equalTo": wallet_address}}]},"order":"BLOCK_NUMBER_DESC"}
+        
+    transfers = []
+    
+    # Make the request with the provided query and variables until all transfers are fetched
+    while True:
+        response = requests.post(url, json={"query": query,"variables": variables})
+        data = response.json()
+
+        # Extract nodes and pageInfo from the response
+        transfer_data = data.get("data", {}).get("transfers", {})
+        nodes = transfer_data.get("nodes", [])
+        page_info = transfer_data.get("pageInfo", {})
+
+        # Add the nodes to the result list
+        transfers.extend(nodes)
+
+        # Check if there are more pages
+        if not page_info.get("hasNextPage"):
+            break
+
+        # Update variables for the next request with the endCursor from the current response
+        variables["after"] = page_info["endCursor"]
+
+    return transfers
