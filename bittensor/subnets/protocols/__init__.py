64130837a89d9cdd1bd3213d5b5ae933c663a4f8
crazydevlegend <crazy010323@gmail.com>
2024-01-12 15:34:15 -0500

    Removed imports for empty protocols (temp)

--- bittensor/subnets/protocols/__init__.py
+++ bittensor/subnets/protocols/__init__.py
@@ -3,13 +3,16 @@ from . import sn2
 from . import sn3
 from . import sn4
 from . import sn5
-from . import sn6
+
+# from . import sn6
 from . import sn7
 from . import sn8
-from . import sn9
+
+# from . import sn9
 from . import sn10
 from . import sn11
-from . import sn12
+
+# from . import sn12
 from . import sn13
 from . import sn14
 from . import sn15
@@ -21,12 +24,16 @@ from . import sn20
 from . import sn21
 from . import sn22
 from . import sn23
-from . import sn24
+
+# from . import sn24
 from . import sn25
 from . import sn26
 from . import sn27
 from . import sn28
 from . import sn29
-from . import sn30
-from . import sn31
+
+# from . import sn30
+
+# from . import sn31
+
 from . import sn32
