173b68a0aeb493fb5d724e742df1e66d18fe5e03
Gus <gus@opentensor.dev>
2024-02-07 19:26:53 -0500

    naming

--- bittensor/extrinsics/registration.py
+++ bittensor/extrinsics/registration.py
@@ -325,10 +325,6 @@ class MaxAttemptsException(Exception):
     pass
 
 
-class MaxAttemptedException(Exception):
-    pass
-
-
 def run_faucet_extrinsic(
     subtensor: "bittensor.subtensor",
     wallet: "bittensor.wallet",
@@ -470,7 +466,7 @@ def run_faucet_extrinsic(
         except MaxSuccessException:
             return True, f"Max successes reached: {3}"
 
-        except MaxAttemptedException:
+        except MaxAttemptsException:
             return False, f"Max attempts reached: {max_allowed_attempts}"
 
 
