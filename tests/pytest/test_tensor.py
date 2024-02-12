d23f0d18bff1bafddc84a6a095eb128699b51e30
wolfentensor <gregory.wolf@gmail.com>
2024-02-12 08:56:56 -0700

    unit tests

--- tests/pytest/test_tensor.py
+++ tests/pytest/test_tensor.py
@@ -11,3 +11,16 @@ def test_cast_dtype_torch_dtype():
 
 def test_cast_dtype_str_valid():
     assert cast_dtype("float32") == "float32"
+
+
+def test_cast_dtype_str_invalid():
+    with pytest.raises(ValueError):
+        cast_dtype("invalid_dtype")
+
+def test_cast_dtype_type_error():
+    with pytest.raises(TypeError):
+        cast_dtype(123)
+
+# Unit tests for cast_shape
+def test_cast_shape_none():
+    assert cast_shape(None) == "None"
