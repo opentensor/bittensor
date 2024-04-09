af138478b9d25ec966c1bed1ec9ae1d48705995a
andreea-popescu-reef <160024917+andreea-popescu-reef@users.noreply.github.com>
2024-04-09 21:29:32 +0800

    replace torch with numpy

--- tests/unit_tests/test_synapse.py
+++ tests/unit_tests/test_synapse.py
@@ -15,7 +15,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 import json
-import torch
 import base64
 import typing
 import pytest
