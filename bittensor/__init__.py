f3c624564752ffa879168aed720bd4507a422a95
Roman Chkhaidze <basfroman@gmail.com>
2024-05-09 16:34:16 -0700

    Retrieving error types from the metadata of the Substrate palette "SubtensorModule" for the btcli console (logic)

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -286,7 +286,7 @@ from .chain_data import (
     ProposalCallData,
     ProposalVoteData,
 )
-from .subtensor import subtensor as subtensor
+from .subtensor_module import subtensor as subtensor
 from .cli import cli as cli, COMMANDS as ALL_COMMANDS
 from .btlogging import logging
 from .metagraph import metagraph as metagraph
