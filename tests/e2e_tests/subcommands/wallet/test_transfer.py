ebffbbbf3818b4f546e74e88fcab0bd5219fcc27
Liam <liam@opentensor.dev>
2024-05-16 19:23:52 +0400

    chore: refactor

--- tests/e2e_tests/subcommands/wallet/test_transfer.py
+++ tests/e2e_tests/subcommands/wallet/test_transfer.py
@@ -1,4 +1,3 @@
-
 from bittensor.commands.transfer import TransferCommand
 from ...utils import setup_wallet
 import bittensor
@@ -6,7 +5,7 @@ import bittensor
 
 # Example test using the local_chain fixture
 def test_example(local_chain):
-    keypair = setup_wallet("//Alice")
+    (keypair, wallet_path) = setup_wallet("//Alice")
 
     parser = bittensor.cli.__create_parser__()
     config = bittensor.config(
@@ -24,17 +23,21 @@ def test_example(local_chain):
             "--subtensor.chain_endpoint",
             "ws://localhost:9945",
             "--wallet.path",
-            "/tmp/btcli-wallet",
+            wallet_path,
         ],
     )
     cli_instance = bittensor.cli(config)
 
-    acc_before = local_chain.query('System', 'Account', [keypair.ss58_address])
+    acc_before = local_chain.query("System", "Account", [keypair.ss58_address])
     TransferCommand.run(cli_instance)
-    acc_after = local_chain.query('System', 'Account', [keypair.ss58_address])
+    acc_after = local_chain.query("System", "Account", [keypair.ss58_address])
 
     expected_transfer = 2_000_000_000
     tolerance = 200_000  # Tx fee tolerance
 
-    actual_difference = acc_before.value['data']['free'] - acc_after.value['data']['free']
-    assert expected_transfer <= actual_difference <= expected_transfer + tolerance, f"Expected transfer with tolerance: {expected_transfer} <= {actual_difference} <= {expected_transfer + tolerance}"
+    actual_difference = (
+        acc_before.value["data"]["free"] - acc_after.value["data"]["free"]
+    )
+    assert (
+        expected_transfer <= actual_difference <= expected_transfer + tolerance
+    ), f"Expected transfer with tolerance: {expected_transfer} <= {actual_difference} <= {expected_transfer + tolerance}"
