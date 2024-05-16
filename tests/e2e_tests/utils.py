be0a75839c659ce0b2c153e04e14d9e403bb51d4
Liam <liam@opentensor.dev>
2024-05-16 19:23:52 +0400

    chore: refactor

--- tests/e2e_tests/utils.py
+++ tests/e2e_tests/utils.py
@@ -4,8 +4,9 @@ import bittensor
 
 def setup_wallet(uri: str):
     keypair = Keypair.create_from_uri(uri)
-    wallet = bittensor.wallet(path="/tmp/btcli-e2e-wallet-{}".format(uri))
+    wallet_path = "/tmp/btcli-e2e-wallet-{}".format(uri)
+    wallet = bittensor.wallet(wallet_path)
     wallet.set_coldkey(keypair=keypair, encrypt=False, overwrite=True)
     wallet.set_coldkeypub(keypair=keypair, encrypt=False, overwrite=True)
     wallet.set_hotkey(keypair=keypair, encrypt=False, overwrite=True)
-    return keypair
+    return (keypair, wallet_path)
