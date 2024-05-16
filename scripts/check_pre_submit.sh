62e5c8ec1a1f0e37d62a2bec6ef9446412083eb7
Roman <roman@opentensor.dev>
2024-05-16 11:39:40 -0700

    Add Pre-commit Checker in scripts. Helps reduce CI calls.

--- scripts/check_pre_submit.sh
+++ scripts/check_pre_submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # black checks formating
-echo ">>> Run the pre-submit format check with \`black .\`"
-black .
+echo ">>> Run the pre-submit format check with \`black .\`."
+python3 -m black --exclude '(env|venv|.eggs|.git)' .
 
-echo ">>> Run the pre-submit format check with \`mypy\`"
+echo ">>> Run the pre-submit format check with \`mypy\`."
 
 # mypy checks python versions compatibility
 versions=("3.9" "3.10" "3.11")
@@ -15,4 +15,4 @@ done
 
 # flake8 checks errors count in bittensor folder
 error_count=$(flake8 bittensor/ --count)
-echo ">>> Flake8 found ${error_count} errors"
+echo ">>> Flake8 found ${error_count} errors."
