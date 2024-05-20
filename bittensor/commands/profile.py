f9f3bf17e15e7dde408a1701a47574d770e67750
Tao Ceti <davionkalhen@gmail.com>
2024-05-20 09:07:16 -0700

    updated profile command

--- bittensor/commands/profile.py
+++ bittensor/commands/profile.py
@@ -11,16 +11,12 @@ import bittensor
 # Local
 from . import defaults
 
-
-PROFILE_ATTRIBUTES = [
-    "name",
-    "path",
-    "coldkey",
-    "hotkey",
-    "subtensor_network",
-    "active_netuid",
-]
-
+SAVED_ATTRIBUTES = {
+    "profile": ["name", "path"],
+    "subtensor": ["network", "chain_endpoint"],
+    "wallet": ["name", "hotkey", "path"],
+    "netuid": True,
+}
 
 class ProfileCommand:
     """
@@ -33,44 +29,101 @@ class ProfileCommand:
 
     @staticmethod
     def run(cli):
-        ProfileCommand._run(cli)
+        try:
+            subtensor: "bittensor.subtensor" = bittensor.subtensor(
+                config=cli.config, log_verbose=False
+            )
+            ProfileCommand._run(cli, subtensor)
+        finally:
+            if "subtensor" in locals():
+                subtensor.close()
+                bittensor.logging.debug("closing subtensor connection")
+        
 
     @staticmethod
-    def _run(cli: "bittensor.cli"):
+    def _run(cli: "bittensor.cli", subtensor: "bittensor.subtensor"):
         config = cli.config.copy()
-        for attribute in PROFILE_ATTRIBUTES:
-            if defaults.profile.get(attribute):
+        #Set the chain_endpoint to match the network unless user defined.
+        if config.subtensor.network is not defaults.subtensor.network and config.subtensor.chain_endpoint is bittensor.__finney_entrypoint__:
+            (_, config.subtensor.chain_endpoint) = subtensor.determine_chain_endpoint_and_network(config.subtensor.network)
+
+        for (key, attributes) in SAVED_ATTRIBUTES.items():
+            #if attributes isn't a list then just raw values
+            if not isinstance(attributes, list):
+                if config.no_prompt:
+                    continue
                 setattr(
                     config,
-                    attribute,
+                    key,
                     Prompt.ask(
-                        f"Enter {attribute}", default=defaults.profile[attribute]
+                        f"Enter {key}",
+                        default=getattr(config, key, None)
                     ),
                 )
-            else:
+                continue
+            sub_attr = getattr(config, key, None)
+            for attribute in attributes:
+                if config.no_prompt:
+                    continue
+                attr_key = f"{key}.{attribute}"
                 setattr(
-                    config,
+                    config[key],
                     attribute,
-                    Prompt.ask(f"Enter {attribute}"),
+                    Prompt.ask(
+                        f"Enter {attr_key}",
+                        choices=None if attribute != "network" else bittensor.__networks__,
+                        default=getattr(sub_attr, attribute, None),
+                    ),
                 )
-
+                if attribute == "network" and config.subtensor.chain_endpoint is bittensor.__finney_entrypoint__:
+                    (_, config.subtensor.chain_endpoint) = subtensor.determine_chain_endpoint_and_network(config.subtensor.network)
         ProfileCommand._write_profile(config)
 
     @staticmethod
     def _write_profile(config: "bittensor.config"):
-        path = os.path.expanduser(config.path)
-
-        os.makedirs(path, exist_ok=True)
-
-        profile = (
-            f"name = {config.name}\n"
-            f"coldkey = {config.coldkey}\n"
-            f"hotkey = {config.hotkey}\n"
-            f"subtensor_network = {config.subtensor_network}\n"
-            f"active_netuid = {config.active_netuid}\n"
+        path = os.path.expanduser(config.profile.path)
+        try:
+            os.makedirs(path, exist_ok=True)
+        except Exception as e:
+            bittensor.__console__.print(
+                f":cross_mark: [red]Failed to write profile[/red]:[bold white] {e}"
+            )
+            return
+        
+        if os.path.exists(f"{path}{config.profile.name}") and not config.no_prompt:
+            overwrite = None
+            while overwrite not in ["y", "n"]:
+                overwrite = Prompt.ask(f"Profile {config.profile.name} already exists. Overwrite?")
+                if overwrite:
+                    overwrite = overwrite.lower()
+            if overwrite == "n":
+                bittensor.__console__.print(
+                    ":cross_mark: [red]Failed to write profile[/red]:[bold white] User denied."
+                    )
+                return
+            
+        profile = bittensor.config()
+        #Create a profile clone with only the saved attributes
+        for key in SAVED_ATTRIBUTES.keys():
+            if isinstance(SAVED_ATTRIBUTES[key], list):
+                getattr(profile, key, None)
+                profile[key] = bittensor.config()
+                for attribute in SAVED_ATTRIBUTES[key]:
+                    setattr(profile[key], attribute, getattr(config[key], attribute))
+            else:
+                setattr(profile, key, getattr(config, key))
+        try:
+            with open(f"{path}{config.profile.name}", "w+") as f:
+                f.write(str(profile))
+        except Exception as e:
+            bittensor.__console__.print(
+                f":cross_mark: [red]Failed to write profile[/red]:[bold white] {e}"
+            )
+            return
+        
+        bittensor.__console__.print(
+            f":white_check_mark: [bold green]Profile {config.profile.name} written to {path}[/bold green]"
         )
-        with open(f"{path}{config.name}", "w+") as f:
-            f.write(profile)
 
     @staticmethod
     def check_config(config: "bittensor.config"):
@@ -80,3 +133,18 @@ class ProfileCommand:
     def add_args(parser: argparse.ArgumentParser):
         list_parser = parser.add_parser("create", help="""Create profile""")
         list_parser.set_defaults(func=ProfileCommand.run)
+        list_parser.add_argument(
+            "--profile.name",
+            type=str,
+            default=defaults.profile.name,
+            help="The name of the profile",
+        )
+        list_parser.add_argument(
+            "--profile.path",
+            type=str,
+            default=defaults.profile.path,
+            help="The path to the profile directory",
+        )
+        bittensor.subtensor.add_args(list_parser)
+        bittensor.wallet.add_args(list_parser)
+
