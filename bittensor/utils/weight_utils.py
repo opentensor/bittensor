dc5317246f0cad60fc4587a06cdad9a5d1f46e1c
bhimes <benhimes@opentensor.dev>
2024-05-21 21:56:31 +0200

    Flake8 and README update.

--- bittensor/utils/weight_utils.py
+++ bittensor/utils/weight_utils.py
@@ -18,7 +18,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-import os
 import numpy as np
 import bittensor
 from numpy.typing import NDArray
