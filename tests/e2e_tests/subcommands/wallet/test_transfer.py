0115dfe1b32d5d98465a2f1aff882590357a9e70
open-junius <zhou@opentensor.dev>
2024-05-27 19:22:07 +0800

    revert transfer

--- tests/e2e_tests/subcommands/wallet/test_transfer.py
+++ tests/e2e_tests/subcommands/wallet/test_transfer.py
@@ -1,11 +1,30 @@
-from ...utils import get_wallet
+from ...utils import setup_wallet
+from bittensor.commands.transfer import TransferCommand
 from bittensor import wallet
 from bittensor.subtensor import subtensor
 from substrateinterface import Keypair
 
 # Example test using the local_chain fixture
 def test_transfer(local_chain: subtensor):
-    wallet = get_wallet("//Alice", "//Bob")
-    amount = 1
-    assert local_chain.transfer(wallet, wallet.hotkey.ss58_address, amount=amount, wait_for_finalization=True, wait_for_inclusion=True)
-   
+    (keypair, exec_command) = setup_wallet("//Alice")
+    acc_before = local_chain.get_balance(keypair.ss58_address)
+    exec_command(
+        TransferCommand,
+        [
+            "wallet",
+            "transfer",
+            "--amount",
+            "2",
+            "--dest",
+            "5GpzQgpiAKHMWNSH3RN4GLf96GVTDct9QxYEFAY7LWcVzTbx",
+        ],
+    )
+    acc_after = local_chain.get_balance(keypair.ss58_address)
+
+    expected_transfer = 2_000_000_000
+    tolerance = 200_000  # Tx fee tolerance
+
+    actual_difference = acc_before - acc_after
+    assert (
+        expected_transfer <= actual_difference <= expected_transfer + tolerance
+    ), f"Expected transfer with tolerance: {expected_transfer} <= {actual_difference} <= {expected_transfer + tolerance}"
