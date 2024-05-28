b030a64e4b5ac035bcf2a451011e1833d5c02a33
open-junius <zhou@opentensor.dev>
2024-05-28 12:34:12 +0800

    try add remove stake case

--- tests/e2e_tests/subcommands/stake/test_stake_add.py
+++ tests/e2e_tests/subcommands/stake/test_stake_add.py
@@ -5,7 +5,7 @@ from ...utils import new_wallet, sudo_call_set_network_limit
 
 
 # Example test using the local_chain fixture
-def test_stake_add(local_chain, capsys):
+def test_stake_add(local_chain):
     (wallet, exec_command) = new_wallet("//Alice", "//Bob")
     assert sudo_call_set_network_limit(local_chain, wallet)
 
@@ -39,7 +39,7 @@ def test_stake_add(local_chain, capsys):
         == 0
     )
 
-    stake_amount = 1
+    stake_amount = 2
     exec_command(StakeCommand, ["stake", "add", "--amount", str(stake_amount)])
     exact_stake = local_chain.query(
         "SubtensorModule", "TotalHotkeyStake", [wallet.hotkey.ss58_address]
@@ -51,3 +51,13 @@ def test_stake_add(local_chain, capsys):
         exact_stake > stake_amount_in_rao - withdraw_loss
         and exact_stake <= stake_amount_in_rao
     )
+
+    # we can test remove after set the stake rate limit larger than 1
+    # remove_amount = 1
+    # exec_command(StakeCommand, ["stake", "remove", "--amount", str(remove_amount)])
+    # assert (
+    #     local_chain.query(
+    #         "SubtensorModule", "TotalHotkeyStake", [wallet.hotkey.ss58_address]
+    #     ).serialize()
+    #     == exact_stake - remove_amount * 1_000_000_000
+    # )
