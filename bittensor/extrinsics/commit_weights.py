d2aec6d85810b8a5def81966802c6f5f3e3267c3
opendansor <daniel@opentensor.dev>
2024-05-28 14:29:09 -0700

    Float -> int for np

--- bittensor/extrinsics/commit_weights.py
+++ bittensor/extrinsics/commit_weights.py
@@ -16,12 +16,13 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+""" Module commit weights and reveal weights extrinsic. """
+
 from typing import Tuple, List
+
 from rich.prompt import Confirm
 
 import bittensor
-
-""" Module commit weights and reveal weights extrinsic. """
 
 
 def commit_weights_extrinsic(
