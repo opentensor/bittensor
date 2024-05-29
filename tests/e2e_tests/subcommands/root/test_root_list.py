e653dabdcdaca214c321cf207db0f1bc7ff63a23
open-junius <zhou@opentensor.dev>
2024-05-29 22:29:33 +0800

    more root test

--- tests/e2e_tests/subcommands/root/test_root_list.py
+++ tests/e2e_tests/subcommands/root/test_root_list.py
@@ -1,15 +1,31 @@
 from bittensor.commands.root import RootList
-from ...utils import setup_wallet
+from ...utils import new_wallet, sudo_call_set_network_limit
+from bittensor.commands.network import RegisterSubnetworkCommand
+import bittensor
 
 
 # Example test using the local_chain fixture
-def test_stake_show(local_chain, capsys):
-    (keypair, exec_command) = setup_wallet("//Alice")
+def test_root_list(local_chain, capsys):
+    (wallet, exec_command) = new_wallet("//Alice", "//Bob")
+
+    # exec_command(RootList, ["root", "list"])
+    # captured = capsys.readouterr()
+    # lines = captured.out.split("\n")
+
+    # assert len(lines) == 4
+    # assert "Root Network" in lines[0]
+    # assert "UID  NAME  ADDRESS  STAKE" in lines[1]
+
+    # exec_command(RegisterSubnetworkCommand, ["s", "create"])
 
     exec_command(RootList, ["root", "list"])
     captured = capsys.readouterr()
-    lines = captured.out.split("\n")
+    lines = captured.out.splitlines()
+
+    for line in lines:
+        bittensor.logging.info(line)
 
     assert len(lines) == 4
     assert "Root Network" in lines[0]
     assert "UID  NAME  ADDRESS  STAKE" in lines[1]
+    assert "1" in lines[2]
