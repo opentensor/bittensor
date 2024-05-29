3a3a26b733b3ba5e754b61d8f886804e4269900e
Liam <liam@opentensor.dev>
2024-05-29 13:44:02 +0400

    feat: exec_command returns output lines

--- tests/e2e_tests/utils.py
+++ tests/e2e_tests/utils.py
@@ -1,6 +1,8 @@
 from substrateinterface import Keypair
 from typing import List
 import bittensor
+import io
+import contextlib
 
 
 def setup_wallet(uri: str):
@@ -27,6 +29,20 @@ def setup_wallet(uri: str):
             args=args,
         )
         cli_instance = bittensor.cli(config)
-        command.run(cli_instance)
+
+        stdout_io = io.StringIO()
+        stderr_io = io.StringIO()
+
+        with contextlib.redirect_stdout(stdout_io), contextlib.redirect_stderr(
+            stderr_io
+        ):
+            command.run(cli_instance)
+
+        stdout_lines = stdout_io.getvalue().split("\n")
+        stderr_lines = stderr_io.getvalue().split("\n")
+        print("STDOUT: " + stdout_io.getvalue())
+        print("STDERR: " + stderr_io.getvalue())
+
+        return stdout_lines, stderr_lines
 
     return (keypair, exec_command)
