e9359ed7afee747e80e111b6c821c78bd8b82512
Benjamin Himes <benhimes@opentensor.dev>
2024-05-30 15:29:56 +0200

    Changed env params

--- .circleci/check_pr_status.sh
+++ .circleci/check_pr_status.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-PR_NUMBER=$CIRCLE_PULL_REQUEST
-REPO_OWNER=$(echo $CIRCLE_PROJECT_USERNAME)
-REPO_NAME=$(echo $CIRCLE_PROJECT_REPONAME)
-
-PR_NUMBER=${PR_NUMBER##*/}
-
-PR_DETAILS=$(curl -s \
-  "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls/$PR_NUMBER")
+PR_DETAILS=$(curl -s $CIRCLE_PULL_REQUEST)
 
 IS_DRAFT=$(echo "$PR_DETAILS" | jq -r .draft)
 
