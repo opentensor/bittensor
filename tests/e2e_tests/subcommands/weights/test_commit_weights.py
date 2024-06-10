cf8ebacfcdd5cb4da5ccb20ae4df4cb39f636bfe
open-junius <zhou@opentensor.dev>
2024-06-10 21:44:07 +0800

    fix test error

--- tests/e2e_tests/subcommands/weights/test_commit_weights.py
+++ tests/e2e_tests/subcommands/weights/test_commit_weights.py
@@ -17,7 +17,7 @@ from tests.e2e_tests.utils import setup_wallet
 
 def test_commit_and_reveal_weights(local_chain):
     # Register root as Alice
-    keypair, exec_command, wallet_path = setup_wallet("//Alice")
+    keypair, exec_command, wallet_path = setup_wallet("//Alice", True)
     exec_command(RegisterSubnetworkCommand, ["s", "create"])
 
     # define values
