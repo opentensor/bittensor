86b9ee73f0a579e5f63af39c144996ad250fe517
Roman <roman@opentensor.dev>
2024-06-11 11:18:59 -0700

    Refactoring for `extrinsics/serving.py`

--- bittensor/extrinsics/serving.py
+++ bittensor/extrinsics/serving.py
@@ -1,24 +1,34 @@
 # The MIT License (MIT)
 # Copyright © 2021 Yuma Rao
 # Copyright © 2023 Opentensor Foundation
-
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 # and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
+#
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of
 # the Software.
-
+#
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 # THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
+
+"""
+This module provides functions for interacting with the Bittensor network, including serving axon endpoints,
+publishing metadata, and performing extrinsics related to network services.
+"""
+
 import json
+from typing import Optional
+
+from retry import retry
+from rich.prompt import Confirm
+
 import bittensor
 import bittensor.utils.networking as net
-from rich.prompt import Confirm
 from ..errors import MetadataError
 
 
@@ -35,36 +45,27 @@ def serve_extrinsic(
     wait_for_finalization=True,
     prompt: bool = False,
 ) -> bool:
-    r"""Subscribes a Bittensor endpoint to the subtensor chain.
+    """Subscribes a Bittensor endpoint to the subtensor chain.
 
     Args:
-        wallet (bittensor.wallet):
-            Bittensor wallet object.
-        ip (str):
-            Endpoint host port i.e., ``192.122.31.4``.
-        port (int):
-            Endpoint port number i.e., ``9221``.
-        protocol (int):
-            An ``int`` representation of the protocol.
-        netuid (int):
-            The network uid to serve on.
-        placeholder1 (int):
-            A placeholder for future use.
-        placeholder2 (int):
-            A placeholder for future use.
-        wait_for_inclusion (bool):
-            If set, waits for the extrinsic to enter a block before returning ``true``, or returns ``false`` if the extrinsic fails to enter the block within the timeout.
-        wait_for_finalization (bool):
-            If set, waits for the extrinsic to be finalized on the chain before returning ``true``, or returns ``false`` if the extrinsic fails to be finalized within the timeout.
-        prompt (bool):
-            If ``true``, the call waits for confirmation from the user before proceeding.
+        subtensor (bittensor.subtensor): Bittensor subtensor object.
+        wallet (bittensor.wallet): Bittensor wallet object.
+        ip (str): Endpoint host port i.e., ``192.122.31.4``.
+        port (int): Endpoint port number i.e., ``9221``.
+        protocol (int): An ``int`` representation of the protocol.
+        netuid (int): The network uid to serve on.
+        placeholder1 (int): A placeholder for future use.
+        placeholder2 (int): A placeholder for future use.
+        wait_for_inclusion (bool): If set, waits for the extrinsic to enter a block before returning ``true``, or returns ``false`` if the extrinsic fails to enter the block within the timeout.
+        wait_for_finalization (bool): If set, waits for the extrinsic to be finalized on the chain before returning ``true``, or returns ``false`` if the extrinsic fails to be finalized within the timeout.
+        prompt (bool): If ``true``, the call waits for confirmation from the user before proceeding.
+
     Returns:
-        success (bool):
-            Flag is ``true`` if extrinsic was finalized or included in the block. If we did not wait for finalization / inclusion, the response is ``true``.
+        success (bool): Flag is ``true`` if extrinsic was finalized or included in the block. If we did not wait for finalization / inclusion, the response is ``true``.
     """
     # Decrypt hotkey
     wallet.hotkey
-    params: "bittensor.AxonServeCallParams" = {
+    params: "subtensor.AxonServeCallParams" = {
         "version": bittensor.__version_as_int__,
         "ip": net.ip_to_int(ip),
         "port": port,
@@ -115,7 +116,7 @@ def serve_extrinsic(
     bittensor.logging.debug(
         f"Serving axon with: AxonInfo({wallet.hotkey.ss58_address},{ip}:{port}) -> {subtensor.network}:{netuid}"
     )
-    success, error_message = subtensor._do_serve_axon(
+    success, error_message = subtensor.do_serve_axon(
         wallet=wallet,
         call_params=params,
         wait_for_finalization=wait_for_finalization,
@@ -123,7 +124,7 @@ def serve_extrinsic(
     )
 
     if wait_for_inclusion or wait_for_finalization:
-        if success == True:
+        if success is True:
             bittensor.logging.debug(
                 f"Axon served with: AxonInfo({wallet.hotkey.ss58_address},{ip}:{port}) on {subtensor.network}:{netuid} "
             )
@@ -145,45 +146,31 @@ def serve_axon_extrinsic(
     wait_for_finalization: bool = True,
     prompt: bool = False,
 ) -> bool:
-    r"""Serves the axon to the network.
+    """Serves the axon to the network.
 
     Args:
-        netuid ( int ):
-            The ``netuid`` being served on.
-        axon (bittensor.Axon):
-            Axon to serve.
-        wait_for_inclusion (bool):
-            If set, waits for the extrinsic to enter a block before returning ``true``, or returns ``false`` if the extrinsic fails to enter the block within the timeout.
-        wait_for_finalization (bool):
-            If set, waits for the extrinsic to be finalized on the chain before returning ``true``, or returns ``false`` if the extrinsic fails to be finalized within the timeout.
-        prompt (bool):
-            If ``true``, the call waits for confirmation from the user before proceeding.
+        subtensor (bittensor.subtensor): Bittensor subtensor object.
+        netuid ( int ): The ``netuid`` being served on.
+        axon (bittensor.Axon): Axon to serve.
+        wait_for_inclusion (bool): If set, waits for the extrinsic to enter a block before returning ``true``, or returns ``false`` if the extrinsic fails to enter the block within the timeout.
+        wait_for_finalization (bool): If set, waits for the extrinsic to be finalized on the chain before returning ``true``, or returns ``false`` if the extrinsic fails to be finalized within the timeout.
+        prompt (bool): If ``true``, the call waits for confirmation from the user before proceeding.
+
     Returns:
-        success (bool):
-            Flag is ``true`` if extrinsic was finalized or included in the block. If we did not wait for finalization / inclusion, the response is ``true``.
+        success (bool): Flag is ``true`` if extrinsic was finalized or included in the block. If we did not wait for finalization / inclusion, the response is ``true``.
     """
     axon.wallet.hotkey
     axon.wallet.coldkeypub
     external_port = axon.external_port
 
     # ---- Get external ip ----
-    if axon.external_ip == None:
+    if axon.external_ip is None:
         try:
             external_ip = net.get_external_ip()
-            bittensor.__console__.print(
-                ":white_heavy_check_mark: [green]Found external ip: {}[/green]".format(
-                    external_ip
-                )
-            )
-            bittensor.logging.success(
-                prefix="External IP", suffix="<blue>{}</blue>".format(external_ip)
-            )
-        except Exception as E:
-            raise RuntimeError(
-                "Unable to attain your external ip. Check your internet connection. error: {}".format(
-                    E
-                )
-            ) from E
+            bittensor.__console__.print(f":white_heavy_check_mark: [green]Found external ip: {external_ip}[/green]")
+            bittensor.logging.success(prefix="External IP", suffix=f"<blue>{external_ip}</blue>")
+        except Exception as e:
+            raise RuntimeError(f"Unable to attain your external ip. Check your internet connection. error: {e}") from e
     else:
         external_ip = axon.external_ip
 
@@ -204,7 +191,7 @@ def publish_metadata(
     subtensor: "bittensor.subtensor",
     wallet: "bittensor.wallet",
     netuid: int,
-    type: str,
+    type_: str,
     data: bytes,
     wait_for_inclusion: bool = False,
     wait_for_finalization: bool = True,
@@ -213,69 +200,55 @@ def publish_metadata(
     Publishes metadata on the Bittensor network using the specified wallet and network identifier.
 
     Args:
-        subtensor (bittensor.subtensor):
-            The subtensor instance representing the Bittensor blockchain connection.
-        wallet (bittensor.wallet):
-            The wallet object used for authentication in the transaction.
-        netuid (int):
-            Network UID on which the metadata is to be published.
-        type (str):
-            The data type of the information being submitted. It should be one of the following: ``'Sha256'``, ``'Blake256'``, ``'Keccak256'``, or ``'Raw0-128'``. This specifies the format or hashing algorithm used for the data.
-        data (str):
-            The actual metadata content to be published. This should be formatted or hashed according to the ``type`` specified. (Note: max ``str`` length is 128 bytes)
-        wait_for_inclusion (bool, optional):
-            If ``True``, the function will wait for the extrinsic to be included in a block before returning. Defaults to ``False``.
-        wait_for_finalization (bool, optional):
-            If ``True``, the function will wait for the extrinsic to be finalized on the chain before returning. Defaults to ``True``.
+        subtensor (bittensor.subtensor): The subtensor instance representing the Bittensor blockchain connection.
+        wallet (bittensor.wallet): The wallet object used for authentication in the transaction.
+        netuid (int): Network UID on which the metadata is to be published.
+        type_ (str): The data type of the information being submitted. It should be one of the following: ``'Sha256'``, ``'Blake256'``, ``'Keccak256'``, or ``'Raw0-128'``. This specifies the format or hashing algorithm used for the data.
+        data (str): The actual metadata content to be published. This should be formatted or hashed according to the ``type`` specified. (Note: max ``str`` length is 128 bytes)
+        wait_for_inclusion (bool, optional): If ``True``, the function will wait for the extrinsic to be included in a block before returning. Defaults to ``False``.
+        wait_for_finalization (bool, optional): If ``True``, the function will wait for the extrinsic to be finalized on the chain before returning. Defaults to ``True``.
 
     Returns:
-        bool:
-            ``True`` if the metadata was successfully published (and finalized if specified). ``False`` otherwise.
+        bool: ``True`` if the metadata was successfully published (and finalized if specified). ``False`` otherwise.
 
     Raises:
-        MetadataError:
-            If there is an error in submitting the extrinsic or if the response from the blockchain indicates failure.
+        MetadataError: If there is an error in submitting the extrinsic or if the response from the blockchain indicates failure.
     """
 
     wallet.hotkey
 
-    with subtensor.substrate as substrate:
-        call = substrate.compose_call(
-            call_module="Commitments",
-            call_function="set_commitment",
-            call_params={"netuid": netuid, "info": {"fields": [[{f"{type}": data}]]}},
-        )
+    call = subtensor.substrate.compose_call(
+        call_module="Commitments",
+        call_function="set_commitment",
+        call_params={"netuid": netuid, "info": {"fields": [[{f"{type_}": data}]]}},
+    )
 
-        extrinsic = substrate.create_signed_extrinsic(call=call, keypair=wallet.hotkey)
-        response = substrate.submit_extrinsic(
-            extrinsic,
-            wait_for_inclusion=wait_for_inclusion,
-            wait_for_finalization=wait_for_finalization,
-        )
-        # We only wait here if we expect finalization.
-        if not wait_for_finalization and not wait_for_inclusion:
-            return True
-        response.process_events()
-        if response.is_success:
-            return True
-        else:
-            raise MetadataError(response.error_message)
+    extrinsic = subtensor.substrate.create_signed_extrinsic(call=call, keypair=wallet.hotkey)
+    response = subtensor.substrate.submit_extrinsic(
+        extrinsic,
+        wait_for_inclusion=wait_for_inclusion,
+        wait_for_finalization=wait_for_finalization,
+    )
+    # We only wait here if we expect finalization.
+    if not wait_for_finalization and not wait_for_inclusion:
+        return True
 
-
-from retry import retry
-from typing import Optional
+    response.process_events()
+    if response.is_success:
+        return True
+    else:
+        raise MetadataError(response.error_message)
 
 
 def get_metadata(self, netuid: int, hotkey: str, block: Optional[int] = None) -> str:
     @retry(delay=2, tries=3, backoff=2, max_delay=4)
     def make_substrate_call_with_retry():
-        with self.substrate as substrate:
-            return substrate.query(
-                module="Commitments",
-                storage_function="CommitmentOf",
-                params=[netuid, hotkey],
-                block_hash=None if block == None else substrate.get_block_hash(block),
-            )
+        return self.substrate.query(
+            module="Commitments",
+            storage_function="CommitmentOf",
+            params=[netuid, hotkey],
+            block_hash=None if block is None else self.substrate.get_block_hash(block),
+        )
 
     commit_data = make_substrate_call_with_retry()
     return commit_data.value
