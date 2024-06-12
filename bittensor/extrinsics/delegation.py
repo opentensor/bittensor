f9ac0edc038b57f684e61c587c34eb021f802d75
Benjamin Himes <benhimes@opentensor.dev>
2024-06-12 17:28:59 +0200

    Moved location of import asyncio to after the license string.

--- bittensor/extrinsics/delegation.py
+++ bittensor/extrinsics/delegation.py
@@ -1,7 +1,6 @@
 # The MIT License (MIT)
 # Copyright © 2021 Yuma Rao
 # Copyright © 2023 Opentensor Foundation
-import asyncio
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
@@ -16,7 +15,7 @@ import asyncio
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-
+import asyncio
 from typing import Union, Optional
 
 from rich.prompt import Confirm
