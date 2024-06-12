c0425c86e659d532b9b87171dfc51194d71e6dd4
gus-opentensor <158077861+gus-opentensor@users.noreply.github.com>
2024-06-12 19:27:00 -0400

    Merge pull request #2016 from opentensor/fix/allow-unstake-below-network-min
    [Fix] Allow unstake below network min

--- bittensor/extrinsics/registration.py
+++ bittensor/extrinsics/registration.py
@@ -462,6 +462,8 @@ def run_faucet_extrinsic(
                 if attempts == max_allowed_attempts:
                     raise MaxAttemptsException
                 attempts += 1
+                # Wait a bit before trying again
+                time.sleep(1)
 
             # Successful registration
             else:
@@ -473,6 +475,8 @@ def run_faucet_extrinsic(
 
                 if successes == 3:
                     raise MaxSuccessException
+
+                attempts = 1  # Reset attempts on success
                 successes += 1
 
         except KeyboardInterrupt:
