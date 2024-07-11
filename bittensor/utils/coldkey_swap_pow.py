2b109d7db981f43fb1163792555afe8cff22c1c2
Benjamin Himes <benhimes@opentensor.dev>
2024-07-11 22:01:18 +0200

    Correctly cleans up multiprocessing Processes (including Queues), which removes the "leaked semaphores" warning after running schedule coldkey swap.

--- bittensor/utils/coldkey_swap_pow.py
+++ bittensor/utils/coldkey_swap_pow.py
@@ -985,11 +985,14 @@ def _solve_for_coldkey_swap_difficulty_cuda(
 
 
 def _terminate_workers_and_wait_for_exit(
-    workers: List[multiprocessing.Process],
+    workers: List[Union[multiprocessing.Process, multiprocessing.queues.Queue]],
 ) -> None:
     for worker in workers:
-        worker.terminate()
-        worker.join()
+        if isinstance(worker, multiprocessing.queues.Queue):
+            worker.join_thread()
+        else:
+            worker.join()
+        worker.close()
 
 
 def create_pow_for_coldkey_swap(
