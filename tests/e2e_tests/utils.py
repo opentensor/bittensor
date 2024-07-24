fe44291248b087c9cc89d1d247e0c09a3b52721e
μ <maarten@coldint.io>
2024-07-24 22:41:40 +0000

    e2e_tests: improvements
    - Replace wait_for_interval(360) calls with wait_epoch(), reducing the
      number of magic constants and preparing for optionally changing the
      tempo used in tests.
    - Automatically determine tempo from on-chain data.
    - Make wait functions async, eliminating time.sleep() which basically
      halts everything, which is not beneficial to other coroutines.

--- tests/e2e_tests/utils.py
+++ tests/e2e_tests/utils.py
@@ -1,4 +1,5 @@
 import logging
+import asyncio
 import os
 import shutil
 import subprocess
@@ -124,14 +125,29 @@ def call_add_proposal(substrate: SubstrateInterface, wallet: bittensor.wallet) -
     return response.is_success
 
 
-def wait_interval(tempo, subtensor, netuid=1):
+async def wait_epoch(subtensor, netuid=1):
+    q_tempo = [
+        v.value
+        for [k, v] in subtensor.query_map_subtensor("Tempo")
+        if k.value == netuid
+    ]
+    if len(q_tempo) == 0:
+        raise Exception("could not determine tempo")
+    tempo = q_tempo[0]
+    logging.info(f"tempo = {tempo}")
+    await wait_interval(tempo, subtensor, netuid)
+
+
+async def wait_interval(tempo, subtensor, netuid=1):
     interval = tempo + 1
     current_block = subtensor.get_current_block()
     last_epoch = current_block - 1 - (current_block + netuid + 1) % interval
     next_tempo_block_start = last_epoch + interval
     last_reported = None
     while current_block < next_tempo_block_start:
-        time.sleep(1)  # Wait for 1 second before checking the block number again
+        await asyncio.sleep(
+            1
+        )  # Wait for 1 second before checking the block number again
         current_block = subtensor.get_current_block()
         if last_reported is None or current_block - last_reported >= 10:
             last_reported = current_block
