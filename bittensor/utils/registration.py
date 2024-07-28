e8d1e6969e6dc51a8d5af8b13f3a671936730de1
Benjamin Himes <benhimes@opentensor.dev>
2024-07-28 11:44:16 +0200

    Ruff.

--- bittensor/utils/registration.py
+++ bittensor/utils/registration.py
@@ -562,7 +562,7 @@ def _solve_for_difficulty_fast(
         while still updating the block information after a different number of nonces,
         to increase the transparency of the process while still keeping the speed.
     """
-    if num_processes == None:
+    if num_processes is None:
         # get the number of allowed processes for this process
         num_processes = min(1, get_cpu_count())
 
@@ -780,8 +780,8 @@ class _UsingSpawnStartMethod:
 
     def __enter__(self):
         self._old_start_method = multiprocessing.get_start_method(allow_none=True)
-        if self._old_start_method == None:
-            self._old_start_method = "spawn"  # default to spawn
+        if self._old_start_method is None:
+            self._old_start_method = "spawn"  # default to spawnn
 
         multiprocessing.set_start_method("spawn", force=self._force)
 
