de7df722ea250b395e6f1243ea829843a742815c
ibraheem-opentensor <ibraheem@opentensor.dev>
2024-07-29 17:08:49 -0700

    Init commit

--- tests/e2e_tests/subcommands/root/test_root_view_proposal.py
+++ tests/e2e_tests/subcommands/root/test_root_view_proposal.py
@@ -1,24 +1,26 @@
+import bittensor
+from bittensor import logging
 from bittensor.commands.senate import ProposalsCommand
 
 from ...utils import (
-    setup_wallet,
     call_add_proposal,
+    setup_wallet,
 )
-import bittensor
 
 
 def test_root_view_proposal(local_chain, capsys):
+    logging.info("Testing test_root_view_proposal")
     keypair, exec_command, wallet = setup_wallet("//Alice")
 
     proposals = local_chain.query("Triumvirate", "Proposals").serialize()
 
-    assert len(proposals) == 0
+    assert len(proposals) == 0, "Proposals are not 0"
 
     call_add_proposal(local_chain, wallet)
 
     proposals = local_chain.query("Triumvirate", "Proposals").serialize()
 
-    assert len(proposals) == 1
+    assert len(proposals) == 1, "Added proposal not found"
 
     exec_command(
         ProposalsCommand,
@@ -62,3 +64,4 @@ def test_root_view_proposal(local_chain, capsys):
     ), f"Expected line starting with '0x78b8a348690f565efe3730cd8189f7388c0a896b6fd090276639c9130c0eba47', got {lines[3].strip()}"
     assert lines[4].strip() == "\x00)", f"Expected '\x00)', got {lines[4].strip()}"
     assert lines[5].strip() == "", f"Expected empty line, got {lines[5].strip()}"
+    logging.info("Finished test_root_view_proposal")
