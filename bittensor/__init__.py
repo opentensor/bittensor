5b7343499840e758dc8338797e1298dde7ba788a
unconst <jake@bittensor.com>
2024-08-14 13:46:50 -0500

    rao

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -91,9 +91,9 @@ __ss58_format__ = 42
 # Wallet ss58 address length
 __ss58_address_length__ = 48
 
-__networks__ = ["dtao", "local", "finney", "test", "archive", "dev"]
+__networks__ = ["rao", "local", "finney", "test", "archive", "dev"]
 
-__dtao_entrypoint__ = "wss://dtao-demo.chain.opentensor.ai:443"
+__rao_entrypoint__ = "wss://rao.chain.opentensor.ai:443"
 
 __finney_entrypoint__ = "wss://entrypoint-finney.opentensor.ai:443"
 
