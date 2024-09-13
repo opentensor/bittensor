f634ef6edde80f5d49ad8855be060a23c4470de4
Roman <roman@opentensor.dev>
2024-09-13 14:01:50 -0700

    ProposalVoteData

--- bittensor/core/chain_data/proposal_vote_data.py
+++ bittensor/core/chain_data/proposal_vote_data.py
@@ -3,6 +3,16 @@ from typing import List, TypedDict
 
 # Senate / Proposal data
 class ProposalVoteData(TypedDict):
+    """
+    This TypedDict represents the data structure for a proposal vote in the Senate.
+
+    Attributes:
+        index (int): The index of the proposal.
+        threshold (int): The threshold required for the proposal to pass.
+        ayes (List[str]): List of senators who voted 'aye'.
+        nays (List[str]): List of senators who voted 'nay'.
+        end (int): The ending timestamp of the voting period.
+    """
     index: int
     threshold: int
     ayes: List[str]
