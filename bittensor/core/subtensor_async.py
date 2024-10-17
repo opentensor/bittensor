ef56750219c30fd9f87a8f8ed5f644af6f89f248
Roman <roman@opentensor.dev>
2024-10-17 14:22:35 -0700

    create the copy of `bittensor/core/subtensor.py` with async suffix.

--- bittensor/core/subtensor_async.py
+++ bittensor/core/subtensor_async.py
@@ -16,8 +16,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 """
-The ``bittensor.core.subtensor.Subtensor`` module in Bittensor serves as a crucial interface for interacting with the Bittensor
-blockchain, facilitating a range of operations essential for the decentralized machine learning network.
+Async version of `bittensor.core.subtensor.Subtensor` module.
 """
 
 import argparse
