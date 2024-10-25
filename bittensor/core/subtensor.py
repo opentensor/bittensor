99d206a05ab079a3972ef307751cd332da90277e
Roman <167799377+roman-opentensor@users.noreply.github.com>
2024-10-25 14:47:12 -0700

    add Global Weight to metagraph + test (#2371)
    * add Global Weight to metagraph + test
    
    * ruff

--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -75,7 +75,13 @@ from bittensor.core.extrinsics.transfer import (
     transfer_extrinsic,
 )
 from bittensor.core.metagraph import Metagraph
-from bittensor.utils import networking, torch, ss58_to_vec_u8, u16_normalized_float
+from bittensor.utils import (
+    networking,
+    torch,
+    ss58_to_vec_u8,
+    u16_normalized_float,
+    u64_normalized_float,
+)
 from bittensor.utils.balance import Balance
 from bittensor.utils.btlogging import logging
 from bittensor.utils.registration import legacy_torch_api_compat
@@ -2052,6 +2058,21 @@ class Subtensor:
             return None
 
         return DelegateInfo.from_vec_u8(result)
+
+    def get_global_weight(
+        self, netuid: int, block: Optional[str] = None
+    ) -> Optional[float]:
+        """Returns the subnet Global Weight across all subnets."""
+        result = self.substrate.query(
+            module="SubtensorModule",
+            storage_function="GlobalWeight",
+            params=[netuid],
+            block_hash=None if block is None else self.substrate.get_block_hash(block),
+        )
+        if hasattr(result, "value"):
+            return u64_normalized_float(result.value)
+
+        return None
 
     # Subnet 27 uses this method
     _do_serve_prometheus = do_serve_prometheus
