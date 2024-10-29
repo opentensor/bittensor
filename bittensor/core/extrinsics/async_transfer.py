d1e182dfc91a404c46f44336e9203ad098c6c699
Roman <167799377+roman-opentensor@users.noreply.github.com>
2024-10-29 14:46:00 -0700

    Update bittensor/core/extrinsics/async_transfer.py
    Co-authored-by: Benjamin Himes <37844818+thewhaleking@users.noreply.github.com>

--- bittensor/core/extrinsics/async_transfer.py
+++ bittensor/core/extrinsics/async_transfer.py
@@ -198,7 +198,7 @@ async def transfer_extrinsic(
             )
             console.print(
                 f"Balance:\n"
-                f"  [blue]{account_balance}[/blue] :arrow_right: [green]{new_balance[wallet.coldkey.ss58_address]}[/green]"
+                f"  [blue]{account_balance}[/blue] :arrow_right: [green]{new_balance[wallet.coldkeypub.ss58_address]}[/green]"
             )
             return True
 
