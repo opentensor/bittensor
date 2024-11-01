6c8638aa1611fc246a22fbe70b3b07f140fe0f3f
open-junius <zhou@opentensor.dev>
2024-11-01 01:02:56 +0100

    update netuid

--- tests/e2e_tests/test_liquid_alpha.py
+++ tests/e2e_tests/test_liquid_alpha.py
@@ -33,7 +33,7 @@ def test_liquid_alpha(local_chain):
         AssertionError: If any of the checks or verifications fail
     """
     u16_max = 65535
-    netuid = 1
+    netuid = 2
     logging.info("Testing test_liquid_alpha_enabled")
 
     # Register root as Alice
@@ -44,9 +44,9 @@ def test_liquid_alpha(local_chain):
     assert local_chain.query("SubtensorModule", "NetworksAdded", [1]).serialize()
 
     # Register a neuron to the subnet
-    assert register_neuron(
-        local_chain, alice_wallet, netuid
-    ), "Unable to register Alice as a neuron"
+    # assert register_neuron(
+    #     local_chain, alice_wallet, netuid
+    # ), "Unable to register Alice as a neuron"
 
     # Stake to become to top neuron after the first epoch
     add_stake(local_chain, alice_wallet, netuid, bittensor.Balance.from_tao(100_000))
@@ -77,7 +77,7 @@ def test_liquid_alpha(local_chain):
     ), "Unable to enable liquid alpha"
 
     assert subtensor.get_subnet_hyperparameters(
-        netuid=1
+        netuid=netuid,
     ).liquid_alpha_enabled, "Failed to enable liquid alpha"
 
     # Attempt to set alpha high & low after enabling the hyperparameter
@@ -90,10 +90,10 @@ def test_liquid_alpha(local_chain):
         call_params=call_params,
     ), "Unable to set alpha_values"
     assert (
-        subtensor.get_subnet_hyperparameters(netuid=1).alpha_high == 54099
+        subtensor.get_subnet_hyperparameters(netuid=netuid).alpha_high == 54099
     ), "Failed to set alpha high"
     assert (
-        subtensor.get_subnet_hyperparameters(netuid=1).alpha_low == 87
+        subtensor.get_subnet_hyperparameters(netuid=netuid).alpha_low == 87
     ), "Failed to set alpha low"
 
     # Testing alpha high upper and lower bounds
@@ -169,10 +169,10 @@ def test_liquid_alpha(local_chain):
     ), "Unable to set liquid alpha values"
 
     assert (
-        subtensor.get_subnet_hyperparameters(netuid=1).alpha_high == 53083
+        subtensor.get_subnet_hyperparameters(netuid=1).alpha_high == 58982
     ), "Failed to set alpha high"
     assert (
-        subtensor.get_subnet_hyperparameters(netuid=1).alpha_low == 6553
+        subtensor.get_subnet_hyperparameters(netuid=1).alpha_low == 45875
     ), "Failed to set alpha low"
 
     # Disable Liquid Alpha
