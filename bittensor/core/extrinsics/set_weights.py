2e565d5cee78ac9a8649237b735c77f5829c42a0
Roman <roman@opentensor.dev>
2024-11-04 10:12:16 -0800

    ruff

--- bittensor/core/extrinsics/set_weights.py
+++ bittensor/core/extrinsics/set_weights.py
@@ -179,7 +179,9 @@ def set_weights_extrinsic(
             logging.success(f"<green>Finalized!</green> Set weights: {str(success)}")
             return True, "Successfully set weights and Finalized."
         else:
-            error_message = format_error_message(error_message, substrate=subtensor.substrate)
+            error_message = format_error_message(
+                error_message, substrate=subtensor.substrate
+            )
             logging.error(error_message)
             return False, error_message
 
