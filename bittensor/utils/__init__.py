c304e5b8803d4330b27a6fa650695ff9de0af6c2
Benjamin Himes <benhimes@opentensor.dev>
2024-11-14 16:15:27 +0200

    Added unit test

--- bittensor/utils/__init__.py
+++ bittensor/utils/__init__.py
@@ -409,8 +409,8 @@ def hex_to_bytes(hex_str: str) -> bytes:
     """
     Converts a hex-encoded string into bytes. Handles 0x-prefixed and non-prefixed hex-encoded strings.
     """
-    try:
+    if hex_str.startswith("0x"):
         bytes_result = bytes.fromhex(hex_str[2:])
-    except ValueError:
+    else:
         bytes_result = bytes.fromhex(hex_str)
     return bytes_result
