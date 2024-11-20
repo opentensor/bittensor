6c7f10b31b41959df64f2e792d3771f44472ce66
Roman <roman@opentensor.dev>
2024-11-19 23:02:39 -0800

    add test for `async_registration.register_extrinsic`

--- tests/unit_tests/extrinsics/test_async_registration.py
+++ tests/unit_tests/extrinsics/test_async_registration.py
@@ -105,7 +105,9 @@ async def test_do_pow_register_failure(subtensor, mocker):
     mocker.patch.object(
         subtensor.substrate, "submit_extrinsic", return_value=fake_response
     )
-    mocked_format_error_message = mocker.patch.object(async_registration, "format_error_message")
+    mocked_format_error_message = mocker.patch.object(
+        async_registration, "format_error_message"
+    )
 
     # Call
     result_error_message = await async_registration._do_pow_register(
@@ -118,18 +120,16 @@ async def test_do_pow_register_failure(subtensor, mocker):
     )
 
     # Asserts
-    subtensor.substrate.compose_call.asseert_awaited_once_with(
-
-    )
+    subtensor.substrate.compose_call.asseert_awaited_once_with()
     subtensor.substrate.create_signed_extrinsic.asseert_awaited_once_with(
-        fake_call, fake_wallet.hotkey
+        call=fake_call, keypair=fake_wallet.hotkey
     )
     subtensor.substrate.submit_extrinsic.asseert_awaited_once_with(
-        fake_extrinsic, wait_for_inclusion=True, wait_for_finalization=True
+        extrinsic=fake_extrinsic, wait_for_inclusion=True, wait_for_finalization=True
     )
 
     mocked_format_error_message.assert_called_once_with(
-        fake_err_message, substrate=subtensor.substrate
+        error_message=fake_err_message, substrate=subtensor.substrate
     )
     assert result_error_message == (False, mocked_format_error_message.return_value)
 
@@ -177,3 +177,246 @@ async def test_do_pow_register_no_waiting(subtensor, mocker):
     )
     assert result is True
     assert error_message is None
+
+
+@pytest.mark.asyncio
+async def test_register_extrinsic_success(subtensor, mocker):
+    """Tests successful registration."""
+    # Preps
+    fake_wallet = mocker.Mock(autospec=Wallet)
+    fake_wallet.hotkey.ss58_address = "hotkey_ss58"
+    fake_wallet.coldkey.ss58_address = "coldkey_ss58"
+
+    mocked_subnet_exists = mocker.patch.object(
+        subtensor, "subnet_exists", return_value=True
+    )
+    mocked_get_neuron = mocker.patch.object(
+        subtensor,
+        "get_neuron_for_pubkey_and_subnet",
+        return_value=mocker.Mock(is_null=True),
+    )
+    mocked_create_pow = mocker.patch.object(
+        async_registration,
+        "create_pow_async",
+        return_value=mocker.Mock(is_stale_async=mocker.AsyncMock(return_value=False)),
+    )
+    mocked_do_pow_register = mocker.patch.object(
+        async_registration, "_do_pow_register", return_value=(True, None)
+    )
+    mocked_is_hotkey_registered = mocker.patch.object(
+        subtensor, "is_hotkey_registered", return_value=True
+    )
+
+    # Call
+    result = await async_registration.register_extrinsic(
+        subtensor=subtensor,
+        wallet=fake_wallet,
+        netuid=1,
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
+    )
+
+    # Asserts
+    mocked_subnet_exists.assert_called_once_with(1)
+    mocked_get_neuron.assert_called_once_with(hotkey_ss58="hotkey_ss58", netuid=1)
+    mocked_create_pow.assert_called_once()
+    mocked_do_pow_register.assert_called_once()
+    mocked_is_hotkey_registered.assert_called_once_with(
+        netuid=1, hotkey_ss58="hotkey_ss58"
+    )
+    assert result is True
+
+
+@pytest.mark.asyncio
+async def test_register_extrinsic_success_with_cuda(subtensor, mocker):
+    """Tests successful registration with CUDA enabled."""
+    # Preps
+    fake_wallet = mocker.Mock(autospec=Wallet)
+    fake_wallet.hotkey.ss58_address = "hotkey_ss58"
+    fake_wallet.coldkey.ss58_address = "coldkey_ss58"
+
+    mocked_subnet_exists = mocker.patch.object(
+        subtensor, "subnet_exists", return_value=True
+    )
+    mocked_get_neuron = mocker.patch.object(
+        subtensor,
+        "get_neuron_for_pubkey_and_subnet",
+        return_value=mocker.Mock(is_null=True),
+    )
+    mocker.patch("torch.cuda.is_available", return_value=True)
+    mocked_create_pow = mocker.patch.object(
+        async_registration,
+        "create_pow_async",
+        return_value=mocker.Mock(is_stale_async=mocker.AsyncMock(return_value=False)),
+    )
+    mocked_do_pow_register = mocker.patch.object(
+        async_registration, "_do_pow_register", return_value=(True, None)
+    )
+    mocked_is_hotkey_registered = mocker.patch.object(
+        subtensor, "is_hotkey_registered", return_value=True
+    )
+
+    # Call
+    result = await async_registration.register_extrinsic(
+        subtensor=subtensor,
+        wallet=fake_wallet,
+        netuid=1,
+        cuda=True,
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
+    )
+
+    # Asserts
+    mocked_subnet_exists.assert_called_once_with(1)
+    mocked_get_neuron.assert_called_once_with(hotkey_ss58="hotkey_ss58", netuid=1)
+    mocked_create_pow.assert_called_once()
+    mocked_do_pow_register.assert_called_once()
+    mocked_is_hotkey_registered.assert_called_once_with(
+        netuid=1, hotkey_ss58="hotkey_ss58"
+    )
+    assert result is True
+
+
+@pytest.mark.asyncio
+async def test_register_extrinsic_failed_with_cuda(subtensor, mocker):
+    """Tests failed registration with CUDA enabled."""
+    # Preps
+    fake_wallet = mocker.Mock(autospec=Wallet)
+    fake_wallet.hotkey.ss58_address = "hotkey_ss58"
+    fake_wallet.coldkey.ss58_address = "coldkey_ss58"
+
+    mocked_subnet_exists = mocker.patch.object(
+        subtensor, "subnet_exists", return_value=True
+    )
+    mocked_get_neuron = mocker.patch.object(
+        subtensor,
+        "get_neuron_for_pubkey_and_subnet",
+        return_value=mocker.Mock(is_null=True),
+    )
+    mocker.patch("torch.cuda.is_available", return_value=False)
+
+    # Call
+    result = await async_registration.register_extrinsic(
+        subtensor=subtensor,
+        wallet=fake_wallet,
+        netuid=1,
+        cuda=True,
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
+    )
+
+    # Asserts
+    mocked_subnet_exists.assert_called_once_with(1)
+    mocked_get_neuron.assert_called_once_with(hotkey_ss58="hotkey_ss58", netuid=1)
+    assert result is False
+
+
+@pytest.mark.asyncio
+async def test_register_extrinsic_subnet_not_exists(subtensor, mocker):
+    """Tests registration when subnet does not exist."""
+    # Preps
+    fake_wallet = mocker.Mock(autospec=Wallet)
+
+    mocked_subnet_exists = mocker.patch.object(
+        subtensor, "subnet_exists", return_value=False
+    )
+
+    # Call
+    result = await async_registration.register_extrinsic(
+        subtensor=subtensor,
+        wallet=fake_wallet,
+        netuid=1,
+    )
+
+    # Asserts
+    mocked_subnet_exists.assert_called_once_with(1)
+    assert result is False
+
+
+@pytest.mark.asyncio
+async def test_register_extrinsic_already_registered(subtensor, mocker):
+    """Tests registration when the key is already registered."""
+    # Preps
+    fake_wallet = mocker.Mock(autospec=Wallet)
+    mocked_get_neuron = mocker.patch.object(
+        subtensor,
+        "get_neuron_for_pubkey_and_subnet",
+        return_value=mocker.Mock(is_null=False),
+    )
+
+    # Call
+    result = await async_registration.register_extrinsic(
+        subtensor=subtensor,
+        wallet=fake_wallet,
+        netuid=1,
+    )
+
+    # Asserts
+    mocked_get_neuron.assert_called_once_with(
+        hotkey_ss58=fake_wallet.hotkey.ss58_address, netuid=1
+    )
+    assert result is True
+
+
+# commented until bittensor/core/extrinsics/async_registration.py:258 (TODO)
+# @pytest.mark.asyncio
+# async def test_register_extrinsic_max_attempts_reached(subtensor, mocker):
+#     # Preps
+#     fake_wallet = mocker.Mock(autospec=Wallet)
+#     fake_wallet.hotkey.ss58_address = "hotkey_ss58"
+#     fake_wallet.coldkey.ss58_address = "coldkey_ss58"
+#
+#     stale_responses = iter([False, False, False])
+#
+#     async def is_stale_side_effect(*args, **kwargs):
+#         return next(stale_responses, True)
+
+#     fake_pow_result = mocker.Mock()
+#     fake_pow_result.is_stale_async = mocker.AsyncMock(side_effect=is_stale_side_effect)
+#
+#     mocked_subnet_exists = mocker.patch.object(
+#         subtensor, "subnet_exists", return_value=True
+#     )
+#     mocked_get_neuron = mocker.patch.object(
+#         subtensor,
+#         "get_neuron_for_pubkey_and_subnet",
+#         return_value=mocker.Mock(is_null=True),
+#     )
+#     mocked_create_pow = mocker.patch.object(
+#         async_registration,
+#         "create_pow_async",
+#         return_value=fake_pow_result,
+#     )
+#     mocked_do_pow_register = mocker.patch.object(
+#         async_registration,
+#         "_do_pow_register",
+#         return_value=(False, "Test Error"),
+#     )
+#
+#     # Call
+#     result = await async_registration.register_extrinsic(
+#         subtensor=subtensor,
+#         wallet=fake_wallet,
+#         netuid=1,
+#         max_allowed_attempts=3,
+#         wait_for_inclusion=True,
+#         wait_for_finalization=True,
+#     )
+#
+#     # Asserts
+#     mocked_subnet_exists.assert_called_once_with(1)
+#     mocked_get_neuron.assert_called_once_with(
+#         hotkey_ss58="hotkey_ss58", netuid=1
+#     )
+#     assert mocked_create_pow.call_count == 3
+#     assert mocked_do_pow_register.call_count == 3
+#
+#     mocked_do_pow_register.assert_called_with(
+#         subtensor=subtensor,
+#         netuid=1,
+#         wallet=fake_wallet,
+#         pow_result=fake_pow_result,
+#         wait_for_inclusion=True,
+#         wait_for_finalization=True,
+#     )
+#     assert result is False
