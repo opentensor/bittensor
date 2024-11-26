05390a10dd1cfccdd4371b2f1635d99f41c28298
Benjamin Himes <37844818+thewhaleking@users.noreply.github.com>
2024-11-26 23:51:27 +0200

    remove uses of return scale obj (#2479)
    decode_scale's return_scale_obj arg was removed, but its references were not.

--- bittensor/utils/async_substrate_interface.py
+++ bittensor/utils/async_substrate_interface.py
@@ -1613,8 +1613,7 @@ class AsyncSubstrateInterface:
                 q = bytes(query_value)
             else:
                 q = query_value
-            obj = await self.decode_scale(value_scale_type, q, True)
-            result = obj
+            result = await self.decode_scale(value_scale_type, q)
         if asyncio.iscoroutinefunction(result_handler):
             # For multipart responses as a result of subscriptions.
             message, bool_result = await result_handler(response, subscription_id)
@@ -2346,7 +2345,6 @@ class AsyncSubstrateInterface:
             return await self.decode_scale(
                 constant.type,
                 bytes(constant.constant_value),
-                return_scale_obj=True,
             )
         else:
             return None
@@ -2571,7 +2569,6 @@ class AsyncSubstrateInterface:
                         item_key_obj = await self.decode_scale(
                             type_string=f"({', '.join(key_type_string)})",
                             scale_bytes=bytes.fromhex(item[0][len(prefix) :]),
-                            return_scale_obj=True,
                         )
 
                         # strip key_hashers to use as item key
@@ -2592,9 +2589,7 @@ class AsyncSubstrateInterface:
                         item_bytes = hex_to_bytes(item[1])
 
                         item_value = await self.decode_scale(
-                            type_string=value_type,
-                            scale_bytes=item_bytes,
-                            return_scale_obj=True,
+                            type_string=value_type, scale_bytes=item_bytes
                         )
                     except Exception as _:
                         if not ignore_decoding_errors:
