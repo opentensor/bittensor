4417284a93d5e39612ca326df32cc88cb69d567e
Reinier Heeres <reinier@coldint.io>
2024-12-21 13:57:37 +0000

    set_weights: prevent exception when not registered
    - The return value of get_uid_for_hotkey_on_subnet was not checked. If
      the hotkey is not registered, this call will return None, resulting
      in an exception later on.

--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -1817,6 +1817,11 @@ class Subtensor:
         retries = 0
         success = False
         uid = self.get_uid_for_hotkey_on_subnet(wallet.hotkey.ss58_address, netuid)
+        if uid is None:
+            return (
+                False,
+                f"Hotkey {wallet.hotkey.ss58_address} not registered in subnet {netuid}",
+            )
 
         if self.commit_reveal_enabled(netuid=netuid) is True:
             # go with `commit reveal v3` extrinsic
