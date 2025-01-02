a452058d81b2862882e73ad97d35f76d60ebacaa
Roman <roman@opentensor.dev>
2025-01-02 10:47:49 -0800

    add async `bittensor.core.extrinsics.async_weights.reveal_weights_extrinsic`

--- bittensor/core/extrinsics/async_weights.py
+++ bittensor/core/extrinsics/async_weights.py
@@ -33,7 +33,7 @@ async def _do_set_weights(
 
     Args:
         subtensor (subtensor.core.async_subtensor.AsyncSubtensor): Async Subtensor instance.
-        wallet (bittensor.wallet): The wallet associated with the neuron setting the weights.
+        wallet (bittensor_wallet.Wallet): The wallet associated with the neuron setting the weights.
         uids (List[int]): List of neuron UIDs for which weights are being set.
         vals (List[int]): List of weight values corresponding to each UID.
         netuid (int): Unique identifier for the network.
@@ -99,8 +99,8 @@ async def set_weights_extrinsic(
     """Sets the given weights and values on chain for wallet hotkey account.
 
     Args:
-        subtensor (bittensor.subtensor): Bittensor subtensor object.
-        wallet (bittensor.wallet): Bittensor wallet object.
+        subtensor (bittensor.core.async_subtensor.AsyncSubtensor): Bittensor subtensor object.
+        wallet (bittensor_wallet.Wallet): Bittensor wallet object.
         netuid (int): The ``netuid`` of the subnet to set weights for.
         uids (Union[NDArray[np.int64], torch.LongTensor, list]): The ``uint64`` uids of destination neurons.
         weights (Union[NDArray[np.float32], torch.FloatTensor, list]): The weights to set. These must be ``float`` s and correspond to the passed ``uid`` s.
@@ -166,7 +166,7 @@ async def _do_commit_weights(
     This method constructs and submits the transaction, handling retries and blockchain communication.
 
     Args:
-        subtensor (bittensor.core.subtensor.Subtensor): The subtensor instance used for blockchain interaction.
+        subtensor (bittensor.core.async_subtensor.AsyncSubtensor): The subtensor instance used for blockchain interaction.
         wallet (bittensor_wallet.Wallet): The wallet associated with the neuron committing the weights.
         netuid (int): The unique identifier of the subnet.
         commit_hash (str): The hash of the neuron's weights to be committed.
@@ -226,7 +226,7 @@ async def commit_weights_extrinsic(
     This function is a wrapper around the `do_commit_weights` method.
 
     Args:
-        subtensor (bittensor.core.subtensor.Subtensor): The subtensor instance used for blockchain interaction.
+        subtensor (bittensor.core.async_subtensor.AsyncSubtensor): The subtensor instance used for blockchain interaction.
         wallet (bittensor_wallet.Wallet): The wallet associated with the neuron committing the weights.
         netuid (int): The unique identifier of the subnet.
         commit_hash (str): The hash of the neuron's weights to be committed.
@@ -255,4 +255,121 @@ async def commit_weights_extrinsic(
         return True, success_message
     else:
         logging.error(f"Failed to commit weights: {error_message}")
+        return False, error_message
+
+
+async def do_reveal_weights(
+    subtensor: "AsyncSubtensor",
+    wallet: "Wallet",
+    netuid: int,
+    uids: list[int],
+    values: list[int],
+    salt: list[int],
+    version_key: int,
+    wait_for_inclusion: bool = False,
+    wait_for_finalization: bool = False,
+) -> tuple[bool, Optional[dict]]:
+    """
+    Internal method to send a transaction to the Bittensor blockchain, revealing the weights for a specific subnet.
+    This method constructs and submits the transaction, handling retries and blockchain communication.
+
+    Args:
+        subtensor (bittensor.core.async_subtensor.AsyncSubtensor): The subtensor instance used for blockchain interaction.
+        wallet (bittensor_wallet.Wallet): The wallet associated with the neuron revealing the weights.
+        netuid (int): The unique identifier of the subnet.
+        uids (list[int]): List of neuron UIDs for which weights are being revealed.
+        values (list[int]): List of weight values corresponding to each UID.
+        salt (list[int]): List of salt values corresponding to the hash function.
+        version_key (int): Version key for compatibility with the network.
+        wait_for_inclusion (bool): Waits for the transaction to be included in a block.
+        wait_for_finalization (bool): Waits for the transaction to be finalized on the blockchain.
+
+    Returns:
+        tuple[bool, Optional[str]]: A tuple containing a success flag and an optional error message.
+
+    This method ensures that the weight revelation is securely recorded on the Bittensor blockchain, providing
+        transparency and accountability for the neuron's weight distribution.
+    """
+
+    call = await subtensor.substrate.compose_call(
+        call_module="SubtensorModule",
+        call_function="reveal_weights",
+        call_params={
+            "netuid": netuid,
+            "uids": uids,
+            "values": values,
+            "salt": salt,
+            "version_key": version_key,
+        },
+    )
+    extrinsic = await subtensor.substrate.create_signed_extrinsic(
+        call=call,
+        keypair=wallet.hotkey,
+    )
+    response = await subtensor.substrate.submit_extrinsic(
+        extrinsic=extrinsic,
+        wait_for_inclusion=wait_for_inclusion,
+        wait_for_finalization=wait_for_finalization,
+    )
+
+    if not wait_for_finalization and not wait_for_inclusion:
+        return True, None
+
+    if await response.is_success:
+        return True, None
+    else:
+        return False, await response.error_message
+
+
+async def reveal_weights_extrinsic(
+    subtensor: "AsyncSubtensor",
+    wallet: "Wallet",
+    netuid: int,
+    uids: list[int],
+    weights: list[int],
+    salt: list[int],
+    version_key: int,
+    wait_for_inclusion: bool = False,
+    wait_for_finalization: bool = False,
+) -> tuple[bool, str]:
+    """
+    Reveals the weights for a specific subnet on the Bittensor blockchain using the provided wallet.
+    This function is a wrapper around the `_do_reveal_weights` method.
+
+    Args:
+        subtensor (bittensor.core.subtensor.Subtensor): The subtensor instance used for blockchain interaction.
+        wallet (bittensor_wallet.Wallet): The wallet associated with the neuron revealing the weights.
+        netuid (int): The unique identifier of the subnet.
+        uids (list[int]): List of neuron UIDs for which weights are being revealed.
+        weights (list[int]): List of weight values corresponding to each UID.
+        salt (list[int]): List of salt values corresponding to the hash function.
+        version_key (int): Version key for compatibility with the network.
+        wait_for_inclusion (bool): Waits for the transaction to be included in a block.
+        wait_for_finalization (bool): Waits for the transaction to be finalized on the blockchain.
+
+    Returns:
+        tuple[bool, str]: ``True`` if the weight revelation is successful, False otherwise. And `msg`, a string value describing the success or potential error.
+
+    This function provides a user-friendly interface for revealing weights on the Bittensor blockchain, ensuring proper error handling and user interaction when required.
+    """
+
+    success, error_message = await do_reveal_weights(
+        subtensor=subtensor,
+        wallet=wallet,
+        netuid=netuid,
+        uids=uids,
+        values=weights,
+        salt=salt,
+        version_key=version_key,
+        wait_for_inclusion=wait_for_inclusion,
+        wait_for_finalization=wait_for_finalization,
+    )
+
+    if success:
+        success_message = "Successfully revealed weights."
+        logging.info(success_message)
+        return True, success_message
+    else:
+        error_message = format_error_message(error_message)
+        logging.error(f"Failed to reveal weights: {error_message}")
         return False, error_message
