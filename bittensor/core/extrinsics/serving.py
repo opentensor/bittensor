a10e88d1acb29946b0fb424fbaed9bbd688019cd
Roman Chkhaidze <basfroman@gmail.com>
2025-01-02 17:55:12 -0800

    update serving extrinsics

--- bittensor/core/extrinsics/serving.py
+++ bittensor/core/extrinsics/serving.py
@@ -1,6 +1,6 @@
 from typing import Optional, TYPE_CHECKING
 
-from bittensor.core.extrinsics.async_serving import (
+from bittensor.core.extrinsics.asyncex.serving import (
     do_serve_axon as async_do_serve_axon,
     serve_axon_extrinsic as async_serve_axon_extrinsic,
     publish_metadata as async_publish_metadata,
@@ -24,13 +24,14 @@ def do_serve_axon(
     wait_for_finalization: bool = True,
 ) -> tuple[bool, Optional[dict]]:
     return execute_coroutine(
-        async_do_serve_axon(
+        coroutine=async_do_serve_axon(
             subtensor=self.async_subtensor,
             wallet=wallet,
             call_params=call_params,
             wait_for_inclusion=wait_for_inclusion,
             wait_for_finalization=wait_for_finalization,
-        )
+        ),
+        event_loop=self.event_loop,
     )
 
 
@@ -43,14 +44,15 @@ def serve_axon_extrinsic(
     certificate: Optional["Certificate"] = None,
 ) -> bool:
     return execute_coroutine(
-        async_serve_axon_extrinsic(
+        coroutine=async_serve_axon_extrinsic(
             subtensor=subtensor.async_subtensor,
             netuid=netuid,
             axon=axon,
             wait_for_inclusion=wait_for_inclusion,
             wait_for_finalization=wait_for_finalization,
             certificate=certificate,
-        )
+        ),
+        event_loop=subtensor.event_loop,
     )
 
 
@@ -64,7 +66,7 @@ def publish_metadata(
     wait_for_finalization: bool = True,
 ) -> bool:
     return execute_coroutine(
-        async_publish_metadata(
+        coroutine=async_publish_metadata(
             subtensor=self.async_subtensor,
             wallet=wallet,
             netuid=netuid,
@@ -72,7 +74,8 @@ def publish_metadata(
             data=data,
             wait_for_inclusion=wait_for_inclusion,
             wait_for_finalization=wait_for_finalization,
-        )
+        ),
+        event_loop=self.event_loop,
     )
 
 
@@ -80,10 +83,11 @@ def get_metadata(
     self: "Subtensor", netuid: int, hotkey: str, block: Optional[int] = None
 ) -> str:
     return execute_coroutine(
-        async_get_metadata(
+        coroutine=async_get_metadata(
             subtensor=self.async_subtensor,
             netuid=netuid,
             hotkey=hotkey,
             block=block,
-        )
+        ),
+        event_loop=self.event_loop,
     )
