36b4cbb5d1dd644a2f99fa0bab7827f79d772ea6
Roman <roman@opentensor.dev>
2025-01-03 21:25:23 -0800

    update test for async_substrate_interface

--- tests/unit_tests/utils/test_async_substrate_interface.py
+++ tests/unit_tests/utils/test_async_substrate_interface.py
@@ -1,38 +1,14 @@
 import pytest
-import asyncio
+from websockets.exceptions import InvalidURI
+
 from bittensor.utils import async_substrate_interface
-from typing import Any
 
 
 @pytest.mark.asyncio
-async def test_wait_for_block_invalid_result_handler():
-    chain_interface = async_substrate_interface.AsyncSubstrateInterface(
-        "dummy_endpoint"
-    )
-
-    with pytest.raises(ValueError):
-
-        async def dummy_handler(
-            block_data: dict[str, Any], extra_arg
-        ):  # extra argument
-            return block_data.get("header", {}).get("number", -1) == 2
-
-        await chain_interface.wait_for_block(
-            block=2, result_handler=dummy_handler, task_return=False
+async def test_invalid_url_raises_exception():
+    """Test that invalid URI raises an InvalidURI exception."""
+    with pytest.raises(InvalidURI):
+        async_substrate_interface.AsyncSubstrateInterface(
+            "non_existent_entry_point"
         )
 
-
-@pytest.mark.asyncio
-async def test_wait_for_block_async_return():
-    chain_interface = async_substrate_interface.AsyncSubstrateInterface(
-        "dummy_endpoint"
-    )
-
-    async def dummy_handler(block_data: dict[str, Any]) -> bool:
-        return block_data.get("header", {}).get("number", -1) == 2
-
-    result = await chain_interface.wait_for_block(
-        block=2, result_handler=dummy_handler, task_return=True
-    )
-
-    assert isinstance(result, asyncio.Task)
