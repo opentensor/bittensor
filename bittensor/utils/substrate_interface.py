8db31c09ab0faaf61015635e1a0071f630560f62
Benjamin Himes <benhimes@opentensor.dev>
2025-01-08 20:35:40 +0200

    Added skeleton methods for all used Substrate methods for ease of use with IDEs or whatever.

--- bittensor/utils/substrate_interface.py
+++ bittensor/utils/substrate_interface.py
@@ -3910,6 +3910,15 @@ class AsyncSubstrateInterface:
             return await co
 
 
+class SyncWebsocket:
+    def __init__(self, websocket: "Websocket", event_loop: asyncio.AbstractEventLoop):
+        self._ws = websocket
+        self._event_loop = event_loop
+
+    def close(self):
+        execute_coroutine(self._ws.shutdown(), event_loop=self._event_loop)
+
+
 class SubstrateInterface:
     """
     A wrapper around AsyncSubstrateInterface that allows for using all the calls from it in a synchronous context
@@ -3928,6 +3937,7 @@ class SubstrateInterface:
         substrate: Optional["AsyncSubstrateInterface"] = None,
     ):
         event_loop = substrate.event_loop if substrate else event_loop
+        self.url = url
         self._async_instance = (
             AsyncSubstrateInterface(
                 url=url,
@@ -3944,9 +3954,21 @@ class SubstrateInterface:
             else substrate
         )
         self.event_loop = event_loop or asyncio.get_event_loop()
+        self.websocket = SyncWebsocket(self._async_instance.ws, self.event_loop)
+
+    @property
+    def last_block_hash(self):
+        return self._async_instance.last_block_hash
+
+    @property
+    def metadata(self):
+        return self._async_instance.metadata
 
     def __del__(self):
         execute_coroutine(self._async_instance.close())
+
+    def _run(self, coroutine):
+        return execute_coroutine(coroutine, self.event_loop)
 
     def __getattr__(self, name):
         attr = getattr(self._async_instance, name)
@@ -3954,46 +3976,193 @@ class SubstrateInterface:
         if asyncio.iscoroutinefunction(attr):
 
             def sync_method(*args, **kwargs):
-                return self.event_loop.run_until_complete(attr(*args, **kwargs))
+                return self._run(attr(*args, **kwargs))
 
             return sync_method
         elif asyncio.iscoroutine(attr):
             # indicates this is an async_property
-            return self.event_loop.run_until_complete(attr)
+            return self._run(attr)
         else:
             return attr
 
-    # def query(
-    #     self,
-    #     module: str,
-    #     storage_function: str,
-    #     params: Optional[list] = None,
-    #     block_hash: Optional[str] = None,
-    #     raw_storage_key: Optional[bytes] = None,
-    #     subscription_handler=None,
-    #     reuse_block_hash: bool = False,
-    # ) -> "ScaleType":
-    #     return self.event_loop.run_until_complete(
-    #         self._async_instance.query(
-    #             module,
-    #             storage_function,
-    #             params,
-    #             block_hash,
-    #             raw_storage_key,
-    #             subscription_handler,
-    #             reuse_block_hash,
-    #         )
-    #     )
-    #
-    # def get_constant(
-    #     self,
-    #     module_name: str,
-    #     constant_name: str,
-    #     block_hash: Optional[str] = None,
-    #     reuse_block_hash: bool = False,
-    # ) -> Optional["ScaleType"]:
-    #     return self.event_loop.run_until_complete(
-    #         self._async_instance.get_constant(
-    #             module_name, constant_name, block_hash, reuse_block_hash
-    #         )
-    #     )
+    def query(
+        self,
+        module: str,
+        storage_function: str,
+        params: Optional[list] = None,
+        block_hash: Optional[str] = None,
+        raw_storage_key: Optional[bytes] = None,
+        subscription_handler=None,
+        reuse_block_hash: bool = False,
+    ) -> "ScaleType":
+        return self._run(
+            self._async_instance.query(
+                module,
+                storage_function,
+                params,
+                block_hash,
+                raw_storage_key,
+                subscription_handler,
+                reuse_block_hash,
+            )
+        )
+
+    def get_constant(
+        self,
+        module_name: str,
+        constant_name: str,
+        block_hash: Optional[str] = None,
+        reuse_block_hash: bool = False,
+    ) -> Optional["ScaleType"]:
+        return self._run(
+            self._async_instance.get_constant(
+                module_name, constant_name, block_hash, reuse_block_hash
+            )
+        )
+
+    def submit_extrinsic(
+        self,
+        extrinsic: GenericExtrinsic,
+        wait_for_inclusion: bool = False,
+        wait_for_finalization: bool = False,
+    ) -> "ExtrinsicReceipt":
+        return self._run(
+            self._async_instance.submit_extrinsic(
+                extrinsic, wait_for_inclusion, wait_for_finalization
+            )
+        )
+
+    def close(self):
+        return self._run(self._async_instance.close())
+
+    def create_scale_object(
+        self,
+        type_string: str,
+        data: Optional[ScaleBytes] = None,
+        block_hash: Optional[str] = None,
+        **kwargs,
+    ) -> "ScaleType":
+        return self._run(
+            self._async_instance.create_scale_object(
+                type_string, data, block_hash, **kwargs
+            )
+        )
+
+    def rpc_request(
+        self,
+        method: str,
+        params: Optional[list],
+        block_hash: Optional[str] = None,
+        reuse_block_hash: bool = False,
+    ) -> Any:
+        return self._run(
+            self._async_instance.rpc_request(
+                method, params, block_hash, reuse_block_hash
+            )
+        )
+
+    def get_block_number(self, block_hash: Optional[str] = None) -> int:
+        return self._run(self._async_instance.get_block_number(block_hash))
+
+    def create_signed_extrinsic(
+        self,
+        call: GenericCall,
+        keypair: Keypair,
+        era: Optional[dict] = None,
+        nonce: Optional[int] = None,
+        tip: int = 0,
+        tip_asset_id: Optional[int] = None,
+        signature: Optional[Union[bytes, str]] = None,
+    ) -> "GenericExtrinsic":
+        return self._run(
+            self._async_instance.create_signed_extrinsic(
+                call, keypair, era, nonce, tip, tip_asset_id, signature
+            )
+        )
+
+    def compose_call(
+        self,
+        call_module: str,
+        call_function: str,
+        call_params: Optional[dict] = None,
+        block_hash: Optional[str] = None,
+    ) -> GenericCall:
+        return self._run(
+            self._async_instance.compose_call(
+                call_module, call_function, call_params, block_hash
+            )
+        )
+
+    def get_block_hash(self, block_id: int) -> str:
+        return self._run(self._async_instance.get_block_hash(block_id))
+
+    def get_payment_info(self, call: GenericCall, keypair: Keypair) -> dict[str, Any]:
+        return self._run(self._async_instance.get_payment_info(call, keypair))
+
+    def get_chain_head(self) -> str:
+        return self._run(self._async_instance.get_chain_head())
+
+    def get_events(self, block_hash: Optional[str] = None) -> list:
+        return self._run(self._async_instance.get_events(block_hash))
+
+    def query_map(
+        self,
+        module: str,
+        storage_function: str,
+        params: Optional[list] = None,
+        block_hash: Optional[str] = None,
+        max_results: Optional[int] = None,
+        start_key: Optional[str] = None,
+        page_size: int = 100,
+        ignore_decoding_errors: bool = False,
+        reuse_block_hash: bool = False,
+    ) -> "QueryMapResult":
+        return self._run(
+            self._async_instance.query_map(
+                module,
+                storage_function,
+                params,
+                block_hash,
+                max_results,
+                start_key,
+                page_size,
+                ignore_decoding_errors,
+                reuse_block_hash,
+            )
+        )
+
+    def query_multi(
+        self, storage_keys: list[StorageKey], block_hash: Optional[str] = None
+    ) -> list:
+        return self._run(self._async_instance.query_multi(storage_keys, block_hash))
+
+    def get_block(
+        self,
+        block_hash: Optional[str] = None,
+        block_number: Optional[int] = None,
+        ignore_decoding_errors: bool = False,
+        include_author: bool = False,
+        finalized_only: bool = False,
+    ) -> Optional[dict]:
+        return self._run(
+            self._async_instance.get_block(
+                block_hash,
+                block_number,
+                ignore_decoding_errors,
+                include_author,
+                finalized_only,
+            )
+        )
+
+    def create_storage_key(
+        self,
+        pallet: str,
+        storage_function: str,
+        params: Optional[list] = None,
+        block_hash: str = None,
+    ) -> StorageKey:
+        return self._run(
+            self._async_instance.create_storage_key(
+                pallet, storage_function, params, block_hash
+            )
+        )
