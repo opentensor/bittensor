c150eeb1e41a3ff93b74af5807158a398f9110da
Cameron Fairchild <cameron@opentensor.dev>
2025-01-22 13:46:01 -0500

    add test for fixed float and comment

--- bittensor/utils/balance.py
+++ bittensor/utils/balance.py
@@ -287,6 +287,13 @@ class Balance:
 
 
 class FixedPoint(TypedDict):
+    """
+    Represents a fixed point ``U64F64`` number.
+    Where ``bits`` is a U128 representation of the fixed point number.
+
+    This matches the type of the Alpha shares.
+    """
+
     bits: int
 
 
