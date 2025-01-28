6734673d0958a50a232a042dfbfad6e75af36c07
Roman <roman@opentensor.dev>
2025-01-28 14:46:22 -0800

    update `bittensor.core.chain_data` sub-package

--- bittensor/core/chain_data/__init__.py
+++ bittensor/core/chain_data/__init__.py
@@ -6,9 +6,11 @@ information, SCALE encoding/decoding, and custom RPC type registry.
 from scalecodec.types import GenericCall
 
 from .axon_info import AxonInfo
+from .chain_identity import ChainIdentity
 from .delegate_info import DelegateInfo
 from .delegate_info_lite import DelegateInfoLite
 from .ip_info import IPInfo
+from .metagraph_info import MetagraphInfo
 from .neuron_info import NeuronInfo
 from .neuron_info_lite import NeuronInfoLite
 from .neuron_certificate import NeuronCertificate
@@ -17,8 +19,32 @@ from .proposal_vote_data import ProposalVoteData
 from .scheduled_coldkey_swap_info import ScheduledColdkeySwapInfo
 from .stake_info import StakeInfo
 from .subnet_hyperparameters import SubnetHyperparameters
+from .subnet_identity import SubnetIdentity
 from .subnet_info import SubnetInfo
 from .weight_commit_info import WeightCommitInfo
 from .utils import custom_rpc_type_registry, decode_account_id, process_stake_data
 
 ProposalCallData = GenericCall
+
+__all__ = [
+    AxonInfo,
+    ChainIdentity,
+    DelegateInfo,
+    DelegateInfoLite,
+    IPInfo,
+    MetagraphInfo,
+    NeuronInfo,
+    NeuronInfoLite,
+    NeuronCertificate,
+    PrometheusInfo,
+    ProposalVoteData,
+    ScheduledColdkeySwapInfo,
+    StakeInfo,
+    SubnetHyperparameters,
+    SubnetInfo,
+    SubnetIdentity,
+    custom_rpc_type_registry,
+    decode_account_id,
+    process_stake_data,
+    ProposalCallData,
+]
