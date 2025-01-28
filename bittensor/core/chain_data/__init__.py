af9e7a9dc6c5cab65f2d3b479b4ce508b957bb01
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-01-28 13:33:05 -0800

    Updates chain_data

--- bittensor/core/chain_data/__init__.py
+++ bittensor/core/chain_data/__init__.py
@@ -15,9 +15,12 @@ from .neuron_certificate import NeuronCertificate
 from .prometheus_info import PrometheusInfo
 from .proposal_vote_data import ProposalVoteData
 from .scheduled_coldkey_swap_info import ScheduledColdkeySwapInfo
+from .subnet_state import SubnetState
 from .stake_info import StakeInfo
 from .subnet_hyperparameters import SubnetHyperparameters
 from .subnet_info import SubnetInfo
+from .dynamic_info import DynamicInfo
+from .subnet_identity import SubnetIdentity
 from .weight_commit_info import WeightCommitInfo
 from .utils import custom_rpc_type_registry, decode_account_id, process_stake_data
 
