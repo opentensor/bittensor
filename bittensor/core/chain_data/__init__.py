61f0ec551e40e79571c4d8e4a5c2b10a31e48d45
Roman <roman@opentensor.dev>
2025-01-28 14:58:54 -0800

    update `bittensor/core/chain_data/__init__.py`

--- bittensor/core/chain_data/__init__.py
+++ bittensor/core/chain_data/__init__.py
@@ -9,6 +9,7 @@ from .axon_info import AxonInfo
 from .chain_identity import ChainIdentity
 from .delegate_info import DelegateInfo
 from .delegate_info_lite import DelegateInfoLite
+from .dynamic_info import DynamicInfo
 from .ip_info import IPInfo
 from .metagraph_info import MetagraphInfo
 from .neuron_info import NeuronInfo
@@ -17,13 +18,11 @@ from .neuron_certificate import NeuronCertificate
 from .prometheus_info import PrometheusInfo
 from .proposal_vote_data import ProposalVoteData
 from .scheduled_coldkey_swap_info import ScheduledColdkeySwapInfo
-from .subnet_state import SubnetState
 from .stake_info import StakeInfo
 from .subnet_hyperparameters import SubnetHyperparameters
 from .subnet_identity import SubnetIdentity
 from .subnet_info import SubnetInfo
-from .dynamic_info import DynamicInfo
-from .subnet_identity import SubnetIdentity
+from .subnet_state import SubnetState
 from .weight_commit_info import WeightCommitInfo
 from .utils import custom_rpc_type_registry, decode_account_id, process_stake_data
 
@@ -34,20 +33,23 @@ __all__ = [
     ChainIdentity,
     DelegateInfo,
     DelegateInfoLite,
+    DynamicInfo,
     IPInfo,
     MetagraphInfo,
     NeuronInfo,
     NeuronInfoLite,
     NeuronCertificate,
     PrometheusInfo,
+    ProposalCallData,
     ProposalVoteData,
     ScheduledColdkeySwapInfo,
     StakeInfo,
     SubnetHyperparameters,
-    SubnetInfo,
     SubnetIdentity,
+    SubnetInfo,
+    SubnetState,
+    WeightCommitInfo,
     custom_rpc_type_registry,
     decode_account_id,
     process_stake_data,
-    ProposalCallData,
 ]
