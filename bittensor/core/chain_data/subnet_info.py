d4b067eb37b01fb77f3b2ad744c1869c1a4d42a4
Tomasz Żyźniewski <tomasz.zyzniewski@reef.pl>
2025-01-29 18:28:31 +0100

    feat: use bt_decode

--- bittensor/core/chain_data/subnet_info.py
+++ bittensor/core/chain_data/subnet_info.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
+from typing import Any
 
 import bt_decode
 
+from bittensor.core.chain_data.info_base import InfoBase
 from bittensor.core.chain_data.utils import decode_account_id
 from bittensor.utils import u16_normalized_float
 from bittensor.utils.balance import Balance
 
 
 @dataclass
-class SubnetInfo:
+class SubnetInfo(InfoBase):
     """Dataclass for subnet info."""
 
     netuid: int
@@ -31,33 +33,32 @@ class SubnetInfo:
     owner_ss58: str
 
     @classmethod
+    def _fix_decoded(cls, decoded: Any) -> "SubnetInfo":
+        return SubnetInfo(
+            netuid=decoded.netuid,
+            rho=decoded.rho,
+            kappa=decoded.kappa,
+            difficulty=decoded.difficulty,
+            immunity_period=decoded.immunity_period,
+            max_allowed_validators=decoded.max_allowed_validators,
+            min_allowed_weights=decoded.min_allowed_weights,
+            max_weight_limit=decoded.max_weights_limit,
+            scaling_law_power=decoded.scaling_law_power,
+            subnetwork_n=decoded.subnetwork_n,
+            max_n=decoded.max_allowed_uids,
+            blocks_since_epoch=decoded.blocks_since_last_step,
+            tempo=decoded.tempo,
+            modality=decoded.network_modality,
+            connection_requirements={
+                str(int(netuid)): u16_normalized_float(int(req))
+                for (netuid, req) in decoded.network_connect
+            },
+            emission_value=decoded.emission_values,
+            burn=Balance.from_rao(decoded.burn),
+            owner_ss58=decode_account_id(decoded.owner),
+        )
+
+    @classmethod
     def list_from_vec_u8(cls, vec_u8: bytes) -> list["SubnetInfo"]:
         decoded = bt_decode.SubnetInfo.decode_vec_option(vec_u8)
-        result = []
-        for d in decoded:
-            result.append(
-                SubnetInfo(
-                    netuid=d.netuid,
-                    rho=d.rho,
-                    kappa=d.kappa,
-                    difficulty=d.difficulty,
-                    immunity_period=d.immunity_period,
-                    max_allowed_validators=d.max_allowed_validators,
-                    min_allowed_weights=d.min_allowed_weights,
-                    max_weight_limit=d.max_weights_limit,
-                    scaling_law_power=d.scaling_law_power,
-                    subnetwork_n=d.subnetwork_n,
-                    max_n=d.max_allowed_uids,
-                    blocks_since_epoch=d.blocks_since_last_step,
-                    tempo=d.tempo,
-                    modality=d.network_modality,
-                    connection_requirements={
-                        str(int(netuid)): u16_normalized_float(int(req))
-                        for (netuid, req) in d.network_connect
-                    },
-                    emission_value=d.emission_values,
-                    burn=Balance.from_rao(d.burn),
-                    owner_ss58=decode_account_id(d.owner),
-                )
-            )
-        return result
+        return [cls._fix_decoded(d) for d in decoded]
