17bef1b7cefac4eccf2056ea1339dd26959f6bca
Roman <roman@opentensor.dev>
2025-02-04 16:21:38 -0800

    type annotations, docstrings, logging, None results

--- bittensor/core/async_subtensor.py
+++ bittensor/core/async_subtensor.py
@@ -1304,7 +1304,7 @@ class AsyncSubtensor(SubtensorMixin):
         block: Optional[int] = None,
         block_hash: Optional[str] = None,
         reuse_block: bool = False,
-    ) -> MetagraphInfo:
+    ) -> Optional[MetagraphInfo]:
         """
         Retrieves the MetagraphInfo dataclass from the node for a single subnet (netuid)
 
@@ -1329,7 +1329,10 @@ class AsyncSubtensor(SubtensorMixin):
             params=[netuid],
             block_hash=block_hash,
         )
-        return MetagraphInfo.from_dict(query.decode())
+        if query.value is None:
+            logging.error(f"Subnet {netuid} does not exist.")
+            return None
+        return MetagraphInfo.from_dict(query.value)
 
     async def get_all_metagraphs_info(
         self,
