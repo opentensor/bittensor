0551deba39d0ca2cb5db45888bea4937cb4c4295
teenager-ETH <sunsets01.bombast@icloud.com>
2025-02-08 23:15:29 +0100

    Update registration.py

--- bittensor/core/extrinsics/registration.py
+++ bittensor/core/extrinsics/registration.py
@@ -119,7 +119,7 @@ def register_extrinsic(
 
     Returns:
         success (bool):
-            Flag is ``true`` if extrinsic was finalized or uncluded in the block. If we did not wait for finalization / inclusion, the response is ``true``.
+            Flag is ``true`` if extrinsic was finalized or included in the block. If we did not wait for finalization / inclusion, the response is ``true``.
     """
     if not subtensor.subnet_exists(netuid):
         logging.error(
