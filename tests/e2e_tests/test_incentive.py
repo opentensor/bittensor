2ee6f7a70e80371afdaf744a5368098f72ee7464
Tomasz Żyźniewski <tomasz.zyzniewski@reef.pl>
2025-03-11 12:59:54 +0100

    Revert "Updates test_incentive"
    This reverts commit 74817179d10c82a1b7178d02e9fa969a3f1d3868.

--- tests/e2e_tests/test_incentive.py
+++ tests/e2e_tests/test_incentive.py
@@ -2,17 +2,13 @@ import asyncio
 
 import pytest
 
-from bittensor import Balance
-
 from tests.e2e_tests.utils.chain_interactions import (
     sudo_set_hyperparameter_values,
     wait_epoch,
-    sudo_set_admin_utils,
 )
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("local_chain", [False], indirect=True)
 async def test_incentive(local_chain, subtensor, templates, alice_wallet, bob_wallet):
     """
     Test the incentive mechanism and interaction of miners/validators
@@ -35,21 +31,6 @@ async def test_incentive(local_chain, subtensor, templates, alice_wallet, bob_wa
     # Verify subnet <netuid> created successfully
     assert subtensor.subnet_exists(netuid), "Subnet wasn't created successfully"
 
-    # Change tempo to 10
-    tempo_set = 10
-    assert (
-        sudo_set_admin_utils(
-            local_chain,
-            alice_wallet,
-            call_function="sudo_set_tempo",
-            call_params={"netuid": netuid, "tempo": tempo_set},
-            return_error_message=True,
-        )[0]
-        is True
-    )
-    tempo = subtensor.get_subnet_hyperparameters(netuid=netuid).tempo
-    assert tempo_set == tempo
-
     # Register Bob as a neuron on the subnet
     assert subtensor.burned_register(
         bob_wallet, netuid
@@ -59,27 +40,6 @@ async def test_incentive(local_chain, subtensor, templates, alice_wallet, bob_wa
     assert (
         len(subtensor.neurons(netuid=netuid)) == 2
     ), "Alice & Bob not registered in the subnet"
-
-    # Add stake for Alice
-    assert subtensor.add_stake(
-        alice_wallet,
-        netuid=netuid,
-        amount=Balance.from_tao(1_000),
-        wait_for_inclusion=True,
-        wait_for_finalization=True,
-    ), "Failed to add stake for Alice"
-
-    # Wait for the first epoch to pass
-    await wait_epoch(subtensor, netuid)
-
-    # Add further stake so validator permit is activated
-    assert subtensor.add_stake(
-        alice_wallet,
-        netuid=netuid,
-        amount=Balance.from_tao(1_000),
-        wait_for_inclusion=True,
-        wait_for_finalization=True,
-    ), "Failed to add stake for Alice"
 
     # Get latest metagraph
     metagraph = subtensor.metagraph(netuid)
@@ -110,11 +70,11 @@ async def test_incentive(local_chain, subtensor, templates, alice_wallet, bob_wa
 
     async with templates.miner(bob_wallet, netuid):
         async with templates.validator(alice_wallet, netuid):
-            # Wait for the Validator to process and set_weights
+            # wait for the Validator to process and set_weights
             await asyncio.sleep(5)
 
             # Wait few epochs
-            await wait_epoch(subtensor, netuid, times=2)
+            await wait_epoch(subtensor, netuid, times=4)
 
             # Refresh metagraph
             metagraph = subtensor.metagraph(netuid)
