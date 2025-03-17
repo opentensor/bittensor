f7c22739c11cc60581e8f88141363d88ddb795a9
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-03-17 15:26:10 -0700

    Improves all apis

--- tests/e2e_tests/test_stake_fee.py
+++ tests/e2e_tests/test_stake_fee.py
@@ -10,161 +10,108 @@ async def test_stake_fee_api(local_chain, subtensor, alice_wallet, bob_wallet):
 
     Steps:
         1. Register a subnet through Alice
-        2. Set up test parameters
-        3. Test stake fees for different scenarios:
+        2. Test stake fees for:
             - Adding new stake
             - Removing stake
-            - Moving between subnets
-            - Moving between hotkeys
-            - Moving between coldkeys
-            - Swapping between subnets
+            - Moving stake between hotkeys/subnets/coldkeys
     """
-    MIN_STAKE_FEE = Balance.from_rao(50_000)
+
     netuid = 2
     root_netuid = 0
-    stake_amount = Balance.from_tao(100).rao  # 100 TAO
+    stake_amount = Balance.from_tao(100)  # 100 TAO
+    min_stake_fee = Balance.from_rao(50_000)
 
     # Register subnet as Alice
     assert subtensor.register_subnet(alice_wallet), "Unable to register the subnet"
     assert subtensor.subnet_exists(netuid), "Subnet wasn't created successfully"
 
-    # Add_stake (new stake)
-    stake_fee_0 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=None,
-        origin_netuid=None,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=alice_wallet.hotkey.ss58_address,
-        destination_netuid=netuid,
-        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
+    # Test add_stake fee
+    stake_fee_0 = subtensor.get_stake_add_fee(
         amount=stake_amount,
+        netuid=netuid,
+        coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
+        hotkey_ss58=alice_wallet.hotkey.ss58_address,
     )
     assert isinstance(stake_fee_0, Balance), "Stake fee should be a Balance object"
     assert (
-        stake_fee_0 >= MIN_STAKE_FEE
+        stake_fee_0 >= min_stake_fee
     ), "Stake fee should be greater than the minimum stake fee"
 
-    # Remove stake
-    stake_fee_1 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        origin_netuid=root_netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=None,
-        destination_netuid=None,
-        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
+    # Test unstake fee
+    stake_fee_1 = subtensor.get_unstake_fee(
         amount=stake_amount,
+        netuid=root_netuid,
+        coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
+        hotkey_ss58=bob_wallet.hotkey.ss58_address,
     )
     assert isinstance(stake_fee_1, Balance), "Stake fee should be a Balance object"
     assert (
-        stake_fee_1 >= MIN_STAKE_FEE
+        stake_fee_1 >= min_stake_fee
     ), "Stake fee should be greater than the minimum stake fee"
 
-    # Move from root to non-root
-    stake_fee_2 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=alice_wallet.hotkey.ss58_address,
-        origin_netuid=root_netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=alice_wallet.hotkey.ss58_address,
-        destination_netuid=netuid,
-        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        amount=stake_amount,
-    )
-    assert isinstance(stake_fee_2, Balance), "Stake fee should be a Balance object"
-    assert (
-        stake_fee_2 >= MIN_STAKE_FEE
-    ), "Stake fee should be greater than the minimum stake fee"
+    # Test various stake movement scenarios
+    movement_scenarios = [
+        # Move from root to non-root
+        {
+            "origin_netuid": root_netuid,
+            "origin_hotkey": alice_wallet.hotkey.ss58_address,
+            "origin_coldkey": alice_wallet.coldkeypub.ss58_address,
+            "dest_netuid": netuid,
+            "dest_hotkey": alice_wallet.hotkey.ss58_address,
+            "dest_coldkey": alice_wallet.coldkeypub.ss58_address,
+        },
+        # Move between hotkeys on root
+        {
+            "origin_netuid": root_netuid,
+            "origin_hotkey": alice_wallet.hotkey.ss58_address,
+            "origin_coldkey": alice_wallet.coldkeypub.ss58_address,
+            "dest_netuid": root_netuid,
+            "dest_hotkey": bob_wallet.hotkey.ss58_address,
+            "dest_coldkey": alice_wallet.coldkeypub.ss58_address,
+        },
+        # Move between coldkeys
+        {
+            "origin_netuid": root_netuid,
+            "origin_hotkey": bob_wallet.hotkey.ss58_address,
+            "origin_coldkey": alice_wallet.coldkeypub.ss58_address,
+            "dest_netuid": root_netuid,
+            "dest_hotkey": bob_wallet.hotkey.ss58_address,
+            "dest_coldkey": bob_wallet.coldkeypub.ss58_address,
+        },
+    ]
 
-    # Move between hotkeys on root
-    stake_fee_3 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=alice_wallet.hotkey.ss58_address,
-        origin_netuid=root_netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        destination_netuid=root_netuid,
-        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        amount=stake_amount,
-    )
-    assert isinstance(stake_fee_3, Balance), "Stake fee should be a Balance object"
-    assert (
-        stake_fee_3 >= MIN_STAKE_FEE
-    ), "Stake fee should be greater than the minimum stake fee"
+    for scenario in movement_scenarios:
+        stake_fee = subtensor.get_stake_movement_fee(
+            amount=stake_amount,
+            origin_netuid=scenario["origin_netuid"],
+            origin_hotkey_ss58=scenario["origin_hotkey"],
+            origin_coldkey_ss58=scenario["origin_coldkey"],
+            destination_netuid=scenario["dest_netuid"],
+            destination_hotkey_ss58=scenario["dest_hotkey"],
+            destination_coldkey_ss58=scenario["dest_coldkey"],
+        )
+        assert isinstance(stake_fee, Balance), "Stake fee should be a Balance object"
+        assert (
+            stake_fee >= min_stake_fee
+        ), "Stake fee should be greater than the minimum stake fee"
 
-    # Move between coldkeys on root
-    stake_fee_4 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        origin_netuid=root_netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        destination_netuid=root_netuid,
-        destination_coldkey_ss58=bob_wallet.coldkeypub.ss58_address,
-        amount=stake_amount,
-    )
-    assert isinstance(stake_fee_4, Balance), "Stake fee should be a Balance object"
-    assert (
-        stake_fee_4 >= MIN_STAKE_FEE
-    ), "Stake fee should be greater than the minimum stake fee"
-
-    # Swap from non-root to root
-    stake_fee_5 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        origin_netuid=netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        destination_netuid=root_netuid,
-        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        amount=stake_amount,
-    )
-    assert isinstance(stake_fee_5, Balance), "Stake fee should be a Balance object"
-    assert (
-        stake_fee_5 >= MIN_STAKE_FEE
-    ), "Stake fee should be greater than the minimum stake fee"
-
-    # Move between hotkeys on non-root
-    stake_fee_6 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        origin_netuid=netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=alice_wallet.hotkey.ss58_address,
-        destination_netuid=netuid,
-        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        amount=stake_amount,
-    )
-    assert isinstance(stake_fee_6, Balance), "Stake fee should be a Balance object"
-    assert (
-        stake_fee_6 >= MIN_STAKE_FEE
-    ), "Stake fee should be greater than the minimum stake fee"
-
-    # Move between coldkeys on non-root
-    stake_fee_7 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        origin_netuid=netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        destination_netuid=netuid,
-        destination_coldkey_ss58=bob_wallet.coldkeypub.ss58_address,
-        amount=stake_amount,
-    )
-    assert isinstance(stake_fee_7, Balance), "Stake fee should be a Balance object"
-    assert (
-        stake_fee_7 >= MIN_STAKE_FEE
-    ), "Stake fee should be greater than the minimum stake fee"
-
-    # Swap from non-root to non-root (between subnets)
+    # Test cross-subnet movement
     netuid2 = 3
     assert subtensor.register_subnet(
         alice_wallet
     ), "Unable to register the second subnet"
     assert subtensor.subnet_exists(netuid2), "Second subnet wasn't created successfully"
 
-    stake_fee_8 = subtensor.get_stake_fee(
-        origin_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        origin_netuid=netuid,
-        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
-        destination_hotkey_ss58=bob_wallet.hotkey.ss58_address,
-        destination_netuid=netuid2,
-        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
+    stake_fee = subtensor.get_stake_movement_fee(
         amount=stake_amount,
+        origin_netuid=netuid,
+        origin_hotkey_ss58=bob_wallet.hotkey.ss58_address,
+        origin_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
+        destination_netuid=netuid2,
+        destination_hotkey_ss58=bob_wallet.hotkey.ss58_address,
+        destination_coldkey_ss58=alice_wallet.coldkeypub.ss58_address,
     )
-    assert isinstance(stake_fee_8, Balance), "Stake fee should be a Balance object"
+    assert isinstance(stake_fee, Balance), "Stake fee should be a Balance object"
     assert (
-        stake_fee_8 >= MIN_STAKE_FEE
+        stake_fee >= min_stake_fee
     ), "Stake fee should be greater than the minimum stake fee"
