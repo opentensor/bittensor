59403312c3e99e0c8e9320f197c43faa711ef49f
Roman <roman@opentensor.dev>
2025-04-04 17:33:21 -0700

    try to fix flaky test, wait epoch + 1 block as next epoch fist one

--- tests/e2e_tests/utils/chain_interactions.py
+++ tests/e2e_tests/utils/chain_interactions.py
@@ -110,7 +110,7 @@ def next_tempo(current_block: int, tempo: int, netuid: int) -> int:
     interval = tempo + 1
     last_epoch = current_block - 1 - (current_block + netuid + 1) % interval
     next_tempo_ = last_epoch + interval
-    return next_tempo_
+    return next_tempo_ + 1
 
 
 async def wait_interval(
