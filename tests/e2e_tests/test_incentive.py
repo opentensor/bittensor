0d74ba4e9c7a0a0b424bf2d7ef62dde05f6fa0cf
Roman <roman@opentensor.dev>
2025-04-09 17:54:27 -0700

    add wait_for to miner start ,wrapp tests result info while

--- tests/e2e_tests/test_incentive.py
+++ tests/e2e_tests/test_incentive.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 import pytest
 
+import bittensor
 from bittensor.utils.btlogging import logging
 from tests.e2e_tests.utils.chain_interactions import (
     root_set_subtensor_hyperparameter_values,
@@ -110,66 +111,73 @@ async def test_incentive(local_chain, subtensor, templates, alice_wallet, bob_wa
     assert error is None
     assert status is True
 
-    async with templates.miner(bob_wallet, netuid):
-        subtensor.wait_for_block(subtensor.block + 4)
+    async with templates.miner(bob_wallet, netuid) as miner:
+        logging.console.info(f"Waiting for miner started.")
+        await asyncio.wait_for(miner.started.wait(), 120)
 
         async with templates.validator(alice_wallet, netuid) as validator:
-            subtensor.wait_for_block(subtensor.block + 4)
-
             # wait for the Validator to process and set_weights
-            logging.console.info(f"Waiting for validator to set weights: {validator}")
+            logging.console.info(f"Waiting for validator to set weights.")
             await asyncio.wait_for(validator.set_weights.wait(), 120)
-            logging.console.info(f"Validator got weights: {validator}")
 
     # Sometimes the network does not have time to release data, and it requires several additional blocks (subtensor issue)
     # Call get_metagraph_info since if faster and chipper
-    extra_time = time.time()
-    while subtensor.get_metagraph_info(netuid).incentives[0] == 0:
-        logging.console.info(
-            f"Additional fast block to wait chain data updated: {subtensor.block}"
-        )
-        if time.time() - extra_time > 120:
-            pytest.skip(
-                "Skipping due to FLAKY TEST. Check the same tests with another Python version or run again."
-            )
+    # extra_time = time.time()
+    # while subtensor.get_metagraph_info(netuid).incentives[0] == 0:
+    #     logging.console.info(
+    #         f"Additional fast block to wait chain data updated: {subtensor.block}"
+    #     )
+    #     if time.time() - extra_time > 120:
+    #         pytest.skip(
+    #             "Skipping due to FLAKY TEST. Check the same tests with another Python version or run again."
+    #         )
+    #
+    #     await asyncio.sleep(0.25)
 
-        await asyncio.sleep(0.25)
+    while True:
+        logging.console.info("Current block: ", subtensor.block)
 
-    # Refresh metagraph
-    metagraph = subtensor.metagraph(netuid)
+        try:
+            # Refresh metagraph
+            neurons = subtensor.neurons(netuid)
 
-    # Get current emissions and validate that Alice has gotten tao
-    alice_neuron = metagraph.neurons[0]
+            # Get current emissions and validate that Alice has gotten tao
+            alice_neuron = neurons[0]
 
-    assert alice_neuron.validator_permit is True
-    assert alice_neuron.dividends == 1.0
-    assert alice_neuron.stake.tao > 0
-    assert alice_neuron.validator_trust > 0.99
-    assert alice_neuron.incentive < 0.5
-    assert alice_neuron.consensus < 0.5
-    assert alice_neuron.rank < 0.5
+            assert alice_neuron.validator_permit is True
+            assert alice_neuron.dividends == 1.0
+            assert alice_neuron.stake.tao > 0
+            assert alice_neuron.validator_trust > 0.99
+            assert alice_neuron.incentive < 0.5
+            assert alice_neuron.consensus < 0.5
+            assert alice_neuron.rank < 0.5
 
-    bob_neuron = metagraph.neurons[1]
+            bob_neuron = neurons[1]
 
-    assert bob_neuron.incentive > 0.5
-    assert bob_neuron.consensus > 0.5
-    assert bob_neuron.rank > 0.5
-    assert bob_neuron.trust == 1
+            assert bob_neuron.incentive > 0.5
+            assert bob_neuron.consensus > 0.5
+            assert bob_neuron.rank > 0.5
+            assert bob_neuron.trust == 1
 
-    bonds = subtensor.bonds(netuid)
+            bonds = subtensor.bonds(netuid)
 
-    assert bonds == [
-        (
-            0,
-            [
-                (0, 65535),
-                (1, 65535),
-            ],
-        ),
-        (
-            1,
-            [],
-        ),
-    ]
+            assert bonds == [
+                (
+                    0,
+                    [
+                        (0, 65535),
+                        (1, 65535),
+                    ],
+                ),
+                (
+                    1,
+                    [],
+                ),
+            ]
 
-    print("✅ Passed test_incentive")
+            logging.console.info("✅ Passed test_incentive")
+            break
+
+        except Exception:
+            logging.console.warning(f"Waiting for incentive to be set.")
+            await asyncio.sleep(0.25)
