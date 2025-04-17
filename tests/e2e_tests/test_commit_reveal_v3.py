4a332e4f6aff74c158413d819fe5bc2f53858527
Roman Chkhaidze <basfroman@gmail.com>
2025-04-16 18:57:04 -0700

    flaky

--- tests/e2e_tests/test_commit_reveal_v3.py
+++ tests/e2e_tests/test_commit_reveal_v3.py
@@ -149,7 +149,7 @@ async def test_commit_and_reveal_weights_cr3(local_chain, subtensor, alice_walle
     )
 
     # Ensure the expected drand round is well in the future
-    assert expected_reveal_round >= latest_drand_round, (
+    assert expected_reveal_round >= latest_drand_round + 1, (
         "Revealed drand pulse is older than the drand pulse right after setting weights"
     )
 
