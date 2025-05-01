7ffcfccf2ac7cf34e75e6675fd662ead44d93b1e
Roman Chkhaidze <basfroman@gmail.com>
2025-04-30 23:59:46 -0700

    update `reveal_weights_extrinsic` for both subtensors

--- bittensor/core/extrinsics/commit_weights.py
+++ bittensor/core/extrinsics/commit_weights.py
@@ -124,6 +124,7 @@ def _do_reveal_weights(
     version_key: int,
     wait_for_inclusion: bool = False,
     wait_for_finalization: bool = False,
+    period: Optional[int] = None,
 ) -> tuple[bool, str]:
     """
     Internal method to send a transaction to the Bittensor blockchain, revealing the weights for a specific subnet.
@@ -139,6 +140,9 @@ def _do_reveal_weights(
         version_key (int): Version key for compatibility with the network.
         wait_for_inclusion (bool): Waits for the transaction to be included in a block.
         wait_for_finalization (bool): Waits for the transaction to be finalized on the blockchain.
+        period (int): The number of blocks during which the transaction will remain valid after it's submitted. If
+            the transaction is not included in a block within that number of blocks, it will expire and be rejected.
+            You can think of it as an expiration date for the transaction.
 
     Returns:
         tuple[bool, str]:
@@ -166,6 +170,7 @@ def _do_reveal_weights(
         wait_for_inclusion,
         wait_for_finalization,
         use_nonce=True,
+        period=period,
         sign_with="hotkey",
         nonce_key="hotkey",
     )
@@ -181,6 +186,7 @@ def reveal_weights_extrinsic(
     version_key: int,
     wait_for_inclusion: bool = False,
     wait_for_finalization: bool = False,
+    period: Optional[int] = None,
 ) -> tuple[bool, str]:
     """
     Reveals the weights for a specific subnet on the Bittensor blockchain using the provided wallet.
@@ -196,6 +202,9 @@ def reveal_weights_extrinsic(
         version_key (int): Version key for compatibility with the network.
         wait_for_inclusion (bool): Waits for the transaction to be included in a block.
         wait_for_finalization (bool): Waits for the transaction to be finalized on the blockchain.
+        period (int): The number of blocks during which the transaction will remain valid after it's submitted. If
+            the transaction is not included in a block within that number of blocks, it will expire and be rejected.
+            You can think of it as an expiration date for the transaction.
 
     Returns:
         tuple[bool, str]:
@@ -216,6 +225,7 @@ def reveal_weights_extrinsic(
         version_key=version_key,
         wait_for_inclusion=wait_for_inclusion,
         wait_for_finalization=wait_for_finalization,
+        period=period,
     )
 
     if success:
