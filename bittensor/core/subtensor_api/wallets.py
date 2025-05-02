f46a2491531990828d6df8cc864c3497a5126689
Roman <roman@opentensor.dev>
2025-05-02 13:29:15 -0700

    to be consistent

--- bittensor/core/subtensor_api/wallets.py
+++ bittensor/core/subtensor_api/wallets.py
@@ -3,12 +3,14 @@ from bittensor.core.subtensor import Subtensor as _Subtensor
 from bittensor.core.async_subtensor import AsyncSubtensor as _AsyncSubtensor
 
 
-class Wallet:
+class Wallets:
     """Class for managing coldkey, hotkey, wallet operations."""
 
     def __init__(self, subtensor: Union["_Subtensor", "_AsyncSubtensor"]):
         self.does_hotkey_exist = subtensor.does_hotkey_exist
-        self.filter_netuids_by_registered_hotkeys = subtensor.filter_netuids_by_registered_hotkeys
+        self.filter_netuids_by_registered_hotkeys = (
+            subtensor.filter_netuids_by_registered_hotkeys
+        )
         self.is_hotkey_registered_any = subtensor.is_hotkey_registered_any
         self.is_hotkey_registered = subtensor.is_hotkey_registered
         self.is_hotkey_delegate = subtensor.is_hotkey_delegate
@@ -27,10 +29,11 @@ class Wallet:
         self.get_stake = subtensor.get_stake
         self.get_stake_add_fee = subtensor.get_stake_add_fee
         self.get_stake_for_coldkey = subtensor.get_stake_for_coldkey
-        self.get_stake_for_coldkey_and_hotkey = subtensor.get_stake_for_coldkey_and_hotkey
+        self.get_stake_for_coldkey_and_hotkey = (
+            subtensor.get_stake_for_coldkey_and_hotkey
+        )
         self.get_stake_for_hotkey = subtensor.get_stake_for_hotkey
         self.get_stake_info_for_coldkey = subtensor.get_stake_info_for_coldkey
         self.get_stake_movement_fee = subtensor.get_stake_movement_fee
         self.get_transfer_fee = subtensor.get_transfer_fee
         self.get_unstake_fee = subtensor.get_unstake_fee
-
