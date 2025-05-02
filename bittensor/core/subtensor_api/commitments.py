16401ccf2f152350931dc5eae96dedb95f1b8ad2
Roman Chkhaidze <basfroman@gmail.com>
2025-05-02 13:28:52 -0700

    ruff

--- bittensor/core/subtensor_api/commitments.py
+++ bittensor/core/subtensor_api/commitments.py
@@ -13,6 +13,8 @@ class Commitments:
         self.get_commitment = subtensor.get_commitment
         self.get_current_weight_commit_info = subtensor.get_current_weight_commit_info
         self.get_revealed_commitment = subtensor.get_revealed_commitment
-        self.get_revealed_commitment_by_hotkey = subtensor.get_revealed_commitment_by_hotkey
+        self.get_revealed_commitment_by_hotkey = (
+            subtensor.get_revealed_commitment_by_hotkey
+        )
         self.set_commitment = subtensor.set_commitment
         self.set_reveal_commitment = subtensor.set_reveal_commitment
