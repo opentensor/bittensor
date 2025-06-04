f3ec701e173fcdbff38cb671546428151cc7e1cf
Roman <roman@opentensor.dev>
2025-06-04 12:46:32 -0700

    fix tests assertions

--- tests/unit_tests/test_subtensor.py
+++ tests/unit_tests/test_subtensor.py
@@ -3490,7 +3490,7 @@ def test_get_metagraph_info_invalid_field_indices(subtensor, wrong_fields):
     """Test get_metagraph_info raises ValueError on invalid field_indices."""
     with pytest.raises(
         ValueError,
-        match="`field_indices` must be a list of SelectiveMetagraphIndex items.",
+        match="`field_indices` must be a list of SelectiveMetagraphIndex enums or ints.",
     ):
         subtensor.get_metagraph_info(netuid=1, field_indices=wrong_fields)
 
