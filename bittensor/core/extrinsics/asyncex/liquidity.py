b3579243478eea10693aa777addbff2e2b96861a
Roman Chkhaidze <basfroman@gmail.com>
2025-06-30 16:39:18 -0700

    add `modify_liquidity_extrinsic`

--- bittensor/core/extrinsics/asyncex/liquidity.py
+++ bittensor/core/extrinsics/asyncex/liquidity.py
@@ -74,6 +74,63 @@ async def add_liquidity_extrinsic(
     )
 
 
+async def modify_liquidity_extrinsic(
+    subtensor: "AsyncSubtensor",
+    wallet: "Wallet",
+    netuid: int,
+    position_id: int,
+    liquidity_delta: Balance,
+    wait_for_inclusion: bool = True,
+    wait_for_finalization: bool = False,
+    period: Optional[int] = None,
+) -> tuple[bool, str]:
+    """Modifies liquidity in liquidity position by adding or removing liquidity from it.
+
+    Arguments:
+        subtensor: The Subtensor client instance used for blockchain interaction.
+        wallet: The wallet used to sign the extrinsic (must be unlocked).
+        netuid: The UID of the target subnet for which the call is being initiated.
+        position_id: The id of the position record in the pool.
+        liquidity_delta: The amount of liquidity to be added or removed (add if positive or remove if negative).
+        wait_for_inclusion: Whether to wait for the extrinsic to be included in a block. Defaults to True.
+        wait_for_finalization: Whether to wait for finalization of the extrinsic. Defaults to False.
+        period: The number of blocks during which the transaction will remain valid after it's submitted. If
+            the transaction is not included in a block within that number of blocks, it will expire and be rejected.
+            You can think of it as an expiration date for the transaction.
+
+    Returns:
+        Tuple[bool, str]:
+            - True and a success message if the extrinsic is successfully submitted or processed.
+            - False and an error message if the submission fails or the wallet cannot be unlocked.
+
+    Note: Modifying is allowed even when user liquidity is enabled in specified subnet.
+        Call `toggle_user_liquidity_extrinsic` to enable/disable user liquidity.
+    """
+    if not (unlock := unlock_key(wallet)).success:
+        logging.error(unlock.message)
+        return False, unlock.message
+
+    call = await subtensor.substrate.compose_call(
+        call_module="Swap",
+        call_function="modify_position",
+        call_params={
+            "hotkey": wallet.hotkey.ss58_address,
+            "netuid": netuid,
+            "position_id": position_id,
+            "liquidity_delta": liquidity_delta,
+        },
+    )
+
+    return await subtensor.sign_and_send_extrinsic(
+        call=call,
+        wallet=wallet,
+        wait_for_inclusion=wait_for_inclusion,
+        wait_for_finalization=wait_for_finalization,
+        use_nonce=True,
+        period=period,
+    )
+
+
 async def remove_liquidity_extrinsic(
     subtensor: "AsyncSubtensor",
     wallet: "Wallet",
