e2a416da856ff0a6f66cb7477b2e5af3995800e3
Roman Chkhaidze <basfroman@gmail.com>
2025-07-01 19:58:10 -0700

    balance message

--- bittensor/utils/balance.py
+++ bittensor/utils/balance.py
@@ -68,7 +68,7 @@ class Balance:
             # Assume tao value for the float
             self.rao = int(balance * pow(10, 9))
         else:
-            raise TypeError("balance must be an int (rao) or a float (tao)")
+            raise TypeError(f"Balance must be an int (rao) or a float (tao). You passed: `{type(balance)}`.")
 
     @property
     def tao(self):
