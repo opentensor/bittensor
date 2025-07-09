8d408b60e97ecd94fc838b0f00276037179530d9
ibraheem-opentensor <ibraheem@opentensor.dev>
2025-07-08 17:17:58 -0700

    update unstake logic

--- bittensor/core/extrinsics/unstaking.py
+++ bittensor/core/extrinsics/unstaking.py
@@ -105,19 +105,19 @@ def unstake_extrinsic(
 
         if safe_staking:
             pool = subtensor.subnet(netuid=netuid)
-            received_amount, _ = pool.alpha_to_tao_with_slippage(unstaking_balance)
-            base_price = unstaking_balance.rao / received_amount.rao
-            price_with_tolerance = base_price * (1 - rate_tolerance)
+            base_price = pool.price.tao
 
-            # For logging
-            base_rate = pool.price.tao
+            if pool.is_dynamic:
+                price_with_tolerance = base_price * (1 - rate_tolerance)
+            else:
+                price_with_tolerance = base_price
 
             logging.info(
                 f":satellite: [magenta]Safe Unstaking from:[/magenta] "
                 f"netuid: [green]{netuid}[/green], amount: [green]{unstaking_balance}[/green], "
                 f"tolerance percentage: [green]{rate_tolerance * 100}%[/green], "
                 f"price limit: [green]{price_with_tolerance}[/green], "
-                f"original price: [green]{base_rate}[/green], "
+                f"original price: [green]{base_price}[/green], "
                 f"with partial unstake: [green]{allow_partial_stake}[/green] "
                 f"on [blue]{subtensor.network}[/blue][magenta]...[/magenta]"
             )
