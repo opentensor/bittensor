8f90473f1acdde27627c83593198dc59334146f0
Roman Chkhaidze <basfroman@gmail.com>
2025-07-17 19:12:32 -0700

    update `WeightCommitInfo` with `CRV3WeightCommitsV2` (subtensor struct)

--- bittensor/core/chain_data/weight_commit_info.py
+++ bittensor/core/chain_data/weight_commit_info.py
@@ -8,30 +8,35 @@ class WeightCommitInfo:
     Data class representing weight commit information.
 
     Attributes:
-        ss58 (str): The SS58 address of the committer
-        commit_hex (str): The serialized weight commit data as hex string
-        reveal_round (int): The round number for reveal
+        ss58: The SS58 address of the committer
+        commit_block: The block number of the commitment.
+        commit_hex: The serialized weight commit data as hex string
+        reveal_round: The round number for reveal
     """
 
     ss58: str
+    commit_block: int
     commit_hex: str
     reveal_round: int
 
     @classmethod
-    def from_vec_u8(cls, data: tuple) -> tuple[str, str, int]:
+    def from_vec_u8(cls, data: tuple) -> tuple[str, int, str, int]:
         """
         Creates a WeightCommitInfo instance
 
         Args:
-            data (tuple): Tuple containing ((AccountId,), (commit_data,), round_number)
+            data (tuple): Tuple containing ((AccountId,), (commit_block, ) (commit_data,), round_number)
 
         Returns:
             WeightCommitInfo: A new instance with the decoded data
         """
-        account_id, commit_data, round_number = data
+        account_id, commit_block, commit_data, round_number = data
 
         account_id_ = account_id[0] if isinstance(account_id, tuple) else account_id
+        commit_block = (
+            commit_block[0] if isinstance(commit_block, tuple) else commit_block
+        )
         commit_data = commit_data[0] if isinstance(commit_data, tuple) else commit_data
         commit_hex = "0x" + "".join(format(x, "02x") for x in commit_data)
 
-        return decode_account_id(account_id_), commit_hex, round_number
+        return decode_account_id(account_id_), commit_block, commit_hex, round_number
