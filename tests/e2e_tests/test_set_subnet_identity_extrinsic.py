0bd10fc617ffc32331f0c9ee0985565014884bcf
Roman <roman@opentensor.dev>
2025-08-29 13:53:22 -0700

    async test for `tests/e2e_tests/test_set_subnet_identity_extrinsic.py`

--- tests/e2e_tests/test_set_subnet_identity_extrinsic.py
+++ tests/e2e_tests/test_set_subnet_identity_extrinsic.py
@@ -4,22 +4,21 @@ from bittensor.core.chain_data import SubnetIdentity
 from bittensor.utils.btlogging import logging
 
 
-@pytest.mark.asyncio
-async def test_set_subnet_identity_extrinsic_happy_pass(subtensor, alice_wallet):
+def test_set_subnet_identity_extrinsic_happy_pass(subtensor, alice_wallet):
     logging.console.info(
-        "[magenta]Testing `set_subnet_identity_extrinsic` with success result.[/magenta]"
+        "Testing [blue]test_set_subnet_identity_extrinsic_happy_pass[/blue]"
     )
 
-    netuid = subtensor.get_total_subnets()  # 2
+    netuid = subtensor.subnets.get_total_subnets()  # 2
 
     # Register a subnet, netuid 2
-    assert subtensor.register_subnet(alice_wallet), "Subnet wasn't created"
+    assert subtensor.subnets.register_subnet(alice_wallet), "Subnet wasn't created"
 
     # Verify subnet <netuid> created successfully
-    assert subtensor.subnet_exists(netuid), "Subnet wasn't created successfully"
+    assert subtensor.subnets.subnet_exists(netuid), "Subnet wasn't created successfully"
 
     # Make sure subnet_identity is empty
-    assert subtensor.subnet(netuid).subnet_identity is None, (
+    assert subtensor.subnets.subnet(netuid).subnet_identity is None, (
         "Subnet identity should be None before set"
     )
 
@@ -37,7 +36,7 @@ async def test_set_subnet_identity_extrinsic_happy_pass(subtensor, alice_wallet)
 
     # Set SubnetIdentity to subnet
     assert (
-        subtensor.set_subnet_identity(
+        subtensor.subnets.set_subnet_identity(
             wallet=alice_wallet,
             netuid=netuid,
             subnet_identity=subnet_identity,
@@ -46,13 +45,69 @@ async def test_set_subnet_identity_extrinsic_happy_pass(subtensor, alice_wallet)
     ), "Set subnet identity failed"
 
     # Check SubnetIdentity of the subnet
-    assert subtensor.subnet(netuid).subnet_identity == subnet_identity
+    assert subtensor.subnets.subnet(netuid).subnet_identity == subnet_identity
+
+    logging.console.success(
+        "✅ Passed [blue]test_set_subnet_identity_extrinsic_happy_pass[/blue]"
+    )
 
 
 @pytest.mark.asyncio
-async def test_set_subnet_identity_extrinsic_failed(
-    subtensor, alice_wallet, bob_wallet
+async def test_set_subnet_identity_extrinsic_happy_pass_async(
+    async_subtensor, alice_wallet
 ):
+    logging.console.info(
+        "Testing [blue]test_set_subnet_identity_extrinsic_happy_pass_async[/blue]"
+    )
+
+    netuid = await async_subtensor.subnets.get_total_subnets()  # 2
+
+    # Register a subnet, netuid 2
+    assert await async_subtensor.subnets.register_subnet(alice_wallet), (
+        "Subnet wasn't created"
+    )
+
+    # Verify subnet <netuid> created successfully
+    assert await async_subtensor.subnets.subnet_exists(netuid), (
+        "Subnet wasn't created successfully"
+    )
+
+    # Make sure subnet_identity is empty
+    assert (await async_subtensor.subnets.subnet(netuid)).subnet_identity is None, (
+        "Subnet identity should be None before set"
+    )
+
+    # Prepare SubnetIdentity for subnet
+    subnet_identity = SubnetIdentity(
+        subnet_name="e2e test subnet",
+        github_repo="e2e test repo",
+        subnet_contact="e2e test contact",
+        subnet_url="e2e test url",
+        logo_url="e2e test logo url",
+        discord="e2e test discord",
+        description="e2e test description",
+        additional="e2e test additional",
+    )
+
+    # Set SubnetIdentity to subnet
+    assert (
+        await async_subtensor.subnets.set_subnet_identity(
+            wallet=alice_wallet,
+            netuid=netuid,
+            subnet_identity=subnet_identity,
+        )
+    )[0] is True, "Set subnet identity failed"
+
+    # Check SubnetIdentity of the subnet
+    assert (
+        await async_subtensor.subnets.subnet(netuid)
+    ).subnet_identity == subnet_identity
+    logging.console.success(
+        "✅ Passed [blue]test_set_subnet_identity_extrinsic_happy_pass_async[/blue]"
+    )
+
+
+def test_set_subnet_identity_extrinsic_failed(subtensor, alice_wallet, bob_wallet):
     """
     Test case for verifying the behavior of the `set_subnet_identity_extrinsic` function in the
     scenario where the result of the function is expected to fail. It ensures proper handling
@@ -67,19 +122,19 @@ async def test_set_subnet_identity_extrinsic_failed(
         @pytest.mark.asyncio: Marks this test as an asynchronous test.
     """
     logging.console.info(
-        "[magenta]Testing `set_subnet_identity_extrinsic` with failed result.[/magenta]"
+        "Testing [blue]test_set_subnet_identity_extrinsic_failed[/blue]"
     )
 
-    netuid = subtensor.get_total_subnets()  # 2
+    netuid = subtensor.subnets.get_total_subnets()  # 2
 
     # Register a subnet, netuid 2
-    assert subtensor.register_subnet(alice_wallet), "Subnet wasn't created"
+    assert subtensor.subnets.register_subnet(alice_wallet), "Subnet wasn't created"
 
     # Verify subnet <netuid> created successfully
-    assert subtensor.subnet_exists(netuid), "Subnet wasn't created successfully"
+    assert subtensor.subnets.subnet_exists(netuid), "Subnet wasn't created successfully"
 
     # Make sure subnet_identity is empty
-    assert subtensor.subnet(netuid).subnet_identity is None, (
+    assert subtensor.subnets.subnet(netuid).subnet_identity is None, (
         "Subnet identity should be None before set"
     )
 
@@ -97,10 +152,78 @@ async def test_set_subnet_identity_extrinsic_failed(
 
     # Set SubnetIdentity to subnet with wrong wallet
     assert (
-        subtensor.set_subnet_identity(
+        subtensor.subnets.set_subnet_identity(
             wallet=bob_wallet,
             netuid=netuid,
             subnet_identity=subnet_identity,
         )[0]
         is False
     ), "Set subnet identity failed"
+
+    logging.console.success(
+        "✅ Passed [blue]test_set_subnet_identity_extrinsic_failed[/blue]"
+    )
+
+
+@pytest.mark.asyncio
+async def test_set_subnet_identity_extrinsic_failed_async(
+    async_subtensor, alice_wallet, bob_wallet
+):
+    """
+    Async test case for verifying the behavior of the `set_subnet_identity_extrinsic` function in the
+    scenario where the result of the function is expected to fail. It ensures proper handling
+    and validation when attempting to set the subnet identity under specific conditions.
+
+    Args:
+        subtensor: The instance of the subtensor class under test.
+        alice_wallet: A mock or test wallet associated with Alice, used for creating a subnet.
+        bob_wallet: A mock or test wallet associated with Bob, used for setting the subnet identity.
+
+    Decorators:
+        @pytest.mark.asyncio: Marks this test as an asynchronous test.
+    """
+    logging.console.info(
+        "Testing [blue]test_set_subnet_identity_extrinsic_failed[/blue]"
+    )
+
+    netuid = await async_subtensor.subnets.get_total_subnets()  # 2
+
+    # Register a subnet, netuid 2
+    assert await async_subtensor.subnets.register_subnet(alice_wallet), (
+        "Subnet wasn't created"
+    )
+
+    # Verify subnet <netuid> created successfully
+    assert await async_subtensor.subnets.subnet_exists(netuid), (
+        "Subnet wasn't created successfully"
+    )
+
+    # Make sure subnet_identity is empty
+    assert (await async_subtensor.subnets.subnet(netuid)).subnet_identity is None, (
+        "Subnet identity should be None before set"
+    )
+
+    # Prepare SubnetIdentity for subnet
+    subnet_identity = SubnetIdentity(
+        subnet_name="e2e test subnet",
+        github_repo="e2e test repo",
+        subnet_contact="e2e test contact",
+        subnet_url="e2e test url",
+        logo_url="e2e test logo url",
+        discord="e2e test discord",
+        description="e2e test description",
+        additional="e2e test additional",
+    )
+
+    # Set SubnetIdentity to subnet with wrong wallet
+    assert (
+        await async_subtensor.subnets.set_subnet_identity(
+            wallet=bob_wallet,
+            netuid=netuid,
+            subnet_identity=subnet_identity,
+        )
+    )[0] is False, "Set subnet identity failed"
+
+    logging.console.success(
+        "✅ Passed [blue]test_set_subnet_identity_extrinsic_failed[/blue]"
+    )
