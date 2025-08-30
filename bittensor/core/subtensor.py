07c612c0e564e44ca60d5f658868b0bb5760d355
Roman <roman@opentensor.dev>
2025-08-29 17:55:40 -0700

    update `commit_weights_extrinsic` extrinsic and subtensor call with `raise_error=True`

--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -3558,6 +3558,7 @@ class Subtensor(SubtensorMixin):
                     wait_for_inclusion=wait_for_inclusion,
                     wait_for_finalization=wait_for_finalization,
                     period=period,
+                    raise_error=True,
                 )
                 if success:
                     break
