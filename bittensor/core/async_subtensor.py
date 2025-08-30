18b715f5c49cf6f9ac706b2e79ac8f728b64caee
Roman Chkhaidze <basfroman@gmail.com>
2025-08-30 03:07:24 -0700

    set raise_error=True for `reveal_weights`

--- bittensor/core/async_subtensor.py
+++ bittensor/core/async_subtensor.py
@@ -5100,6 +5100,7 @@ class AsyncSubtensor(SubtensorMixin):
                     wait_for_inclusion=wait_for_inclusion,
                     wait_for_finalization=wait_for_finalization,
                     period=period,
+                    raise_error=True,
                 )
                 if success:
                     break
--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -3922,6 +3922,7 @@ class Subtensor(SubtensorMixin):
                     wait_for_inclusion=wait_for_inclusion,
                     wait_for_finalization=wait_for_finalization,
                     period=period,
+                    raise_error=True,
                 )
                 if success:
                     break
