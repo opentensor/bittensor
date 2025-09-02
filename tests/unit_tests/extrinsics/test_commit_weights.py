8785d6ab4be63edaa3d8c626acb4fef1f52cbcd3
Roman <roman@opentensor.dev>
2025-09-02 16:13:37 -0700

    add test coverage for `bittensor.core.extrinsics.commit_weights` module

--- tests/unit_tests/extrinsics/test_commit_weights.py
+++ tests/unit_tests/extrinsics/test_commit_weights.py
@@ -1,58 +1,119 @@
+import pytest
+
 from bittensor.core.extrinsics.commit_weights import (
-    _do_reveal_weights,
+    commit_weights_extrinsic,
+    reveal_weights_extrinsic,
 )
-from bittensor.core.settings import version_as_int
 
 
-def test_do_reveal_weights(subtensor, fake_wallet, mocker):
-    """Verifies that the `_do_reveal_weights` method interacts with the right substrate methods."""
+@pytest.mark.parametrize(
+    "sign_and_send_return",
+    [
+        (True, "Success"),
+        (False, "Failure"),
+    ],
+    ids=["success", "failure"],
+)
+def test_commit_weights_extrinsic(subtensor, fake_wallet, mocker, sign_and_send_return):
+    """Tests `commit_weights_extrinsic` calls proper methods."""
     # Preps
-    fake_wallet.hotkey.ss58_address = "hotkey"
-
-    netuid = 1
-    uids = [1, 2, 3, 4]
-    values = [1, 2, 3, 4]
-    salt = [4, 2, 2, 1]
-    wait_for_inclusion = True
-    wait_for_finalization = True
-
-    mocker.patch.object(subtensor, "sign_and_send_extrinsic")
+    fake_netuid = 1
+    fake_weights = mocker.Mock()
+    mocked_compose_call = mocker.patch.object(subtensor.substrate, "compose_call")
+    mocked_sign_and_send_extrinsic = mocker.patch.object(
+        subtensor, "sign_and_send_extrinsic", return_value=sign_and_send_return
+    )
 
     # Call
-    result = _do_reveal_weights(
+    result = commit_weights_extrinsic(
         subtensor=subtensor,
         wallet=fake_wallet,
-        netuid=netuid,
-        uids=uids,
-        values=values,
-        salt=salt,
-        version_key=version_as_int,
-        wait_for_inclusion=wait_for_inclusion,
-        wait_for_finalization=wait_for_finalization,
+        netuid=fake_netuid,
+        commit_hash=fake_weights,
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
+        period=None,
+        raise_error=False,
     )
 
     # Asserts
-    subtensor.substrate.compose_call.assert_called_once_with(
+    mocked_compose_call.assert_called_once_with(
         call_module="SubtensorModule",
-        call_function="reveal_weights",
-        call_params={
-            "netuid": netuid,
-            "uids": uids,
-            "values": values,
-            "salt": salt,
-            "version_key": version_as_int,
-        },
+        call_function="commit_weights",
+        call_params={"netuid": fake_netuid, "commit_hash": fake_weights},
     )
-
-    subtensor.sign_and_send_extrinsic(
-        call=subtensor.substrate.compose_call.return_value,
+    mocked_sign_and_send_extrinsic.assert_called_once_with(
+        call=mocked_compose_call.return_value,
         wallet=fake_wallet,
-        wait_for_inclusion=wait_for_inclusion,
-        wait_for_finalization=wait_for_finalization,
-        period=None,
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
         nonce_key="hotkey",
+        period=None,
+        raise_error=False,
         sign_with="hotkey",
         use_nonce=True,
     )
+    assert result == sign_and_send_return
 
-    assert result == subtensor.sign_and_send_extrinsic.return_value
+
+@pytest.mark.parametrize(
+    "sign_and_send_return",
+    [
+        (True, "Success"),
+        (False, "Failure"),
+    ],
+    ids=["success", "failure"],
+)
+def test_reveal_weights_extrinsic(subtensor, fake_wallet, mocker, sign_and_send_return):
+    """Tests `reveal_weights_extrinsic` calls proper methods."""
+    # Preps
+    fake_netuid = 1
+    fake_uids = mocker.Mock()
+    fake_weights = mocker.Mock()
+    fake_salt = mocker.Mock()
+    fake_version_key = mocker.Mock()
+
+    mocked_compose_call = mocker.patch.object(subtensor.substrate, "compose_call")
+    mocked_sign_and_send_extrinsic = mocker.patch.object(
+        subtensor, "sign_and_send_extrinsic", return_value=sign_and_send_return
+    )
+
+    # Call
+    result = reveal_weights_extrinsic(
+        subtensor=subtensor,
+        wallet=fake_wallet,
+        netuid=fake_netuid,
+        uids=fake_uids,
+        weights=fake_weights,
+        salt=fake_salt,
+        version_key=fake_version_key,
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
+        period=None,
+        raise_error=False,
+    )
+
+    # Asserts
+    mocked_compose_call.assert_called_once_with(
+        call_module="SubtensorModule",
+        call_function="reveal_weights",
+        call_params={
+            "netuid": fake_netuid,
+            "uids": fake_uids,
+            "values": fake_weights,
+            "salt": fake_salt,
+            "version_key": fake_version_key,
+        },
+    )
+    mocked_sign_and_send_extrinsic.assert_called_once_with(
+        call=mocked_compose_call.return_value,
+        wallet=fake_wallet,
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
+        nonce_key="hotkey",
+        period=None,
+        raise_error=False,
+        sign_with="hotkey",
+        use_nonce=True,
+    )
+    assert result == sign_and_send_return
