6586c44c59485a5ea3e0a7df1f3e8f4101685721
Roman <roman@opentensor.dev>
2025-09-02 13:40:29 -0700

    bring back gather logic

--- bittensor/core/async_subtensor.py
+++ bittensor/core/async_subtensor.py
@@ -1776,7 +1776,7 @@ class AsyncSubtensor(SubtensorMixin):
             result[hotkey_ss58_address] = commitment_message
         return result
 
-    # TODO: deprecated in SDKv10
+    # TODO: remove in SDKv10
     async def get_current_weight_commit_info(
         self,
         netuid: int,
@@ -1817,7 +1817,7 @@ class AsyncSubtensor(SubtensorMixin):
         commits = result.records[0][1] if result.records else []
         return [WeightCommitInfo.from_vec_u8(commit) for commit in commits]
 
-    # TODO: deprecated in SDKv10
+    # TODO: remove in SDKv10
     async def get_current_weight_commit_info_v2(
         self,
         netuid: int,
