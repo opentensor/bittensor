123ced443a1077e51f29a3b4d684e43358160853
Roman Chkhaidze <basfroman@gmail.com>
2025-09-02 15:28:04 -0700

    `._do_commit_weights` logic is included in the main code `.commit_weights_extrinsic` + fixed/removed tests

--- tests/unit_tests/extrinsics/test_commit_weights.py
+++ tests/unit_tests/extrinsics/test_commit_weights.py
@@ -1,60 +1,7 @@
 from bittensor.core.extrinsics.commit_weights import (
-    _do_commit_weights,
     _do_reveal_weights,
 )
 from bittensor.core.settings import version_as_int
-
-
-def test_do_commit_weights(subtensor, fake_wallet, mocker):
-    """Successful _do_commit_weights call."""
-    # Preps
-    netuid = 1
-    commit_hash = "fake_commit_hash"
-    wait_for_inclusion = True
-    wait_for_finalization = True
-
-    mocker.patch.object(subtensor, "sign_and_send_extrinsic", return_value=(False, ""))
-    mocker.patch.object(subtensor, "get_block_hash", return_value=1)
-
-    mocked_format_error_message = mocker.Mock()
-    mocker.patch(
-        "bittensor.core.subtensor.format_error_message",
-        mocked_format_error_message,
-    )
-
-    # Call
-    result = _do_commit_weights(
-        subtensor=subtensor,
-        wallet=fake_wallet,
-        netuid=netuid,
-        commit_hash=commit_hash,
-        wait_for_inclusion=wait_for_inclusion,
-        wait_for_finalization=wait_for_finalization,
-    )
-
-    # Assertions
-    subtensor.substrate.compose_call.assert_called_once_with(
-        call_module="SubtensorModule",
-        call_function="commit_weights",
-        call_params={
-            "netuid": netuid,
-            "commit_hash": commit_hash,
-        },
-    )
-
-    subtensor.sign_and_send_extrinsic.assert_called_once_with(
-        call=subtensor.substrate.compose_call.return_value,
-        wallet=fake_wallet,
-        wait_for_inclusion=wait_for_inclusion,
-        wait_for_finalization=wait_for_finalization,
-        period=None,
-        nonce_key="hotkey",
-        sign_with="hotkey",
-        use_nonce=True,
-        raise_error=True,
-    )
-
-    assert result == (False, "")
 
 
 def test_do_reveal_weights(subtensor, fake_wallet, mocker):
