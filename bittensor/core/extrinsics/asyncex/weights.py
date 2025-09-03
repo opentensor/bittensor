08df25d09b9f11dc288bd1ba4ffa93dfbd84f1f0
Roman <roman@opentensor.dev>
2025-09-02 17:10:26 -0700

    `._do_set_weights` logic is included in the main code `.set_weights_extrinsic`

--- bittensor/core/extrinsics/asyncex/weights.py
+++ bittensor/core/extrinsics/asyncex/weights.py
@@ -1,14 +1,16 @@
-"""This module provides sync functionality for working with weights in the Bittensor network."""
+"""This module provides async functionality for working with weights in the Bittensor network."""
 
 from typing import Union, TYPE_CHECKING, Optional
 
 import numpy as np
 from numpy.typing import NDArray
 
-from bittensor.core.settings import version_as_int
 from bittensor.utils import get_function_name
 from bittensor.utils.btlogging import logging
-from bittensor.utils.weight_utils import convert_and_normalize_weights_and_uids
+from bittensor.utils.weight_utils import (
+    convert_and_normalize_weights_and_uids,
+    convert_uids_and_weights,
+)
 
 if TYPE_CHECKING:
     from bittensor_wallet import Wallet
@@ -72,12 +74,10 @@ async def commit_weights_extrinsic(
     )
 
     if success:
-        success_message = "✅ [green]Successfully committed weights.[green]"
-        logging.info(success_message)
-        return True, success_message
-
-    logging.error(f"Failed to commit weights: {message}")
-    return False, message
+        logging.info(message)
+    else:
+        logging.error(f"{get_function_name}: {message}")
+    return success, message
 
 
 async def reveal_weights_extrinsic(
@@ -150,72 +150,6 @@ async def reveal_weights_extrinsic(
     return success, message
 
 
-async def _do_set_weights(
-    subtensor: "AsyncSubtensor",
-    wallet: "Wallet",
-    netuid: int,
-    uids: list[int],
-    vals: list[int],
-    version_key: int = version_as_int,
-    wait_for_inclusion: bool = False,
-    wait_for_finalization: bool = False,
-    period: Optional[int] = None,
-) -> tuple[bool, str]:  # (success, error_message)
-    """
-    Internal method to send a transaction to the Bittensor blockchain, setting weights for specified neurons. This
-    method constructs and submits the transaction, handling retries and blockchain communication.
-
-    Args:
-        subtensor (subtensor.core.async_subtensor.AsyncSubtensor): Async Subtensor instance.
-        wallet (bittensor_wallet.Wallet): The wallet associated with the neuron setting the weights.
-        uids (List[int]): List of neuron UIDs for which weights are being set.
-        vals (List[int]): List of weight values corresponding to each UID.
-        netuid (int): Unique identifier for the network.
-        version_key (int, optional): Version key for compatibility with the network.
-        wait_for_inclusion (bool, optional): Waits for the transaction to be included in a block.
-        wait_for_finalization (bool, optional): Waits for the transaction to be finalized on the blockchain.
-        period (Optional[int]): The number of blocks during which the transaction will remain valid after it's submitted. If
-            the transaction is not included in a block within that number of blocks, it will expire and be rejected.
-            You can think of it as an expiration date for the transaction.
-
-    Returns:
-        tuple[bool, str]:
-            `True` if the weight commitment is successful, `False` otherwise.
-            `msg` is a string value describing the success or potential error.
-
-    This method is vital for the dynamic weighting mechanism in Bittensor, where neurons adjust their
-        trust in other neurons based on observed performance and contributions.
-    """
-    call = await subtensor.substrate.compose_call(
-        call_module="SubtensorModule",
-        call_function="set_weights",
-        call_params={
-            "dests": uids,
-            "weights": vals,
-            "netuid": netuid,
-            "version_key": version_key,
-        },
-    )
-    success, message = await subtensor.sign_and_send_extrinsic(
-        call=call,
-        wallet=wallet,
-        wait_for_inclusion=wait_for_inclusion,
-        wait_for_finalization=wait_for_finalization,
-        period=period,
-        use_nonce=True,
-        nonce_key="hotkey",
-        sign_with="hotkey",
-    )
-
-    # We only wait here if we expect finalization.
-    if not wait_for_finalization and not wait_for_inclusion:
-        return True, "Not waiting for finalization or inclusion."
-
-    if success:
-        return success, "Successfully set weights."
-    return success, message
-
-
 async def set_weights_extrinsic(
     subtensor: "AsyncSubtensor",
     wallet: "Wallet",
@@ -226,6 +160,7 @@ async def set_weights_extrinsic(
     wait_for_inclusion: bool = False,
     wait_for_finalization: bool = False,
     period: Optional[int] = 8,
+    raise_error: bool = False,
 ) -> tuple[bool, str]:
     """Sets the given weights and values on chain for a given wallet hotkey account.
 
@@ -244,12 +179,17 @@ async def set_weights_extrinsic(
         period (Optional[int]): The number of blocks during which the transaction will remain valid after it's submitted. If
             the transaction is not included in a block within that number of blocks, it will expire and be rejected.
             You can think of it as an expiration date for the transaction.
+        raise_error: raises the relevant exception rather than returning `False` if unsuccessful.
 
     Returns:
         tuple[bool, str]:
             `True` if the weight commitment is successful, `False` otherwise.
             `msg` is a string value describing the success or potential error.
     """
+    # Convert types.
+    uids, weights = convert_uids_and_weights(uids, weights)
+
+    # Reformat and normalize.
     weight_uids, weight_vals = convert_and_normalize_weights_and_uids(uids, weights)
 
     logging.info(
@@ -257,30 +197,31 @@ async def set_weights_extrinsic(
         f"[blue]{subtensor.network}[/blue] "
         f"[magenta]...[/magenta]"
     )
-    try:
-        success, message = await _do_set_weights(
-            subtensor=subtensor,
-            wallet=wallet,
-            netuid=netuid,
-            uids=weight_uids,
-            vals=weight_vals,
-            version_key=version_key,
-            wait_for_finalization=wait_for_finalization,
-            wait_for_inclusion=wait_for_inclusion,
-            period=period,
-        )
 
-        if not wait_for_finalization and not wait_for_inclusion:
-            return True, message
+    call = await subtensor.substrate.compose_call(
+        call_module="SubtensorModule",
+        call_function="set_weights",
+        call_params={
+            "dests": weight_uids,
+            "weights": weight_vals,
+            "netuid": netuid,
+            "version_key": version_key,
+        },
+    )
+    success, message = await subtensor.sign_and_send_extrinsic(
+        call=call,
+        wallet=wallet,
+        wait_for_inclusion=wait_for_inclusion,
+        wait_for_finalization=wait_for_finalization,
+        period=period,
+        use_nonce=True,
+        nonce_key="hotkey",
+        sign_with="hotkey",
+        raise_error=raise_error,
+    )
 
-        if success is True:
-            message = "Successfully set weights and Finalized."
-            logging.success(f":white_heavy_check_mark: [green]{message}[/green]")
-            return True, message
-
-        logging.error(f"[red]Failed[/red] set weights. Error: {message}")
-        return False, message
-
-    except Exception as error:
-        logging.error(f":cross_mark: [red]Failed[/red] set weights. Error: {error}")
-        return False, str(error)
+    if success:
+        logging.info(message)
+    else:
+        logging.error(f"{get_function_name}: {message}")
+    return success, message
