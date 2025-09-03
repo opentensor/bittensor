c289c8eca8f72ed3d41c3c8183f7497a3b299967
Roman <roman@opentensor.dev>
2025-09-03 15:39:50 -0700

    `.add_liquidity_extrinsic` and `subtensor.add_liquidity`

--- tests/unit_tests/test_subtensor.py
+++ tests/unit_tests/test_subtensor.py
@@ -3949,8 +3949,9 @@ def test_add_liquidity(subtensor, fake_wallet, mocker):
         price_high=Balance.from_tao(130).rao,
         hotkey=None,
         wait_for_inclusion=True,
-        wait_for_finalization=False,
+        wait_for_finalization=True,
         period=None,
+        raise_error=False,
     )
     assert result == mocked_extrinsic.return_value
 
