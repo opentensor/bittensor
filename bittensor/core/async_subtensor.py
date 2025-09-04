2083d880c4b5b1ca06a599fd21fa129747d4ae02
Roman <roman@opentensor.dev>
2025-09-03 22:08:27 -0700

    change default values

--- bittensor/core/async_subtensor.py
+++ bittensor/core/async_subtensor.py
@@ -5322,7 +5322,7 @@ class AsyncSubtensor(SubtensorMixin):
         certificate: Optional[Certificate] = None,
         period: Optional[int] = None,
         raise_error: bool = False,
-        wait_for_inclusion: bool = False,
+        wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
     ) -> bool:
         """
