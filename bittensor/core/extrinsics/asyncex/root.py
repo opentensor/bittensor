48015cdffad879cee449baa866e0d6d623be85f7
Roman <roman@opentensor.dev>
2025-09-03 21:21:43 -0700

    `.root_register_extrinsic`, `subtensor.burned_register` and `subtensor.root_register`

--- bittensor/core/extrinsics/asyncex/root.py
+++ bittensor/core/extrinsics/asyncex/root.py
@@ -37,26 +37,26 @@ async def _get_limits(subtensor: "AsyncSubtensor") -> tuple[int, float]:
 async def root_register_extrinsic(
     subtensor: "AsyncSubtensor",
     wallet: "Wallet",
+    period: Optional[int] = None,
+    raise_error: bool = False,
     wait_for_inclusion: bool = True,
     wait_for_finalization: bool = True,
-    period: Optional[int] = None,
 ) -> bool:
-    """Registers the wallet to the root network.
+    """
+    Registers the neuron to the root network.
 
     Arguments:
-        subtensor (bittensor.core.async_subtensor.AsyncSubtensor): The AsyncSubtensor object
-        wallet (bittensor_wallet.Wallet): Bittensor wallet object.
-        wait_for_inclusion (bool): If set, waits for the extrinsic to enter a block before returning `True`, or returns
-            `False` if the extrinsic fails to enter the block within the timeout.
-        wait_for_finalization (bool): If set, waits for the extrinsic to be finalized on the chain before returning
-            `True`, or returns `False` if the extrinsic fails to be finalized within the timeout.
-        period (Optional[int]): The number of blocks during which the transaction will remain valid after it's submitted. If
-            the transaction is not included in a block within that number of blocks, it will expire and be rejected.
-            You can think of it as an expiration date for the transaction.
+        subtensor: Subtensor instance to interact with the blockchain.
+        wallet: Bittensor Wallet instance.
+        period: The number of blocks during which the transaction will remain valid after it's submitted. If the
+            transaction is not included in a block within that number of blocks, it will expire and be rejected. You can
+            think of it as an expiration date for the transaction.
+        raise_error: Raises a relevant exception rather than returning `False` if unsuccessful.
+        wait_for_inclusion: Whether to wait for the inclusion of the transaction.
+        wait_for_finalization: Whether to wait for the finalization of the transaction.
 
     Returns:
-        `True` if extrinsic was finalized or included in the block. If we did not wait for finalization/inclusion,
-            the response is `True`.
+        bool: True if the subnet registration was successful, False otherwise.
     """
     netuid = 0
     logging.info(
@@ -114,6 +114,7 @@ async def root_register_extrinsic(
         wait_for_inclusion=wait_for_inclusion,
         wait_for_finalization=wait_for_finalization,
         period=period,
+        raise_error=raise_error,
     )
 
     if not success:
