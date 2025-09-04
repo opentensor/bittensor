e64e506dfa6e4e4dbd87cd7017e69c067404207b
Roman Chkhaidze <basfroman@gmail.com>
2025-09-03 23:44:16 -0700

    `.start_call_extrinsic`, `subtensor.start_call`

--- bittensor/core/extrinsics/start_call.py
+++ bittensor/core/extrinsics/start_call.py
@@ -12,23 +12,25 @@ def start_call_extrinsic(
     subtensor: "Subtensor",
     wallet: "Wallet",
     netuid: int,
+    period: Optional[int] = None,
+    raise_error: bool = False,
     wait_for_inclusion: bool = True,
     wait_for_finalization: bool = False,
-    period: Optional[int] = None,
 ) -> tuple[bool, str]:
     """
     Submits a start_call extrinsic to the blockchain, to trigger the start call process for a subnet (used to start a
     new subnet's emission mechanism).
 
-    Args:
+    Parameters:
         subtensor (Subtensor): The Subtensor client instance used for blockchain interaction.
         wallet (Wallet): The wallet used to sign the extrinsic (must be unlocked).
         netuid (int): The UID of the target subnet for which the call is being initiated.
-        wait_for_inclusion (bool, optional): Whether to wait for the extrinsic to be included in a block. Defaults to True.
-        wait_for_finalization (bool, optional): Whether to wait for finalization of the extrinsic. Defaults to False.
         period: The number of blocks during which the transaction will remain valid after it's submitted. If
             the transaction is not included in a block within that number of blocks, it will expire and be rejected.
             You can think of it as an expiration date for the transaction.
+        raise_error: Raises a relevant exception rather than returning `False` if unsuccessful.
+        wait_for_inclusion: Whether to wait for the extrinsic to be included in a block.
+        wait_for_finalization: Whether to wait for finalization of the extrinsic.
 
     Returns:
         Tuple[bool, str]:
@@ -51,6 +53,7 @@ def start_call_extrinsic(
         wait_for_inclusion=wait_for_inclusion,
         wait_for_finalization=wait_for_finalization,
         period=period,
+        raise_error=raise_error,
     )
 
     if not wait_for_finalization and not wait_for_inclusion:
