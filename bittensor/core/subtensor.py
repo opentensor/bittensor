da64f7e3c9f0b2e20615b4d99f93aa29d074407b
Roman <roman@opentensor.dev>
2025-09-05 19:30:43 -0700

    update `add_liquidity_extrinsic` + `modify_liquidity_extrinsic` + `remove_liquidity_extrinsic` + `toggle_user_liquidity_extrinsic`

--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -3365,7 +3365,7 @@ class Subtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> tuple[bool, str]:
+    ) -> ExtrinsicResponse:
         """
         Adds liquidity to the specified price range.
 
@@ -3581,7 +3581,7 @@ class Subtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> tuple[bool, str]:
+    ) -> ExtrinsicResponse:
         """Modifies liquidity in liquidity position by adding or removing liquidity from it.
 
         Parameters:
@@ -3801,7 +3801,7 @@ class Subtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> tuple[bool, str]:
+    ) -> ExtrinsicResponse:
         """Remove liquidity and credit balances back to wallet's hotkey stake.
 
         Parameters:
@@ -4478,7 +4478,7 @@ class Subtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> tuple[bool, str]:
+    ) -> ExtrinsicResponse:
         """Allow to toggle user liquidity for specified subnet.
 
         Parameters:
