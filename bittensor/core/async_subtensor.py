89c58f5e9d88675b0a4ac8cc036cdb9a3937a280
Roman <roman@opentensor.dev>
2025-09-08 16:28:24 -0700

    `burned_register_extrinsic`, `register_subnet_extrinsic`, `register_extrinsic`, + `set_subnet_identity_extrinsic` + related subtensor calls + fixed tests

--- bittensor/core/async_subtensor.py
+++ bittensor/core/async_subtensor.py
@@ -4896,7 +4896,7 @@ class AsyncSubtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ):
+    ) -> ExtrinsicResponse:
         """
         Registers a neuron on the Bittensor subnet with provided netuid using the provided wallet.
 
@@ -5340,7 +5340,7 @@ class AsyncSubtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> tuple[bool, str]:
+    ) -> ExtrinsicResponse:
         """
         Sets the identity of a subnet for a specific wallet and network.
 
--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -3742,7 +3742,7 @@ class Subtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> bool:
+    ) -> ExtrinsicResponse:
         """
         Registers a neuron on the Bittensor subnet with provided netuid using the provided wallet.
 
@@ -4173,7 +4173,7 @@ class Subtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> tuple[bool, str]:
+    ) -> ExtrinsicResponse:
         """
         Sets the identity of a subnet for a specific wallet and network.
 
