79600f711caf0b171a14224393c6e0c285993155
Roman <roman@opentensor.dev>
2025-09-08 14:27:42 -0700

    update `transfer_stake_extrinsic`, `swap_stake_extrinsic` and `move_stake_extrinsic` + related tests

--- bittensor/core/async_subtensor.py
+++ bittensor/core/async_subtensor.py
@@ -4851,7 +4851,7 @@ class AsyncSubtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> bool:
+    ) -> ExtrinsicResponse:
         """
         Moves stake to a different hotkey and/or subnet.
 
@@ -5665,7 +5665,7 @@ class AsyncSubtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> bool:
+    ) -> ExtrinsicResponse:
         """
         Moves stake between subnets while keeping the same coldkey-hotkey pair ownership.
         Like subnet hopping - same owner, same hotkey, just changing which subnet the stake is in.
@@ -5814,7 +5814,7 @@ class AsyncSubtensor(SubtensorMixin):
         raise_error: bool = False,
         wait_for_inclusion: bool = True,
         wait_for_finalization: bool = True,
-    ) -> bool:
+    ) -> ExtrinsicResponse:
         """
         Transfers stake from one subnet to another while changing the coldkey owner.
 
