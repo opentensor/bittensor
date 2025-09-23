2e3ccb96f0742e06eb3a669d1e5862f433be5f8e
bdhimes <b@latent.to>
2025-09-23 23:29:48 +0200

    More thoughts.

--- tests/helpers/generate_new_websocket_data.py
+++ tests/helpers/generate_new_websocket_data.py
@@ -15,7 +15,28 @@ before this makes it into the codebase:
  - sent websocket begin with `WEBSOCKET_SEND> `
  - both are stringified JSON
  - logging level is DEBUG
- - metadata and metadatav15 (metadata at version) must be discarded, or rather just dumped to their respective txt files
- - metadata/metadatav15 txt files are just the "result" portion of the response:
+ - metadata and metadataV15 (metadata at version) must be discarded, or rather just dumped to their respective txt files
+ - metadata/metadataV15 txt files are just the "result" portion of the response:
     e.g. `{"jsonrpc": "2.0", "id": _id, "result": METADATA}`
+ - This script should NOT overwrite "retry_archive", as that uses a specific cycling mechanism to cycle between possible
+    runtime responses
+ - the data is structured as follows:
+    seed_name: {
+      rpc_method: {
+        params: {
+          response
+        }
+      }
+    }
+
+ - seed_name is basically just the name of the test that is being run. Specifying the seed name tells the FakeWebsocket
+    which set of data to use
+ - some workflows may specify the same parameters for a given RPC call, and expect different results. This is alleviated
+    typically by specifying block hashes/numbers, though that does not always fit well into the tests.
+ - the tests should be short and concise, testing just a single "thing" because of the possibility of conflicting rpc
+    requests with the same params
+ - all requests include {"json_rpc": "2.0"} in them — this can be removed for the sake of saving space, but is not
+    imperative to do so
+ - the websocket logger will include the id of the request: these should be stripped away, as they are dynamically
+    created in SubstrateInterface, and then attached to the next response by FakeWebsocket
 """
