6c6e752adef0dd7940fcbf1350b7d2106381c035
Roman <roman@opentensor.dev>
2025-09-23 14:02:02 -0700

    fix tests

--- bittensor/core/subtensor_api/subnets.py
+++ bittensor/core/subtensor_api/subnets.py
@@ -19,7 +19,6 @@ class Subnets:
         self.get_parents = subtensor.get_parents
         self.get_children = subtensor.get_children
         self.get_children_pending = subtensor.get_children_pending
-        self.get_current_weight_commit_info = subtensor.get_current_weight_commit_info
         self.get_hyperparameter = subtensor.get_hyperparameter
         self.get_liquidity_list = subtensor.get_liquidity_list
         self.get_neuron_for_pubkey_and_subnet = (
