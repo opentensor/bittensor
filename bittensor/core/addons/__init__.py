1a959f721abbc595d6b13d9367e63edd5f8d64b6
Roman Chkhaidze <basfroman@gmail.com>
2025-09-29 16:10:27 -0700

    `bittensor.core.subtensor_api` moved to `bittensor.core.addons.subtensor_api`

---
+++ bittensor/core/addons/__init__.py
@@ -0,0 +1,10 @@
+"""
+The `addons` sub-package contains optional extensions and logic augmentations for the core functionality of the project.
+
+Modules placed in this package may include experimental features, alternative implementations, developer tools, or
+enhancements that extend or customize core behavior. These components are not always critical for the main application,
+but can be enabled or imported as needed for advanced use cases, internal tooling, or feature expansion.
+
+Use this package to keep optional, modular, or feature-gated logic separate from the primary codebase while maintaining
+discoverability and structure.
+"""
