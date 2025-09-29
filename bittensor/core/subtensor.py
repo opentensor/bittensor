9816ee3a3cdd9296880873525cbb9596dbc13134
Roman <roman@opentensor.dev>
2025-09-29 11:01:24 -0700

    add `ExtrinsicResponse.extrinsic_receipt`

--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -3144,6 +3144,7 @@ class Subtensor(SubtensorMixin):
                 wait_for_inclusion=wait_for_inclusion,
                 wait_for_finalization=wait_for_finalization,
             )
+            extrinsic_response.extrinsic_receipt = response
             # We only wait here if we expect finalization.
             if not wait_for_finalization and not wait_for_inclusion:
                 extrinsic_response.message = (
