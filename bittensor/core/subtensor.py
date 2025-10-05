b1132d42afffc7eca87a569e6184a823a9184a9a
Roman Chkhaidze <basfroman@gmail.com>
2025-10-05 02:38:55 -0700

    add `set_trace` for `log_verbose=True`

--- bittensor/core/subtensor.py
+++ bittensor/core/subtensor.py
@@ -184,6 +184,7 @@ class Subtensor(SubtensorMixin):
             archive_endpoints=archive_endpoints,
         )
         if self.log_verbose:
+            logging.set_trace()
             logging.info(
                 f"Connected to {self.network} network and {self.chain_endpoint}."
             )
