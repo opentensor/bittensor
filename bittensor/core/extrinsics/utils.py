9cd49fffdfbab92c48011f4b84a70970a84e6b12
Roman Chkhaidze <basfroman@gmail.com>
2025-10-06 14:08:00 -0700

    more updates

--- bittensor/core/extrinsics/utils.py
+++ bittensor/core/extrinsics/utils.py
@@ -84,7 +84,7 @@ def sudo_call_extrinsic(
     raise_error: bool = False,
     wait_for_inclusion: bool = True,
     wait_for_finalization: bool = True,
-    sn_owner_call: bool = False,
+    root_call: bool = False,
 ) -> ExtrinsicResponse:
     """Execute a sudo call extrinsic.
 
@@ -103,7 +103,7 @@ def sudo_call_extrinsic(
         raise_error: Raises a relevant exception rather than returning `False` if unsuccessful.
         wait_for_inclusion: Whether to wait for the inclusion of the transaction.
         wait_for_finalization: Whether to wait for the finalization of the transaction.
-        sn_owner_call: True, if the subnet owner makes a call.
+        root_call: False, if the subnet owner makes a call.
 
     Returns:
         ExtrinsicResponse: The result object of the extrinsic execution.
@@ -121,7 +121,7 @@ def sudo_call_extrinsic(
             call_function=call_function,
             call_params=call_params,
         )
-        if not sn_owner_call:
+        if not root_call:
             call = subtensor.substrate.compose_call(
                 call_module="Sudo",
                 call_function="sudo",
