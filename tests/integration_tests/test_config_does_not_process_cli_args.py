75666162528cb39c693c54b35850b2a76750bc97
Roman Chkhaidze <basfroman@gmail.com>
2025-10-06 18:41:54 -0700

    add tests

---
+++ tests/integration_tests/test_config_does_not_process_cli_args.py
@@ -0,0 +1,51 @@
+import argparse
+import sys
+
+import pytest
+
+import bittensor as bt
+from bittensor.core.config import InvalidConfigFile
+
+
+def _config_call():
+    """Create a config object from the bt cli args."""
+    parser = argparse.ArgumentParser()
+    bt.Axon.add_args(parser)
+    bt.Subtensor.add_args(parser)
+    bt.AsyncSubtensor.add_args(parser)
+    bt.Wallet.add_args(parser)
+    bt.logging.add_args(parser)
+    bt.PriorityThreadPoolExecutor.add_args(parser)
+    config = bt.Config(parser)
+    return config
+
+
+TEST_ARGS = [
+    "bittensor",
+    "--config", "path/to/config.yaml",
+    "--strict",
+    "--no_version_checking"
+]
+
+
+def test_bittensor_cli_parser_enabled(monkeypatch):
+    """Tests that the bt cli args are processed."""
+    monkeypatch.setenv("BT_PARSE_CLI_ARGS", "true")
+
+    monkeypatch.setattr(sys, "argv", TEST_ARGS)
+
+    with pytest.raises(InvalidConfigFile) as error:
+        _config_call()
+
+    assert "No such file or directory" in str(error)
+
+
+def test_bittensor_cli_parser_disabled(monkeypatch):
+    """Tests that the bt cli args are not processed."""
+    monkeypatch.setattr(sys, "argv", TEST_ARGS)
+
+    config = _config_call()
+
+    assert config.config is False
+    assert config.strict is False
+    assert config.no_version_checking is False
