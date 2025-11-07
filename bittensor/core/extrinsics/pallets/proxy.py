b128c20b4bf2fe4a7e75641ce558a95d0965650c
Roman Chkhaidze <basfroman@gmail.com>
2025-11-07 10:50:46 -0800

    add bittensor/core/extrinsics/pallets/proxy.py pallet

---
+++ bittensor/core/extrinsics/pallets/proxy.py
@@ -0,0 +1,162 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Optional
+
+from .base import CallBuilder as _BasePallet, Call
+
+if TYPE_CHECKING:
+    from scalecodec import GenericCall
+
+
+@dataclass
+class Proxy(_BasePallet):
+    """Factory class for creating GenericCall objects for Proxy pallet functions.
+
+    This class provides methods to create GenericCall instances for all Proxy pallet extrinsics.
+
+    Works with both sync (Subtensor) and async (AsyncSubtensor) instances. For async operations, pass an AsyncSubtensor
+    instance and await the result.
+
+    Example:
+        # Sync usage
+        call = Proxy(subtensor).add_proxy(delegate="5DE..", proxy_type="Any", delay=0)
+        response = subtensor.sign_and_send_extrinsic(call=call, ...)
+
+        # Async usage
+        call = await Proxy(async_subtensor).add_proxy(delegate="5DE..", proxy_type="Any", delay=0)
+        response = await async_subtensor.sign_and_send_extrinsic(call=call, ...)
+    """
+
+    def add_proxy(
+        self,
+        delegate: str,
+        proxy_type: str,
+        delay: int,
+    ) -> Call:
+        """Returns GenericCall instance for Subtensor function Proxy.add_proxy.
+
+        Parameters:
+            delegate: The SS58 address of the delegate proxy account.
+            proxy_type: The type of proxy permissions (e.g., "Any", "NonTransfer", "Governance", "Staking", "IdentityJudgement", "CancelProxy", "Auction").
+            delay: The number of blocks before the proxy can be used.
+
+        Returns:
+            GenericCall instance.
+        """
+        return self.create_composed_call(
+            delegate=delegate,
+            proxy_type=proxy_type,
+            delay=delay,
+        )
+
+    def remove_proxy(
+        self,
+        delegate: str,
+        proxy_type: str,
+        delay: int,
+    ) -> Call:
+        """Returns GenericCall instance for Subtensor function Proxy.remove_proxy.
+
+        Parameters:
+            delegate: The SS58 address of the delegate proxy account to remove.
+            proxy_type: The type of proxy permissions to remove (e.g., "Any", "NonTransfer", "Governance", "Staking", "IdentityJudgement", "CancelProxy", "Auction").
+            delay: The number of blocks before the proxy removal takes effect.
+
+        Returns:
+            GenericCall instance.
+        """
+        return self.create_composed_call(
+            delegate=delegate,
+            proxy_type=proxy_type,
+            delay=delay,
+        )
+
+    def create_pure(
+        self,
+        proxy_type: str,
+        delay: int,
+        index: int,
+    ) -> Call:
+        """Returns GenericCall instance for Subtensor function Proxy.create_pure.
+
+        Parameters:
+            proxy_type: The type of proxy permissions for the pure proxy (e.g., "Any", "NonTransfer", "Governance", "Staking", "IdentityJudgement", "CancelProxy", "Auction").
+            delay: The number of blocks before the pure proxy can be used.
+            index: The index to use for generating the pure proxy account address.
+
+        Returns:
+            GenericCall instance.
+        """
+        return self.create_composed_call(
+            proxy_type=proxy_type,
+            delay=delay,
+            index=index,
+        )
+
+    def kill_pure(
+        self,
+        spawner: str,
+        proxy: str,
+        proxy_type: str,
+        height: int,
+        ext_index: int,
+    ) -> Call:
+        """Returns GenericCall instance for Subtensor function Proxy.kill_pure.
+
+        Parameters:
+            spawner: The SS58 address of the account that spawned the pure proxy.
+            proxy: The SS58 address of the pure proxy account to kill.
+            proxy_type: The type of proxy permissions (e.g., "Any", "NonTransfer", "Governance", "Staking", "IdentityJudgement", "CancelProxy", "Auction").
+            height: The block height at which the pure proxy was created.
+            ext_index: The extrinsic index at which the pure proxy was created.
+
+        Returns:
+            GenericCall instance.
+        """
+        return self.create_composed_call(
+            spawner=spawner,
+            proxy=proxy,
+            proxy_type=proxy_type,
+            height=height,
+            ext_index=ext_index,
+        )
+
+    def proxy(
+        self,
+        real: str,
+        force_proxy_type: Optional[str],
+        call: "GenericCall",
+    ) -> Call:
+        """Returns GenericCall instance for Subtensor function Proxy.proxy.
+
+        Parameters:
+            real: The SS58 address of the real account on whose behalf the call is being made.
+            force_proxy_type: The type of proxy to use for the call. If None, any proxy type can be used. Otherwise, must match one of the allowed proxy types (e.g., "Any", "NonTransfer", "Governance", "Staking", "IdentityJudgement", "CancelProxy", "Auction").
+            call: The inner call to be executed on behalf of the real account.
+
+        Returns:
+            GenericCall instance.
+        """
+        return self.create_composed_call(
+            real=real,
+            force_proxy_type=force_proxy_type,
+            call=call,
+        )
+
+    def announce(
+        self,
+        real: str,
+        call_hash: str,
+    ) -> Call:
+        """Returns GenericCall instance for Subtensor function Proxy.announce.
+
+        Parameters:
+            real: The SS58 address of the real account on whose behalf the call will be made.
+            call_hash: The hash of the call that will be executed in the future.
+
+        Returns:
+            GenericCall instance.
+        """
+        return self.create_composed_call(
+            real=real,
+            call_hash=call_hash,
+        )
