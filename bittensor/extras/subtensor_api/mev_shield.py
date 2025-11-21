4fad186d13171537163cc283dd437ee9a064be7b
Roman Chkhaidze <basfroman@gmail.com>
2025-11-21 15:43:18 -0800

    update SubtensorApi

---
+++ bittensor/extras/subtensor_api/mev_shield.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from bittensor.core.async_subtensor import AsyncSubtensor as _AsyncSubtensor
+from bittensor.core.subtensor import Subtensor as _Subtensor
+
+
+class MevShield:
+    """Class for managing MEV Shield operations."""
+
+    def __init__(self, subtensor: Union["_Subtensor", "_AsyncSubtensor"]):
+        # Storage queries
+        self.get_mev_shield_current_key = subtensor.get_mev_shield_current_key
+        self.get_mev_shield_epoch = subtensor.get_mev_shield_epoch
+        self.get_mev_shield_next_key = subtensor.get_mev_shield_next_key
+        self.get_mev_shield_submission = subtensor.get_mev_shield_submission
+
+        # Extrinsics
+        self.mev_announce_next_key = subtensor.mev_announce_next_key
+        self.mev_execute_revealed = subtensor.mev_execute_revealed
+        self.mev_submit_encrypted = subtensor.mev_submit_encrypted
