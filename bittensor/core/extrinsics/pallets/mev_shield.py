89245b0e4bb416c09993ef66c05155bc4971833d
Roman Chkhaidze <basfroman@gmail.com>
2025-11-21 15:43:34 -0800

    update pallet functions call builder

--- bittensor/core/extrinsics/pallets/mev_shield.py
+++ bittensor/core/extrinsics/pallets/mev_shield.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from scalecodec import GenericCall
+
 from .base import CallBuilder as _BasePallet, Call
 
 
@@ -27,6 +29,70 @@ class MevShield(_BasePallet):
         )
         response = await async_subtensor.sign_and_send_extrinsic(call=call, ...)
     """
+
+    def announce_next_key(
+        self,
+        public_key: bytes,
+        epoch: int,
+    ) -> Call:
+        """Returns GenericCall instance for MevShield function announce_next_key.
+
+        This function allows Aura validators to announce the next ML-KEM-768 public key that will be used for encryption
+        in the next block. The key will be rotated from NextKey to CurrentKey at the beginning of the next block.
+
+        Parameters:
+            public_key: The ML-KEM-768 public key as bytes. Must be exactly 1184 bytes for ML-KEM-768.
+            epoch: The epoch number associated with this key.
+
+        Returns:
+            GenericCall instance ready for extrinsic submission.
+
+        Note:
+            Only current Aura validators can call this function. The public_key length must be exactly 1184 bytes
+            (MAX_KYBER768_PK_LENGTH). This is enforced by the pallet.
+        """
+        return self.create_composed_call(
+            public_key=public_key,
+            epoch=epoch,
+        )
+
+    def execute_revealed(
+        self,
+        id: str,
+        signer: str,
+        nonce: int,
+        call: "GenericCall",
+        signature: bytes,
+    ) -> Call:
+        """Returns GenericCall instance for MevShield function execute_revealed.
+
+        This function is executed by the block author (validator) to execute a decrypted extrinsic. It verifies the
+        commitment, signature, and nonce before dispatching the inner call.
+
+        Parameters:
+            id: The submission ID (hash of signer + commitment + ciphertext). Must be a hex string with "0x" prefix.
+            signer: The SS58 address of the account that originally submitted the encrypted extrinsic.
+            nonce: The nonce that was used when creating the payload_core.
+            call: The GenericCall object to execute (the decrypted call).
+            signature: The signature bytes (MultiSignature format) that was included in the encrypted payload.
+
+        Returns:
+            GenericCall instance ready for extrinsic submission.
+
+        Note:
+            This is an unsigned extrinsic that can only be called by the block author. It verifies:
+            1. Commitment matches the stored submission
+            2. Signature is valid over "mev-shield:v1" + genesis_hash + payload_core
+            3. Nonce matches the current account nonce
+            4. Then dispatches the inner call from the signer
+        """
+        return self.create_composed_call(
+            id=id,
+            signer=signer,
+            nonce=nonce,
+            call=call,
+            signature=signature,
+        )
 
     def submit_encrypted(
         self,
