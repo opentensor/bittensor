08cf924d708f446ec958a7267f6b6efd6d3ddbf2
Roman Chkhaidze <basfroman@gmail.com>
2025-12-02 21:01:58 -0800

    non-fast-block e2e test

--- tests/e2e_tests/test_mev_shield.py
+++ tests/e2e_tests/test_mev_shield.py
@@ -18,7 +18,7 @@ from tests.e2e_tests.utils import (
 TEMPO_TO_SET = 3
 
 
-# @pytest.mark.parametrize("local_chain", [False], indirect=True)
+@pytest.mark.parametrize("local_chain", [False], indirect=True)
 def test_mev_shield_happy_path(
     subtensor, alice_wallet, bob_wallet, charlie_wallet, dave_wallet, local_chain
 ):
