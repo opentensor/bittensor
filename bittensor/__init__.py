a497601fcca89d2caa90cf08a9f72f85c3fbe7f8
Dairus <dairusokoh@gmail.com>
2026-01-04 20:55:46 +0100

    Patch AsyncSubstrateInterface to fix memory leak
    Apply memory leak patch for AsyncSubstrateInterface.

--- bittensor/__init__.py
+++ bittensor/__init__.py
@@ -1,3 +1,7 @@
 from .core.settings import __version__, DEFAULTS, DEFAULT_NETWORK
 from .utils.btlogging import logging
+from .utils.async_substrate_interface_patch import apply_patch
+# Apply the memory leak patch for AsyncSubstrateInterface
+apply_patch()
+
 from .utils.easy_imports import *
