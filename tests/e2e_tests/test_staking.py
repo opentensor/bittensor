81c5037e9385f196c1a821e7c8097a294a70c1b4
ibraheem-latent <i@latent.to>
2026-02-26 13:21:20 -0800

    testing

--- tests/e2e_tests/test_staking.py
+++ tests/e2e_tests/test_staking.py
@@ -444,7 +444,7 @@ def test_batch_operations(subtensor, alice_wallet, bob_wallet):
     )
 
     assert CloseInValue(  # Make sure we are within 0.0002 TAO due to tx fees
-        balances[bob_wallet.coldkey.ss58_address], Balance.from_rao(1_000_000)
+        balances[bob_wallet.coldkey.ss58_address], Balance.from_rao(1_500_000)
     ) == Balance.from_tao(999_999.7956)
 
     assert balances[alice_wallet.coldkey.ss58_address] > alice_balance
@@ -570,7 +570,7 @@ async def test_batch_operations_async(async_subtensor, alice_wallet, bob_wallet)
     )
 
     assert CloseInValue(  # Make sure we are within 0.0002 TAO due to tx fees
-        balances[bob_wallet.coldkey.ss58_address], Balance.from_rao(1_000_000)
+        balances[bob_wallet.coldkey.ss58_address], Balance.from_rao(1_500_000)
     ) == Balance.from_tao(999_999.7956)
 
     assert balances[alice_wallet.coldkey.ss58_address] > alice_balance
