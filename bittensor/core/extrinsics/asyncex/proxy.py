606e3006d4f461414ac1f664c1c290c63a03d702
Dera Okeke <okekechidera97@gmail.com>
2026-06-08 16:34:38 +0100

    docs: updated kill pure docs

--- bittensor/core/extrinsics/asyncex/proxy.py
+++ bittensor/core/extrinsics/asyncex/proxy.py
@@ -466,9 +466,8 @@ async def kill_pure_proxy_extrinsic(
 
     Parameters:
         subtensor: Subtensor instance with the connection to the chain.
-        wallet: Bittensor wallet object. The `wallet.coldkey.ss58_address` must be the spawner of the pure proxy (the
-            account that created it via :meth:`create_pure_proxy_extrinsic`). The spawner must have an `Any` proxy
-            relationship with the pure proxy.
+        wallet: Bittensor wallet object. The `wallet.coldkey.ss58_address` The wallet.coldkey.ss58_address can either be the spawner or an account
+            with an "Any" proxy relationship to the pure proxy.
         pure_proxy_ss58: The SS58 address of the pure proxy account to be killed. This is the address that was returned
             in the :meth:`create_pure_proxy_extrinsic` response.
         spawner: The SS58 address of the spawner account (the account that originally created the pure proxy via
